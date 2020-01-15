87528327ee15d9a300ec2284b947d9c6339c5ce0
David Bour <dbour@eng.ucsd.edu>
2020-01-15 15:58:24 -0800

    Refactor of py2neo <-> JSON
    - refactor:
    
      Refactored the casing to use snake_to_camel and camel_to_snake to
      adhere to JS/Python styling standards. The blueprint can now properly
      go from a neo4j query to a serialized response that is almost a
      direct mapping to vis.js data format.
    
      TODO: Fix the TODOs involving the query parsing and figure
      how to enforce a common query standard or parsing standard
      since neo4j queries can return any sets of 'paths', 'nodes' and
      'relationships'
    
    tags: refactor

--- appserver/neo4japp/services/neo4j_service.py
+++ appserver/neo4japp/services/neo4j_service.py
@@ -1,5 +1,5 @@
 from neo4japp.services.common import BaseDao
-
+from neo4japp.models import GraphNode, GraphRelationship
 
 class Neo4JService(BaseDao):
     def __init__(self, graph_session):
@@ -7,6 +7,16 @@ class Neo4JService(BaseDao):
 
     def execute_cypher(self, query):
         # TODO: Sanitize the queries
-        result = self.graph_session.run(query)
-        for record in result:
-            print(record)
+        records = list(self.graph_session.run(query))
+        if not records:
+            return None
+        # TODO: Fix this parsing, it's only getting one record
+        for record in records:
+            node_dict = dict()
+            rel_dict = dict()
+            graph_node = GraphNode.from_py2neo(record['node'])
+            node_dict[graph_node.id] = graph_node
+            graph_rel = GraphRelationship.from_py2neo(record['relationship'])
+            rel_dict[graph_rel.id] = graph_rel
+        return dict(nodes=[n.to_dict() for n in node_dict.values()],
+                    edges=[r.to_dict() for r in rel_dict.values()])
