af5b052829071ed7bdff55a6252ad7e9f707bb27
David Bour <dbour@eng.ucsd.edu>
2020-01-15 14:20:22 -0800

    NEO4J Model Class Creation
    - feature:
    
      Added a proxy model to map py2neo <-> json <-> vis.js format.

---
+++ appserver/neo4japp/services/models/neo4j.py
@@ -0,0 +1,63 @@
+from flask import json
+from py2neo import Node, Relationship
+from neo4japp.encoders import VISJSEncoder
+from neo4japp.services.models import NEO4JBase
+
+
+class GraphNode(NEO4JBase):
+    def __init__(self, id, label, data, sub_labels, display_name):
+        self.id = id
+        self.label = label
+        self.data = data
+        self.sub_labels = sub_labels
+        self.display_name = display_name
+
+    @staticmethod
+    def property_filter(properties, only=None, include=None, exclude=None, keyfn=None):
+        if only:
+            attrs = only
+        else:
+            exclude = exclude or []
+            attrs = (include or []) + [k for k in properties.keys() if k not in exclude]
+
+        keyfn = keyfn or (lambda x: x)
+        retval = {}
+        for k in attrs:
+            key = keyfn(k)
+            retval[key] = properties[k]
+        return retval
+
+    @classmethod
+    def from_py2neo(cls, node: Node, prop_filter_fn=None, primary_label_fn=None, display_fn=None):
+        labels = [l for l in node.labels]
+        prop_filter_fn = prop_filter_fn or (lambda x: x)
+        primary_label = labels[0] if not primary_label_fn else primary_label_fn(node)
+        data = prop_filter_fn({k: v for k, v in dict(node).items()})
+        display_name = None if not display_fn else display_fn(node)
+        return cls(node.identity, primary_label, data, labels, display_name)
+
+
+class GraphRelationship(NEO4JBase):
+    def __init__(self, id, label, data, to, _from):
+        self.id = id
+        self.label = label
+        self.data = data
+        self.to = to
+        self._from = _from
+
+    @classmethod
+    def from_dict(cls, d):
+        copy = d.copy()
+        copy['_from'] = copy['from']
+        del copy['from']
+        return super().from_dict(copy)
+
+    def to_dict(self):
+        copy = self.__dict__.copy()
+        copy['from'] = copy['_from']
+        del copy['_from']
+        return copy
+
+    @classmethod
+    def from_py2neo(cls, rel: Relationship):
+        return cls(rel.id, type(rel).__name__, dict(rel), rel.start_node.identity, rel.end_node.identity)
