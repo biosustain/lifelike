d09ddca982f489b14a47103fd4414d0a9beba7df
Binh Vu <bvu@Binhs-MacBook-Pro.local>
2020-01-17 14:24:26 -0800

    Added upload prototype.
    Frontend and backend for uploading excel files.

--- appserver/neo4japp/services/neo4j_service.py
+++ appserver/neo4japp/services/neo4j_service.py
@@ -1,13 +1,68 @@
-from neo4japp.services.common import BaseDao
+import attr
+
+from typing import Dict, List
+
+from werkzeug.datastructures import FileStorage
+
+from neo4japp.factory import cache
 from neo4japp.models import GraphNode, GraphRelationship
+from neo4japp.util import CamelDictMixin, compute_hash
+from neo4japp.services.common import BaseDao
+
+from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+from py2neo import (
+    Graph,
+    Node,
+    Transaction,
+    NodeMatch,
+    Relationship,
+    RelationshipMatch,
+    RelationshipMatcher,
+)
+
+
+@attr.s(frozen=True)
+class FileNameAndSheets(CamelDictMixin):
+    @attr.s(frozen=True)
+    class SheetNameAndColumnNames(CamelDictMixin):
+        sheet_name: str = attr.ib()
+        # key is column name, value is column index
+        sheet_column_names: List[Dict[str, int]] = attr.ib()
+
+    sheets: List[SheetNameAndColumnNames] = attr.ib()
+    filename: str = attr.ib()
+
+
+@attr.s(frozen=True)
+class Neo4jColumnMapping(CamelDictMixin):
+    """The int values are the column index
+    from the excel files."""
+    @attr.s(frozen=True)
+    class Neo4jNodeMapping(CamelDictMixin):
+        node_label: Dict[int, str] = attr.ib()
+        node_properties: Dict[int, str] = attr.ib()
+
+    # @attr.s(frozen=True)
+    # class Neo4jEdgeMapping(CamelDictMixin):
+    #     node_label: Dict[int, str] = attr.ib()
+    #     node_properties: Dict[int, str] = attr.ib()
+
+    source_node: Neo4jNodeMapping = attr.ib()
+    target_node: Neo4jNodeMapping = attr.ib()
+    edge: int = attr.ib()
+    file_name: str = attr.ib()
+    sheet_name: str = attr.ib()
+
 
 class Neo4JService(BaseDao):
-    def __init__(self, graph_session):
-        super().__init__(graph_session)
+    def __init__(self, graph):
+        super().__init__(graph)
 
     def execute_cypher(self, query):
         # TODO: Sanitize the queries
-        records = self.graph_session.run(query).data()
+        records = self.graph.run(query).data()
         if not records:
             return None
         node_dict = dict()
@@ -19,3 +74,202 @@ class Neo4JService(BaseDao):
             rel_dict[graph_rel.id] = graph_rel
         return dict(nodes=[n.to_dict() for n in node_dict.values()],
                     edges=[r.to_dict() for r in rel_dict.values()])
+
+    def parse_file(self, f: FileStorage) -> Workbook:
+        workbook = load_workbook(f)
+        print(f.filename)
+        cache.set(f.filename, workbook)
+        return workbook
+
+    def get_workbook_sheet_names_and_columns(
+        self,
+        filename: str,
+        workbook: Workbook,
+    ) -> FileNameAndSheets:
+        sheet_list = []
+        for i in range(0, 2):
+            workbook.active = i
+            current_ws: Worksheet = workbook.active
+
+            sheet_col_names = []
+            # loop through just the first row
+            for i, cols in enumerate(current_ws.iter_cols(
+                max_row=1,
+                max_col=len(list(current_ws.columns)) - 1,
+                values_only=True,
+            )):
+                if cols[0]:
+                    sheet_col_names.append({cols[0]: i})
+
+            sheet_list.append(FileNameAndSheets.SheetNameAndColumnNames(
+                sheet_name=current_ws.title,
+                sheet_column_names=sheet_col_names,
+            ))
+        sheets = FileNameAndSheets(sheets=sheet_list, filename=filename)
+        return sheets
+
+    def create_neo4j_json_mapping(
+        self,
+        column_mappings: Neo4jColumnMapping,
+    ) -> dict:
+        nodes = []
+        edges = []
+
+        source_col_idx_node_label = {}
+        source_col_idx_node_prop = {}
+        target_col_idx_node_label = {}
+        target_col_idx_node_prop = {}
+
+        # TODO: why is the key changing to str?
+        for k, v in column_mappings.source_node.node_label.items():
+            source_col_idx_node_label[int(k)] = v
+
+        for k, v in column_mappings.source_node.node_properties.items():
+            source_col_idx_node_prop[int(k)] = v
+
+        for k, v in column_mappings.target_node.node_label.items():
+            target_col_idx_node_label[int(k)] = v
+
+        for k, v in column_mappings.target_node.node_properties.items():
+            target_col_idx_node_prop[int(k)] = v
+
+        workbook = cache.get(column_mappings.file_name)
+
+        for i, name in enumerate(workbook.sheetnames):
+            if name == column_mappings.sheet_name:
+                workbook.active = i
+                break
+
+        current_ws = workbook.active
+
+        for row in current_ws.iter_rows(
+            min_row=2,
+            max_row=len(list(current_ws.rows)) - 1,
+            max_col=len(list(current_ws.columns)) - 1,
+            values_only=True,
+        ):
+            src_node = {}  # type: ignore
+            src_node_props = {}
+            src_node_label = list(source_col_idx_node_label.values())[0]
+
+            # source node
+            for k, v in source_col_idx_node_prop.items():
+                src_node_props[v] = row[k]
+
+            src_node['data'] = src_node_props
+            src_node['label'] = src_node_label  # type: ignore
+            src_node['hash'] = compute_hash(src_node)  # type: ignore
+            nodes.append(src_node)
+
+            if len(target_col_idx_node_label) > 0:
+                tgt_node = {}  # type: ignore
+                tgt_node_props = {}
+                tgt_node_label = list(target_col_idx_node_label.values())[0]
+
+                # target node
+                for k, v in target_col_idx_node_prop.items():
+                    tgt_node_props[v] = row[k]
+
+                tgt_node['data'] = tgt_node_props
+                tgt_node['label'] = tgt_node_label  # type: ignore
+                tgt_node['hash'] = compute_hash(tgt_node)  # type: ignore
+                nodes.append(tgt_node)
+
+                # edge
+                edge = {}  # type: ignore
+                edge['data'] = {}
+                edge['src'] = src_node['hash']
+                edge['dest'] = tgt_node['hash']
+                edge['label'] = row[column_mappings.edge]
+                edges.append(edge)
+        return {'edges': edges, 'nodes': nodes}
+
+    def search_for_relationship_with_label_and_properties(
+        self,
+        label: str,
+        props: dict,
+        src: Node,
+        dest: Node,
+    ) -> RelationshipMatch:
+        if label is None or not label:
+            raise Exception(
+                'No label provided for search query! Is the JSON object malformed?\n' +
+                'Props:\n' +
+                ''.join([f'\t{key}: {value}\n' for key, value in props.items()])
+            )
+        rm = RelationshipMatcher(self.graph)
+        return rm.match([src, dest], label, **props).first()
+
+    def goc_relationship_from_data(
+        self,
+        node_hash_map: dict,
+        rel_data: dict,
+        tx: Transaction,
+    ) -> Relationship:
+        label = rel_data['label']
+        src_node = node_hash_map[rel_data['src']]
+        dest_node = node_hash_map[rel_data['dest']]
+        props = rel_data['data']
+
+        existing_relationship = self.search_for_relationship_with_label_and_properties(
+            label, props, src_node, dest_node)
+
+        if existing_relationship:
+            return existing_relationship
+        else:
+            new_relationship = Relationship(src_node, label, dest_node, **props)
+            tx.create(new_relationship)
+            return new_relationship
+
+    def search_for_node_with_label_and_properties(
+        self,
+        label: str,
+        props: dict,
+    ) -> NodeMatch:
+        if label is None or not label:
+            raise Exception(
+                'No label provided for search query! Is the JSON object malformed?\n' +
+                'Props:\n' +
+                ''.join([f'\t{key}: {value}\n' for key, value in props.items()])
+            )
+        return self.graph.nodes.match(label, **props).first()
+
+    def goc_node_from_data(
+        self,
+        node_data: dict,
+        tx: Transaction,
+    ) -> Node:
+        label = node_data['label']
+        props = node_data['data']
+
+        existing_node = self.search_for_node_with_label_and_properties(label, props)
+
+        if existing_node:
+            return existing_node
+        else:
+            new_node = Node(label, **props)
+            tx.create(new_node)
+            return new_node
+
+    def export_to_neo4j(self, column_mappings: Neo4jColumnMapping) -> None:
+        print(column_mappings)
+        neo4j_mapping = self.create_neo4j_json_mapping(column_mappings)
+
+        tx = self.graph.begin()
+        # bar = Bar('Creating nodes...', max=len(neo4j_mapping['nodes']))
+        node_hash_map = dict()
+
+        for node in neo4j_mapping['nodes']:
+            n = self.goc_node_from_data(node, tx)
+            node_hash_map[node['hash']] = n
+        #     bar.next()
+        # bar.finish()
+
+        # bar = Bar('Creating edges...', max=len(neo4j_mapping['edges']))
+        for edge in neo4j_mapping['edges']:
+            _ = self.goc_relationship_from_data(node_hash_map, edge, tx)
+        #     bar.next()
+        # bar.finish()
+
+        tx.commit()
+        print('Done')
---
+++ appserver/neo4japp/util.py
@@ -0,0 +1,287 @@
+import attr
+import functools
+import hashlib
+import itertools
+
+from decimal import Decimal, InvalidOperation
+from enum import EnumMeta
+from json import JSONDecodeError
+from typing import Any, List, Optional, Type
+
+from flask import json, jsonify, request
+
+
+def encode_to_str(obj):
+    """Converts different types into a string representation. """
+    if isinstance(obj, str):
+        return obj
+    elif isinstance(obj, Enum):
+        return obj.name
+    elif isinstance(obj, Decimal):
+        return str(obj)
+    else:
+        raise TypeError(f'No conversion definition for {obj}')
+
+
+def snake_to_camel_dict(d, new_dict: dict) -> dict:
+    """Converts a snake_case dict to camelCase while taking into
+    consideration a nested list or dict as a value.
+    """
+    if type(d) is not dict:
+        return d
+    for k, v in d.items():
+        if type(v) is list:
+            new_dict.update({snake_to_camel(encode_to_str(k)): [snake_to_camel_dict(i, {}) for i in v]})  # noqa
+        elif type(v) is dict:
+            new_dict.update({snake_to_camel(encode_to_str(k)): snake_to_camel_dict(v, {})})  # noqa
+        else:
+            new_dict.update({snake_to_camel(encode_to_str(k)): v})
+    return new_dict
+
+
+def snake_to_camel(s):
+    if not s:
+        return s
+    parts = s.split('_')
+    return parts[0] + ''.join(x.capitalize() or '_' for x in parts[1:])
+
+
+def camel_to_snake(s):
+    """Converts camelCase to snake_case.
+
+    The SO (stackoverflow) answer has a simple function:
+
+        import re
+        def convert(name):
+            s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+            return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+    But its performance can be improved by about 33% with this
+    function, a simple hand translation of the regex matching
+    algorithm.  Measured with timeit, speed improves from 122 ms to 78
+    ms after 10K invocation
+
+    """
+    if not s:
+        return s
+    if (len(s) == 1):
+        return s.lower()
+
+    buf = [s[0].lower()]
+    prev_is_uppercase = s[0].isupper()
+
+    # scan from the second char, with one lookahead
+    normal, lookahead = itertools.tee(s[1:])
+    next(lookahead)
+
+    for (c, ahead) in itertools.zip_longest(normal, lookahead):
+        # only add an underscore in front of an uppercase if it is not
+        # preceded by an uppercase and is followed by a lowercase
+        if c.isupper():
+            if not prev_is_uppercase or (ahead and ahead.islower()):
+                buf.append('_')
+            prev_is_uppercase = True
+        else:
+            prev_is_uppercase = False
+        buf.append(c.lower())
+
+    return ''.join(buf)
+
+
+def camel_to_snake_dict(d, new_dict: dict) -> dict:
+    """Converts a camelCase dict to snake case while taking into
+    consideration a nested list or dict as a value.
+    """
+    if type(d) is not dict:
+        return d
+    for k, v in d.items():
+        if type(v) is list:
+            new_dict.update({camel_to_snake(k): [camel_to_snake_dict(i, {}) for i in v]})  # noqa
+        elif type(v) is dict:
+            new_dict.update({camel_to_snake(k): camel_to_snake_dict(v, {})})  # noqa
+        elif type(v) is str:
+            # check if a number then convert to Decimal
+            # otherwise check if a string representation of dictionary
+            try:
+                v = Decimal(v)
+                # convert back because this is just to avoid
+                # json.loads() from converting to a float
+                v = str(v)
+                new_dict.update({camel_to_snake(k): v})
+                continue
+            except InvalidOperation:
+                pass
+            try:
+                v = json.loads(v)
+                new_dict.update({camel_to_snake(k): camel_to_snake_dict(v, {})})  # noqa
+            except JSONDecodeError:
+                # not a string representation of dictionary or number type
+                new_dict.update({camel_to_snake(k): v})
+        else:
+            # for booleans, etc
+            new_dict.update({camel_to_snake(k): v})
+    return new_dict
+
+
+class CamelDictMixin:
+    @classmethod
+    def build_from_dict(cls, json_dict):
+        """Returns an instance of the class based on a
+        snake_case dictionary. Takes into consideration
+        values of key in dictionary that are also instances of
+        attrs classes. Handle deep nesting by recursing
+        back into cls.build_from_dict() using the subclass typing.
+
+        Argument:
+            json_dict is a snake_case dictionary used to instantiate
+        """
+        cls_attrs = attr.fields(cls)
+        attributes = {}
+
+        for a in cls_attrs:
+            if a.name in json_dict:
+                # this is to check if a mypy typing
+                if hasattr(a.type, '__args__') and a.type.__args__:
+                    # check for Unions[<typing>, None], etc
+                    # and get the first type
+                    attr_type = a.type.__args__[0]
+                else:
+                    # for built in types or mypy typing
+                    # that doesn't have __args__
+                    attr_type = a.type
+
+                # if the value of key is also an attrs class
+                # create an instance of it
+                value = json_dict.get(a.name)
+                if (value and
+                   issubclass(attr_type, CamelDictMixin)):
+                    # assumption is if attr_type is a subclass
+                    # then value must be type dict
+                    if isinstance(value, list):
+                        cls_list = []
+                        for v in value:
+                            cls_list.append(attr_type.build_from_dict(v))
+                        attributes[a.name] = cls_list
+                    else:
+                        attributes[a.name] = attr_type.build_from_dict(value)
+                elif (value and
+                      isinstance(attr_type, EnumMeta)):
+                        try:
+                            attributes[a.name] = attr_type[str.upper(value)]
+                        except TypeError:
+                            # probably already an enum
+                            attributes[a.name] = value
+                else:
+                    attributes[a.name] = value
+        try:
+            return cls(**attributes)
+        except TypeError as err:
+            error = err.args[0].replace('__init__()', 'Server request')
+            raise Exception(error)
+
+    def to_dict(self):
+        """Convert an attr.s class into a dict with camel case key values for
+        JSON serialization.
+
+        Converts Decimal to str and Enum to the Enum name.
+
+        Recurse is set to true in `attr.asdict` to recurse into classes
+        that are also attrs classes.
+        """
+
+        return snake_to_camel_dict(attr.asdict(self), {})
+
+
+@attr.s(frozen=True)
+class SuccessResponse(CamelDictMixin):
+    result: Any = attr.ib()  # Union[ReconBase, CamelDictMixin, List[Union[ReconBase, CamelDictMixin]], str, bool]  # noqa
+    status_code: int = attr.ib(validator=attr.validators.instance_of(int))
+
+
+@attr.s(frozen=True)
+class FileTransfer():
+    model_file: Any = attr.ib()  # actually Response type
+    status_code: int = attr.ib(validator=attr.validators.instance_of(int))
+
+
+def jsonify_with_class(
+        request_class: Optional[Type[CamelDictMixin]] = None,
+        has_file: bool = False,
+):
+    """Returns a conversion decorator.
+
+    For use by flask blueprints to map client request to
+    a data model, and return server response as JSON.
+
+    This decorator must be passed the model class it is expected
+    to map the client request data to.
+
+    Raises IllegalArgumentException if the request does not have the
+    correct attribute field.
+    """
+    def converter(f):
+        @functools.wraps(f)
+        def decorator(*args, **kwargs):
+            request_data = None
+            request_object = None
+            success_object = None
+
+            if request_class:
+                try:
+                    # assumes file upload will always be used
+                    # with `request.form`
+                    # as our only file upload related implementation
+                    # uses `request.form`
+                    if has_file:
+                        request_data = request.form.to_dict()
+                        request_data['file_input'] = request.files.get('fileInput')  # noqa
+                    else:
+                        # set to silent to return as None if empty
+                        request_data = request.get_json(silent=True)
+                        if request_data is None:
+                            request_data = request.args.to_dict()
+
+                    if request_data:
+                        request_object = request_class.build_from_dict(
+                            camel_to_snake_dict(request_data, new_dict={})
+                        )
+                    else:
+                        request_object = request_class()
+                except TypeError as err:
+                    error = err.args[0].replace('__init__()', 'Server request')
+                    raise Exception(error)
+                success_object = f(request_object, *args, **kwargs)
+            else:
+                success_object = f(*args, **kwargs)
+
+            if isinstance(success_object, SuccessResponse):
+                # check type of success object to determine how to get
+                # the actual data it holds
+                result = success_object.result
+                if isinstance(result, CamelDictMixin):
+                    result = result.to_dict()
+                elif (isinstance(result, list)):
+                    for index, _ in enumerate(result):
+                        if isinstance(result[index], CamelDictMixin):
+                            result[index] = result[index].to_dict()  # noqa
+
+                return (
+                    jsonify({'result': result}),
+                    success_object.status_code,
+                )
+            else:
+                return success_object.model_file, success_object.status_code
+        return decorator
+    return converter
+
+
+def compute_hash(data: dict, **kwargs) -> str:
+    """Returns the hash value of args"""
+    h = hashlib.new(kwargs.get('alg') or 'sha256')
+    to_json = json.dumps(data, sort_keys=True)
+    h.update(bytearray(to_json, 'utf-8'))
+    hexdigest = h.hexdigest()
+
+    if 'limit' in kwargs:
+        return hexdigest[:kwargs['limit']]
+    return hexdigest
