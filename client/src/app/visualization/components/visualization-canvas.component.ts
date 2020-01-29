ca93b9f55c46b3e5fd6cdd7437e144e65f59ad95
David Bour <dbour@eng.ucsd.edu>
2020-01-29 12:14:49 -0800

    Users can expand/collapse node
    - feature:
    
      Client side nodes can now expand and collapse. Also modified the API
      for expansion to take a generic node id instead of a biocyc id.

--- client/src/app/visualization/components/visualization-canvas.component.ts
+++ client/src/app/visualization/components/visualization-canvas.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Neo4jResults } from '../../interfaces';
-import { DataSet } from 'vis-data';
-import { Network } from 'vis-network';
+import {
+    Component,
+    Input,
+    EventEmitter,
+    OnInit,
+    Output,
+} from '@angular/core';
+import { Neo4jGraphConfig } from '../../interfaces';
+import { Network, DataSet } from 'vis-network';
 
 @Component({
     selector: 'app-visualization-canvas',
@@ -9,24 +14,61 @@ import { Network } from 'vis-network';
     styleUrls: ['./visualization-canvas.component.scss'],
 })
 export class VisualizationCanvasComponent implements OnInit {
-    @Input() data: Neo4jResults;
-    @Input() config: object;
+    @Output() expandAndCollapseNodeId = new EventEmitter<{nodeId: number, edgeIds: Array<number>}>();
+
+
+    @Input() nodes: DataSet<any, any>;
+    @Input() edges: DataSet<any, any>;
+    // Configuration for the graph view. See vis.js docs
+    @Input() config: Neo4jGraphConfig;
 
     networkGraph: Network;
-    nodes: DataSet<any, any>;
-    edges: DataSet<any, any>;
 
     constructor() {}
 
     ngOnInit() {
-        if (this.data) {
-            this.initializeVisualizer();
-        }
+        const container = document.getElementById('network-viz');
+        const data = {
+            nodes: this.nodes,
+            edges: this.edges,
+        };
+        this.networkGraph = new Network(container, data, this.config);
+        this.visualizerSetupEventBinds();
     }
 
-    initializeVisualizer() {
-        const container = document.getElementById('network-viz');
-        const { nodes, edges } = this.data;
-        this.networkGraph = new Network(container, {nodes, edges}, this.config);
+    /**
+     * Contains all of the event handling features for the
+     * network graph.
+     */
+    visualizerSetupEventBinds() {
+
+        this.networkGraph.on('hoverNode', (params) => {
+            // This produces an 'enlarge effect'
+            const node = this.nodes.get(params.node);
+            const updatedNode = {...node, size: this.config.nodes.size * 1.5};
+            this.nodes.update(updatedNode);
+        });
+
+        this.networkGraph.on('blurNode', (params) => {
+            // This produces a 'shrink effect'
+            const node = this.nodes.get(params.node);
+            const updateNode = {...node, size: this.config.nodes.size};
+            this.nodes.update(updateNode);
+        });
+
+        this.networkGraph.on('stabilizationIterationsDone', () => {
+            // Disables the 'physics' engine after initial
+            // physics calculations.
+            this.networkGraph.setOptions({physics: false});
+        });
+
+        this.networkGraph.on('doubleClick', (params) => {
+            const nodeId = params.nodes[0];
+            const edgeIds = params.edges;
+            // Check if event is double clicking a node
+            if (nodeId) {
+                this.expandAndCollapseNodeId.emit({ nodeId, edgeIds });
+            }
+        });
     }
 }
--- client/src/app/visualization/containers/visualization.component.ts
+++ client/src/app/visualization/containers/visualization.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { VisualizationService } from '../services/visualization.service';
-import { Neo4jResults } from '../../interfaces';
+import { Neo4jResults, Neo4jGraphConfig } from '../../interfaces';
+import { Network, DataSet } from 'vis-network';
+import { VisualizationCanvasComponent } from '../components/visualization-canvas.component';
 
 @Component({
     selector: 'app-visualization',
@@ -10,23 +12,129 @@ import { Neo4jResults } from '../../interfaces';
 export class VisualizationComponent implements OnInit {
 
     networkGraphData: Neo4jResults;
-    networkGraphConfig: object;
+    networkGraphConfig: Neo4jGraphConfig;
+    nodes: DataSet<any, any>;
+    edges: DataSet<any, any>;
+
+    @ViewChild(VisualizationCanvasComponent, {static: false}) canvas: VisualizationCanvasComponent;
 
     constructor(private visService: VisualizationService) {}
 
     ngOnInit() {
-        this.visService.getAllOrganisms().subscribe((result: Neo4jResults) => {
-            this.networkGraphData = result;
+        this.visService.getAllOrganisms().subscribe((results: Neo4jResults) => {
+            this.networkGraphData = this.setupInitialProperties(results);
+            this.nodes = new DataSet(this.networkGraphData.nodes);
+            this.edges = new DataSet(this.networkGraphData.edges);
         });
         this.networkGraphConfig = this.visualizationConfig();
     }
 
-    visualizationConfig() {
+    /**
+     * Used for adding properties custom properties on initial setup.
+     * Is different from convertToVisJSFormat which is a reusable utility
+     * function to rearrange custom properties.
+     * @param result - neo4j results from AP call
+     */
+    setupInitialProperties(result: Neo4jResults) {
+        // Sets the node expand state to initially be false
+        // Used for collapse/expand
+        const setExpandProperty = result.nodes.map((n) => {
+            return {...n, expanded: false};
+        });
+        return this.convertToVisJSFormat({nodes: setExpandProperty, edges: result.edges});
+    }
+
+    /**
+     * This function is used to modify the API response to a format
+     * vis.js will understand. vis.js uses a limited set
+     * of properties for rendering the network graph.
+     * @param result - a list of nodes and edges for conversion
+     */
+    convertToVisJSFormat(results: Neo4jResults): Neo4jResults {
+        let { nodes, edges } = results;
+        nodes = nodes.map((n) => {
+            return {
+                ...n,
+                primaryLabel: n.label,
+                label: n.displayName,
+            };
+        });
+        edges = edges.map((e) => {
+            return {...e, arrows: 'to'};
+        });
+        return {nodes, edges};
+    }
+
+    /**
+     * A configuration from vis.js
+     * See the API for more information.
+     */
+    visualizationConfig(): Neo4jGraphConfig {
         const config = {
+            interaction: {
+                hover: true,
+            },
             physics: {
                 enabled: true,
+            },
+            nodes: {
+                size: 25,
+                shape: 'dot',
             }
         };
         return config;
+    }
+
+    /**
+     * A helper method to clean up all nodes related
+     * to the edges in question.
+     * @param edgeIds - array of edge ids to remove
+     */
+    collapseAllChildren(edgeIds: Array<number>) {
+        if (edgeIds.length === 0) {
+            return;
+        } else {
+            const nodesToRemove = edgeIds.map((eid) => {
+                const edge = this.edges.get(eid);
+                const node = edge.from;
+                this.edges.remove(eid);
+                return node;
+            });
+            nodesToRemove.map((nid) => {
+                const edges = this.canvas.networkGraph.getConnectedEdges(nid) as Array<number>;
+                this.nodes.remove(nid);
+                this.collapseAllChildren(edges);
+            });
+        }
+    }
+
+    expandAndCollapseNode(results: {nodeId: number, edgeIds: Array<number>}) {
+        const { nodeId, edgeIds } = results;
+        const nodeRef = this.nodes.get(nodeId);
+
+        if (nodeRef.expanded) {
+            const edgeIdsFiltered = edgeIds.filter((eid) => {
+                const edge = this.edges.get(eid);
+                return edge.to === nodeId;
+            });
+            this.collapseAllChildren(edgeIdsFiltered );
+            const updatedNodeState = {...nodeRef, expanded: !nodeRef.expanded};
+            this.nodes.update(updatedNodeState);
+        } else {
+            this.visService.expandNode(nodeId).subscribe((r: Neo4jResults) => {
+                const visJSDataFormat = this.convertToVisJSFormat(r);
+                const { edges } = visJSDataFormat;
+                let { nodes } = visJSDataFormat;
+                // Sets the node expand state to true
+                nodes = nodes.map((n) => {
+                    if (n.id === nodeId) {
+                        return {...n, expanded: !nodeRef.expanded};
+                    }
+                    return n;
+                });
+                this.nodes.update(nodes);
+                this.edges.update(edges);
+            });
+        }
     }
 }
