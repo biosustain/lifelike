ba14f33251b08ecc4663cd5c92187ad4302bb099
David Bour <dbour@eng.ucsd.edu>
2020-01-29 13:07:06 -0800

    TypeScript interfaces addition
    - refactor:
    
      Added additional interfaces for client nodes and edges

--- client/src/app/interfaces/neo4j.interface.ts
+++ client/src/app/interfaces/neo4j.interface.ts
@@ -33,11 +33,42 @@ export interface Neo4jColumnMapping {
   sheetName: string;
 }
 
-export interface Neo4jResults {
-  nodes: Array<{[key: string]: any}>;
-  edges: Array<{[key: string]: any}>;
+/** Node representation from the backend */
+export interface GraphNode {
+  id: number;
+  label: string;
+  data: {[key: string]: any};
+  subLabels: Array<string>;
+  displayName: string;
 }
 
+/** Edge represenattion from the backend */
+export interface GraphRelationship {
+  id: number;
+  label: string;
+  data: {[key: string]: any};
+  to: number;
+  from: number;
+}
+
+/** VisJS Node Representations for Client */
+export interface VisNode extends GraphNode {
+  primaryLabel?: string; // Label to display in VisJS
+  expanded?: boolean; // Whether a node has been expanded
+}
+
+/** VisJS Edge Representations for Client */
+export interface VisEdge extends GraphRelationship {
+  arrows?: string;
+}
+
+export interface Neo4jResults {
+  nodes: Array<GraphNode | VisNode>;
+  edges: Array<GraphRelationship | VisEdge>;
+}
+
+// Used for vis.js configuration
+// https://visjs.github.io/vis-network/docs/network/configure.html#
 export interface Neo4jGraphConfig {
   [key: string]: any;
 }
