5b87e5d7205ff42efe32b5739ce978e373358c2e
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-02-02 17:10:36 -0800

    Centralize canvas updates to the visualizatin comp
    Previously both the container component and the context menu made direct
    changes to the canvas. The methods that did so have been migrated to the
    visualization component to keep canvas updates centralized.
    
    Now, the context menu simply notifies the visualizer that changes have
    been requested, rather than performing those changes itself. Similarly,
    the container no lonoger directly mutates the graph (it does still
    indirectly change it by getting new data however).

--- client/src/app/visualization/components/context-menu/context-menu.component.ts
+++ client/src/app/visualization/components/context-menu/context-menu.component.ts
@@ -1,125 +1,129 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, Output, EventEmitter } from '@angular/core';
 
 import { createPopper, VirtualElement, Instance } from '@popperjs/core';
-import { IdType } from 'vis-network';
 
 import { ContextMenuControlService, ContextMenuDetails } from '../../services/context-menu-control.service';
 import { Subscription } from 'rxjs';
 
+// TODO KG-17: Need to use Tippy so we can set a delay on the tooltip appearing for submenus
 @Component({
-  selector: 'app-context-menu',
-  templateUrl: './context-menu.component.html',
-  styleUrls: ['./context-menu.component.scss'],
+    selector: 'app-context-menu',
+    templateUrl: './context-menu.component.html',
+    styleUrls: ['./context-menu.component.scss'],
 })
 export class ContextMenuComponent implements OnInit, OnDestroy {
-  @Input() selectedNodes: IdType[];
-  @Input() selectedEdges: IdType[];
+    @Input() selectedNodeEdgeLabels: Set<string>;
 
-  virtualElement: VirtualElement;
-  popper: Instance;
-  contextMenu: HTMLElement;
-  subMenus: string[] = ['group-1-submenu'];
+    @Output() groupNeighborsWithRelationship: EventEmitter<string> = new EventEmitter();
 
-  hideContextMenuSubscription: Subscription;
-  updatePopperSubscription: Subscription;
+    virtualElement: VirtualElement;
+    popper: Instance;
+    contextMenu: HTMLElement;
+    subMenus: string[] = ['group-1-submenu'];
 
-  constructor(
-    private contextMenuControlService: ContextMenuControlService,
-  ) {
-    this.hideContextMenuSubscription = this.contextMenuControlService.hideContextMenu$.subscribe(hideContextMenu => {
-      if (hideContextMenu) {
-        this.hideMenu();
-      } else {
-        this.showMenu();
-      }
-    });
+    hideContextMenuSubscription: Subscription;
+    updatePopperSubscription: Subscription;
 
-    this.updatePopperSubscription = this.contextMenuControlService.updatePopper$.subscribe((details: ContextMenuDetails) => {
-      this.updatePopper(details.posX, details.posY);
-    });
-  }
+    constructor(
+        private contextMenuControlService: ContextMenuControlService,
+    ) {
+        this.hideContextMenuSubscription = this.contextMenuControlService.hideContextMenu$.subscribe(hideContextMenu => {
+            if (hideContextMenu) {
+                this.hideMenu();
+            } else {
+                this.showMenu();
+            }
+        });
 
-  ngOnInit() {
-    this.contextMenu = document.querySelector('#***ARANGO_USERNAME***-menu');
-    this.setupPopper();
-  }
+        this.updatePopperSubscription = this.contextMenuControlService.updatePopper$.subscribe((details: ContextMenuDetails) => {
+            this.updatePopper(details.posX, details.posY);
+        });
 
-  ngOnDestroy() {
-    this.hideContextMenuSubscription.unsubscribe();
-    this.updatePopperSubscription.unsubscribe();
-  }
+        this.selectedNodeEdgeLabels = new Set<string>();
+    }
 
-  // TODO KG-17: Need to use Tippy so we can set a delay on the tooltip appearing for submenus
+    ngOnInit() {
+        this.contextMenu = document.querySelector('#***ARANGO_USERNAME***-menu');
+        this.setupPopper();
+    }
 
-  generateRect(x = 0, y = 0) {
-    return () => ({
-      width: 0,
-      height: 0,
-      top: y,
-      right: x,
-      bottom: y,
-      left: x,
-    });
-  }
+    ngOnDestroy() {
+        this.hideContextMenuSubscription.unsubscribe();
+        this.updatePopperSubscription.unsubscribe();
+    }
 
-  setupPopper() {
-    this.virtualElement = {
-      getBoundingClientRect: this.generateRect(),
-    };
-    this.popper = createPopper(this.virtualElement, this.contextMenu, {
-      modifiers: [
-        {
-          name: 'offset',
-          options: {
-            offset: [0, 0],
-          },
-        },
-      ],
-      placement: 'right-start',
-    });
-  }
+    generateRect(x = 0, y = 0) {
+        return () => ({
+            width: 0,
+            height: 0,
+            top: y,
+            right: x,
+            bottom: y,
+            left: x,
+        });
+    }
 
-  updatePopper(posX: number, posY: number) {
-    this.virtualElement.getBoundingClientRect = this.generateRect(posX, posY);
-    this.popper.update();
-  }
+    setupPopper() {
+        this.virtualElement = {
+            getBoundingClientRect: this.generateRect(),
+        };
+        this.popper = createPopper(this.virtualElement, this.contextMenu, {
+            modifiers: [
+                {
+                name: 'offset',
+                options: {
+                    offset: [0, 0],
+                },
+                },
+            ],
+            placement: 'right-start',
+        });
+    }
 
-  showMenu() {
-    // First hide any submenus that might have been open (e.g. a user opened a context menu,
-    // hovered over a submenu, then opened a new context menu)
-    this.hideAllSubMenus();
-    this.contextMenu.style.display = 'block';
-  }
+    updatePopper(posX: number, posY: number) {
+        this.virtualElement.getBoundingClientRect = this.generateRect(posX, posY);
+        this.popper.update();
+    }
 
-  hideMenu() {
-    this.contextMenu.style.display = 'none';
-    this.hideAllSubMenus();
-  }
+    showMenu() {
+        // First hide any submenus that might have been open (e.g. a user opened a context menu,
+        // hovered over a submenu, then opened a new context menu)
+        this.hideAllSubMenus();
+        this.contextMenu.style.display = 'block';
+    }
 
-  showGroupByRelSubMenu() {
-    this.hideAllSubMenus();
+    hideMenu() {
+        this.contextMenu.style.display = 'none';
+        this.hideAllSubMenus();
+    }
 
-    const contextMenuItem = document.querySelector('#group-by-rel-menu-item');
-    const tooltip = document.querySelector('#group-1-submenu') as HTMLElement;
-    tooltip.style.display = 'block';
-    createPopper(contextMenuItem, tooltip, {
-      modifiers: [
-        {
-          name: 'offset',
-          options: {
-            offset: [0, 0],
-          },
-        },
-      ],
-      placement: 'right-start',
-    });
-  }
+    showGroupByRelSubMenu() {
+        this.hideAllSubMenus();
 
-  hideAllSubMenus() {
-    this.subMenus.forEach(subMenu => {
-      const tooltip = document.querySelector(`#${subMenu}`) as HTMLElement;
-      tooltip.style.display = 'none';
-    });
+        const contextMenuItem = document.querySelector('#group-by-rel-menu-item');
+        const tooltip = document.querySelector('#group-1-submenu') as HTMLElement;
+        tooltip.style.display = 'block';
+        createPopper(contextMenuItem, tooltip, {
+            modifiers: [
+                {
+                name: 'offset',
+                options: {
+                    offset: [0, 0],
+                },
+                },
+            ],
+            placement: 'right-start',
+        });
+    }
 
-  }
+    hideAllSubMenus() {
+        this.subMenus.forEach(subMenu => {
+            const tooltip = document.querySelector(`#${subMenu}`) as HTMLElement;
+            tooltip.style.display = 'none';
+        });
+    }
+
+    requestGroupByRelationship(rel: string) {
+        this.groupNeighborsWithRelationship.emit(rel);
+    }
 }
--- client/src/app/visualization/components/visualization-canvas/visualization-canvas.component.ts
+++ client/src/app/visualization/components/visualization-canvas/visualization-canvas.component.ts
@@ -2,12 +2,13 @@ import {
     Component,
     Input,
     EventEmitter,
-    OnChanges,
     OnInit,
     Output,
 } from '@angular/core';
-import { Neo4jGraphConfig } from '../../../interfaces';
+
 import { Network, DataSet, IdType } from 'vis-network';
+
+import { Neo4jGraphConfig, VisNode } from '../../../interfaces';
 import { ContextMenuControlService } from '../../services/context-menu-control.service';
 
 @Component({
@@ -17,7 +18,7 @@ import { ContextMenuControlService } from '../../services/context-menu-control.s
     providers: [ContextMenuControlService],
 })
 export class VisualizationCanvasComponent implements OnInit {
-    @Output() expandAndCollapseNodeId = new EventEmitter<{nodeId: number, edgeIds: Array<number>}>();
+    @Output() expandNode = new EventEmitter<number>();
 
     @Input() nodes: DataSet<any, any>;
     @Input() edges: DataSet<any, any>;
@@ -25,12 +26,17 @@ export class VisualizationCanvasComponent implements OnInit {
     @Input() config: Neo4jGraphConfig;
 
     networkGraph: Network;
-    selectedNodes: IdType[] = [];
-    selectedEdges: IdType[] = [];
+    selectedNodes: IdType[];
+    selectedNodeEdgeLabels: Set<string>;
+    selectedEdges: IdType[];
 
     constructor(
         private contextMenuControlService: ContextMenuControlService,
-    ) {}
+    ) {
+        this.selectedNodes = [];
+        this.selectedEdges = [];
+        this.selectedNodeEdgeLabels = new Set<string>();
+    }
 
     ngOnInit() {
         const container = document.getElementById('network-viz');
@@ -38,6 +44,11 @@ export class VisualizationCanvasComponent implements OnInit {
             nodes: this.nodes,
             edges: this.edges,
         };
+        // TODO KG-17: It seems the network graph automatically updates itself any time
+        // this.nodes or this.edges is edited...need to test this out.
+        // Likewise, this.nodes/edges are not copied when
+        // sent to the context-menu as inputs, so changing them there will change them
+        // here.
         this.networkGraph = new Network(container, data, this.config);
         this.visualizerSetupEventBinds();
     }
@@ -54,6 +65,90 @@ export class VisualizationCanvasComponent implements OnInit {
         }
     }
 
+    collapseNeighbors(***ARANGO_USERNAME***Node: VisNode) {
+        // Get all the nodes connected to the ***ARANGO_USERNAME*** node, before removing edges
+        const connectedNodes = this.networkGraph.getConnectedNodes(***ARANGO_USERNAME***Node.id) as IdType[];
+        (this.networkGraph.getConnectedEdges(***ARANGO_USERNAME***Node.id) as IdType[]).forEach(
+            connectedEdge => {
+                this.edges.remove(connectedEdge);
+            }
+        );
+
+        // If a previously connected node has no remaining edges (i.e. it is not connected
+        // to any other neighbor), remove it.
+        connectedNodes.forEach((connectedNodeId: number) => {
+            const connectedEdges = this.networkGraph.getConnectedEdges(connectedNodeId);
+            if (connectedEdges.length == 0) {
+                this.nodes.remove(connectedNodeId);
+            }
+        });
+    }
+
+    expandOrCollapseNode(nodeId: number) {
+        const nodeRef: VisNode = this.nodes.get(nodeId);
+
+        if (nodeRef.expanded) {
+            // Updates node expand state
+            const updatedNodeState = {...nodeRef, expanded: !nodeRef.expanded};
+            this.nodes.update(updatedNodeState);
+            // 'Collapse' all neighbor nodes that do not themselves have neighbors
+            this.collapseNeighbors(nodeRef);
+        } else {
+            // Need to request new data from the parent when nodes are expanded
+            this.expandNode.emit(nodeId);
+        }
+    }
+
+    /**
+     * Gets a set of labels from the edges connected to the input node.
+     * @param selectedNode the ID of the node whose edge labels we want to get
+     */
+    getConnectedEdgeLabels(selectedNode: IdType) {
+        const connectedEdges = this.networkGraph.getConnectedEdges(selectedNode);
+        connectedEdges.forEach((edge) => {
+            this.selectedNodeEdgeLabels.add(this.edges.get(edge).label);
+        });
+    }
+
+    /**
+     * Creates a cluster node of all the neighbors connected to the currently selected
+     * node cnonected by the input relationship.
+     * @param rel a string representing the relationship the neighbors will be clustered on
+     */
+    groupNeighborsWithRelationship(rel: string) {
+        const neighborNodesWithRel = [];
+        const connectedEdgesWithRel = [];
+        const ***ARANGO_USERNAME***Node = this.selectedNodes[0];
+
+        this.networkGraph.getConnectedEdges(***ARANGO_USERNAME***Node).filter((edgeId) => {
+            return this.edges.get(edgeId).label === rel;
+        }).map((connectedEdgeWithRel) => {
+            // TODO: Remove these edges from the list
+            connectedEdgesWithRel.push(connectedEdgeWithRel);
+            const connectedNodes  = this.networkGraph.getConnectedNodes(connectedEdgeWithRel) as IdType[];
+            connectedNodes.filter(
+                nodeId => nodeId !== ***ARANGO_USERNAME***Node
+            ).map(
+                // TODO: Remove these nodes from the list
+                neighborNodeWithRel => neighborNodesWithRel.push(neighborNodeWithRel)
+            );
+        });
+        this.networkGraph.cluster({
+            joinCondition: (node) => neighborNodesWithRel.includes(node.id),
+            clusterNodeProperties: {
+                // id: 'my-cluster',
+                label: rel,
+                borderWidth: 3,
+                color: 'red',
+                shape: 'database'
+              }
+        });
+        // TODO KG-17: Immediately removing nodes seems to also remove the cluster...might be related to the
+        // note I left in the visualization.canvas.component regarding updates/deletions of this.nodes/edges.
+
+        // TODO KG-17: Add a new node representing the cluster
+    }
+
     /**
      * Contains all of the event handling features for the
      * network graph.
@@ -61,29 +156,35 @@ export class VisualizationCanvasComponent implements OnInit {
     visualizerSetupEventBinds() {
         this.networkGraph.on('click', (params) => {
             this.onClickCallback(params);
+            // TODO: May want to disable some of the default behaviors. For example,
+            // if a user selected some nodes, and then clicks anywhere, all the nodes are
+            // deselected. This may be contrary to what users expect (e.g. I would expect
+            // that if I selected some things and I clicked on one of them I wouldn't
+            // deselect everything). There may also be other behaviors we don't want.
         });
 
-        this.networkGraph.on('hoverNode', (params) => {
-            // This produces an 'enlarge effect'
-            const node = this.nodes.get(params.node);
-            const updatedNode = {...node, size: this.config.nodes.size * 1.5};
-            this.nodes.update(updatedNode);
-        });
+        // TODO KG-17: These are currently causing new nodes to be created if the hovered node is a cluster.
+        // Most likely this is because we don't yet add a new node to the list when the cluster is created,
+        // this.networkGraph.on('hoverNode', (params) => {
+        //     // This produces an 'enlarge effect'
+        //     const node = this.nodes.get(params.node);
+        //     const updatedNode = {...node, size: this.config.nodes.size * 1.5};
+        //     this.nodes.update(updatedNode);
+        // });
 
-        this.networkGraph.on('blurNode', (params) => {
-            // This produces a 'shrink effect'
-            const node = this.nodes.get(params.node);
-            const updateNode = {...node, size: this.config.nodes.size};
-            this.nodes.update(updateNode);
-        });
+        // this.networkGraph.on('blurNode', (params) => {
+        //     // This produces a 'shrink effect'
+        //     const node = this.nodes.get(params.node);
+        //     const updateNode = {...node, size: this.config.nodes.size};
+        //     this.nodes.update(updateNode);
+        // });
 
         this.networkGraph.on('doubleClick', (params) => {
 
             const nodeId = params.nodes[0];
-            const edgeIds = params.edges;
             // Check if event is double clicking a node
             if (nodeId) {
-                this.expandAndCollapseNodeId.emit({ nodeId, edgeIds });
+                this.expandOrCollapseNode(nodeId);
             }
         });
 
@@ -123,8 +224,11 @@ export class VisualizationCanvasComponent implements OnInit {
           }
         } else {
           this.networkGraph.unselectAll();
-          this.selectedNodes = [];
-          this.selectedEdges = [];
+        }
+
+        this.selectedNodeEdgeLabels.clear();
+        if (this.selectedNodes.length === 1) {
+            this.getConnectedEdgeLabels(this.selectedNodes[0]);
         }
         this.contextMenuControlService.showContextMenu();
       }
