e282db6128fba6a238eb81f64e951f033ba38ead
Binh Vu <binhvu.dev@gmail.com>
2020-02-03 12:22:28 -0800

    Merge from master

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { AppLink, MenuLink } from 'toolbar-menu';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'client';
+
+  homeLink = '/home';
+  icons = ['table-edit', 'beaker-outline', 'outline-menu'];
+  sidenavLinks: AppLink[] = [
+    {
+      tooltip: 'First Widget',
+      routerLink: '/first-widget',
+      svgIcon: 'beaker-outline'
+    },
+    {
+      tooltip: 'Benchling',
+      externalLink: 'https://dtubiosustaintest.benchling.com',
+      materialIcon: 'help'
+    }
+  ];
+  userMenuLinks: MenuLink[] = [
+    {
+      materialIcon: 'person',
+      displayText: 'Name'
+    },
+    {
+      materialIcon: 'lock',
+      displayText: 'Log Out',
+      routerLink: '/home'
+    }
+  ];
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    for (const icon of this.icons) {
+      iconRegistry.addSvgIcon(icon, sanitizer.bypassSecurityTrustResourceUrl(`assets/icons/${icon}.svg`));
+    }
+  }
 }
---
+++ client/src/app/visualization/services/visualization.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { Neo4jResults } from '../../interfaces';
+
+@Injectable()
+export class VisualizationService {
+    readonly visApi = '/api/neo4j';
+
+    constructor(private http: HttpClient) {}
+
+    /**
+     * getAllOrganisms will fetch all available organisms
+     * within the NEO4J database.
+     * ** NOTE ** Use this as
+     * a test endpoint only as the database could potentially
+     * not have any organisms depending on which database
+     * is seeded.
+     */
+    getAllOrganisms() {
+        return this.http.get<{result: Neo4jResults}>(
+            `${this.visApi}/organisms`
+        ).pipe(map(resp => resp.result));
+    }
+
+    /**
+     * expandNode will take a node id and return all children
+     * of the dept of 1.
+     * @param nodeId the node id from the database
+     */
+    expandNode(nodeId: number) {
+        return this.http.post<{result: Neo4jResults}>(
+            `${this.visApi}/expand`, {nodeId},
+        ).pipe(map(resp => resp.result));
+    }
+}
