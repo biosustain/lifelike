fa47e9f3f1e2bd3f09d632ab9a0e40f6889492ce
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-02-03 15:47:35 -0800

    Add node ref table tooltip
    The reference table appears when the user hovers over a clustered node.
    This will definitely need some visual touching up. In particular, the
    sizing/dimensions of the cluster svg are a bit tricky.

---
+++ client/src/app/visualization/components/reference-table/reference-table.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { VirtualElement, Instance, createPopper } from '@popperjs/core';
+
+import { Subscription } from 'rxjs';
+
+import { ReferenceTableRow } from 'src/app/interfaces';
+
+import { ReferenceTableControlService, ReferenceTableDetails } from '../../services/reference-table-control.service';
+
+// KG-17: Should consider creating a generalized parent class for tooltip menus,
+// as it stands, the context menu and reference table components share a lot of code
+@Component({
+  selector: 'app-reference-table',
+  templateUrl: './reference-table.component.html',
+  styleUrls: ['./reference-table.component.scss']
+})
+export class ReferenceTableComponent implements OnInit {
+    @Input() tableNodes: ReferenceTableRow[];
+
+    @Output() referenceTableRowClickEvent: EventEmitter<ReferenceTableRow>;
+
+    virtualElement: VirtualElement;
+    popper: Instance;
+    referenceTable: HTMLElement;
+
+    hideReferenceTableSubscription: Subscription;
+    updatePopperSubscription: Subscription;
+
+    constructor(
+        private referenceTableControlService: ReferenceTableControlService,
+    ) {
+        this.hideReferenceTableSubscription = this.referenceTableControlService.hideReferenceTable$.subscribe(hideReferenceTable => {
+            if (hideReferenceTable) {
+                this.hideMenu();
+            } else {
+                this.showMenu();
+            }
+        });
+
+        this.updatePopperSubscription = this.referenceTableControlService.updatePopper$.subscribe((details: ReferenceTableDetails) => {
+            this.updatePopper(details.posX, details.posY);
+        });
+
+        this.referenceTableRowClickEvent = new EventEmitter<ReferenceTableRow>();
+    }
+
+    ngOnInit() {
+        this.referenceTable = document.querySelector('#reference-table');
+        this.setupPopper();
+    }
+
+    ngOnDestroy() {
+        this.hideReferenceTableSubscription.unsubscribe();
+        this.updatePopperSubscription.unsubscribe();
+    }
+
+    generateRect(x = 0, y = 0) {
+        return () => ({
+            width: 0,
+            height: 0,
+            top: y,
+            right: x,
+            bottom: y,
+            left: x,
+        });
+    }
+
+    setupPopper() {
+        this.virtualElement = {
+            getBoundingClientRect: this.generateRect(),
+        };
+        this.popper = createPopper(this.virtualElement, this.referenceTable, {
+            modifiers: [
+                {
+                name: 'offset',
+                options: {
+                    offset: [0, 0],
+                },
+                },
+            ],
+            placement: 'right-start',
+        });
+    }
+
+    updatePopper(posX: number, posY: number) {
+        this.virtualElement.getBoundingClientRect = this.generateRect(posX, posY);
+        this.popper.update();
+    }
+
+    showMenu() {
+        this.referenceTable.style.display = 'block';
+    }
+
+    hideMenu() {
+        this.referenceTable.style.display = 'none';
+    }
+
+    openMetadataSidebarForNode(node: ReferenceTableRow) {
+        this.referenceTableRowClickEvent.emit(node);
+    }
+}
