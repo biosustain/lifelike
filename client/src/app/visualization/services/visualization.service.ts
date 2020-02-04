7371f8309418f4d737929d8c6fd4517491f94147
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-02-04 13:57:11 -0800

    Update baseUrl in tsconfig
    Changed the baseUrl to 'src'. We should never use relative imports like
    '../../../interfaces' anymore. Instead, use 'app/interfaces'. Relative
    imports within a module are probably fine, for example using
    '../services/awesome-service' inside a 'AwesomeComponent'.

--- client/src/app/visualization/services/visualization.service.ts
+++ client/src/app/visualization/services/visualization.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { map } from 'rxjs/operators';
 
-import { Neo4jResults } from '../../interfaces';
+import { Neo4jResults } from 'app/interfaces';
 
 @Injectable()
 export class VisualizationService {
