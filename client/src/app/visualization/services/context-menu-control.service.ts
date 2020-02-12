91fb55e7bb2e8eb96da86a5fdcad1b24cd4e4e03
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-02-12 12:51:06 -0800

    Add delay to group by rel submenu appearance
    We can use the method applied here for any tooltip we want to delay teh
    appearance of.

--- client/src/app/visualization/services/context-menu-control.service.ts
+++ client/src/app/visualization/services/context-menu-control.service.ts
@@ -1,10 +1,40 @@
 import { Injectable } from '@angular/core';
 
 import { TooltipControlService } from 'app/shared/services/tooltip-control-service';
+import { Subject, Observable, race, interval } from 'rxjs';
+import { mapTo, first } from 'rxjs/operators';
 
 @Injectable()
 export class ContextMenuControlService extends TooltipControlService {
+    delayGroupByRelSource = new Subject<boolean>();
+    interruptGroupByRelSource = new Subject<boolean>();
+    showGroupByRelResultSource = new Subject<boolean>();
+
+    delayGroupByRel$: Observable<boolean>;
+    interruptGroupByRel$: Observable<boolean>;
+    showGroupByRelResult$: Observable<boolean>;
+
     constructor() {
         super();
+
+        this.delayGroupByRel$ = this.delayGroupByRelSource.asObservable();
+        this.interruptGroupByRel$ = this.interruptGroupByRelSource.asObservable();
+        this.showGroupByRelResult$ = this.showGroupByRelResultSource.asObservable();
+
+        this.delayGroupByRel$.subscribe(() => {
+            const example = race(
+                this.interruptGroupByRelSource.pipe(mapTo(false)),
+                interval(200).pipe(mapTo(true)),
+            ).pipe(first());
+            example.subscribe(val => this.showGroupByRelResultSource.next(val));
+        });
+    }
+
+    delayGroupByRel() {
+        this.delayGroupByRelSource.next(true);
+    }
+
+    interruptGroupByRel() {
+        this.interruptGroupByRelSource.next(true);
     }
 }
