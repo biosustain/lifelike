67d525458f93bd34b273862a3a550e99cb06acdc
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-02-14 13:12:44 -0800

    Add onSelect callbacks for nodes and edges
    Also adds some responsivity to the data sidebar panel depending on the
    type of thing currently selected. No actual data is being sent to
    sidebar currently.

--- client/src/app/interfaces/visualization.interface.ts
+++ client/src/app/interfaces/visualization.interface.ts
@@ -9,3 +9,22 @@ export interface GetLabelsResult {
     validLabels: Set<string>;
     invalidLabels: Set<string>;
 }
+
+export interface SidenavEntity {
+    data: any;
+}
+
+export interface SidenavNodeEntity extends SidenavEntity {
+    edges: any[];
+}
+
+export interface SidenavEdgeEntity extends SidenavEntity {
+    to: any;
+    from: any;
+    references: any;
+}
+
+export interface SidenavClusterEntity extends SidenavEntity {
+    includes: any[];
+    referencesMap: any;
+}
