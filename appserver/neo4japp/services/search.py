acd6e18fce81869ddd40fd0091ce5e43235d5380
David Bour <dbour@eng.ucsd.edu>
2020-02-24 10:29:50 -0800

    Predictive Search Refactor
    - refactor:
    
      Changed the concept of 'prefix' search to predictive search. Using the
      full text search as an autocomplete is not the correct approach. The
      'prefix' search is now a predictive search and will remain so until
      its needed to be an autocomplete.

--- appserver/neo4japp/services/search.py
+++ appserver/neo4japp/services/search.py
@@ -1,29 +1,47 @@
+import re
 from neo4japp.services.common import BaseDao
 from neo4japp.models import GraphNode
+from py2neo import cypher
 
 class SearchService(BaseDao):
 
     def __init__(self, graph):
         super().__init__(graph)
 
-    def prefix_search(self, term: str):
-        query = """
-            MATCH (c:Chemical)
-            WHERE c.name STARTS WITH $search_term
-            RETURN c AS node
-            UNION
-            MATCH (d:Disease)
-            WHERE d.name STARTS WITH $search_term
-            RETURN d AS node
-            UNION
-            MATCH (g:Gene)
-            WHERE g.name STARTS WITH $search_term
-            RETURN g AS node
-            UNION
-            MATCH (t:Taxonomy)
-            WHERE t.name STARTS WITH $search_term
-            RETURN t as node
+    def _fulltext_query_sanitizer(self, query):
+        """ Ensures the query is syntactically correct
+        and safe from cypher injections for Neo4j full
+        text search.
+
+        Special characters in Lucene
+        + - && || ! ( ) { } [ ] ^ " ~ * ? : \ /
+        See docs: http://lucene.apache.org/ for more information
+        We will escape these characters with a forward ( \ ) slash
         """
-        records = self.graph.run(query, parameters={'search_term': term.strip()}).data()
-        nodes = [GraphNode.from_py2neo(n['node'], display_fn=lambda x: x.get('name')) for n in records]
+        lucene_chars = re.compile(r'([\+\-\&\|!\(\)\{\}\[\]\^"~\*\?:\/])')
+        def escape(m):
+            """ Adds an escape '\' character """
+            char = m.group(1)
+            return r'\{c}'.format(c=char)
+        query = re.sub(lucene_chars, escape, query)
+        return query
+
+    def predictive_search(self, term: str, limit: int = 5):
+        """ Performs a predictive search; not necessarily a prefix based autocomplete.
+        # TODO: Potentially make this solely prefix based (similar to Google search)"""
+        query_term = self._fulltext_query_sanitizer(term)
+        if not query_term:
+            return dict(nodes=[], edges=[])
+        # The cypher escape helps prevent whitespace separated
+        # words from being searched as two individual words.
+        # The asterisk is a Lucene syntax for wild card search.
+        query_term = '{q}*'.format(q=cypher.cypher_escape(query_term))
+        cypher_query = """
+            CALL db.index.fulltext.queryNodes("nameAndId", $search_term)
+            YIELD node RETURN node LIMIT $size
+        """
+        records = self.graph.run(cypher_query, parameters={
+                                 'search_term': query_term, 'size': limit}).data()
+        nodes = [GraphNode.from_py2neo(
+            n['node'], display_fn=lambda x: x.get('name')) for n in records]
         return dict(nodes=[n.to_dict() for n in nodes], edges=[])
