49b57276723239935bbc019eae471d1d56a4d5e5
David Bour <dbour@eng.ucsd.edu>
2020-02-26 14:38:30 -0800

    Client Search Flow
    - feature:
    
      Allows a user to search and see results on the visualization canvas
      page. Will have to re-do the flow as the navigation is not intuitive
      and is becoming complex.

--- client/src/app/visualization/components/search-list/search-list.component.ts
+++ client/src/app/visualization/components/search-list/search-list.component.ts
@@ -1,5 +1,14 @@
-import { Component, Input, OnChanges } from '@angular/core';
-import { FTSQueryRecord, SearchRecord } from 'app/interfaces';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnChanges,
+    Output,
+} from '@angular/core';
+import {
+    FTSQueryRecord,
+    SearchRecord
+} from 'app/interfaces';
 
 @Component({
     selector: 'app-search-list',
@@ -8,16 +17,38 @@ import { FTSQueryRecord, SearchRecord } from 'app/interfaces';
 })
 export class SearchListComponent implements OnChanges {
 
+    // Shows/Hides the 'no results' info card
+    showNoResultsWarning = false;
+
     displayResults: Array<SearchRecord> = [];
 
+    @Output() navigateToViz = new EventEmitter();
+    @Output() fetchMoreResults = new EventEmitter();
     @Input() searchResults: Array<FTSQueryRecord>;
+    // Shows/Hides 'show more' button
+    @Input() showMoreVisible = false;
+    // Optional total results amount display
+    @Input() totalResultsAmount: number;
+    // Optional search term to display
+    @Input() searchTerm: string;
 
     constructor() {}
 
     ngOnChanges() {
         if (this.searchResults) {
-            this.displayResults = this.searchResults.map(d => this.convertToRecord(d));
+            this.showNoResultsWarning = true;
+            this.displayResults = this.searchResults.map(result => this.convertToRecord(result));
         }
+    }
+
+    showMore() {
+        this.fetchMoreResults.emit();
+    }
+
+    navigateToVisualizer() {
+        this.totalResultsAmount = 0;
+        this.showNoResultsWarning = false;
+        this.navigateToViz.emit();
     }
 
     convertToRecord(record: FTSQueryRecord): SearchRecord {
--- client/src/app/visualization/containers/visualization-search.component.ts
+++ client/src/app/visualization/containers/visualization-search.component.ts
@@ -14,13 +14,27 @@ export class VisualizationSearchComponent implements OnInit {
     @ViewChild('autoCompleteSearch', {read: MatAutocompleteTrigger, static: true})
     autoCompleteSearch: MatAutocompleteTrigger;
 
+    // Alerts that a search has been invoked
+    @Output() searchSubmitted = new EventEmitter<boolean>();
+    // Alerts a result has been selected from an autocomplete
     @Output() selectedResult = new EventEmitter<GraphNode>();
+    // Alerts a navigation has been clicked
+    @Output() navigateBack = new EventEmitter<boolean>();
 
-    autocompleteResults: Array<FTSQueryRecord> = [];
+
+    searchResults: FTSResult;
+    searchResultRecords: Array<FTSQueryRecord>;
 
     searchForm = new FormGroup({
         search: new FormControl(''),
     });
+
+    // Shows/Hides 'show more' button
+    showMoreVisible = false;
+    // Total Results
+    totalResults = 0;
+    // Search Query
+    searchQuery: string;
 
     constructor(private searchService: SearchService) {}
 
@@ -60,7 +74,17 @@ export class VisualizationSearchComponent implements OnInit {
         const query = this.searchForm.value.search;
         this.searchService.fullTextSearch(query).subscribe(
             (r: FTSResult) => {
-                this.autocompleteResults = r.nodes;
+                this.searchSubmitted.emit(true);
+                const { page, limit, total, nodes } = r;
+                this.totalResults = total;
+                this.searchQuery = query;
+                this.searchResults = r;
+                this.searchResultRecords = nodes;
+                if ((page * limit) < total) {
+                    this.showMoreVisible = true;
+                } else {
+                    this.showMoreVisible = false;
+                }
             },
             error => {
                 // #TODO: Generic error handler
@@ -78,5 +102,25 @@ export class VisualizationSearchComponent implements OnInit {
         //         // #TODO: Generic error handler
         //     }
         // );
+    }
+
+    showMore() {
+        const { query, page, limit, total } = this.searchResults;
+        if (this.searchResultRecords.length <= total) {
+            this.searchService.fullTextSearch(query, page + 1, limit).subscribe(
+                (r: FTSResult) => {
+                    this.searchResults = r;
+                    this.searchResultRecords = [
+                        ...this.searchResultRecords, ...r.nodes];
+                    if ((total - this.searchResultRecords.length) === 0) {
+                        this.showMoreVisible = false;
+                    }
+                }
+            );
+        }
+    }
+
+    navigateToVisualizer() {
+        this.navigateBack.emit(false);
     }
 }
