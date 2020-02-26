d662f3268e53357b61bce419cfc5017387d20e9f
Mustafa <makman09@gmail.com>
2020-02-26 11:42:30 -0800

    integrate so good so far

--- appserver/config.py
+++ appserver/config.py
@@ -1,9 +1,26 @@
 import os
 
 class Base():
+    """Default values"""
     SITE_NAME = 'neo4j example application'
     SECRET_KEY = os.environ.get('SECRET_KEY', 'secrets')
 
+    POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+    POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+    POSTGRES_USER = os.environ.get('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    POSTGRES_DB = os.environ.get('POSTGRES_DB')
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%s:%s@%s:%s/%s' % (
+        POSTGRES_USER,
+        POSTGRES_PASSWORD,
+        POSTGRES_HOST,
+        POSTGRES_PORT,
+        POSTGRES_DB
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SUPPORTED_LOCALES = ['en']
 
 class Development(Base):
     """Development configurations"""
--- appserver/neo4japp/database.py
+++ appserver/neo4japp/database.py
@@ -1,5 +1,7 @@
 import os
 from flask import g
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from py2neo import Graph
 
 # TODO: Set these in a more appropriate location
@@ -8,6 +10,9 @@ graph = Graph(
     uri=os.environ.get('NEO4J_HOST'),
     password=os.environ.get('NEO4J_USER')
 )
+
+db = SQLAlchemy()
+ma = Marshmallow()
 
 def get_neo4j_service_dao():
     if 'neo4j_dao' not in g:
