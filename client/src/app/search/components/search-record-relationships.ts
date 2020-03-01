41863d68f7039127b7674bdb8fed3d969c03e8a0
David Bour <dbour@eng.ucsd.edu>
2020-03-01 11:25:00 -0800

    - Styling changes to the record display - Moved the inline template of the record list into its own file

--- client/src/app/search/components/search-record-relationships.ts
+++ client/src/app/search/components/search-record-relationships.ts
@@ -4,10 +4,11 @@ import { FTSQueryRecord } from 'app/interfaces';
 @Component({
     selector: 'app-search-record-relationships',
     template: `
-        <mat-card class="mat-elevation-z1">
+        <mat-card class="mat-elevation-z0 flat-record-card">
             <mat-card-title>{{ node.node.label }}</mat-card-title>
         </mat-card>
     `,
+    styleUrls: ['./search-record.component.scss']
 })
 export class SearchRecordRelationshipsComponent {
     @Input() node: FTSQueryRecord;
