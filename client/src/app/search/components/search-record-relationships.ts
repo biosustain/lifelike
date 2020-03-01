9790d89d75c11603f017eacd9ae56ba1b66ff5e3
David Bour <dbour@eng.ucsd.edu>
2020-03-01 11:29:59 -0800

    - Modified style of literature result records

--- client/src/app/search/components/search-record-relationships.ts
+++ client/src/app/search/components/search-record-relationships.ts
@@ -5,7 +5,8 @@ import { FTSQueryRecord } from 'app/interfaces';
     selector: 'app-search-record-relationships',
     template: `
         <mat-card class="mat-elevation-z0 flat-record-card">
-            <mat-card-title>{{ node.node.label }}</mat-card-title>
+            <mat-card-title>Literature Reference</mat-card-title>
+            <mat-card-subtitle>ID: {{ node.node.data.id }}</mat-card-subtitle>
         </mat-card>
     `,
     styleUrls: ['./search-record.component.scss']
