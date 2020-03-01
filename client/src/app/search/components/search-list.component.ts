b8a763c7dea21c5824f3a1f173792e57319781b8
David Bour <dbour@eng.ucsd.edu>
2020-03-01 11:25:00 -0800

    - Styling changes to the record display - Moved the inline template of the record list into its own file

--- client/src/app/search/components/search-list.component.ts
+++ client/src/app/search/components/search-list.component.ts
@@ -3,22 +3,7 @@ import { FTSQueryRecord, SearchQuery } from 'app/interfaces';
 
 @Component({
     selector: 'app-search-list',
-    template: `
-        <mat-card>
-            <mat-card-title>Showing results for "{{ currentQuery }}"</mat-card-title>
-            <mat-card-subtitle>About {{ totalRecords }} results.</mat-card-subtitle>
-            <div class="search-record-container" *ngFor="let n of nodes; let i = index" [ngSwitch]="n.node.label">
-                <app-search-record-relationships [node]="n" *ngSwitchCase="'Reference'"></app-search-record-relationships>
-                <app-search-record-node [node]="n" *ngSwitchDefault></app-search-record-node>
-                <mat-card class="mat-elevation-z1" *ngIf="(i % 10) === 0 && i !== 0">
-                    <mat-card-subtitle>Page {{ i/10 }}</mat-card-subtitle>
-                </mat-card>
-            </div>
-            <div id="search-show-more-btn" *ngIf="showMoreVisible">
-                <button color="primary" mat-raised-button visClickDebounce (debounceClick)="getMoreResults()">More Results</button>
-            </div>
-        </mat-card>
-    `,
+    templateUrl: './search-list.component.html',
     styleUrls: ['./search-list.component.scss'],
 })
 export class SearchListComponent implements OnChanges {
