d1008cf9bdb2446946b57d8b4cb6b8f055e9f972
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-03-02 08:01:54 -0800

    Enable Karma component testing
    Had to update a few files so that our dev environment is compatible with
    Karma testing.

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,6 +1,35 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// We need to disable sandbox when we are running in side docker,
+// because chrome's sandbox needs more permissions than docker
+// normally allows so won't work unless docker is run in privileged
+// mode.
+
+// Code below is taken from is-docker, nearbly verbatim
+// https://github.com/sindresorhus/is-docker/blob/master/index.js
+
+const fs = require('fs');
+
+function hasDockerEnv() {
+	try {
+		fs.statSync('/.dockerenv');
+		return true;
+	} catch (err) {
+		return false;
+	}
+}
+
+function hasDockerCGroup() {
+	try {
+		return fs.readFileSync('/proc/self/cgroup', 'utf8').indexOf('docker') !== -1;
+	} catch (err) {
+		return false;
+	}
+}
+
+const inDocker = hasDockerEnv() || hasDockerCGroup();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -12,21 +41,35 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+          '--remote-debugging-address=0.0.0.0',
+        ],
+      },
+    },
+    client:{
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      captureConsole: true,
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/client'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+        dir: require('path').join(__dirname, './coverage/client'),
+        reports: ['html', 'lcovonly', 'text-summary'],
+        fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    browsers: ['ChromeCustom'],
+    singleRun: false
   });
 };
