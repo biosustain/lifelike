430d855c5008ef1c5406840409549e66f6ffd6be
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-03-02 11:37:30 -0800

    Update sidenav component specs

--- client/src/app/visualization/components/sidenav-cluster-view/sidenav-cluster-view.component.spec.ts
+++ client/src/app/visualization/components/sidenav-cluster-view/sidenav-cluster-view.component.spec.ts
@@ -1,25 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { configureTestSuite } from 'ng-bullet';
+
+import { SharedModule } from 'app/shared/shared.module';
 
 import { SidenavClusterViewComponent } from './sidenav-cluster-view.component';
 
-describe('SidenavClusterViewComponent', () => {
-  let component: SidenavClusterViewComponent;
-  let fixture: ComponentFixture<SidenavClusterViewComponent>;
+fdescribe('SidenavClusterViewComponent', () => {
+    let component: SidenavClusterViewComponent;
+    let fixture: ComponentFixture<SidenavClusterViewComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SidenavClusterViewComponent ]
-    })
-    .compileComponents();
-  }));
+    configureTestSuite(() => {
+        TestBed.configureTestingModule({
+            imports: [SharedModule],
+            declarations: [ SidenavClusterViewComponent ]
+        })
+        .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SidenavClusterViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SidenavClusterViewComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
