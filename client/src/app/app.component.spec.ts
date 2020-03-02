2b730f394613b111eda895496c28945b71cad675
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-03-02 08:32:13 -0800

    Add ng-bullet

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,7 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { configureTestSuite } from 'ng-bullet';
 
 import { ToolbarMenuModule } from 'toolbar-menu';
 
@@ -12,35 +14,39 @@ import { Neo4jModule } from './upload/neo4j.module';
 import { VisualizationModule } from './visualization/visualization.module';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        Neo4jModule,
-        VisualizationModule,
-        // ngrx
-        RootStoreModule,
-        VisualizationModule,
-        ToolbarMenuModule,
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    let fixture: ComponentFixture<AppComponent>;
+    let instance: AppComponent;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    configureTestSuite(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+                BrowserModule,
+                AppRoutingModule,
+                HttpClientModule,
+                Neo4jModule,
+                VisualizationModule,
+                // ngrx
+                RootStoreModule,
+                VisualizationModule,
+                ToolbarMenuModule,
+            ],
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
+    });
 
-  it(`should have as title 'client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('client');
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        instance = fixture.debugElement.componentInstance;
+    });
+
+    it('should create the app', () => {
+        expect(fixture).toBeTruthy();
+    });
+
+    it(`should have as title 'client'`, () => {
+        expect(instance.title).toEqual('client');
+    });
 });
