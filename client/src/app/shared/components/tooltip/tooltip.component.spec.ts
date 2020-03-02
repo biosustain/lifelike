4dbd82e4a6865bc98d0e34862bdfb24d40eb425f
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-03-02 12:28:41 -0800

    Update misc specs
    This concludes the updating of all existing component specs, as well as
    the addition of any specs that didn't exist for the visualizer tool. I
    don't think I will be adding any specs for the import tool in this
    branch.

--- client/src/app/shared/components/tooltip/tooltip.component.spec.ts
+++ client/src/app/shared/components/tooltip/tooltip.component.spec.ts
@@ -1,25 +1,37 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { configureTestSuite } from 'ng-bullet';
 
 import { TooltipComponent } from './tooltip.component';
 
 describe('TooltipComponent', () => {
-  let component: TooltipComponent;
-  let fixture: ComponentFixture<TooltipComponent>;
+    let component: TooltipComponent;
+    let fixture: ComponentFixture<TooltipComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TooltipComponent ]
-    })
-    .compileComponents();
-  }));
+    configureTestSuite(() => {
+        TestBed.configureTestingModule({
+            declarations: [ TooltipComponent ]
+        })
+        .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TooltipComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TooltipComponent);
+        component = fixture.componentInstance;
+        component.tooltipOptions = null;
+        component.tooltipSelector = '#mock-element';
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        const mockElement = document.createElement('div');
+        mockElement.setAttribute('id', 'mock-element');
+
+        // Mock document.querySelector so we can create a valid tooltip in the component
+        // (querySelector is called once in the ngOnInit of the TooltipComponent)
+        document.querySelector = jasmine.createSpy('HTML Element').and.returnValue(mockElement);
+
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/visualization/services/reference-table-control.service.spec.ts
+++ client/src/app/visualization/services/reference-table-control.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
+import { configureTestSuite } from 'ng-bullet';
+
+import { SharedModule } from 'app/shared/shared.module';
+
 import { ReferenceTableControlService } from './reference-table-control.service';
+import { VisualizationService } from './visualization.service';
 
 describe('ReferenceTableControlService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    configureTestSuite(() => TestBed.configureTestingModule({
+        imports: [SharedModule],
+        providers: [
+            ReferenceTableControlService,
+            VisualizationService,
+        ],
+    }));
 
-  it('should be created', () => {
-    const service: ReferenceTableControlService = TestBed.get(ReferenceTableControlService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: ReferenceTableControlService = TestBed.get(ReferenceTableControlService);
+        expect(service).toBeTruthy();
+    });
 });
