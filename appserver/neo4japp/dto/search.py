2954472d9202a57acf6fdb202bdfd3554c4a1cdb
David Bour <dbour@eng.ucsd.edu>
2020-03-02 14:48:13 -0800

    Full Text Search Refactor - Includes Reference MetaData
    - refactor:
    
      API for full text search now returns additional data for
      the reference record types such as the publication year
      and title.

--- appserver/neo4japp/dto/search.py
+++ appserver/neo4japp/dto/search.py
@@ -1,5 +1,5 @@
 import attr
-from typing import List
+from typing import Optional, List
 
 from neo4japp.models import GraphNode
 from neo4japp.util import CamelDictMixin
@@ -10,6 +10,17 @@ class FTSQueryRecord(CamelDictMixin):
     """ Single record from a full text query in Neo4j """
     node: GraphNode = attr.ib()
     score: int = attr.ib()
+
+
+@attr.s(frozen=True)
+class FTSReferenceRecord(FTSQueryRecord):
+    """ Reference record with metadata """
+    publication_title: str = attr.ib()
+    publication_year: int = attr.ib()
+    publication_id: int = attr.ib()
+    relationship: str = attr.ib()
+    chemical: Optional[GraphNode] = attr.ib(default=None)
+    disease: Optional[GraphNode] = attr.ib(default=None)
 
 
 @attr.s(frozen=True)
