696afa73b92328e9ca62af72b28768d883ed98cf
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-03-02 11:37:30 -0800

    Update sidenav component specs

--- client/src/app/visualization/components/sidenav-edge-view/sidenav-edge-view.component.spec.ts
+++ client/src/app/visualization/components/sidenav-edge-view/sidenav-edge-view.component.spec.ts
@@ -1,25 +1,60 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { configureTestSuite } from 'ng-bullet';
+
+import { SharedModule } from 'app/shared/shared.module';
 
 import { SidenavEdgeViewComponent } from './sidenav-edge-view.component';
+import { SidenavEdgeEntity } from 'app/interfaces';
 
-describe('SidenavEdgeViewComponent', () => {
-  let component: SidenavEdgeViewComponent;
-  let fixture: ComponentFixture<SidenavEdgeViewComponent>;
+fdescribe('SidenavEdgeViewComponent', () => {
+    let component: SidenavEdgeViewComponent;
+    let fixture: ComponentFixture<SidenavEdgeViewComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SidenavEdgeViewComponent ]
-    })
-    .compileComponents();
-  }));
+    const mockSidenavEdgeEntity: SidenavEdgeEntity = {
+        data: null,
+        to: {
+            data: {id: 'MOCK_NODE_1_ID', name: 'MockNode1'},
+            displayName: 'Mock Node 1',
+            id: 1,
+            label: 'Mock Node 1',
+            subLabels: ['MockNode'],
+            expanded: true,
+            primaryLabel: 'Mock Node',
+            color: null,
+        },
+        from:
+        {
+            data: {id: 'MOCK_NODE_2_ID', name: 'MockNode2'},
+            displayName: 'Mock Node 2',
+            id: 2,
+            label: 'Mock Node 2',
+            subLabels: ['MockNode'],
+            expanded: true,
+            primaryLabel: 'Mock Node',
+            color: null,
+        },
+        association: 'Mock Association',
+        references: [],
+    };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SidenavEdgeViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    configureTestSuite(() => {
+        TestBed.configureTestingModule({
+            imports: [SharedModule],
+            declarations: [ SidenavEdgeViewComponent ]
+        })
+        .compileComponents();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SidenavEdgeViewComponent);
+        component = fixture.componentInstance;
+        component.edgeEntity = mockSidenavEdgeEntity;
+
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
