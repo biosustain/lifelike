910bfb5842c511ac260414f6f445eaf2b2f5882b
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-03-04 12:15:00 -0800

    Add specs to reference table component
    Also updates the node/edge sidenav view component specs.

--- client/src/app/visualization/components/reference-table/reference-table.component.spec.ts
+++ client/src/app/visualization/components/reference-table/reference-table.component.spec.ts
@@ -1,7 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { configureTestSuite } from 'ng-bullet';
 
+import { of } from 'rxjs';
+
+import { DuplicateNodeEdgePair, GetReferenceTableDataResult, VisEdge } from 'app/interfaces';
 import { SharedModule } from 'app/shared/shared.module';
 
 import { ReferenceTableComponent } from './reference-table.component';
@@ -12,6 +15,35 @@ import { VisualizationService } from '../../services/visualization.service';
 describe('ReferenceTableComponent', () => {
     let component: ReferenceTableComponent;
     let fixture: ComponentFixture<ReferenceTableComponent>;
+    let referenceTableControlService: ReferenceTableControlService;
+    let visualizationService: VisualizationService;
+
+    // Actual data doesn't matter, just need a list with at least one element
+    const mockTableData: DuplicateNodeEdgePair[] = [
+        {
+            node: null,
+            edge: null,
+        }
+    ];
+
+    const mockVisEdge: VisEdge = {
+            id: 1,
+            label: 'Mock Edge',
+            from: null,
+            to: null,
+            data: null,
+            arrows: null,
+    };
+
+    const mockReferenceTableDataResult: GetReferenceTableDataResult = {
+        referenceTableRows: [
+            {
+                nodeDisplayName: 'Mock Node',
+                snippetCount: 3,
+                edge: mockVisEdge,
+            }
+        ],
+    };
 
     configureTestSuite(() => {
         TestBed.configureTestingModule({
@@ -27,24 +59,85 @@ describe('ReferenceTableComponent', () => {
 
     beforeEach(() => {
         fixture = TestBed.createComponent(ReferenceTableComponent);
+        referenceTableControlService = TestBed.get<ReferenceTableControlService>(ReferenceTableControlService);
+        visualizationService = TestBed.get<VisualizationService>(VisualizationService);
+
         component = fixture.componentInstance;
-        component.referenceTableData = [];
         component.tooltipSelector = '#root-table';
         component.tooltipOptions = {
             placement: 'right-start',
         };
-
-        const mockElement = document.createElement('div');
-        mockElement.setAttribute('id', 'root-table');
-
-        // Mock document.querySelector so we can create a valid tooltip in the component
-        // (querySelector is called once in the ngOnInit of the TooltipComponent)
-        document.querySelector = jasmine.createSpy('HTML Element').and.returnValue(mockElement);
 
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should request reference table data when referenceTableData setter is called', () => {
+        const getReferenceTableDataSpy = spyOn(referenceTableControlService, 'getReferenceTableData');
+        component.referenceTableData = mockTableData;
+
+        expect(getReferenceTableDataSpy).toHaveBeenCalledWith(mockTableData);
+    });
+
+    it('should request edge snippets when table row is clicked', async () => {
+        const getAssociationsWithEdgeSpy = spyOn(component, 'getAssociationsWithEdge');
+        component.referenceTableRows = mockReferenceTableDataResult.referenceTableRows;
+        component.showTooltip();
+
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(() => {
+            const referenceTableRowElements = document.getElementsByClassName('reference-table-row');
+
+            expect(referenceTableRowElements.length).toEqual(1);
+
+            referenceTableRowElements[0].dispatchEvent(new Event('click'));
+            expect(getAssociationsWithEdgeSpy).toHaveBeenCalledWith(mockVisEdge);
+        });
+    });
+
+    it('should hide the tooltip when the animationend event fires', () => {
+        const hideTooltipSpy = spyOn(component, 'hideTooltip');
+        component.tooltip.dispatchEvent(new Event('animationend'));
+
+        expect(hideTooltipSpy).toHaveBeenCalled();
+    });
+
+    it('should update reference table rows when referenceTableRowData$ observable emits', () => {
+        const getReferenceTableDataSpy = spyOn(
+            visualizationService, 'getReferenceTableData'
+        ).and.returnValue(of(mockReferenceTableDataResult));
+        component.referenceTableData = mockTableData;
+
+        expect(getReferenceTableDataSpy).toHaveBeenCalledWith(mockTableData);
+        // referenceTableControlService.getReferenceTableDataSource emits a value
+        expect(component.referenceTableRows).toEqual(mockReferenceTableDataResult.referenceTableRows);
+    });
+
+    it('should begin tooltip fadeout when hideTooltip$ observable emits true', (() => {
+        const beginReferenceTableFadeSpy = spyOn(component, 'beginReferenceTableFade');
+
+        referenceTableControlService.hideTooltip();
+        // referenceTableControlService.hideTooltipSource emits true
+        expect(beginReferenceTableFadeSpy).toHaveBeenCalled();
+    }));
+
+    it('should show tooltip when hideTooltip$ observable emits false', () => {
+        const showTooltipSpy = spyOn(component, 'showTooltip');
+
+        referenceTableControlService.showTooltip();
+        // referenceTableControlService.hideTooltipSource emits false
+        expect(showTooltipSpy).toHaveBeenCalled();
+    });
+
+    it('should update popper when updatePopper$ observable emits', () => {
+        const updatePopperSpy = spyOn(component, 'updatePopper');
+
+        referenceTableControlService.updatePopper(0, 0);
+        // referenceTableControlService.hideTooltipSource emits {x: 0, y: 0}
+        expect(updatePopperSpy).toHaveBeenCalledWith(0, 0);
     });
 });
