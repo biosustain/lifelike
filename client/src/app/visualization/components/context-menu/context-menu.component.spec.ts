34d2e15aa975ab9046fe224adf6957fad3d94e76
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-03-05 15:45:53 -0800

    Add context-menu specs part 1

--- client/src/app/visualization/components/context-menu/context-menu.component.spec.ts
+++ client/src/app/visualization/components/context-menu/context-menu.component.spec.ts
@@ -4,6 +4,8 @@ import { configureTestSuite } from 'ng-bullet';
 
 import { SharedModule } from 'app/shared/shared.module';
 
+import { IdType } from 'vis-network';
+
 import { ContextMenuComponent } from './context-menu.component';
 
 import { ContextMenuControlService } from '../../services/context-menu-control.service';
@@ -11,19 +13,30 @@ import { ContextMenuControlService } from '../../services/context-menu-control.s
 describe('ContextMenuComponent', () => {
     let component: ContextMenuComponent;
     let fixture: ComponentFixture<ContextMenuComponent>;
+    let contextMenuControlService: ContextMenuControlService;
+
+    let mockSelectedNodeIds: IdType[];
+    let mockSelectedEdgeIds: IdType[];
+    let mockSelectedNodeEdgeLabels: Set<string>;
 
     configureTestSuite(() => {
         TestBed.configureTestingModule({
             imports: [SharedModule],
             declarations: [ ContextMenuComponent ],
             providers: [ContextMenuControlService],
-        })
-        .compileComponents();
+        });
     });
 
     beforeEach(() => {
+        // Reset mock data before every test so changes don't carry over between tests
+        mockSelectedNodeIds = [1];
+        mockSelectedEdgeIds = [1];
+        mockSelectedNodeEdgeLabels = new Set<string>(['Mock Edge 1', 'Mock Edge 2']);
+
         fixture = TestBed.createComponent(ContextMenuComponent);
         component = fixture.componentInstance;
+        contextMenuControlService = TestBed.get<ContextMenuControlService>(ContextMenuControlService);
+
         component.selectedNodeIds = [];
         component.selectedEdgeIds = [];
         component.selectedNodeEdgeLabels = new Set<string>();
@@ -38,5 +51,87 @@ describe('ContextMenuComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should show \'Settings\' option even if no nodes or edges are selected', async () => {
+        component.showTooltip();
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(() => {
+            const settingsGroupElement = document.getElementById('settings-group');
+            expect(settingsGroupElement).toBeTruthy();
+        });
+
+    });
+
+    it('should show \'Remove Selected Node(s)\' if at least one node is selected', async () => {
+        component.selectedNodeIds = mockSelectedNodeIds;
+        component.showTooltip();
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(() => {
+            const nodeSelectionGroupElement = document.getElementById('node-selection-group');
+            expect(nodeSelectionGroupElement).toBeTruthy();
+        });
+    });
+
+    it('should show \'Remove Selected Edge(s)\' if at least one node is selected', async () => {
+        component.selectedEdgeIds = mockSelectedEdgeIds;
+        component.showTooltip();
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(() => {
+            const edgeSelectionGroupElement = document.getElementById('edge-selection-group');
+            expect(edgeSelectionGroupElement).toBeTruthy();
+        });
+    });
+
+    it('should show \'Group by Relationship\' option if a single node with at least one connecting edge is selected', async () => {
+        component.selectedNodeIds = mockSelectedNodeIds;
+        component.selectedNodeEdgeLabels = mockSelectedNodeEdgeLabels;
+        component.showTooltip();
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(() => {
+            const groupByRelElement = document.getElementById('group-by-rel-menu-item');
+            expect(groupByRelElement).toBeTruthy();
+        });
+    });
+
+    // it('should show submenu when \'Group by Relationship\' is hovered', () => {
+    //     // component.selectedNodeIds = mockSelectedNodeIds;
+    //     // component.selectedNodeEdgeLabels = mockSelectedNodeEdgeLabels;
+    // });
+
+    // it('should hide the contextmenu and submenus once the fade timeout completes', fakeAsync(() => {}));
+
+    // it('should request neighbor selection if \'Select Neighbors\' is clicked', () => {});
+
+    // it('should request node removal if \'Remove Selected Node(s)\' is clicked', () => {});
+
+    // it('should request edge removal if \'Remove Selected Edge(s)\' is clicked', () => {});
+
+    it('should begin tooltip fadeout when hideTooltip$ observable emits true', (() => {
+        const beginContextMenuFadeSpy = spyOn(component, 'beginContextMenuFade');
+
+        contextMenuControlService.hideTooltip();
+        // referenceTableControlService.hideTooltipSource emits true
+        expect(beginContextMenuFadeSpy).toHaveBeenCalled();
+    }));
+
+    it('should show tooltip when hideTooltip$ observable emits false', () => {
+        const showTooltipSpy = spyOn(component, 'showTooltip');
+
+        contextMenuControlService.showTooltip();
+        // referenceTableControlService.hideTooltipSource emits false
+        expect(showTooltipSpy).toHaveBeenCalled();
+    });
+
+    it('should update popper when updatePopper$ observable emits', () => {
+        const updatePopperSpy = spyOn(component, 'updatePopper');
+
+        contextMenuControlService.updatePopper(0, 0);
+        // referenceTableControlService.hideTooltipSource emits {x: 0, y: 0}
+        expect(updatePopperSpy).toHaveBeenCalledWith(0, 0);
     });
 });
