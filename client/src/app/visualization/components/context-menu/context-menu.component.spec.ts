3b9b673b42ac6f3e50c5a16456c0c0fe3594987b
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-03-06 14:28:47 -0800

    Add context-menu specs part 2
    With this the context-menu specs are done!

--- client/src/app/visualization/components/context-menu/context-menu.component.spec.ts
+++ client/src/app/visualization/components/context-menu/context-menu.component.spec.ts
@@ -1,10 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
 
 import { configureTestSuite } from 'ng-bullet';
 
-import { SharedModule } from 'app/shared/shared.module';
-
 import { IdType } from 'vis-network';
+
+import { SharedModule } from 'app/shared/shared.module';
 
 import { ContextMenuComponent } from './context-menu.component';
 
@@ -61,7 +61,6 @@ describe('ContextMenuComponent', () => {
             const settingsGroupElement = document.getElementById('settings-group');
             expect(settingsGroupElement).toBeTruthy();
         });
-
     });
 
     it('should show \'Remove Selected Node(s)\' if at least one node is selected', async () => {
@@ -98,32 +97,162 @@ describe('ContextMenuComponent', () => {
         });
     });
 
-    // it('should show submenu when \'Group by Relationship\' is hovered', () => {
-    //     // component.selectedNodeIds = mockSelectedNodeIds;
-    //     // component.selectedNodeEdgeLabels = mockSelectedNodeEdgeLabels;
-    // });
+    it('should show submenu when \'Group by Relationship\' is hovered', async () => {
+        const showGroupByRelSubMenuSpy = spyOn(component, 'showGroupByRelSubMenu');
+        component.selectedNodeIds = mockSelectedNodeIds;
+        component.selectedNodeEdgeLabels = mockSelectedNodeEdgeLabels;
+        component.showTooltip();
+        fixture.detectChanges();
 
-    // it('should hide the contextmenu and submenus once the fade timeout completes', fakeAsync(() => {}));
+        await fixture.whenStable().then(() => {
+            const groupByRelRowElement = document.getElementById('group-by-rel-menu-item');
+            groupByRelRowElement.dispatchEvent(new Event('mouseenter'));
+            expect(showGroupByRelSubMenuSpy).toHaveBeenCalled();
+        });
+    });
 
-    // it('should request neighbor selection if \'Select Neighbors\' is clicked', () => {});
+    it('should show grouping submenu after a delay', async () => {
+        jasmine.clock().install();
 
-    // it('should request node removal if \'Remove Selected Node(s)\' is clicked', () => {});
+        const delayGroupByRelSpy = spyOn(contextMenuControlService, 'delayGroupByRel').and.callThrough();
+        component.selectedNodeIds = mockSelectedNodeIds;
+        component.selectedNodeEdgeLabels = mockSelectedNodeEdgeLabels;
+        component.showTooltip();
+        fixture.detectChanges();
 
-    // it('should request edge removal if \'Remove Selected Edge(s)\' is clicked', () => {});
+        await fixture.whenStable().then(async () => {
+            component.showGroupByRelSubMenu();
+            expect(delayGroupByRelSpy).toHaveBeenCalled();
 
-    it('should begin tooltip fadeout when hideTooltip$ observable emits true', (() => {
-        const beginContextMenuFadeSpy = spyOn(component, 'beginContextMenuFade');
+            // wait for the delay to expire
+            jasmine.clock().tick(250);
 
+            fixture.detectChanges();
+            await fixture.whenStable().then(() => {
+                const groupByRelSubmenuElement = document.getElementById('single-node-selection-group-1-submenu');
+                expect(groupByRelSubmenuElement.style.display).toBe('block');
+
+                jasmine.clock().uninstall();
+            });
+        });
+    });
+
+    it('should interrupt showing the group by relationship submenu if the user hovers away', async () => {
+        const mouseLeaveNodeRowSpy = spyOn(component, 'mouseLeaveNodeRow').and.callThrough();
+        const interruptGroupByRelSpy = spyOn(contextMenuControlService, 'interruptGroupByRel');
+        component.selectedNodeIds = mockSelectedNodeIds;
+        component.selectedNodeEdgeLabels = mockSelectedNodeEdgeLabels;
+        component.showTooltip();
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(() => {
+            const groupByRelRowElement = document.getElementById('group-by-rel-menu-item');
+            groupByRelRowElement.dispatchEvent(new Event('mouseleave'));
+
+            expect(mouseLeaveNodeRowSpy).toHaveBeenCalled();
+            expect(interruptGroupByRelSpy).toHaveBeenCalled();
+        });
+    });
+
+    it('should not show the group by relationship submenu if an interrupt is sent', async () => {
+        jasmine.clock().install();
+
+        const delayGroupByRelSpy = spyOn(contextMenuControlService, 'delayGroupByRel').and.callThrough();
+        component.selectedNodeIds = mockSelectedNodeIds;
+        component.selectedNodeEdgeLabels = mockSelectedNodeEdgeLabels;
+        component.showTooltip();
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(async () => {
+            component.showGroupByRelSubMenu();
+            expect(delayGroupByRelSpy).toHaveBeenCalled();
+
+            // wait a bit before interrupting
+            jasmine.clock().tick(50);
+            contextMenuControlService.interruptGroupByRel();
+            // wait for the remainder of the normal delay time
+            jasmine.clock().tick(155);
+
+            fixture.detectChanges();
+            await fixture.whenStable().then(() => {
+                const groupByRelSubmenuElement = document.getElementById('single-node-selection-group-1-submenu');
+                expect(groupByRelSubmenuElement.style.display).toBe('none');
+                jasmine.clock().uninstall();
+            });
+        });
+    });
+
+    it('should request neighbor selection if \'Select Neighbors\' is clicked', async () => {
+        const requestNeighborSelectionSpy = spyOn(component, 'requestNeighborSelection');
+        component.selectedNodeIds = mockSelectedNodeIds;
+        component.showTooltip();
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(() => {
+            const selectNeighborsElement = document.getElementById('select-neighbors-menu-item');
+            selectNeighborsElement.dispatchEvent(new Event('click'));
+            expect(requestNeighborSelectionSpy).toHaveBeenCalled();
+        });
+    });
+
+    it('should request node removal if \'Remove Selected Node(s)\' is clicked', async () => {
+        const requestNodeRemovalSpy = spyOn(component, 'requestNodeRemoval');
+        component.selectedNodeIds = mockSelectedNodeIds;
+        component.showTooltip();
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(() => {
+            const removeSelectedNodesElement = document.getElementById('remove-selected-nodes-menu-item');
+            removeSelectedNodesElement.dispatchEvent(new Event('click'));
+            expect(requestNodeRemovalSpy).toHaveBeenCalled();
+        });
+    });
+
+    it('should request edge removal if \'Remove Selected Edge(s)\' is clicked', async () => {
+        const requestEdgeRemovalSpy = spyOn(component, 'requestEdgeRemoval');
+        component.selectedEdgeIds = mockSelectedEdgeIds;
+        component.showTooltip();
+        fixture.detectChanges();
+
+        await fixture.whenStable().then(() => {
+            const removeSelectedEdgesElement = document.getElementById('remove-selected-edges-menu-item');
+            removeSelectedEdgesElement.dispatchEvent(new Event('click'));
+            expect(requestEdgeRemovalSpy).toHaveBeenCalled();
+        });
+    });
+
+    it('should begin tooltip fadeout when hideTooltip$ observable emits true', () => {
+        const beginContextMenuFadeSpy = spyOn(component, 'beginContextMenuFade').and.callFake(
+            // Create a fake implementation of beginContextMenuFade so we don't use the timeout in the original implementation
+            () => {
+                component.contextMenuClass = component.FADEOUT_STYLE;
+                component.beginSubmenuFade();
+            }
+        );
+        const beginSubmenuFadeSpy = spyOn(component, 'beginSubmenuFade');
         contextMenuControlService.hideTooltip();
-        // referenceTableControlService.hideTooltipSource emits true
+
+        // contextMenuControlService.hideTooltipSource emits true
         expect(beginContextMenuFadeSpy).toHaveBeenCalled();
+        expect(beginSubmenuFadeSpy).toHaveBeenCalled();
+    });
+
+    it('should hide the contextmenu and submenus once the fade timeout completes', fakeAsync(() => {
+        const hideTooltipSpy = spyOn(component, 'hideTooltip').and.callThrough();
+        const hideAllSubMenusSpy = spyOn(component, 'hideAllSubMenus');
+        component.beginContextMenuFade();
+
+        flush();
+
+        expect(hideTooltipSpy).toHaveBeenCalled();
+        expect(hideAllSubMenusSpy).toHaveBeenCalled();
     }));
 
     it('should show tooltip when hideTooltip$ observable emits false', () => {
         const showTooltipSpy = spyOn(component, 'showTooltip');
 
         contextMenuControlService.showTooltip();
-        // referenceTableControlService.hideTooltipSource emits false
+        // contextMenuControlService.hideTooltipSource emits false
         expect(showTooltipSpy).toHaveBeenCalled();
     });
 
@@ -131,7 +260,7 @@ describe('ContextMenuComponent', () => {
         const updatePopperSpy = spyOn(component, 'updatePopper');
 
         contextMenuControlService.updatePopper(0, 0);
-        // referenceTableControlService.hideTooltipSource emits {x: 0, y: 0}
+        // contextMenuControlService.hideTooltipSource emits {x: 0, y: 0}
         expect(updatePopperSpy).toHaveBeenCalledWith(0, 0);
     });
 });
