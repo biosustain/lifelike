0d2e553c3ce95d98697546fabafafbc09368abb0
David Bour <dbour@eng.ucsd.edu>
2020-03-09 12:55:45 -0700

    - Moved DTO to correct directory - Unsubscribed from event listeners

--- client/src/app/shared/directives/debounceClick.ts
+++ client/src/app/shared/directives/debounceClick.ts
@@ -5,17 +5,20 @@ import {
     Input,
     OnInit,
     Output,
+    OnDestroy,
 } from '@angular/core';
 
-import { fromEvent } from 'rxjs';
+import { fromEvent, Subscription } from 'rxjs';
 import { map, debounceTime } from 'rxjs/operators';
 
 @Directive({
     selector: '[visClickDebounce]',
 })
-export class DebounceClickDirective implements OnInit {
+export class DebounceClickDirective implements OnInit, OnDestroy {
     @Input() delay = 250;
     @Output() debounceClick: EventEmitter<string> = new EventEmitter();
+
+    inputStreamSub: Subscription;
 
     constructor(private el: ElementRef) {}
 
@@ -24,8 +27,12 @@ export class DebounceClickDirective implements OnInit {
             map((e: any) => e.target.value),
             debounceTime(this.delay),
         );
-        inputStream$.subscribe(e => {
+        this.inputStreamSub = inputStream$.subscribe(e => {
             return this.debounceClick.emit(e);
         });
+    }
+
+    ngOnDestroy() {
+        this.inputStreamSub.unsubscribe();
     }
 }
