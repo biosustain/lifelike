9e9dd176600e13648629e4121517254b0a450034
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-03-10 12:26:57 -0700

    Add visualization component specs part 2
    Visualization component tests are done! We will see warnings in the logs
    that a service call is being made. This is during the ngOnInit of the
    visualization component, and I believe this will be removed once this
    branch is merged into master, since we've removed this call there.

--- client/src/app/visualization/containers/visualization/visualization.component.spec.ts
+++ client/src/app/visualization/containers/visualization/visualization.component.spec.ts
@@ -1,20 +1,27 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { configureTestSuite } from 'ng-bullet';
 
-import { GraphNode, GraphRelationship, Neo4jResults } from 'app/interfaces';
+import { MockComponents } from 'ng-mocks';
+
+import { DataSet } from 'vis-data';
+
+import {
+    ClusteredNode,
+    DuplicateVisEdge,
+    GraphNode,
+    GraphRelationship,
+    Neo4jResults,
+    VisEdge,
+    VisNode,
+} from 'app/interfaces';
 import { SharedModule } from 'app/shared/shared.module';
 
 import { VisualizationComponent } from './visualization.component';
 
 import { VisualizationService } from '../../services/visualization.service';
-import { ContextMenuComponent } from '../../components/context-menu/context-menu.component';
-import { ReferenceTableComponent } from '../../components/reference-table/reference-table.component';
-import { SidenavClusterViewComponent } from '../../components/sidenav-cluster-view/sidenav-cluster-view.component';
-import { SidenavEdgeViewComponent } from '../../components/sidenav-edge-view/sidenav-edge-view.component';
-import { SidenavNodeViewComponent } from '../../components/sidenav-node-view/sidenav-node-view.component';
 import { VisualizationCanvasComponent } from '../../components/visualization-canvas/visualization-canvas.component';
-import { VisualizationQuickbarComponent } from '../../components/visualization-quickbar/visualization-quickbar.component';
 import { VisualizationSearchComponent } from '../../containers/visualization-search/visualization-search.component';
 
 describe('VisualizationComponent', () => {
@@ -24,6 +31,9 @@ describe('VisualizationComponent', () => {
     let mockGraphNode: GraphNode;
     let mockGraphRelationship: GraphRelationship;
     let mockNeo4jResults: Neo4jResults;
+    let mockVisEdge: VisEdge;
+    let mockDuplicateVisEdge: DuplicateVisEdge;
+    let mockClusteredNode: ClusteredNode;
 
     configureTestSuite(() => {
         TestBed.configureTestingModule({
@@ -31,21 +41,18 @@ describe('VisualizationComponent', () => {
                 SharedModule,
             ],
             declarations: [
-                ContextMenuComponent,
-                ReferenceTableComponent,
-                SidenavClusterViewComponent,
-                SidenavEdgeViewComponent,
-                SidenavNodeViewComponent,
                 VisualizationComponent,
-                VisualizationCanvasComponent,
-                VisualizationQuickbarComponent,
-                VisualizationSearchComponent,
+                MockComponents(
+                    VisualizationCanvasComponent,
+                    VisualizationSearchComponent,
+                ),
             ],
             providers: [VisualizationService],
         });
     });
 
     beforeEach(() => {
+        // Mock Neo4j data
         mockGraphNode = {
             id: 1,
             label: 'Mock Node',
@@ -65,10 +72,32 @@ describe('VisualizationComponent', () => {
             edges: [mockGraphRelationship],
         };
 
+        // Mock Vis JS data
+        mockVisEdge = {
+            ...mockGraphRelationship,
+            arrows: 'to',
+        };
+
+        mockDuplicateVisEdge = {
+            ...mockVisEdge,
+            id: 'duplicateEdge:' + `${1}`,
+            duplicateOf: 1,
+            originalFrom: 2,
+            originalTo: 1,
+        };
+
+        mockClusteredNode = {
+            nodeId: 1,
+            edges: [mockDuplicateVisEdge],
+        };
+
         fixture = TestBed.createComponent(VisualizationComponent);
         instance = fixture.debugElement.componentInstance;
 
         instance.legend.set('Mock Node', ['#FFFFFF', '#FFFFFF']);
+        instance.networkGraphData = instance.setupInitialProperties(mockNeo4jResults);
+
+        fixture.detectChanges();
     });
 
     it('should create', () => {
@@ -115,5 +144,109 @@ describe('VisualizationComponent', () => {
             nodes: [convertedMockNode],
             edges: [convertedMockEdge],
         });
+    });
+
+    it('should call expandNode service when child requests a node to be expanded', () => {
+        const expandNodeSpy = spyOn(instance, 'expandNode');
+        const visualizationCanvasComponentMock = fixture.debugElement.query(
+            By.directive(VisualizationCanvasComponent)
+        ).componentInstance as VisualizationCanvasComponent;
+
+        visualizationCanvasComponentMock.expandNode.emit(1);
+
+        expect(expandNodeSpy).toHaveBeenCalledWith(1);
+    });
+
+    it('should call getSnippetsFromEdge service when child requests snippets for edge', () => {
+        const getSnippetsFromEdgeSpy = spyOn(instance, 'getSnippetsFromEdge');
+        const visualizationCanvasComponentMock = fixture.debugElement.query(
+            By.directive(VisualizationCanvasComponent)
+        ).componentInstance as VisualizationCanvasComponent;
+
+        visualizationCanvasComponentMock.getSnippetsFromEdge.emit(mockVisEdge);
+
+        expect(getSnippetsFromEdgeSpy).toHaveBeenCalledWith(mockVisEdge);
+    });
+
+    it('should call getSnippetsFromDuplicateEdge when child requests snippets for duplicate edge', () => {
+        const getSnippetsFromDuplicateEdgeSpy = spyOn(instance, 'getSnippetsFromDuplicateEdge');
+        const visualizationCanvasComponentMock = fixture.debugElement.query(
+            By.directive(VisualizationCanvasComponent)
+        ).componentInstance as VisualizationCanvasComponent;
+
+        visualizationCanvasComponentMock.getSnippetsFromDuplicateEdge.emit(mockDuplicateVisEdge);
+
+        expect(getSnippetsFromDuplicateEdgeSpy).toHaveBeenCalledWith(mockDuplicateVisEdge);
+    });
+
+    it('should call getClusterGraphData when child requests graph data for cluster', () => {
+        const getClusterGraphDataSpy = spyOn(instance, 'getClusterGraphData');
+        const visualizationCanvasComponentMock = fixture.debugElement.query(
+            By.directive(VisualizationCanvasComponent)
+        ).componentInstance as VisualizationCanvasComponent;
+
+        visualizationCanvasComponentMock.getClusterGraphData.emit([mockClusteredNode]);
+
+        expect(getClusterGraphDataSpy).toHaveBeenCalledWith([mockClusteredNode]);
+    });
+
+    it('should call addDuplicatedEdge when child requests that a new duplicate edge be added to the set', () => {
+        const addDuplicateEdgeSpy = spyOn(instance, 'addDuplicatedEdge');
+        const visualizationCanvasComponentMock = fixture.debugElement.query(
+            By.directive(VisualizationCanvasComponent)
+        ).componentInstance as VisualizationCanvasComponent;
+
+        visualizationCanvasComponentMock.addDuplicatedEdge.emit(1);
+
+        expect(addDuplicateEdgeSpy).toHaveBeenCalledWith(1);
+    });
+
+    it('should call removeDuplicatedEdge when child requests that a duplicate edge be removed from the set', () => {
+        const removeDuplicateEdgeSpy = spyOn(instance, 'removeDuplicatedEdge');
+        const visualizationCanvasComponentMock = fixture.debugElement.query(
+            By.directive(VisualizationCanvasComponent)
+        ).componentInstance as VisualizationCanvasComponent;
+
+        visualizationCanvasComponentMock.removeDuplicatedEdge.emit(1);
+
+        expect(removeDuplicateEdgeSpy).toHaveBeenCalledWith(1);
+    });
+
+    it('updateCanvasWithSingleNode should clear the canvas and add a single node', () => {
+        instance.nodes = new DataSet<VisNode | GraphNode>(instance.networkGraphData.nodes);
+        instance.edges = new DataSet<VisEdge | GraphRelationship>(instance.networkGraphData.edges);
+
+        expect(instance.nodes.length).toEqual(1);
+        expect(instance.edges.length).toEqual(1);
+
+        instance.updateCanvasWithSingleNode(mockGraphNode);
+
+        expect(instance.nodes.length).toEqual(1);
+        expect(instance.edges.length).toEqual(0);
+    });
+
+    it('addDuplicatedEdge should attempt to add an edge to the set of duplicates', () => {
+        instance.addDuplicatedEdge(1);
+        instance.addDuplicatedEdge(1);
+        instance.addDuplicatedEdge(2);
+
+        expect(instance.duplicatedEdges.size).toEqual(2);
+        expect(instance.duplicatedEdges.has(1)).toBeTrue();
+        expect(instance.duplicatedEdges.has(2)).toBeTrue();
+    });
+
+    it('removeDuplicatedEdge should remove an edge from the set of duplicates', () => {
+        instance.addDuplicatedEdge(1);
+        instance.addDuplicatedEdge(2);
+
+        instance.removeDuplicatedEdge(1);
+
+        expect(instance.duplicatedEdges.size).toEqual(1);
+        expect(instance.duplicatedEdges.has(1)).toBeFalse();
+
+        instance.removeDuplicatedEdge(2);
+
+        expect(instance.duplicatedEdges.size).toEqual(0);
+        expect(instance.duplicatedEdges.has(2)).toBeFalse();
     });
 });
