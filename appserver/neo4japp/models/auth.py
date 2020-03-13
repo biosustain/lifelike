1abbb9f621b515e68b17e6b6996411c9df474072
David Bour <dbour@eng.ucsd.edu>
2020-03-13 15:00:27 -0700

    - Tied the logged in user to an application context specific object; this allows us to re-use this authorization throughout the application - Moved the 'Project' schema into its own model which is related to the drawing tool

--- appserver/neo4japp/models/auth.py
+++ appserver/neo4japp/models/auth.py
@@ -29,22 +29,3 @@ class AppUser(RDBMSBase):
 class AppUserSchema(ma.ModelSchema):
     class Meta:
         model = AppUser
-
-
-class Project(RDBMSBase):
-    """
-        Model representation of a project drawing in a
-        network graph networking tool
-
-    """
-    id = db.Column(db.Integer, primary_key=True)
-    label = db.Column(db.String(250), nullable=False)
-    description = db.Column(db.Text)
-    date_modified = db.Column(db.DateTime)
-    graph = db.Column(db.JSON)
-    user_id = db.Column(db.Integer, db.ForeignKey('appuser.id'), nullable=False)
-
-
-class ProjectSchema(ma.ModelSchema):
-    class Meta:
-        model = Project
