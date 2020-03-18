7d16cff90b5f07e8239a6c5d1ca5575ad0c5a61c
David Bour <dbour@eng.ucsd.edu>
2020-03-17 21:35:50 -0700

    - New exceptions added to exception handling suite

--- appserver/neo4japp/exceptions.py
+++ appserver/neo4japp/exceptions.py
@@ -9,3 +9,20 @@ class BaseException(Exception):
         retval['name'] = self.name
         retval['message'] = self.message
         return retval
+
+
+class DatabaseError(BaseException):
+    """An error occured in database operation"""
+    def __init__(self, message, additional_msgs=[]) -> None:
+        super().__init__('Database Error', message, additional_msgs)
+
+
+class DuplicateRecord(BaseException):
+    def __init__(self, message, additional_msgs=[]):
+        super().__init__('Duplicate record', message, additional_msgs)
+
+
+class NotAuthorizedException(BaseException):
+    """Signals that the client does not sufficient privilege"""
+    def __init__(self, message, additional_msgs=[]):
+        super().__init__('Unauthorized Action', message, additional_msgs)
