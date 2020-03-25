a2c660f508954a92c57b949641ec50f38767e016
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2020-03-25 13:29:25 +0100

    feat: add file search drawer in drawing tool page

--- client/src/app/drawing-tool/pdf-viewer/pdf-viewer.component.ts
+++ client/src/app/drawing-tool/pdf-viewer/pdf-viewer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 import {
   PdfAnnotationsService,
@@ -9,22 +10,43 @@ import {
   Annotation
 } from '../services/interfaces';
 
+const MOCK_FILES: string[] = [
+  'pdf file number 1',
+  'pdf file number 2',
+  'pdf file number 3',
+  'pdf file number 4',
+  'pdf file number 5',
+  'pdf file number 6',
+  'pdf file number 7',
+  'pdf file number 8',
+  'pdf file number 9',
+  'pdf file number 10',
+];
+
+
 @Component({
   selector: 'app-pdf-viewer',
   templateUrl: './pdf-viewer.component.html',
   styleUrls: ['./pdf-viewer.component.scss']
 })
-export class PdfViewerComponent implements OnInit, AfterViewInit {
+export class PdfViewerComponent implements AfterViewInit {
 
   annotations: Object[] = [];
+  files: string[] = MOCK_FILES; // TODO: fetch from API endpoint
+  filesFilter = new FormControl('');
+  filteredFiles = this.files;
 
   constructor(
     private pdfAnnService: PdfAnnotationsService,
     private dataFlow: DataFlowService
-  ) { }
-
-  ngOnInit() {
+  ) {
+    this.filesFilter.valueChanges.subscribe((value: string) => {
+      this.filteredFiles = this.files.filter(
+        (name: string) => name.includes(value.toLocaleLowerCase())
+      );
+    });
   }
+
   ngAfterViewInit() {
     setTimeout(
       () => {
@@ -53,7 +75,7 @@ export class PdfViewerComponent implements OnInit, AfterViewInit {
 
     const ann_id = node_dom.getAttribute('annotation-id');
     const ann_def: Annotation = this.pdfAnnService.searchForAnnotation(ann_id);
-    
+
     let pay_load = {
       x: mouseEvent.clientX - container_coord.x,
       y: mouseEvent.clientY,
