717d67cfa78126843c2664f778fefc033b10f43f
Mustafa Ahmed Khan III <makman09@gmail.com>
2020-03-31 23:38:15 -0700

    in middle of map-search-channel

---
+++ client/src/app/drawing-tool/map-search-channel/map-search-channel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-search-channel',
+  templateUrl: './map-search-channel.component.html',
+  styleUrls: ['./map-search-channel.component.scss']
+})
+export class MapSearchChannelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
