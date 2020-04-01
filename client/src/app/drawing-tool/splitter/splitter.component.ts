d424c2218f543f25319bb515db90ba41b552d18e
Mustafa Ahmed Khan III <makman09@gmail.com>
2020-04-01 12:26:31 -0700

    finish pdf-viewer side-by-side w/ drawing-tool

--- client/src/app/drawing-tool/splitter/splitter.component.ts
+++ client/src/app/drawing-tool/splitter/splitter.component.ts
@@ -27,6 +27,8 @@ export class SplitterComponent implements OnInit {
 
   splitPanelLength = 0;
 
+  currentApp: string = '';
+
   constructor(
     private injector: Injector,
     private r: ComponentFactoryResolver
@@ -40,23 +42,44 @@ export class SplitterComponent implements OnInit {
 
   }
 
+  /**
+   * 
+   */
+  close() {
+    this.currentApp = '';
+    this.leftPanel.clear();
+    this.splitPanelLength = 0;
+  }  
+
+  /**
+   * 
+   * @param app 
+   */
   openApp(app:string) {
+    if (!app) {
+      this.close();
+      return;
+    }
+
     let factory;
     let ref;
 
     switch(app) {
       case 'map-search':
         factory = this.r.resolveComponentFactory(MapSearchChannelComponent);
-        ref = this.leftPanel.createComponent(factory);
-        ref.changeDetectorRef.detectChanges();
         break;
       case 'pdf-viewer':
         factory = this.r.resolveComponentFactory(PdfViewerComponent);
-        ref = this.leftPanel.createComponent(factory);
-        ref.changeDetectorRef.detectChanges();
         break;
       default:
         break;
     }
+
+    this.leftPanel.clear();
+    this.splitPanelLength = 50;
+    this.currentApp = app;
+
+    ref = this.leftPanel.createComponent(factory);
+    ref.changeDetectorRef.detectChanges();
   }
 }
