11de9b1007a7dd9869cdffa6d3f60d9174a88a36
Julio Melanda <juceme@biosustain.dtu.dk>
2020-04-02 12:25:06 +0200

    Implements Download graph as PDF

--- client/src/app/drawing-tool/drawing-tool/drawing-tool.component.ts
+++ client/src/app/drawing-tool/drawing-tool/drawing-tool.component.ts
@@ -412,6 +412,45 @@ export class DrawingToolComponent implements OnInit, AfterViewInit, OnDestroy {
       });
   }
 
+  /**
+   * Saves and downloads the PDF version of the current map
+   */
+  downloadPDF() {
+    if (!this.saveState) {
+      this._snackBar.open('Please save the project before exporting', null, {
+          duration: 2000,
+        });
+    } else {
+      this.projectService.getPDF(this.project).subscribe (resp => {
+        // It is necessary to create a new blob object with mime-type explicitly set
+        // otherwise only Chrome works like it should
+        var newBlob = new Blob([resp], { type: "application/pdf" });
+
+        // IE doesn't allow using a blob object directly as link href
+        // instead it is necessary to use msSaveOrOpenBlob
+        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveOrOpenBlob(newBlob);
+            return;
+        }
+
+        // For other browsers:
+        // Create a link pointing to the ObjectURL containing the blob.
+        const data = window.URL.createObjectURL(newBlob);
+
+        var link = document.createElement('a');
+        link.href = data;
+        link.download = this.project.label + ".pdf";
+        // this is necessary as link.click() does not work on the latest firefox
+        link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
+
+        setTimeout(function () {
+            // For Firefox it is necessary to delay revoking the ObjectURL
+            window.URL.revokeObjectURL(data);
+            link.remove();
+        }, 100);
+      });
+    }
+  }
   // -- Helpers --
   /**
    * Build key,value pair style dict
