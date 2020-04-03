d1a0c3979efad7c464283fc2620233eebc3b7aa0
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-04-03 12:51:34 -0700

    Fix tslint issues part 2
    Turning this into a 3-parter because my goodness is it a lot of changes.

--- client/src/app/drawing-tool/services/authentication.service.ts
+++ client/src/app/drawing-tool/services/authentication.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpRequest, HttpHandler } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { tap, catchError, switchMap } from 'rxjs/operators';
 import { HttpInterceptor} from '@angular/common/http';
 
@@ -11,7 +11,7 @@ import { HttpInterceptor} from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthenticationService implements HttpInterceptor {
-  base_url = environment.apiUrl;
+  baseUrl = environment.apiUrl;
 
   constructor(
     private http: HttpClient,
@@ -21,8 +21,8 @@ export class AuthenticationService implements HttpInterceptor {
   /**
    * Intercept every request's response where
    * jwt is expired, and renew access token
-   * @param req 
-   * @param next 
+   * @param req represents an http request
+   * @param next represents an httphandler
    */
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
     return next.handle(req)
@@ -30,13 +30,13 @@ export class AuthenticationService implements HttpInterceptor {
         catchError(
           (err) => {
 
-            if (err['status'] === 401 && !err['url'].includes('refresh')) {
+            if (err.status === 401 && !err.url.includes('refresh')) {
               return this.refresh().pipe(
                 switchMap(() => {
                   req = this.updateAuthHeader(req);
                   return next.handle(req);
                 })
-              )
+              );
             }
 
             return Observable.throw(err);
@@ -48,21 +48,23 @@ export class AuthenticationService implements HttpInterceptor {
   /**
    * Create http options with authorization
    * header if boolean set to true
-   * @param with_jwt 
+   * @param withJwt boolean representing whether to return the options with a jwt
    */
-  createHttpOptions(with_jwt=false) {
-    const headers = {
-      'Content-Type':  'application/json'
+  createHttpOptions(withJwt = false) {
+    if (withJwt) {
+      return {
+          headers: new HttpHeaders({
+            'Content-Type':  'application/json',
+            Authorization: 'Bearer ' + localStorage.getItem('access_jwt'),
+          })
+      };
+    } else {
+        return {
+            headers: new HttpHeaders({
+                'Content-Type':  'application/json'
+            })
+        };
     }
-
-    if (with_jwt) {
-      headers['Authorization'] = 'Bearer ' + localStorage.getItem('access_jwt');
-    }
-
-    const httpOptions = {
-      headers: new HttpHeaders(headers)
-    };
-    return httpOptions
   }
 
   /**
@@ -72,45 +74,44 @@ export class AuthenticationService implements HttpInterceptor {
   updateAuthHeader(request: HttpRequest<any>) {
     return request.clone({
       setHeaders: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_jwt')
+        Authorization: 'Bearer ' + localStorage.getItem('access_jwt')
       }
-    })
+    });
   }
 
   /**
    * Renew user access token with their refresh token
    */
   public refresh(): Observable<any> {
-    let jwt = localStorage.getItem('refresh_jwt');
+    const jwt = localStorage.getItem('refresh_jwt');
 
-  	return this.http.post(
-    	this.base_url + '/auth/refresh',
-      {jwt},
-      this.createHttpOptions()
+    return this.http.post(this.baseUrl + '/auth/refresh',
+        {jwt},
+        this.createHttpOptions()
     ).pipe(
-      tap(resp => {
-        localStorage.setItem('access_jwt', resp['access_jwt']);
-        localStorage.setItem('refresh_jwt', resp['refresh_jwt']);
-      })    
+        tap(resp => {
+            localStorage.setItem('access_jwt', resp.access_jwt);
+            localStorage.setItem('refresh_jwt', resp.refresh_jwt);
+        })
     );
   }
-  
+
 
   /**
    * Authenticate users to get a JWT
-   * @param credential_form 
+   * @param credentialForm represents a form object for the user credentials
    */
-  public login(credential_form): Observable<Object> {
+  public login(credentialForm): Observable<any> {
     return this.http.post(
-      this.base_url + '/auth/login',
-      credential_form,
+      this.baseUrl + '/auth/login',
+      credentialForm,
       this.createHttpOptions()
     ).pipe(
       tap(resp => {
-        localStorage.setItem('access_jwt', resp['access_jwt']);
-        localStorage.setItem('refresh_jwt', resp['refresh_jwt']);
+        localStorage.setItem('access_jwt', resp.access_jwt);
+        localStorage.setItem('refresh_jwt', resp.refresh_jwt);
       })
-    )
+    );
   }
 
   /**
