52ce1b764030f0b2cd6e3ed4eea3fe3657e12347
Miguel Rueda <miguerueda86@yahoo.es>
2020-04-06 16:26:16 +0200

    fix: changing endpoint to the new schema

--- appserver/neo4japp/models/files.py
+++ appserver/neo4japp/models/files.py
@@ -1,18 +1,21 @@
 from neo4japp.database import db
-from sqlalchemy.dialects.postgresql import BYTEA
-
+from sqlalchemy.dialects import postgresql
 
 class Files(db.Model):
     __tablename__ = 'files'
-    id = db.Column(db.String(), primary_key=True)
-    filename = db.Column(db.String(60), unique=True)
-    file = db.Column(BYTEA())
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    file_id = db.Column(db.String(36), unique=True, nullable=False)
+    # TODO: unique file name could be problematic
+    # add a setter to increment name before inserting
+    filename = db.Column(db.String(60), unique=True, nullable=False)
+    raw_file = db.Column(postgresql.BYTEA(), nullable=False)
     username = db.Column(db.String(30))
-    creation_date = db.Column(db.DateTime())
+    creation_date = db.Column(db.DateTime, default=db.func.now())
+    annotations = db.Column(postgresql.JSONB, nullable=False, server_default='[]')
 
-    def __init__(self, id, filename, file, username, creation_date):
-        self.id = id
+    def __init__(self, file_id, filename, raw_file, username, annotations):
+        self.file_id = file_id
         self.filename = filename
-        self.file = file
+        self.raw_file = raw_file
         self.username = username
-        self.creation_date = creation_date
+        self.annotations = annotations
