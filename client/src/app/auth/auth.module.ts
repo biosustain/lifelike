00bd4e0ee0c9d688c32bbba17b7935a35eeeef13
David Bour <dbour@eng.ucsd.edu>
2020-04-06 15:36:10 -0700

    Users can log in and log out
    - feat:
    
      Users can now log into their accounts as well as log out. The sessions
      will be stored in the local storage.
    
      Although a user can alter their local storage, every data on the page
      still performs API requests through a JWT token, so invalid tokens
      will render the page blank.

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -3,14 +3,30 @@ import { SharedModule } from 'app/shared/shared.module';
 import { AuthenticationService } from './services/authentication.service';
 import { LoginComponent } from './components/login.component';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './store/reducer';
+import { AuthEffects } from './store/effects';
+
+import { AuthGuard } from './guards/auth-guard.service';
+import { LoginGuard } from './guards/login-guard.service';
+
 const components = [
     LoginComponent,
 ];
 
 @NgModule({
-    imports: [SharedModule],
+    imports: [
+        EffectsModule.forFeature([AuthEffects]),
+        StoreModule.forFeature('auth', reducer),
+        SharedModule,
+    ],
     declarations: components,
-    providers: [AuthenticationService],
+    providers: [
+        AuthGuard,
+        AuthenticationService,
+        LoginGuard,
+    ],
     exports: components,
 })
 export class AuthModule {}
--- client/src/app/auth/store/effects.ts
+++ client/src/app/auth/store/effects.ts
@@ -1,27 +1,67 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
 
 import { AuthenticationService } from '../services/authentication.service';
 
-import { map, switchMap } from 'rxjs/operators';
+import {
+    exhaustMap,
+    map,
+    tap,
+    withLatestFrom
+} from 'rxjs/operators';
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 
 import * as AuthActions from './actions';
+import * as AuthSelectors from './selectors';
+import { State } from './state';
+
+import * as SnackbarActions from 'app/shared/store/snackbar-actions';
 
 @Injectable()
 export class AuthEffects {
     constructor(
         private actions$: Actions,
         private router: Router,
+        private store$: Store<State>,
         private authService: AuthenticationService,
     ) {}
 
     login$ = createEffect(() => this.actions$.pipe(
         ofType(AuthActions.login),
-        switchMap(({ credential }) => {
+        exhaustMap(({ credential }) => {
             const { email, password } = credential;
             return this.authService.login(email, password).pipe(
-                map(user => AuthActions.loginSuccess(user)),
+                map(user => {
+                    const appuser = user.user;
+                    return AuthActions.loginSuccess({user: appuser});
+                }),
+            );
+        })
+    ));
+
+    loginSuccess$ = createEffect(() => this.actions$.pipe(
+        ofType(AuthActions.loginSuccess),
+        withLatestFrom(this.store$.pipe(select(AuthSelectors.selectAuthRedirectUrl))),
+        tap(([_, url]) => this.router.navigate([url]))
+    ), { dispatch: false });
+
+    loginRedirect$ = createEffect(() => this.actions$.pipe(
+        ofType(AuthActions.loginRedirect),
+        tap(_ => this.router.navigate(['/login'])),
+    ), { dispatch: false });
+
+    logout$ = createEffect(() => this.actions$.pipe(
+        ofType(AuthActions.logout),
+        map(_ => {
+            this.authService.logout();
+            this.router.navigate(['/']);
+            return SnackbarActions.displaySnackbar(
+                {payload: {
+                    message: 'You are now logged out!',
+                    action: 'Dismiss',
+                    config: { duration: 5000 },
+                }}
             );
         })
     ));
