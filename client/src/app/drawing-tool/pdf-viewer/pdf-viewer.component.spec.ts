59b33038eab412fc689b9347e64dc6f7855fa9f9
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-04-07 16:49:11 -0700

    Add post-merge cleanup
    Also adds some changes to the drawing tool spec testing module. I was
    going to fix the tests but I now realize that the problems may be very
    tightly coupled to the design of the drawing tool component. It may be
    difficult for me to fix the issues without doing some redesign.

--- client/src/app/drawing-tool/pdf-viewer/pdf-viewer.component.spec.ts
+++ client/src/app/drawing-tool/pdf-viewer/pdf-viewer.component.spec.ts
@@ -1,32 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { APP_BASE_HREF } from '@angular/common';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DrawingToolModule } from '../drawing-tool.module';
 import { PdfViewerComponent } from './pdf-viewer.component';
+import { configureTestSuite } from 'ng-bullet';
 
-xdescribe('PdfViewerComponent', () => {
-  let component: PdfViewerComponent;
-  let fixture: ComponentFixture<PdfViewerComponent>;
+describe('PdfViewerComponent', () => {
+    let component: PdfViewerComponent;
+    let fixture: ComponentFixture<PdfViewerComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        DrawingToolModule
-      ],
-      providers: [
-        {provide: APP_BASE_HREF, useValue : '/' }
-      ]
-    })
-    .compileComponents();
-  }));
+    configureTestSuite(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                DrawingToolModule
+            ],
+            providers: [
+                {provide: APP_BASE_HREF, useValue : '/' }
+            ]
+        });
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PdfViewerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PdfViewerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
