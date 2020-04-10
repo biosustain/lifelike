a6ffd7c5abc261dbbe5fe53142fac54ca8f80199
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-04-10 14:28:06 -0700

    Add clipboard service
    We should be able to use this service anywhere we want to read/write to
    the user's clipboard. I imagine we will want to read more often than we
    want to write, since it will probably be better to create a separate
    service if we want to cache certain data.

---
+++ client/src/app/interfaces/shared.interface.ts
@@ -0,0 +1,11 @@
+export enum Browsers {
+    ANDROID = 'android',
+    BLINK = 'blink',
+    EDGE = 'edge',
+    FIREFOX = 'firefox',
+    IOS = 'ios',
+    SAFARI = 'safari',
+    TRIDENT = 'trident',
+    WEBKIT = 'webkit',
+    UNKNOWN = 'unknown',
+}
