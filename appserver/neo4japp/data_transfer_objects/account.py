51d7b3366de6428ab7576d99b693843e1b2815c3
David Bour <dbour@eng.ucsd.edu>
2020-04-13 13:49:12 -0700

    API Password Change Implemented
    - feat:
    
      API + service implemented for changing users password. The API
      can also be used to update other attributes of the user.
    
    - fix:
    
      Fixed the serialization of auth users to correctly exclude the
      password field.

--- appserver/neo4japp/data_transfer_objects/account.py
+++ appserver/neo4japp/data_transfer_objects/account.py
@@ -6,8 +6,15 @@ from typing import List
 
 
 @attr.s(frozen=True)
-class UserCreationRequest(CamelDictMixin):
+class UserRequest(CamelDictMixin):
     username: str = attr.ib()
     password: str = attr.ib()
+    first_name: str = attr.ib()
+    last_name: str = attr.ib()
     email: str = attr.ib()
     roles: List[str] = attr.ib(default=attr.Factory(list))
+
+
+@attr.s(frozen=True)
+class UserUpdateRequest(UserRequest):
+    new_password: str = attr.ib(default='')
