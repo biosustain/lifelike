29f055ec461953c133ef511c89aa72d6afa16632
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-04-14 16:16:28 -0700

    Update component specs
    Unfortunately, it looks like testing the clipboard API with Karma is a
    bit of a bust at the moment. AFAIK there is no way to grant read/write
    clipboard permission to the headless chrome runner, which effectively
    prevents us from doing any kind of testing (without mocks) on code that
    uses the clipboard API.

--- client/src/app/drawing-tool/drawing-tool/drawing-tool.component.spec.ts
+++ client/src/app/drawing-tool/drawing-tool/drawing-tool.component.spec.ts
@@ -2,12 +2,17 @@ import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
 import { APP_BASE_HREF } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatSnackBar } from '@angular/material';
+import { BrowserModule, By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { configureTestSuite } from 'ng-bullet';
 
 import { MockComponents } from 'ng-mocks';
 
+import { PdfViewerLibModule } from 'pdf-viewer-lib';
+
+import { CopyPasteMapsService } from 'app/drawing-tool/services/copy-paste-maps.service';
+import { ClipboardService } from 'app/shared/services/clipboard.service';
 import { SharedModule } from 'app/shared/shared.module';
 import { RootStoreModule } from 'app/root-store';
 
@@ -18,12 +23,10 @@ import { DrawingToolComponent } from './drawing-tool.component';
 import { DrawingToolContextMenuComponent } from './drawing-tool-context-menu/drawing-tool-context-menu.component';
 import { InfoPanelComponent } from './info-panel/info-panel.component';
 import { PaletteComponent } from './palette/palette.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { PdfViewerLibModule } from 'pdf-viewer-lib';
 
 declare const viewport;
 
-xdescribe('DrawingToolComponent', () => {
+describe('DrawingToolComponent', () => {
   const mockup = {
     id: 'test',
     label: '',
@@ -35,6 +38,7 @@ xdescribe('DrawingToolComponent', () => {
   };
   let component: DrawingToolComponent;
   let fixture: ComponentFixture<DrawingToolComponent>;
+  let clipboardService: ClipboardService;
 
   function addNode(type: string, x: number, y: number) {
     const dragDropEvent: CdkDragDrop<any[], any[]> =
@@ -95,6 +99,8 @@ xdescribe('DrawingToolComponent', () => {
             PdfViewerLibModule,
         ],
         providers: [
+          ClipboardService,
+          CopyPasteMapsService,
           DataFlowService,
           ProjectsService,
           MatSnackBar,
@@ -109,6 +115,7 @@ xdescribe('DrawingToolComponent', () => {
 
     fixture = TestBed.createComponent(DrawingToolComponent);
     component = fixture.componentInstance;
+    clipboardService = TestBed.get<ClipboardService>(ClipboardService);
 
     const dataFlow: DataFlowService = TestBed.get(DataFlowService);
     dataFlow.pushProject2Canvas(mockup);
@@ -121,7 +128,7 @@ xdescribe('DrawingToolComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should be able to draw three nodes on graph', () => {
+  xit('should be able to draw three nodes on graph', () => {
     addNode('gene', 100, -500);
     addNode('chemical', 50, -400);
     addNode('species', 150, -400);
@@ -144,7 +151,7 @@ xdescribe('DrawingToolComponent', () => {
     expect(graph.edges.length).toEqual(3);
   });
 
-  it('should be able to undo properly after 1 click to bring only two nodes', () => {
+  xit('should be able to undo properly after 1 click to bring only two nodes', () => {
     addNode('gene', 100, -500);
     addNode('chemical', 50, -400);
     addNode('species', 150, -400);
@@ -156,7 +163,7 @@ xdescribe('DrawingToolComponent', () => {
     expect(graph.nodes.length).toEqual(2);
   });
 
-  it('should be able to undo 6 total draw actions to have empty graph', () => {
+  xit('should be able to undo 6 total draw actions to have empty graph', () => {
     addNode('gene', 100, -500);
     addNode('chemical', 50, -400);
     addNode('species', 150, -400);
@@ -180,7 +187,7 @@ xdescribe('DrawingToolComponent', () => {
     expect(graph.nodes.length).toEqual(0);
   });
 
-  it('should be able to have complex series of action with undo/redo mixed in and be accurate', () => {
+  xit('should be able to have complex series of action with undo/redo mixed in and be accurate', () => {
     addNode('gene', 100, -500);
     addNode('chemical', 50, -400);
     addNode('species', 150, -400);
@@ -204,6 +211,50 @@ xdescribe('DrawingToolComponent', () => {
     graph = component.visjsNetworkGraph.export();
     expect(graph.nodes.length).toEqual(4);
 
+  });
+
+  // TODO: This test is blocked by clipboard read/write permissions not being authorized in the
+  // headless chrome instance. There doesn't seem to be a way to programmatically give/request permission
+  // at the moment, so this test might be in limbo for the time being.
+  xit('createLinkNodeFromClipboard should create a new link node', async () => {
+      (fixture.debugElement.nativeElement.querySelector('#canvas') as HTMLElement).focus();
+      fixture.detectChanges();
+
+      const recordCmdSpy = spyOn(component, 'recordCommand');
+      const canvasCoords = {x: 0, y: 0};
+      const prevNumNodes = component.visjsNetworkGraph.visNodes.length;
+
+      await clipboardService.writeToClipboard('foobar');
+      await component.createLinkNodeFromClipboard(canvasCoords);
+
+      expect(recordCmdSpy).toHaveBeenCalledWith({
+        action: 'add node',
+        data: {
+            group: 'link',
+            label: 'foobar',
+            ...canvasCoords
+        }
+      });
+      expect(component.visjsNetworkGraph.visNodes.length).toEqual(prevNumNodes + 1);
+  });
+
+  // TODO: See comment regarding KeyboardEvent with init data below
+  xit('should paste the current clipboard selection as a link node if user presses CTRL + V while hovering over the canvas', async () => {
+    const canvasEl = document.getElementById('canvas');
+    canvasEl.focus();
+    fixture.detectChanges();
+
+    canvasEl.dispatchEvent(new Event('focusin'));
+
+    expect(component.pasteEventStream).toBeTruthy();
+
+    const createLinkNodeFromClipboardSpy = spyOn(component, 'createLinkNodeFromClipboard');
+
+    // TODO: For some reason, it looks like this KeyboardEvent loses the provided init data when it is caught by the
+    // event listeners in the component. Not being able to use this feature makes testing this difficult...
+    canvasEl.dispatchEvent(new KeyboardEvent('keydown', {code: 'KeyV', metaKey: true}));
+
+    await expect(createLinkNodeFromClipboardSpy).toHaveBeenCalled();
   });
 
   // TODO LL-233: Add tests for context menu methods
