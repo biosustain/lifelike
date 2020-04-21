3b68290c32d824601be8dab6708db40787bcb038
Binh Vu <binhvu.dev@gmail.com>
2020-04-21 13:57:24 -0700

    Add interval tree to find all annotations that have overlapping intervals.
    For now do nothing with these conflicting annotations. JIRA LL-407 and LL-408 was originally
    suppose to be handling these conflicts, but we decided to hold off until we get a better
    idea of what we want to do with them. So just leave them there for now as a place to gather
    all conflicts.

--- appserver/neo4japp/data_transfer_objects/annotations.py
+++ appserver/neo4japp/data_transfer_objects/annotations.py
@@ -24,3 +24,22 @@ class PDFTokenPositions(CamelDictMixin):
 class PDFTokenPositionsList(CamelDictMixin):
     token_positions: List[PDFTokenPositions] = attr.ib()
     coor_obj_per_pdf_page: Dict[int, List[Union[LTChar, LTAnno]]] = attr.ib()  # noqa
+
+
+@attr.s(frozen=True)
+class Annotation(CamelDictMixin):
+    @attr.s(frozen=True)
+    class TextPosition(CamelDictMixin):
+        value: str = attr.ib()
+        lower_left: Dict[str, float] = attr.ib()
+        upper_right: Dict[str, float] = attr.ib()
+
+    page_number: int = attr.ib()
+    keyword: List[TextPosition] = attr.ib()
+    keyword_length: int = attr.ib()
+    lo_location_offset: int = attr.ib()
+    hi_location_offset: int = attr.ib()
+    keyword_type: str = attr.ib()
+    color: str = attr.ib()
+    id: str = attr.ib()
+    id_type: str = attr.ib()
