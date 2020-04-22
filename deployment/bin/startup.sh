42f3adcd099a04b3d16531d9c0704b5f85fe3412
David Bour <dbour@eng.ucsd.edu>
2020-04-22 12:18:59 -0700

    GitHub Actions Continuous Deployment
    - feat:
      1. Created a workflow script for GitHub actions to continously deploy
         the staging server.
      2. Dropped the other compose files as we defer this to GitHub to
         handle building and orchestrating the containers.

---
+++ deployment/bin/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /srv
+export $(cat staging.env | xargs)
+sudo docker login -u $DOCKER_USER -p "$(cat keyfile.json)" https://gcr.io
+sudo docker pull gcr.io/$PROJECT_ID/kg-appserver-staging:latest
+sudo docker pull gcr.io/$PROJECT_ID/kg-webserver-staging:latest
+gsutil cp gs://kg-secrets/docker-compose.ci.yml docker-compose.ci.yml
+sudo docker-compose -f docker-compose.ci.yml up -d
