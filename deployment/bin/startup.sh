b08d985715c93e56cb2366e915e2603bc5bff6e2
David Bour <dbour@eng.ucsd.edu>
2020-04-23 10:56:13 -0700

    - fix: sudo command needed to find path for gsutil

--- deployment/bin/startup.sh
+++ deployment/bin/startup.sh
@@ -5,5 +5,5 @@ export $(cat staging.env | xargs)
 sudo docker login -u $DOCKER_USER -p "$(cat keyfile.json)" https://gcr.io
 sudo docker pull gcr.io/$PROJECT_ID/kg-appserver-staging:latest
 sudo docker pull gcr.io/$PROJECT_ID/kg-webserver-staging:latest
-gsutil cp gs://kg-secrets/docker-compose.ci.yml docker-compose.ci.yml
+sudo gsutil cp gs://kg-secrets/docker-compose.ci.yml docker-compose.ci.yml
 sudo docker-compose -f docker-compose.ci.yml up -d
