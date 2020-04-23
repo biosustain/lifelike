426104dfac8e92bcf4e503735802c280777e7833
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2020-04-23 11:14:28 +0200

    chore: add RouterTestingModule to pdf-viewer tests

--- client/src/app/drawing-tool/pdf-viewer/pdf-viewer.component.spec.ts
+++ client/src/app/drawing-tool/pdf-viewer/pdf-viewer.component.spec.ts
@@ -1,5 +1,6 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { configureTestSuite } from 'ng-bullet';
 
@@ -13,7 +14,8 @@ describe('PdfViewerComponent', () => {
     configureTestSuite(() => {
         TestBed.configureTestingModule({
             imports: [
-                DrawingToolModule
+                DrawingToolModule,
+                RouterTestingModule,
             ],
             providers: [
                 {provide: APP_BASE_HREF, useValue : '/' }
