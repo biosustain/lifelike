953fed0a7de5cc0ee73069a363bee521f2fe7d83
David Bour <dbour@eng.ucsd.edu>
2020-04-23 13:24:43 -0700

    - fix: image fix where nginx conf was saved in incorrect directory - fix: added a prune script to remove old images

--- deployment/bin/startup.sh
+++ deployment/bin/startup.sh
@@ -7,3 +7,4 @@ sudo docker pull gcr.io/$PROJECT_ID/kg-appserver-staging:latest
 sudo docker pull gcr.io/$PROJECT_ID/kg-webserver-staging:latest
 sudo gsutil cp gs://kg-secrets/docker-compose.ci.yml docker-compose.ci.yml
 sudo docker-compose -f docker-compose.ci.yml up -d
+sudo docker image prune
