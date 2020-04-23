80a43748f8c1122e231a456e134929516de312c1
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2020-04-23 13:44:20 +0200

    refactor: use localStorage instead of Angular Router's optional params

--- client/src/app/drawing-tool/pdf-viewer/pdf-viewer.component.ts
+++ client/src/app/drawing-tool/pdf-viewer/pdf-viewer.component.ts
@@ -1,6 +1,5 @@
 import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { PdfFile } from 'app/interfaces/pdf-files.interface';
 import { PdfFilesService } from 'app/shared/services/pdf-files.service';
@@ -39,20 +38,18 @@ export class PdfViewerComponent implements AfterViewInit, OnDestroy {
     private pdfAnnService: PdfAnnotationsService,
     private dataFlow: DataFlowService,
     private pdf: PdfFilesService,
-    private route: ActivatedRoute,
   ) {
     this.filesFilterSub = this.filesFilter.valueChanges.subscribe(this.updateFilteredFiles);
     this.pdf.getFiles().subscribe((files: PdfFile[]) => {
       this.files = files;
       this.updateFilteredFiles(this.filesFilter.value);
     });
-    // Handle route's optional parameters
-    this.route.paramMap.subscribe((params: ParamMap) => {
-      const fileId = params.get('file_id');
-      if (fileId) {
-        this.openPdf(fileId);
-      }
-    });
+    // Handles opening a pdf from other pages
+    const fileId = localStorage.getItem('fileIdForPdfViewer');
+    if (fileId) {
+      localStorage.removeItem('fileIdForPdfViewer');
+      this.openPdf(fileId);
+    }
   }
 
   ngAfterViewInit() {
