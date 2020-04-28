b9ac28d6e28d1064e0c8a857ee3cfcc312791cc2
Albert Pham <the.sk89q@gmail.com>
2020-04-28 12:43:46 -0700

    De-duplicate uploaded files.

--- appserver/neo4japp/models/files.py
+++ appserver/neo4japp/models/files.py
@@ -1,6 +1,15 @@
 from sqlalchemy.dialects import postgresql
+
 from neo4japp.database import db
 from neo4japp.models.common import RDBMSBase
+
+
+class FileContent(RDBMSBase):
+    __tablename__ = 'files_content'
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    raw_file = db.Column(db.LargeBinary, nullable=False)
+    checksum_sha256 = db.Column(db.Binary(32), nullable=False, index=True)
+    creation_date = db.Column(db.DateTime, nullable=False, default=db.func.now())
 
 
 class Files(RDBMSBase):  # type: ignore
@@ -8,7 +17,8 @@ class Files(RDBMSBase):  # type: ignore
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     file_id = db.Column(db.String(36), unique=True, nullable=False)
     filename = db.Column(db.String(60), nullable=False)
-    raw_file = db.Column(db.LargeBinary, nullable=False)
+    content_id = db.Column(db.Integer, db.ForeignKey('files_content.id', ondelete='CASCADE'))
+    raw_file = db.Column(db.LargeBinary, nullable=True)
     username = db.Column(db.String(30))
     creation_date = db.Column(db.DateTime, default=db.func.now())
     annotations = db.Column(postgresql.JSONB, nullable=False, server_default='[]')
