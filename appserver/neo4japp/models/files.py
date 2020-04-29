eb057653aefdf51f592d24a11da2e75e0b99008a
Albert Pham <the.sk89q@gmail.com>
2020-04-29 16:26:19 -0700

    Get rid of raw_file on files and migrate data to files_content.

--- appserver/neo4japp/models/files.py
+++ appserver/neo4japp/models/files.py
@@ -7,7 +7,7 @@ from neo4japp.models.common import RDBMSBase
 class FileContent(RDBMSBase):
     __tablename__ = 'files_content'
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    raw_file = db.Column(db.LargeBinary)
+    raw_file = db.Column(db.LargeBinary, nullable=False)
     checksum_sha256 = db.Column(db.Binary(32), nullable=False, index=True, unique=True)
     creation_date = db.Column(db.DateTime, nullable=False, default=db.func.now())
 
@@ -17,7 +17,7 @@ class Files(RDBMSBase):  # type: ignore
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     file_id = db.Column(db.String(36), unique=True, nullable=False)
     filename = db.Column(db.String(60), nullable=False)
-    content_id = db.Column(db.Integer, db.ForeignKey('files_content.id', ondelete='CASCADE'))
+    content_id = db.Column(db.Integer, db.ForeignKey('files_content.id', ondelete='CASCADE'), nullable=False)
     username = db.Column(db.String(30))
     creation_date = db.Column(db.DateTime, default=db.func.now())
     annotations = db.Column(postgresql.JSONB, nullable=False, server_default='[]')
