699ff79e717999d56cf137002c7384c9cbbc21b1
Albert Pham <the.sk89q@gmail.com>
2020-04-29 15:33:15 -0700

    Migrate raw_file from old table to new table.

--- appserver/neo4japp/models/files.py
+++ appserver/neo4japp/models/files.py
@@ -7,7 +7,7 @@ from neo4japp.models.common import RDBMSBase
 class FileContent(RDBMSBase):
     __tablename__ = 'files_content'
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    raw_file = db.Column(db.LargeBinary, nullable=False)
+    raw_file = db.Column(db.LargeBinary)
     checksum_sha256 = db.Column(db.Binary(32), nullable=False, index=True, unique=True)
     creation_date = db.Column(db.DateTime, nullable=False, default=db.func.now())
 
@@ -18,7 +18,6 @@ class Files(RDBMSBase):  # type: ignore
     file_id = db.Column(db.String(36), unique=True, nullable=False)
     filename = db.Column(db.String(60), nullable=False)
     content_id = db.Column(db.Integer, db.ForeignKey('files_content.id', ondelete='CASCADE'))
-    raw_file = db.Column(db.LargeBinary, nullable=True)
     username = db.Column(db.String(30))
     creation_date = db.Column(db.DateTime, default=db.func.now())
     annotations = db.Column(postgresql.JSONB, nullable=False, server_default='[]')
