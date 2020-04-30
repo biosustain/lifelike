8f9c83cf9d8bd7cd4edab33022a61f830a6a4106
Julio Melanda <juceme@biosustain.dtu.dk>
2020-04-30 15:13:42 +0200

    Fixes merge conflicts with master

--- client/src/app/drawing-tool/drawing-tool.module.ts
+++ client/src/app/drawing-tool/drawing-tool.module.ts
@@ -30,11 +30,8 @@ import { DrawingToolContextMenuComponent } from './drawing-tool/drawing-tool-con
 import { PaletteComponent } from './drawing-tool/palette/palette.component';
 import { InfoPanelComponent } from './drawing-tool/info-panel/info-panel.component';
 import { SplitterComponent } from './splitter/splitter.component';
-import { MapSearchChannelComponent } from './map-search-channel/map-search-channel.component';
 import { ExportModalComponent } from './drawing-tool/export-modal/export-modal.component';
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
+import { MatDialogModule } from "@angular/material/dialog";
 import { MapPreviewComponent } from './project-list-view/map-preview/map-preview.component';
 import { MapListComponent } from './project-list-view/map-list/map-list.component';
 import { SharedModule } from 'app/shared/shared.module';
