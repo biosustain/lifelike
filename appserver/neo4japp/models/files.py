96e93563b3fa4a9f809b8d9033db49b37d117b93
Albert Pham <the.sk89q@gmail.com>
2020-04-30 08:55:50 -0700

    Reduce line length for lint.

--- appserver/neo4japp/models/files.py
+++ appserver/neo4japp/models/files.py
@@ -17,7 +17,9 @@ class Files(RDBMSBase):  # type: ignore
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     file_id = db.Column(db.String(36), unique=True, nullable=False)
     filename = db.Column(db.String(200), nullable=False)
-    content_id = db.Column(db.Integer, db.ForeignKey('files_content.id', ondelete='CASCADE'), nullable=False)
+    content_id = db.Column(db.Integer,
+                           db.ForeignKey('files_content.id', ondelete='CASCADE'),
+                           nullable=False)
     username = db.Column(db.String(30))
     creation_date = db.Column(db.DateTime, default=db.func.now())
     annotations = db.Column(postgresql.JSONB, nullable=False, server_default='[]')
