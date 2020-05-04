58bd61f6057766ff40f53d801a430aa5ca1984a4
Binh Vu <binhvu.dev@gmail.com>
2020-05-04 14:57:13 -0700

    Add phenotype to LMDB

--- appserver/neo4japp/services/annotations/prepare_databases.py
+++ appserver/neo4japp/services/annotations/prepare_databases.py
@@ -35,6 +35,7 @@ def prepare_lmdb_genes_database(filename: str):
                     'gene_id': gene_id,
                     'id_type': 'NCBI',
                     'tax_id': tax_id,
+                    'name': gene_name,
                     'common_name': {gene_id: normalize_str(gene_name)},
                 }
 
@@ -68,6 +69,7 @@ def prepare_lmdb_chemicals_database(filename: str):
                 chemical = {
                     'chemical_id': chemical_id,
                     'id_type': 'CHEBI',
+                    'name': chemical_name,
                     'common_name': {
                         chemical_id: normalize_str(chemical_name),
                     } if chemical_name != 'null' else {},
@@ -75,7 +77,7 @@ def prepare_lmdb_chemicals_database(filename: str):
 
                 if synonyms:
                     for syn in synonyms:
-                        synonyms_list.append((normalize_str(syn), chemical))
+                        synonyms_list.append((syn, chemical))
 
                 try:
                     if chemical_name != 'null':
@@ -95,17 +97,18 @@ def prepare_lmdb_chemicals_database(filename: str):
             for syn, chemical in synonyms_list:
                 try:
                     if syn != 'null':
-                        entity = transaction.get(syn.encode('utf-8'))
+                        entity = transaction.get(normalize_str(syn).encode('utf-8'))
                         if entity:
                             entity = json.loads(entity)
                             entity['common_name'] = {
                                 **entity['common_name'], **chemical['common_name']}
                             transaction.put(
-                                syn.encode('utf-8'),
+                                normalize_str(syn).encode('utf-8'),
                                 json.dumps(entity).encode('utf-8'))
                         else:
+                            chemical['name'] = syn
                             transaction.put(
-                                syn.encode('utf-8'),
+                                normalize_str(syn).encode('utf-8'),
                                 json.dumps(chemical).encode('utf-8'))
                 except lmdb.BadValsizeError:
                     continue
@@ -129,6 +132,7 @@ def prepare_lmdb_compounds_database(filename: str):
                 compound = {
                     'compound_id': compound_id,
                     'id_type': 'BIOCYC',
+                    'name': compound_name,
                     'common_name': {
                         compound_id: normalize_str(compound_name),
                     } if compound_name != 'null' else {},
@@ -136,7 +140,7 @@ def prepare_lmdb_compounds_database(filename: str):
 
                 if synonyms:
                     for syn in synonyms:
-                        synonyms_list.append((normalize_str(syn), compound))
+                        synonyms_list.append((syn, compound))
 
                 try:
                     if compound_name != 'null':
@@ -156,17 +160,18 @@ def prepare_lmdb_compounds_database(filename: str):
             for syn, compound in synonyms_list:
                 try:
                     if syn != 'null':
-                        entity = transaction.get(syn.encode('utf-8'))
+                        entity = transaction.get(normalize_str(syn).encode('utf-8'))
                         if entity:
                             entity = json.loads(entity)
                             entity['common_name'] = {
                                 **entity['common_name'], **compound['common_name']}
                             transaction.put(
-                                syn.encode('utf-8'),
+                                normalize_str(syn).encode('utf-8'),
                                 json.dumps(entity).encode('utf-8'))
                         else:
+                            compound['name'] = syn
                             transaction.put(
-                                syn.encode('utf-8'),
+                                normalize_str(syn).encode('utf-8'),
                                 json.dumps(compound).encode('utf-8'))
                 except lmdb.BadValsizeError:
                     continue
@@ -188,6 +193,7 @@ def prepare_lmdb_proteins_database(filename: str):
                 protein = {
                     'protein_id': protein_id,
                     'id_type': 'UNIPROT',
+                    'name': protein_name,
                     'common_name': {
                         protein_id: normalize_str(protein_name),
                     } if protein_name != 'null' else {},
@@ -226,6 +232,7 @@ def prepare_lmdb_species_database(filename: str):
                         'tax_id': species_id,
                         'id_type': 'NCBI',
                         'rank': species_rank,
+                        'name': species_name,
                         'common_name': {species_id: normalize_str(species_name)},
                     }
 
@@ -259,12 +266,13 @@ def prepare_lmdb_diseases_database(filename: str):
                 disease = {
                     'disease_id': disease_id,
                     'id_type': 'MESH',
+                    'name': disease_name,
                     'common_name': {
                         disease_id: normalize_str(disease_name),
                     } if disease_name != 'null' else {},
                 }
 
-                synonyms_list.append((normalize_str(synonym), disease))
+                synonyms_list.append((synonym, disease))
 
                 try:
                     if disease_name != 'null':
@@ -283,18 +291,80 @@ def prepare_lmdb_diseases_database(filename: str):
             for syn, disease in synonyms_list:
                 try:
                     if syn != 'null':
-                        entity = transaction.get(syn.encode('utf-8'))
+                        entity = transaction.get(normalize_str(syn).encode('utf-8'))
                         if entity:
                             entity = json.loads(entity)
                             entity['common_name'] = {
                                 **entity['common_name'], **disease['common_name']}
                             transaction.put(
-                                syn.encode('utf-8'),
+                                normalize_str(syn).encode('utf-8'),
                                 json.dumps(entity).encode('utf-8'))
                         else:
+                            disease['name'] = syn
                             transaction.put(
-                                syn.encode('utf-8'),
+                                normalize_str(syn).encode('utf-8'),
                                 json.dumps(disease).encode('utf-8'))
+                except lmdb.BadValsizeError:
+                    continue
+
+
+def prepare_lmdb_phenotypes_database(filename: str):
+    with open(path.join(directory, filename), 'r') as f:
+        map_size = 1099511627776
+        db = lmdb.open(path.join(directory, 'lmdb/phenotype'), map_size=map_size)
+        with db.begin(write=True) as transaction:
+            reader = csv.reader(f, delimiter=',', quotechar='"')
+            # skip headers
+            # line,mesh_id,name,synonym,tree
+            headers = next(reader)
+            synonyms_list = []
+            for line in reader:
+                phenotype_id = line[1]
+                phenotype_name = line[2]
+                synonyms = line[3]
+
+                phenotype = {
+                    'phenotype_id': phenotype_id,
+                    'id_type': 'MESH',
+                    'name': phenotype_name,
+                    'common_name': {
+                        phenotype_id: normalize_str(phenotype_name),
+                    } if phenotype_name != 'null' else {},
+                }
+
+                if synonyms:
+                    for syn in synonyms:
+                        synonyms_list.append((syn, phenotype))
+
+                try:
+                    transaction.put(
+                        normalize_str(phenotype_name).encode('utf-8'),
+                        json.dumps(phenotype).encode('utf-8'))
+                except lmdb.BadValsizeError:
+                    # ignore any keys that are too large
+                    # LMDB has max key size 512 bytes
+                    # can change but larger keys mean performance issues
+                    continue
+
+            # add all synonyms into LMDB
+            # the reason is because a synonym could be a
+            # common name, so we add those first
+            for syn, phenotype in synonyms_list:
+                try:
+                    if syn != 'null':
+                        entity = transaction.get(normalize_str(syn).encode('utf-8'))
+                        if entity:
+                            entity = json.loads(entity)
+                            entity['common_name'] = {
+                                **entity['common_name'], **phenotype['common_name']}
+                            transaction.put(
+                                normalize_str(syn).encode('utf-8'),
+                                json.dumps(entity).encode('utf-8'))
+                        else:
+                            phenotype['name'] = syn
+                            transaction.put(
+                                normalize_str(syn).encode('utf-8'),
+                                json.dumps(phenotype).encode('utf-8'))
                 except lmdb.BadValsizeError:
                     continue
 
@@ -318,12 +388,14 @@ if __name__ == '__main__':
                 print(f'Deleting {path.join(parent, fn)}...')
                 remove(path.join(parent, fn))
 
+
     prepare_lmdb_genes_database(filename='datasets/genes.tsv')
     prepare_lmdb_chemicals_database(filename='datasets/chebi.csv')
     prepare_lmdb_compounds_database(filename='datasets/compounds.csv')
     prepare_lmdb_proteins_database(filename='datasets/proteins.tsv')
     prepare_lmdb_species_database(filename='datasets/taxonomy.tsv')
     prepare_lmdb_diseases_database(filename='datasets/disease.csv')
+    prepare_lmdb_phenotypes_database(filename='datasets/phenotype.csv')
 
     # covid-19
     prepare_lmdb_diseases_database(filename='datasets/covid19_disease.csv')
