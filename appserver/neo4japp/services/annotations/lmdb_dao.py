971024cf17bd681e29f68894751088d7d316f45f
Binh Vu <binhvu.dev@gmail.com>
2020-05-04 16:36:14 -0700

    Modified LMDBDao to only init LMDB transaction if valid path. This is to allow unit tests to work without having LMDB files.

--- appserver/neo4japp/services/annotations/lmdb_dao.py
+++ appserver/neo4japp/services/annotations/lmdb_dao.py
@@ -18,44 +18,53 @@ class LMDBDao:
         diseases_lmdb_path: str='lmdb/diseases',
         phenotypes_lmdb_path: str='lmdb/phenotype',
     ) -> None:
-        self.genes_env = lmdb.open(
-            path=path.join(directory, genes_lmdb_path),
-            readonly=True,
-        )
-        self.chemicals_env = lmdb.open(
-            path=path.join(directory, chemicals_lmdb_path),
-            readonly=True,
-        )
-        self.compounds_env = lmdb.open(
-            path=path.join(directory, compounds_lmdb_path),
-            readonly=True,
-        )
-        self.proteins_env = lmdb.open(
-            path=path.join(directory, proteins_lmdb_path),
-            readonly=True,
-        )
-        self.species_env = lmdb.open(
-            path=path.join(directory, species_lmdb_path),
-            readonly=True,
-        )
-        self.diseases_env = lmdb.open(
-            path=path.join(directory, diseases_lmdb_path),
-            readonly=True,
-        )
-        self.phenotypes_env = lmdb.open(
-            path=path.join(directory, phenotypes_lmdb_path),
-            readonly=True,
-        )
+        if all([
+            genes_lmdb_path,
+            chemicals_lmdb_path,
+            compounds_lmdb_path,
+            proteins_lmdb_path,
+            species_lmdb_path,
+            diseases_lmdb_path,
+            phenotypes_lmdb_path,
+        ]):
+            self.genes_env = lmdb.open(
+                path=path.join(directory, genes_lmdb_path),
+                readonly=True,
+            )
+            self.chemicals_env = lmdb.open(
+                path=path.join(directory, chemicals_lmdb_path),
+                readonly=True,
+            )
+            self.compounds_env = lmdb.open(
+                path=path.join(directory, compounds_lmdb_path),
+                readonly=True,
+            )
+            self.proteins_env = lmdb.open(
+                path=path.join(directory, proteins_lmdb_path),
+                readonly=True,
+            )
+            self.species_env = lmdb.open(
+                path=path.join(directory, species_lmdb_path),
+                readonly=True,
+            )
+            self.diseases_env = lmdb.open(
+                path=path.join(directory, diseases_lmdb_path),
+                readonly=True,
+            )
+            self.phenotypes_env = lmdb.open(
+                path=path.join(directory, phenotypes_lmdb_path),
+                readonly=True,
+            )
 
-        # https://lmdb.readthedocs.io/en/release/#transaction-management
-        # TODO: JIRA LL-330 env should be closed at end of app context
-        self.genes_txn = self.genes_env.begin()
-        self.chemicals_txn = self.chemicals_env.begin()
-        self.compounds_txn = self.compounds_env.begin()
-        self.proteins_txn = self.proteins_env.begin()
-        self.species_txn = self.species_env.begin()
-        self.diseases_txn = self.diseases_env.begin()
-        self.phenotypes_txn = self.phenotypes_env.begin()
+            # https://lmdb.readthedocs.io/en/release/#transaction-management
+            # TODO: JIRA LL-330 env should be closed at end of app context
+            self.genes_txn = self.genes_env.begin()
+            self.chemicals_txn = self.chemicals_env.begin()
+            self.compounds_txn = self.compounds_env.begin()
+            self.proteins_txn = self.proteins_env.begin()
+            self.species_txn = self.species_env.begin()
+            self.diseases_txn = self.diseases_env.begin()
+            self.phenotypes_txn = self.phenotypes_env.begin()
 
     def new_lmdb(self):
         # TODO: JIRA LL-315 from LL-256
