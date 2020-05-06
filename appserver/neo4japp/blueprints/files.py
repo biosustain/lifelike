a291a59bfc512bfa5ae569fa98eac45c62e6ebe1
Albert Pham <the.sk89q@gmail.com>
2020-05-05 17:18:18 -0700

    Fix re-annotations not working due to file dedupe.

--- appserver/neo4japp/blueprints/files.py
+++ appserver/neo4japp/blueprints/files.py
@@ -219,7 +219,11 @@ def reannotate():
     ids = request.get_json()
     outcome: Dict[str, str] = {}  # file id to annotation outcome
     for id in ids:
-        file = Files.query.filter_by(file_id=id).one_or_none()
+        file = db.session \
+            .query(Files.id, Files.filename, Files.annotations, FileContent.raw_file) \
+            .join(FileContent, FileContent.id == Files.content_id) \
+            .filter(Files.file_id == id)\
+            .one_or_none()
         if file is None:
             current_app.logger.error('Could not find file: %s, %s', id, file.filename)
             outcome[id] = AnnotationOutcome.NOT_FOUND.value
