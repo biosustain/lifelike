1dbf0002482446fd38487457bc6246009e85f54a
Albert Pham <the.sk89q@gmail.com>
2020-05-07 11:40:43 -0700

    Fix JS syntax style issues.

--- client/src/app/pdf-viewer/annotation-type.ts
+++ client/src/app/pdf-viewer/annotation-type.ts
@@ -1,31 +1,31 @@
-export type Location = {
-    pageNumber:number;
-    rect: Rect;
+export interface Location {
+  pageNumber: number;
+  rect: Rect;
 }
 
-export type Links = {
-    ncbi?: string;
-    uniprot?: string;
-    wikipedia?: string;
-    google?: string;
+export interface Links {
+  ncbi?: string;
+  uniprot?: string;
+  wikipedia?: string;
+  google?: string;
 }
 
-export type Meta = {
-    type:string;
-    color:string;
-    id?:string;
-    idType?: string;
-    isCustom?: boolean;
-    allText?: string;
-    links?: Links;
-    hyperlink?: string;
+export interface Meta {
+  type: string;
+  color: string;
+  id?: string;
+  idType?: string;
+  isCustom?: boolean;
+  allText?: string;
+  links?: Links;
+  hyperlink?: string;
 }
 
 export type Rect = number[];
 
-export type Annotation = {
-    pageNumber: number;
-    keywords: string[];
-    rects: Rect[];
-    meta: Meta;
+export interface Annotation {
+  pageNumber: number;
+  keywords: string[];
+  rects: Rect[];
+  meta: Meta;
 }
