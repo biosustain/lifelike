36bdb3f4400eec25d8ff0f03b841f2bc1399bdac
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-05-08 11:26:49 -0700

    Refac multi service call to higher order service

--- appserver/neo4japp/services/annotations/annotations_service.py
+++ appserver/neo4japp/services/annotations/annotations_service.py
@@ -31,6 +31,7 @@ from neo4japp.data_transfer_objects import (
     PDFTokenPositions,
     PDFTokenPositionsList,
 )
+from neo4japp.database import get_hybrid_neo4j_postgres_service
 from neo4japp.util import compute_hash
 
 
@@ -424,31 +425,11 @@ class AnnotationsService:
 
         tokens_lowercased = set(tokens.keys())
 
-        def get_gene_to_organism_match_result(
-            genes: List[str],
-            matched_organism_ids: List[str],
-        ) -> Dict[str, Dict[str, str]]:
-            """Returns a map of gene name to gene id."""
-            from neo4japp.database import get_organism_gene_match_service
-            from neo4japp.database import get_neo4j_service_dao
-
-            organism_gene_match_service = get_organism_gene_match_service()
-            neo4j = get_neo4j_service_dao()
-
-            # First check if the gene/organism match exists in the postgres lookup table
-            postgres_result = organism_gene_match_service.get_genes(genes, matched_organism_ids)
-
-            # Collect all the genes that were not matched to an organism in the table, and search
-            # the Neo4j database for them
-            second_round_genes = [gene for gene in genes if gene not in postgres_result.keys()]
-            neo4j_result = neo4j.get_genes_to_organisms(second_round_genes, matched_organism_ids)
-
-            # Join the results of the two queries
-            postgres_result.update(neo4j_result)
-
-            return postgres_result
-
-        match_result = get_gene_to_organism_match_result(list(tokens.keys()), matched_organism_ids)
+        hybrid_neo4j_postgres_service = get_hybrid_neo4j_postgres_service()
+        match_result = hybrid_neo4j_postgres_service.get_gene_to_organism_match_result(
+            genes=list(tokens.keys()),
+            matched_organism_ids=matched_organism_ids,
+        )
 
         for word, token_positions_list in tokens.items():
             # If the "gene" is not matched to any organism in the paper, ignore it
