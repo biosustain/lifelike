45a98296e21cda6977aff4191a38cbbc8d0acf54
Julio Melanda <juceme@biosustain.dtu.dk>
2020-05-13 16:05:35 +0200

    Fixes annotation indexing

--- appserver/neo4japp/services/annotations/index_annotations.py
+++ appserver/neo4japp/services/annotations/index_annotations.py
@@ -15,10 +15,21 @@ if not es.exists('annotations', 'annotations'):
 
 for subdirs, dirs, files in os.walk(os.path.join(directory, 'lmdb')):
     if 'data.mdb' in files:
-        db = lmdb.open(os.path.join(directory, subdirs))
+        print(f'Processing {subdirs}'x½x½)
+        db = lmdb.open(subdirs)
+        type = subdirs.split('/')[-1]
         with db.begin() as transaction:
             cursor = transaction.cursor()
             for key, value in cursor.iternext():
-                entry = {'annotation': key.decode('utf-8'),
-                         'data': json.loads(value.decode('utf-8'))}
+                data = json.loads(value.decode('utf-8'))
+                data['common_name'] = {
+                    'id': next(iter(data['common_name'].keys())),
+                    'normalized_str': next(iter(data['common_name'].values()))
+                }
+                entry = {'annotation': {
+                            'id': key.decode('utf-8'),
+                            'type': type,
+                            'data': data
+                            }
+                         }
                 es.index('annotations', entry)
