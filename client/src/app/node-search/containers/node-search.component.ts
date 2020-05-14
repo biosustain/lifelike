e55ad467cf3b759564094862219c88210fe38ccc
Miguel Rueda <miguerueda86@yahoo.es>
2020-05-14 11:12:00 +0200

    feat: initial mockup for result list

--- client/src/app/node-search/containers/node-search.component.ts
+++ client/src/app/node-search/containers/node-search.component.ts
@@ -4,6 +4,7 @@ import {Component} from '@angular/core';
   selector: 'app-search-collection-page',
   template: `
     <app-node-search-bar></app-node-search-bar>
+    <app-node-result-list></app-node-result-list>
   `,
 })
 export class NodeSearchComponent {
--- client/src/app/node-search/node-result-list/node-result-list.component.ts
+++ client/src/app/node-search/node-result-list/node-result-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+
+export interface Nodes {
+  position: number;
+  database: string;
+  type: string;
+  name: string;
+}
+
+const ELEMENT_DATA: Nodes[] = [
+  {position: 1, database: 'Biocyc', type: 'Gene', name: 'cysB'},
+  {position: 2, database: 'Uniprot', type: 'Gene', name: 'gyrA'},
+  {position: 3, database: 'Go', type: 'Chemical', name: 'adenosine'},
+  {position: 4, database: 'Biocyc', type: 'Gene', name: 'cysB'},
+  {position: 5, database: 'Uniprot', type: 'Gene', name: 'gyrA'},
+  {position: 6, database: 'Go', type: 'Chemical', name: 'adenosine'},
+  {position: 7, database: 'Biocyc', type: 'Gene', name: 'cysB'},
+  {position: 8, database: 'Uniprot', type: 'Gene', name: 'gyrA'},
+  {position: 9, database: 'Go', type: 'Chemical', name: 'adenosine'},
+  {position: 10, database: 'Go', type: 'Chemical', name: 'adenosine'},
+];
 
 @Component({
   selector: 'app-node-result-list',
   templateUrl: './node-result-list.component.html',
   styleUrls: ['./node-result-list.component.scss']
 })
+
 export class NodeResultListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  displayedColumns: string[] = ['name', 'type', 'database', 'actions'];
+  dataSource = new MatTableDataSource<Nodes>(ELEMENT_DATA);
+  selection = new SelectionModel<Nodes>(true, []);
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  constructor() {
   }
 
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onClick() {
+  }
 }
