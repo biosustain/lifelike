e59843a0978a7a097b4dbb49353faf1e43937d31
Miguel Rueda <miguerueda86@yahoo.es>
2020-05-14 19:27:12 +0200

    feat: adding search functionality to search bar

--- client/src/app/node-search/containers/node-search.component.ts
+++ client/src/app/node-search/containers/node-search.component.ts
@@ -1,14 +1,25 @@
 import {Component} from '@angular/core';
+import {FTSQueryRecord} from '../../interfaces';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-search-collection-page',
   template: `
-    <app-node-search-bar></app-node-search-bar>
+    <app-node-search-bar
+      (results)="getResults($event)"
+    ></app-node-search-bar>
     <app-node-result-list></app-node-result-list>
   `,
 })
 export class NodeSearchComponent {
 
+  results: Observable<FTSQueryRecord[]>;
+
   constructor() {
+  }
+
+  getResults(results) {
+    this.results = results;
+    console.log(this.results)
   }
 }
