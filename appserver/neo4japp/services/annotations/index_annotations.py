d6e7f2995eb87fad09b537b2910ac07a43d47e77
Julio Melanda <juceme@biosustain.dtu.dk>
2020-05-18 17:49:00 +0200

    Changes indexing method to bulk

--- appserver/neo4japp/services/annotations/index_annotations.py
+++ appserver/neo4japp/services/annotations/index_annotations.py
@@ -1,35 +1,41 @@
 import os
 import json
-from elasticsearch import Elasticsearch
 import lmdb
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 
-directory = os.path.realpath(os.path.dirname(__file__))
 
-es = Elasticsearch(hosts=['http://elasticsearch'], timeout=5000)
-if not es.exists('annotations', 'annotations'):
-    es.create(
-        'annotations',
-        'annotations',
-        {}
-    )
+def main():
+    directory = os.path.realpath(os.path.dirname(__file__))
 
-for subdirs, dirs, files in os.walk(os.path.join(directory, 'lmdb')):
-    if 'data.mdb' in files:
-        print(f'Processing {subdirs}'x½x½)
-        db = lmdb.open(subdirs)
-        type = subdirs.split('/')[-1]
-        with db.begin() as transaction:
-            cursor = transaction.cursor()
-            for key, value in cursor.iternext():
-                data = json.loads(value.decode('utf-8'))
-                data['common_name'] = {
-                    'id': next(iter(data['common_name'].keys())),
-                    'normalized_str': next(iter(data['common_name'].values()))
-                }
-                entry = {'annotation': {
+    es = Elasticsearch(hosts=['http://elasticsearch'], timeout=5000)
+
+    for subdirs, dirs, files in os.walk(os.path.join(directory, 'lmdb')):
+        if 'data.mdb' in files:
+            process_lmdb(subdirs, es)
+
+
+def process_lmdb(subdirs, es):
+    print(f'Processing {subdirs}')
+    db = lmdb.open(subdirs)
+    type = subdirs.split('/')[-1]
+    entries = []
+    with db.begin() as transaction:
+        cursor = transaction.cursor()
+        for key, value in cursor.iternext():
+            data = json.loads(value.decode('utf-8'))
+            data['common_name'] = {
+                'id': next(iter(data['common_name'].keys())),
+                'normalized_str': next(iter(data['common_name'].values()))
+            }
+            entry = {
+                        '_index': type,
+                        '_source': {
                             'id': key.decode('utf-8'),
-                            'type': type,
                             'data': data
-                            }
-                         }
-                es.index('annotations', entry)
+                        }
+                     }
+            entries.append(entry)
+    bulk(es, entries)
+
+main()
