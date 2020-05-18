990e6c51d237bbbb33877770b73170217f8e0e84
Miguel Rueda <miguerueda86@yahoo.es>
2020-05-18 18:23:57 +0200

    feat: adding paginator functionality and styling the result list component

--- client/src/app/node-search/node-search-bar/node-search-bar.component.ts
+++ client/src/app/node-search/node-search-bar/node-search-bar.component.ts
@@ -1,17 +1,21 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {SearchService} from '../../search/services/search.service';
 import {FTSQueryRecord} from '../../interfaces';
+import {PageActions} from '../containers/node-search.component';
+
 
 @Component({
   selector: 'app-node-search-bar',
   templateUrl: './node-search-bar.component.html',
   styleUrls: ['./node-search-bar.component.scss']
 })
-export class NodeSearchBarComponent implements OnInit {
+export class NodeSearchBarComponent implements OnInit, OnChanges {
 
   @Input() error = '';
   @Output() results = new EventEmitter<any>();
+  @Input() pageActions: PageActions = {pageIndex: 1};
+  paginatorActions: PageActions;
   searchForm = new FormGroup({
     searchInput: new FormControl(''),
   });
@@ -20,13 +24,27 @@ export class NodeSearchBarComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.paginatorActions = this.pageActions;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const propertyName in changes) {
+      const propertyChanges = changes[propertyName];
+      const current = JSON.stringify(propertyChanges.currentValue);
+      const previous = JSON.stringify(propertyChanges.previousValue);
+      if (current !== previous) {
+        this.paginatorActions = this.pageActions;
+        this.onSubmit();
+      }
+    }
   }
 
   onSubmit() {
-    this.searchService.fullTextSearch(this.searchForm.value.searchInput)
-      .subscribe((results) => {
-        this.results.emit(results.nodes as FTSQueryRecord[]);
-      });
+    this.searchService.fullTextSearch(
+      this.searchForm.value.searchInput,
+      this.paginatorActions.pageIndex , 25).subscribe((results) => {
+      this.results.emit(results.nodes as FTSQueryRecord[]);
+    });
   }
 
 }
