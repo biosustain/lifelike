ac70a6080afce707026f0a39739513123a6a4273
Miguel Rueda <miguerueda86@yahoo.es>
2020-05-18 14:23:45 +0200

    feat: making search results fill the result table component

--- client/src/app/node-search/node-result-list/node-result-list.component.ts
+++ client/src/app/node-search/node-result-list/node-result-list.component.ts
@@ -1,27 +1,7 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {SelectionModel} from '@angular/cdk/collections';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
-
-export interface Nodes {
-  position: number;
-  database: string;
-  type: string;
-  name: string;
-}
-
-const ELEMENT_DATA: Nodes[] = [
-  {position: 1, database: 'Biocyc', type: 'Gene', name: 'cysB'},
-  {position: 2, database: 'Uniprot', type: 'Gene', name: 'gyrA'},
-  {position: 3, database: 'Go', type: 'Chemical', name: 'adenosine'},
-  {position: 4, database: 'Biocyc', type: 'Gene', name: 'cysB'},
-  {position: 5, database: 'Uniprot', type: 'Gene', name: 'gyrA'},
-  {position: 6, database: 'Go', type: 'Chemical', name: 'adenosine'},
-  {position: 7, database: 'Biocyc', type: 'Gene', name: 'cysB'},
-  {position: 8, database: 'Uniprot', type: 'Gene', name: 'gyrA'},
-  {position: 9, database: 'Go', type: 'Chemical', name: 'adenosine'},
-  {position: 10, database: 'Go', type: 'Chemical', name: 'adenosine'},
-];
+import {Nodes} from '../containers/node-search.component';
 
 @Component({
   selector: 'app-node-result-list',
@@ -29,17 +9,23 @@ const ELEMENT_DATA: Nodes[] = [
   styleUrls: ['./node-result-list.component.scss']
 })
 
-export class NodeResultListComponent implements OnInit {
+export class NodeResultListComponent implements OnInit, OnChanges {
+  @Input() nodes: Nodes[] = [];
   displayedColumns: string[] = ['name', 'type', 'database', 'actions'];
-  dataSource = new MatTableDataSource<Nodes>(ELEMENT_DATA);
-  selection = new SelectionModel<Nodes>(true, []);
+  dataSource = new MatTableDataSource<Nodes>(this.nodes);
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
   constructor() {
   }
 
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.dataSource = new MatTableDataSource<Nodes>(this.nodes);
+  }
+
 
   onClick() {
   }
