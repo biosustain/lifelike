4c1be754a87c90f90f72f1d9d73e55f41ba12726
Miguel Rueda <miguerueda86@yahoo.es>
2020-05-19 15:11:57 +0200

    feat: adding preview functionality to table component

--- client/src/app/node-search/containers/node-search.component.ts
+++ client/src/app/node-search/containers/node-search.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, SecurityContext} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 export interface Nodes {
-  position: number;
+  link: string;
   domain: string;
   type: string;
   name: string;
@@ -26,18 +27,25 @@ export interface PageActions {
 })
 export class NodeSearchComponent {
 
+  DOMAINS_URL = {
+    CHEBI: 'https://www.ebi.ac.uk/chebi/searchId.do?chebiId=',
+    MESH: 'https://www.ncbi.nlm.nih.gov/mesh/?term=',
+    Text: 'https://pubmed.ncbi.nlm.nih.gov/',
+    NCBI_Gene: 'https://www.ncbi.nlm.nih.gov/gene/',
+    NCBI_Taxonomy: 'https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id='
+  }
   dataSource: Nodes[] = [];
   pageActions: PageActions = {pageIndex: 1};
 
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
   }
 
   getResults(results) {
-    this.dataSource = results.map((data, index) => {
+    this.dataSource = results.map((data) => {
       return {
-        position: index + 1,
-        name: data.node.displayName,
+        link: this.getLink(data),
+        name: this.getName(data),
         type: this.getType(data.node.subLabels),
         domain: this.getDomain(data.node.subLabels)
       };
@@ -56,6 +64,29 @@ export class NodeSearchComponent {
   }
 
   getType(subLabels: string[]) {
-    return subLabels.find(element => !element.match(/^db_*/));
+    const type = subLabels.find(element => !element.match(/^db_*/));
+    return type === 'TopicalDescriptor' ? 'Disease' : type;
+  }
+
+  getLink(data) {
+    const domain = this.getDomain(data.node.subLabels);
+    const type = this.getType(data.node.subLabels);
+    if (domain === 'NCBI' && type === 'Gene') {
+      return this.sanitizer.bypassSecurityTrustUrl(this.DOMAINS_URL[domain + '_' + type] +
+        data.node.data.id);
+    } else if (domain === 'NCBI' && type === 'Taxonomy') {
+      return this.sanitizer.bypassSecurityTrustUrl(this.DOMAINS_URL[domain + '_' + type] +
+        data.node.data.id);
+    } else if (domain === 'Text') {
+      return this.sanitizer.bypassSecurityTrustUrl(this.DOMAINS_URL[domain] +
+        data.publicationId);
+    } else {
+      return this.sanitizer.bypassSecurityTrustUrl(this.DOMAINS_URL[domain] +
+        data.node.data.id.split(':')[1]);
+    }
+  }
+
+  getName(data) {
+    return data.node.displayName;
   }
 }
