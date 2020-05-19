276476b9b1657f976157be681ca2bb0cfbd07205
Miguel Rueda <miguerueda86@yahoo.es>
2020-05-19 21:54:21 +0200

    fix: broken tests

--- client/src/app/node-search/node-search-bar/node-search-bar.component.spec.ts
+++ client/src/app/node-search/node-search-bar/node-search-bar.component.spec.ts
@@ -1,6 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { NodeSearchBarComponent } from './node-search-bar.component';
+import {NodeSearchBarComponent} from './node-search-bar.component';
+import {SharedModule} from '../../shared/shared.module';
+import {NodeSearchModule} from '../node-search.module';
+import {RootStoreModule} from '../../***ARANGO_USERNAME***-store';
+import {BrowserModule} from '@angular/platform-browser';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {SearchService} from '../../search/services/search.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('NodeSearchBarComponent', () => {
   let component: NodeSearchBarComponent;
@@ -8,9 +15,18 @@ describe('NodeSearchBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NodeSearchBarComponent ]
+      imports: [
+        NodeSearchModule,
+        RootStoreModule,
+        BrowserModule,
+        DragDropModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        SearchService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
