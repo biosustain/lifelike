94b3313521d7319f678948e6c7016c5d3afedbed
Binh Vu <binhvu.dev@gmail.com>
2020-05-19 09:19:10 -0700

    Finished up annotations to pubtator script to create data file for nlp

--- appserver/neo4japp/services/annotations/nlp_data_output/annotations_to_pubtator.py
+++ appserver/neo4japp/services/annotations/nlp_data_output/annotations_to_pubtator.py
@@ -5,6 +5,7 @@ import os
 from neo4japp.database import (
     get_annotations_service,
     get_annotations_pdf_parser,
+    get_bioc_document_service,
     get_lmdb_dao,
 )
 from neo4japp.factory import create_app
@@ -16,19 +17,17 @@ directory = os.path.realpath(os.path.dirname(__file__))
 
 
 def write_to_file(
-    annotations_service,
-    pdf_parser,
-    pdf,
+    annotations,
     pubtator_file,
 ):
-    pdf_json = json.load(pdf)
-    identifier = compute_hash(pdf_json, limit=8)
+    annotation_json = json.load(annotations)
+    identifier = compute_hash(annotation_json, limit=8)
     writer = csv.writer(pubtator_file, delimiter='|')
-    writer.writerow([identifier, 't', pdf_json['documents'][0]['id']])
-    writer.writerow([identifier, 'a', repr(pdf_json['documents'][0]['passages'][0]['text'])])
+    writer.writerow([identifier, 't', annotation_json['documents'][0]['id']])
+    writer.writerow([identifier, 'a', repr(annotation_json['documents'][0]['passages'][0]['text'])])
     writer = csv.writer(pubtator_file, delimiter='\t')
 
-    annotations = pdf_json['documents'][0]['passages'][0]['annotations']
+    annotations = annotation_json['documents'][0]['passages'][0]['annotations']
 
     for annotation in annotations:
         writer.writerow([
@@ -36,25 +35,56 @@ def write_to_file(
             annotation['loLocationOffset'],
             annotation['hiLocationOffset'],
             annotation['keyword'],
-            annotation['meta']['idType'],
+            annotation['meta']['keywordType'],
             annotation['meta']['id'],
         ])
+
+
+def create_annotations(
+    annotations_service,
+    bioc_service,
+    filename,
+    pdf_parser,
+    pdf,
+):
+    parsed = pdf_parser.parse_pdf(pdf=pdf)
+    annotations = annotations_service.create_annotations(
+        tokens=pdf_parser.extract_tokens(parsed_chars=parsed),
+    )
+
+    bioc = bioc_service.read(text=pdf_text, file_uri=filename)
+    bioc_json = bioc_service.generate_bioc_json(annotations=annotations, bioc=bioc)
+    return bioc_json
 
 
 def main():
     app = create_app('Functional Test Flask App', config='config.Testing')
     pubtator_file = open(os.path.join(directory, 'pubtator.tsv'), 'w+')
     with app.app_context():
+        bioc_service = get_bioc_document_service()
         service = get_annotations_service(lmdb_dao=get_lmdb_dao())
         parser = get_annotations_pdf_parser()
+
+        for parent, subfolders, filenames in os.walk(os.path.join(directory, 'pdfs/')):
+            for fn in filenames:
+                with open(os.path.join(parent, fn), 'rb') as f:
+                    annotations = create_annotions(
+                        annotations_service=service,
+                        bioc_service=bioc_service,
+                        filename=fn,
+                        pdf_parser=parser,
+                        pdf=f,
+                    )
+
+                    annotation_file = os.path.join(directory, f'annotations/{filename}')
+                    with open(annotation_file, 'w') as a_f:
+                        json.dump(annotations, a_f)
 
         for parent, subfolders, filenames in os.walk(os.path.join(directory, 'annotations/')):
             for fn in filenames:
                 with open(os.path.join(parent, fn), 'rb') as f:
                     write_to_file(
-                        annotations_service=service,
-                        pdf_parser=parser,
-                        pdf=f,
+                        annotations=f,
                         pubtator_file=pubtator_file,
                     )
     pubtator_file.close()
