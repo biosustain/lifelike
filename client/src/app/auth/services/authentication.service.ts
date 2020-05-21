1fd2729eea5eae5c75a99b5bc68cf72770364591
Mustafa Ahmed Khan III <makman09@gmail.com>
2020-05-21 07:13:21 -0700

    fix lint bugs

--- client/src/app/auth/services/authentication.service.ts
+++ client/src/app/auth/services/authentication.service.ts
@@ -113,7 +113,7 @@ export class AuthenticationService {
     // tslint:disable-next-line: prefer-for-of
     for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0) === ' ') { c = c.substring(1,c.length); }
+        while (c.charAt(0) === ' ') { c = c.substring(1, c.length); }
         if (c.indexOf(nameEQ) === 0) { return c.substring(nameEQ.length, c.length); }
     }
     return null;
