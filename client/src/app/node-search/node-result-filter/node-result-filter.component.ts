15b26086ab3756da50f1c25b2f92fcd9398f6b22
Miguel Rueda <miguerueda86@yahoo.es>
2020-05-25 19:39:49 +0200

    feat: styling filter component

--- client/src/app/node-search/node-result-filter/node-result-filter.component.ts
+++ client/src/app/node-search/node-result-filter/node-result-filter.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./node-result-filter.component.scss']
 })
 export class NodeResultFilterComponent implements OnInit {
-  panelOpenState = false;
+  typeOfDomains: string[] = ['CHEBI', 'MESH', 'NCBI', 'GO'].sort();
+  typesOfEntities: string[] = ['GENES', 'CHEMICAL', 'DISEASES', 'TAXONOMY', 'SNIPPET'].sort();
   constructor() { }
 
   ngOnInit() {
