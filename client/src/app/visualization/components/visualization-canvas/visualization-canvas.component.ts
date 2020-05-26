544e02b18ed06ef50fe601ea15ae19f5f4ace391
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-05-26 15:29:16 -0700

    Fix expandOrCollapseNode behavior
    Also removes a few data structures which were no longer necessary. We
    were keeping track of duplicate edges which had been created via
    clustering so we could avoid drawing those edges. The reasoning for this
    was driven by the assumption that there should never be more than one
    copy of an edge drawn on the canvas, either on the map or abstracted
    into a cluster. This is no longer the case. Both the real and the
    clustered version of an node/edge can appear on the map now.

--- client/src/app/visualization/components/visualization-canvas/visualization-canvas.component.ts
+++ client/src/app/visualization/components/visualization-canvas/visualization-canvas.component.ts
@@ -63,8 +63,6 @@ export class VisualizationCanvasComponent implements OnInit {
     @Output() getSnippetsFromEdge = new EventEmitter<VisEdge>();
     @Output() getSnippetsFromDuplicateEdge = new EventEmitter<DuplicateVisEdge>();
     @Output() getClusterData = new EventEmitter<ClusteredNode[]>();
-    @Output() addDuplicatedEdge = new EventEmitter<number>();
-    @Output() removeDuplicatedEdge = new EventEmitter<number>();
 
     @Input() nodes: DataSet<any, any>;
     @Input() edges: DataSet<any, any>;
@@ -116,7 +114,6 @@ export class VisualizationCanvasComponent implements OnInit {
                             nodesToRemove.push(duplicateNode.duplicateOf);
                         }
 
-                        this.addDuplicatedEdge.emit(duplicateEdge.duplicateOf as number);
                         edgesToRemove.push(duplicateEdge.duplicateOf);
                     });
 
@@ -282,6 +279,13 @@ export class VisualizationCanvasComponent implements OnInit {
         // Get all the nodes connected to the ***ARANGO_USERNAME*** node, before removing edges
         const connectedNodes = this.networkGraph.getConnectedNodes(***ARANGO_USERNAME***Node.id) as IdType[];
 
+        // Remove every cluster connected to the ***ARANGO_USERNAME*** node
+        connectedNodes.forEach(connectedNode => {
+            if (this.networkGraph.isCluster(connectedNode)) {
+                this.destroyCluster(connectedNode);
+            }
+        });
+
         this.edges.remove(this.networkGraph.getConnectedEdges(***ARANGO_USERNAME***Node.id) as IdType[]);
 
         // If a previously connected node has no remaining edges (i.e. it is not connected
@@ -297,13 +301,6 @@ export class VisualizationCanvasComponent implements OnInit {
 
     expandOrCollapseNode(nodeId: number) {
         const nodeRef = this.nodes.get(nodeId) as VisNode;
-        const connectedNodes = this.networkGraph.getConnectedNodes(nodeId);
-
-        connectedNodes.forEach(connectedNode => {
-            if (this.networkGraph.isCluster(connectedNode)) {
-                this.safelyOpenCluster(connectedNode);
-            }
-        });
 
         if (nodeRef.expanded) {
             // Updates node expand state
@@ -593,7 +590,6 @@ export class VisualizationCanvasComponent implements OnInit {
             this.networkGraph.getConnectedEdges(duplicateNodeId).map(
                 duplicateEdgeId => this.edges.get(duplicateEdgeId)
             ).forEach(duplicateEdge => {
-                this.removeDuplicatedEdge.emit(duplicateEdge.duplicateOf);
                 edgesToRemove.push(duplicateEdge.id);
                 edgesToAdd.push(this.createOriginalEdgeFromDuplicate(duplicateEdge));
             });
@@ -684,7 +680,6 @@ export class VisualizationCanvasComponent implements OnInit {
                 nodesToRemove.push(duplicateNode.duplicateOf);
             }
 
-            this.addDuplicatedEdge.emit(duplicateEdge.duplicateOf as number);
             edgesToRemove.push(duplicateEdge.duplicateOf);
         });
 
