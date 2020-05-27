4375ff69dac099f0cc50eed6f0924bc4663cb1d1
Binh Vu <binhvu.dev@gmail.com>
2020-05-27 12:07:02 -0700

    Changed entity types to singular.
    (cherry picked from commit 8ae99802dde897bfcb2b1754a5415e39ef97ff05)

--- client/src/app/shared/annotation-types.ts
+++ client/src/app/shared/annotation-types.ts
@@ -8,17 +8,17 @@ export class EntityType {
 }
 
 export const ENTITY_TYPES = [
-  new EntityType('Genes', '#8f7cbf'),
-  new EntityType('Proteins', '#bcbd22'),
-  new EntityType('Diseases', '#fae0b8'),
+  new EntityType('Gene', '#8f7cbf'),
+  new EntityType('Protein', '#bcbd22'),
+  new EntityType('Disease', '#fae0b8'),
   new EntityType('Species', '#3177b8'),
   new EntityType('Companies', '#ff7f7f'),
   new EntityType('Mutations', '#8b5d2e'),
-  new EntityType('Chemicals', '#cee5cb'),
-  new EntityType('Phenotypes', '#edc949'),
+  new EntityType('Chemical', '#cee5cb'),
+  new EntityType('Phenotype', '#edc949'),
   new EntityType('Pathways', '#90eebf'),
   new EntityType('Entities', '#7f7f7f'),
-  new EntityType('Compounds', '#cee5cb'),
+  new EntityType('Compound', '#cee5cb'),
 ];
 
 export const ENTITY_TYPE_MAP = ENTITY_TYPES.reduce((map, item) => {
