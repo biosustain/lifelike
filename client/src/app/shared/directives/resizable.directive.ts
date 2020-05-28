fc4dd943312737dd6b0730ef98b9a8a08c11255b
Mustafa Ahmed Khan III <makman09@gmail.com>
2020-05-28 15:37:41 -0700

    move to oninit hook

--- client/src/app/shared/directives/resizable.directive.ts
+++ client/src/app/shared/directives/resizable.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import 'jqueryui';
 import { isNullOrUndefined } from 'util';
@@ -6,27 +6,29 @@ import { isNullOrUndefined } from 'util';
 @Directive({
   selector: '[appResizable]'
 })
-export class ResizableDirective {
+export class ResizableDirective  implements OnInit {
   @Input() handles = 'n,w,s,e';
   @Input() minHeight = 52;
 
+  el: ElementRef;
+
   constructor(el: ElementRef) {
-
-    setTimeout(() => {
-      if (isNullOrUndefined(
-        el.nativeElement
-      )) {
-        return;
-      }
-
-      $(`#${el.nativeElement.id}`).resizable({
-        handles: this.handles,
-        maxWidth: 500,
-        minWidth: 256,
-        maxHeight: 500,
-        minHeight: this.minHeight
-      });
-    }, 200);
+    this.el = el;
   }
 
+  ngOnInit() {
+    if (isNullOrUndefined(
+      this.el.nativeElement
+    )) {
+      return;
+    }
+
+    $(`#${this.el.nativeElement.id}`).resizable({
+      handles: this.handles,
+      maxWidth: 500,
+      minWidth: 256,
+      maxHeight: 500,
+      minHeight: this.minHeight
+    });
+  }
 }
