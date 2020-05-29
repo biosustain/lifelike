7d7ab6282c9bc9894b694364a43438be7dc6e863
Albert Pham <the.sk89q@gmail.com>
2020-05-29 13:03:02 -0700

    Improve edge selection accuracy.

--- client/src/app/graph-viewer/utils/canvas/graph-edges/line-edge.ts
+++ client/src/app/graph-viewer/utils/canvas/graph-edges/line-edge.ts
@@ -1,5 +1,5 @@
 import { PlacedEdge } from 'app/graph-viewer/styles/styles';
-import { getLinePointIntersectionDistance } from '../../geometry';
+import { distanceUnsq, getLinePointIntersectionDistance } from '../../geometry';
 import { TextElement } from '../text-element';
 import { LineHead } from '../line-heads/line-heads';
 import { Line } from '../lines/lines';
@@ -28,6 +28,10 @@ export class LineEdge implements PlacedEdge {
 
   readonly labelX: number;
   readonly labelY: number;
+  readonly labelMinX: number;
+  readonly labelMaxX: number;
+  readonly labelMinY: number;
+  readonly labelMaxY: number;
 
   constructor(private ctx: CanvasRenderingContext2D, options: StandardEdgeOptions) {
     Object.assign(this, options);
@@ -36,15 +40,52 @@ export class LineEdge implements PlacedEdge {
         Math.min(this.source.x, this.target.x);
       this.labelY = Math.abs(this.source.y - this.target.y) / 2 +
         Math.min(this.source.y, this.target.y);
+
+      // Store these values for faster isPointIntersectingTextbox()
+      this.labelMinX = this.labelX - this.textbox.actualWidth / 2;
+      this.labelMaxX = this.labelX + this.textbox.actualWidth / 2;
+      this.labelMinY = this.labelY - this.textbox.actualHeight / 2;
+      this.labelMaxY = this.labelY + this.textbox.actualHeight / 2;
     }
   }
 
   isPointIntersecting(x: number, y: number): boolean {
+    if (this.isPointIntersectingTextbox(x, y)) {
+      return true;
+    }
+
     const x1 = Math.min(this.source.x, this.target.x);
     const x2 = Math.max(this.source.x, this.target.x);
     const y1 = Math.min(this.source.y, this.target.y);
     const y2 = Math.max(this.source.y, this.target.y);
     return getLinePointIntersectionDistance(x, y, x1, x2, y1, y2) <= 2;
+  }
+
+  private isPointIntersectingTextbox(x: number, y: number): boolean {
+    if (!this.textbox) {
+      return false;
+    }
+
+    return x >= this.labelMinX && x <= this.labelMaxX && y >= this.labelMinY && y <= this.labelMaxY;
+  }
+
+  getPointDistanceUnsq(x: number, y: number): number {
+    if (this.isPointIntersectingTextbox(x, y)) {
+      return 0;
+    }
+
+    const dx = this.target.x - this.source.x;
+    const dy = this.target.y - this.source.y;
+    const l2 = dx * dx + dy * dy;
+
+    if (l2 === 0) {
+      return distanceUnsq(x, y, this.source.x, this.source.y);
+    }
+
+    let t = ((x - this.source.x) * dx + (y - this.source.y) * dy) / l2;
+    t = Math.max(0, Math.min(1, t));
+
+    return distanceUnsq(x, y, this.source.x + t * dx, this.source.y + t * dy);
   }
 
   draw(transform: any): void {
