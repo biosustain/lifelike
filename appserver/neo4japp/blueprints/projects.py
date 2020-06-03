0f9361a0ee49ed129ecf5ef21c6dec385e2e0d28
David Bour <dbour@eng.ucsd.edu>
2020-06-03 08:10:36 -0700

    - refactor: refactored projects API to shorter forms - refactor: refactored add_projects to include a step to add a default   directory when adding a project

--- appserver/neo4japp/blueprints/projects.py
+++ appserver/neo4japp/blueprints/projects.py
@@ -1,7 +1,8 @@
 from flask import request, jsonify, Blueprint, g, abort
 from neo4japp.blueprints.auth import auth
 from neo4japp.database import db
-from neo4japp.models.projects import Projects
+from neo4japp.exceptions import RecordNotFoundException
+from neo4japp.models import Directory, Projects
 
 bp = Blueprint('projects', __name__, url_prefix='/projects')
 
@@ -9,62 +10,44 @@ bp = Blueprint('projects', __name__, url_prefix='/projects')
 @bp.route('/<name>', methods=['GET'])
 @auth.login_required
 def get_project(name):
+    # TODO: Add permission checks here
     user = g.current_user
-    # TODO: Remove hard coded Project here
-    project = Projects.query.one_or_none()
+    projects = Projects.query.filter(Projects.project_name == name).one_or_none()
+    if projects is None:
+        raise RecordNotFoundException(f'Project {name} not found')
 
-    if not project:
-        project = Projects(
-            project_name='beta-project',
-            description='beta project',
-            users=[],
-        )
-        db.session.add(project)
-        db.session.commit()
-    return jsonify(dict(project=project.to_dict()))
+    return jsonify(dict(results=projects.to_dict())), 200
 
 
-@bp.route('/list', methods=['GET'])
+@bp.route('/', methods=['GET'])
 @auth.login_required
 def get_projects():
+    # TODO: Add permission checks here
     user = g.current_user
-    projects = [{
-        'id': row.id,
-        'project_name': row.project_name,
-        'description': row.description,
-        'creation_date': row.creation_date
-    } for row in db.session.query(
-        Projects.id,
-        Projects.project_name,
-        Projects.description,
-        Projects.creation_date)
-        .filter(Projects.users.any(user.id))
-        .all()]
-    return jsonify({'projects': projects})
+    projects_list = db.session.query(Projects).all()  # TODO: paginate
+    return jsonify(dict(results=[p.to_dict() for p in projects_list])), 200
 
 
-@bp.route('/add', methods=['POST'])
+@bp.route('/', methods=['POST'])
 @auth.login_required
 def add_projects():
-    try:
-        data = request.get_json()
-        user = g.current_user
 
-        project = Projects(
-            project_name=data.get("project_name", ""),
-            description=data.get("description", ""),
-            users=[user.id]
-        )
+    data = request.get_json()
+    user = g.current_user
 
-        db.session.add(project)
-        db.session.commit()
+    projects = Projects(
+        project_name=data['projectName'],
+        description=data['description'],
+        users=[user.id],  # TODO: deprecate once migration is complete
+    )
 
-        return {
-            'status': 'success',
-            'project': project.id,
-            'name': project.project_name,
-            'description': project.description,
-            'users': project.users
-        }
-    except Exception:
-        abort(400, 'Project was not added.')
+    db.session.add(projects)
+    db.session.flush()
+
+    # Create a default directory for every project
+    default_dir = Directory(name='/', directory_parent_id=None, projects_id=projects.id)
+
+    db.session.add(default_dir)
+    db.session.commit()
+
+    return jsonify(dict(results=projects.to_dict())), 200
