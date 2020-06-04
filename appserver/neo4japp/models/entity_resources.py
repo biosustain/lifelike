a562ade006ddec35e5f8613a5d82df7c753c2dc6
Julio Melanda <juceme@biosustain.dtu.dk>
2020-06-04 16:17:24 +0200

    Creates database entry for the annotationstyles

---
+++ appserver/neo4japp/models/entity_resources.py
@@ -0,0 +1,24 @@
+from neo4japp.database import db
+from neo4japp.models.common import RDBMSBase
+
+
+class DomainULRsMap(RDBMSBase):
+    """
+    This model stores the relation between knowledge domains and its base URLs
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    domain = db.Column(db.String(128), nullable=False)
+    base_URL = db.Column(db.String(256), nullable=False)
+
+
+class AnnotationStyle(RDBMSBase):
+    """
+    This model stores the styles related to each entity type
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(32), nullable=False)
+    color = db.Column(db.String(8), nullable=False)
+    icon_code = db.Column(db.String(32), nullable=True)
+    style_border = db.Column(db.String(8), nullable=True)
+    style_background = db.Column(db.String(8), nullable=True)
+    style_color = db.Column(db.String(8), nullable=True)
