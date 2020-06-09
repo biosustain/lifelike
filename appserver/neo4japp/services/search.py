4aaed71178f6f129473f42364e6667bc03c82b29
Miguel Rueda <miguerueda86@yahoo.es>
2020-06-09 12:51:11 +0200

    fix: linting issues

--- appserver/neo4japp/services/search.py
+++ appserver/neo4japp/services/search.py
@@ -157,7 +157,7 @@ class SearchService(GraphBaseDao):
                        'YIELD node, score WITH node, score MATCH (node)-[]-(n) ' \
                        'WHERE %s AND NOT n:TopicalDescriptor ' \
                        'WITH node, score, n optional MATCH (n)-[:HAS_TAXONOMY]-(t:Taxonomy) ' \
-                           'RETURN DISTINCT n as node, score, t.id AS taxonomy_id,' \
+                       'RETURN DISTINCT n as node, score, t.id AS taxonomy_id,' \
                        ' t.name AS taxonomy_name, n.namespace as go_class ' \
                        'LIMIT $limit' % filter
 
