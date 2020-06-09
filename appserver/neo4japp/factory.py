dfed8eed736ff30fc1f2393d8443debef6e70cc3
David Bour <dbour.developer@gmail.com>
2020-06-09 13:41:10 -0700

    Temporarily Use Sentry as INFO Logger
    - feat: adds logging.INFO on features such as map addition, projects
      creation, and login to track usage. This should be transferred
      to a more robust solution such as the ELK stack.

--- appserver/neo4japp/factory.py
+++ appserver/neo4japp/factory.py
@@ -18,6 +18,7 @@ from neo4japp.exceptions import (
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
+from sentry_sdk.integrations.logging import ignore_logger
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 
@@ -40,7 +41,7 @@ def create_app(name='neo4japp', config='config.Development'):
     if config == 'config.Staging' or config == 'config.Production':
         sentry_logging = LoggingIntegration(
             level=logging.INFO,
-            event_level=logging.WARNING
+            event_level=logging.INFO,
         )
         sentry_sdk.init(
             dsn=os.environ.get('SENTRY_KEY'),
@@ -48,8 +49,10 @@ def create_app(name='neo4japp', config='config.Development'):
                 sentry_logging,
                 FlaskIntegration(),
                 SqlalchemyIntegration(),
-            ]
+            ],
+            send_default_pii=True,
         )
+        ignore_logger('werkzeug')
 
     app = Flask(name)
     app.config.from_object(config)
