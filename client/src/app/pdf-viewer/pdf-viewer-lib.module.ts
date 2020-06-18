8b6cc45430846d7014af75ac6218bc70528940c3
Anna Sirunian <ansir@biosustain.dtu.dk>
2020-06-18 13:50:57 +0200

    feat: add annotation exclusion panel

--- client/src/app/pdf-viewer/pdf-viewer-lib.module.ts
+++ client/src/app/pdf-viewer/pdf-viewer-lib.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PdfViewerLibComponent } from './pdf-viewer-lib.component';
 import { AnnotationPanelComponent } from './annotation-panel/annotation-panel.component';
+import { ExclusionPanelComponent } from './exclusion-panel/exclusion-panel.component';
 
 import { PdfViewerModule } from './pdf-viewer/pdf-viewer.module';
 
@@ -9,14 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
-  declarations: [PdfViewerLibComponent, AnnotationPanelComponent],
+  declarations: [PdfViewerLibComponent, AnnotationPanelComponent, ExclusionPanelComponent],
   imports: [
     PdfViewerModule,
     CommonModule,
     BrowserAnimationsModule,
     SharedModule
   ],
-  entryComponents: [AnnotationPanelComponent],
+  entryComponents: [AnnotationPanelComponent, ExclusionPanelComponent],
   exports: [PdfViewerLibComponent]
 })
 export class PdfViewerLibModule {
