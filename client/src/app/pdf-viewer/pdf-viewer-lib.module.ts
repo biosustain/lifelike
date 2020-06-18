a15fde9d6c9b353f16e2545c3214a2166b6e4f5d
Anna Sirunian <ansir@biosustain.dtu.dk>
2020-06-18 13:50:22 +0200

    refactor: use shared module

--- client/src/app/pdf-viewer/pdf-viewer-lib.module.ts
+++ client/src/app/pdf-viewer/pdf-viewer-lib.module.ts
@@ -5,31 +5,16 @@ import { AnnotationPanelComponent } from './annotation-panel/annotation-panel.co
 import { PdfViewerModule } from './pdf-viewer/pdf-viewer.module';
 
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatChipsModule, MatDialogModule, MatInputModule, MatSelectModule } from '@angular/material';
-import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
   declarations: [PdfViewerLibComponent, AnnotationPanelComponent],
   imports: [
     PdfViewerModule,
     CommonModule,
-    FormsModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatCheckboxModule,
-    MatSidenavModule,
-    MatDialogModule,
-    MatChipsModule,
-    MatSelectModule,
-    MatInputModule,
-    FlexLayoutModule,
-    MatButtonModule
+    SharedModule
   ],
   entryComponents: [AnnotationPanelComponent],
   exports: [PdfViewerLibComponent]
