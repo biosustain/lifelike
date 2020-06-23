6d9bd52085afedb7cbc54aa016d17b5a20ac9873
Anna Sirunian <ansir@biosustain.dtu.dk>
2020-06-23 12:27:44 +0200

    Revert "refactor: use shared module"
    This reverts commit 73cc0ca1f0950a193da9c2692639768e815ec616.

--- client/src/app/pdf-viewer/pdf-viewer-lib.module.ts
+++ client/src/app/pdf-viewer/pdf-viewer-lib.module.ts
@@ -6,16 +6,31 @@ import { ExclusionPanelComponent } from './exclusion-panel/exclusion-panel.compo
 import { PdfViewerModule } from './pdf-viewer/pdf-viewer.module';
 
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from 'app/shared/shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatChipsModule, MatDialogModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [PdfViewerLibComponent, AnnotationPanelComponent, ExclusionPanelComponent],
   imports: [
     PdfViewerModule,
     CommonModule,
+    FormsModule,
     BrowserAnimationsModule,
-    SharedModule
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatSelectModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatButtonModule
   ],
   entryComponents: [AnnotationPanelComponent, ExclusionPanelComponent],
   exports: [PdfViewerLibComponent]
