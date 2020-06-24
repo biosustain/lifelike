f86543f0bfd59417628f6d9f726de72b1604139e
Julio Melanda <juceme@biosustain.dtu.dk>
2020-06-24 15:54:02 +0200

    implementing missing endpoints

--- appserver/neo4japp/blueprints/entity_resources.py
+++ appserver/neo4japp/blueprints/entity_resources.py
@@ -6,22 +6,22 @@ from neo4japp.models import AnnotationStyle, DomainULRsMap
 bp = Blueprint('annotations', __name__, url_prefix='/annotations')
 
 
-@bp.route('/style', methods=['GET'])
+@bp.route('/style/<string:annotation>', methods=['GET'])
 @auth.login_required
 def get_style(annotation):
     style = AnnotationStyle.query.filter_by(label=annotation)
     return style.get_as_json()
 
 
-@bp.route('/styles', methods=['GET'])
+@bp.route('/style', methods=['GET'])
 @auth.login_required
-def get_all_styles(): #TODO
-    pass
+def get_all_styles():
+    return [x.get_as_json for x in AnnotationStyle.query.all()]
 
 
-@bp.route('/uri', methods=['GET'])
+@bp.route('/uri', methods=['POST'])
 @auth.login_required
-def get_uri(identifier, domain):
+def get_uri():
     uri = DomainULRsMap.query.filter_by(domain=domain)
     return {'uri': uri.base_URL + identifier}
 
