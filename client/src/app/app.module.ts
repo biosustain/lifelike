5b5e96da53ea267a0475d1f1ac0fba5d4b3a7227
Albert Pham <the.sk89q@gmail.com>
2020-06-26 16:02:14 -0700

    Move dashboard and statics components to root for now.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,11 @@ import { AdminModule } from 'app/admin/admin.module';
 import { AuthModule } from 'app/auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LifelikeHomeModule } from 'app/home/lifelike-home.module';
 import { UserFileImportModule } from './user-file-import/user-file-import.module';
 import { SearchModule } from './search/search.module';
 import { SharedModule } from './shared/shared.module';
 import { UserModule } from 'app/users/users.module';
-import { KgStatisticsComponent } from './kg-statistics/kg-statistics.component';
+import { KgStatisticsComponent } from './kg-statistics.component';
 import { ChartsModule } from 'ng2-charts';
 
 import { httpInterceptorProviders } from './shared/http-interceptors';
@@ -25,12 +24,14 @@ import { WorkspaceComponent } from './workspace.component';
 import { WorkspaceOutletComponent } from './workspace-outlet.component';
 import { WorkspaceManager } from './shared/workspace-manager';
 import { UnloadConfirmationGuard } from './shared/guards/UnloadConfirmation.guard';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorkspaceComponent,
     WorkspaceOutletComponent,
+    DashboardComponent,
     KgStatisticsComponent,
   ],
   imports: [
@@ -41,7 +42,6 @@ import { UnloadConfirmationGuard } from './shared/guards/UnloadConfirmation.guar
     AppRoutingModule,
     UserFileImportModule,
     FileBrowserModule,
-    LifelikeHomeModule,
     VisualizationModule,
     UserModule,
     NodeSearchModule,
