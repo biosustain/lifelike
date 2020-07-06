99d58a11f1d3c5c540dd03df442f5a6583920dae
Eren Yagdiran <erenyagdiran@gmail.com>
2020-07-06 15:47:08 +0200

    add prototype

---
+++ nlp/fetch-ai-models.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo gsutil cp -r gs://lifelike_ai_models/bacteria ./models/
+sudo gsutil cp -r gs://lifelike_ai_models/chem ./models/
+sudo gsutil cp -r gs://lifelike_ai_models/disease ./models/
+sudo gsutil cp -r gs://lifelike_ai_models/gene ./models/
+
