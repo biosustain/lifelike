08c2b4c719c3a2bb427abbd39245322696ef84d5
Mustafa Khan <makman09@gmail.com>
2020-07-06 13:10:50 -0700

    refactor dialogs to common-form-dialog in project-space

--- client/src/app/file-browser/components/create-project-dialog/create-project-dialog.component.ts
+++ client/src/app/file-browser/components/create-project-dialog/create-project-dialog.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import {
-  MatDialogRef,
-  MAT_DIALOG_DATA
-} from '@angular/material/dialog';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { CommonFormDialogComponent } from 'app/shared/components/dialog/common-form-dialog.component';
+import { MessageDialog } from 'app/shared/services/message-dialog.service';
 
 @Component({
   selector: 'app-create-project-dialog',
   templateUrl: './create-project-dialog.component.html',
   styleUrls: ['./create-project-dialog.component.scss']
 })
-export class CreateProjectDialogComponent implements OnInit, OnDestroy {
+export class CreateProjectDialogComponent extends CommonFormDialogComponent implements OnInit, OnDestroy {
   form: FormGroup = new FormGroup({
     projectName: new FormControl('', Validators.required),
     description: new FormControl('')
@@ -22,9 +20,9 @@ export class CreateProjectDialogComponent implements OnInit, OnDestroy {
 
   subscription: Subscription;
 
-  constructor(
-    public activeModal: NgbActiveModal
-  ) {}
+  constructor(modal: NgbActiveModal, messageDialog: MessageDialog) {
+    super(modal, messageDialog);
+  }
 
   ngOnInit() {
     this.subscription = this.form.valueChanges.subscribe(
@@ -36,15 +34,13 @@ export class CreateProjectDialogComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  doNothing() {
-    this.activeModal.dismiss();
+  getValue() {
+    return this.form.value;
   }
 
   submit() {
     if (this.form.valid) {
-      this.activeModal.close(
-        this.form.value
-      );
+      super.submit();
     } else {
       this.isInvalid = true;
     }
