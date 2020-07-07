5c3f95537bf29e792ccd05dd9ffc338d1c34ef87
Miguel Rueda <miguerueda86@yahoo.es>
2020-07-07 12:12:24 +0200

    feat: adding search bar component

--- client/src/app/pdf-search/pdf-search.module.ts
+++ client/src/app/pdf-search/pdf-search.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
-import { PdfSearchBarComponent } from './pdf-search-bar/pdf-search-bar.component';
-import { PdfSearchResultsComponent } from './pdf-search-results/pdf-search-results.component';
+import {NgModule} from '@angular/core';
+import {PdfSearchBarComponent} from './pdf-search-bar/pdf-search-bar.component';
+import {PdfSearchResultsComponent} from './pdf-search-results/pdf-search-results.component';
 import {SharedModule} from '../shared/shared.module';
-
+import {PdfSearchComponent} from './containers/pdf-search.component';
 
 
 @NgModule({
-  declarations: [PdfSearchBarComponent, PdfSearchResultsComponent],
+  declarations: [
+    PdfSearchBarComponent,
+    PdfSearchResultsComponent,
+    PdfSearchComponent
+  ],
   imports: [
     SharedModule
   ]
 })
-export class PdfSearchModule { }
+export class PdfSearchModule {
+}
--- client/src/app/search/services/search.service.ts
+++ client/src/app/search/services/search.service.ts
@@ -5,13 +5,16 @@ import {map} from 'rxjs/operators';
 
 import {
   FTSResult,
+  PDFResult
 } from 'app/interfaces';
+import {AuthenticationService} from '../../auth/services/authentication.service';
 
 @Injectable()
 export class SearchService {
   readonly searchApi = '/api/search';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private auth: AuthenticationService) {
   }
 
   fullTextSearch(query: string, page: number = 1, limit: number = 10) {
@@ -26,9 +29,23 @@ export class SearchService {
     ).pipe(map(resp => resp.result));
   }
 
+  pdfFullTextSearch(query: string, offset: number = 0, limit: number = 20) {
+    const options = {
+      headers: this.getAuthHeader(),
+    };
+    return this.http.post<{ result: PDFResult }>(
+      `${this.searchApi}/pdf-search`, {query, offset, limit}, options
+    ).pipe(map(resp => resp.result));
+  }
+
+
   visualizerSearchTemp(query: string, page: number = 1, limit: number = 10, filter: string = 'labels(node)') {
     return this.http.post<{ result: FTSResult }>(
       `${this.searchApi}/viz-search-temp`, {query, page, filter, limit},
     ).pipe(map(resp => resp.result));
+  }
+
+  private getAuthHeader() {
+    return {Authorization: `Bearer ${this.auth.getAccessToken()}`};
   }
 }
