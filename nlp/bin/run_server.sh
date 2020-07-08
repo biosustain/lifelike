38b02e651a2090591b04a850f7c48ea984fec61d
David Bour <dbour.developer@gmail.com>
2020-07-08 13:30:03 -0700

    - feat: add gunicorn and set the startup script to activate the   different environments depending on the flask environment.

--- nlp/bin/run_server.sh
+++ nlp/bin/run_server.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Running development server"
-flask run --host 0.0.0.0 -p 5001
+if [ "$FLASK_ENV" = "development" ]
+then
+    echo "Running development server"
+    flask run --host 0.0.0.0 -p 5001
+fi
+
+if [ "$FLASK_ENV" = "production" ]
+then
+    echo "Running production server"
+    gunicorn -b 0.0.0.0:5001 -w 4 app:app --timeout 1200
+fi
