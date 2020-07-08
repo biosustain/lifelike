84a8cc5c9cd265396633b787f9b68a597ec0182b
Julio Melanda <juceme@biosustain.dtu.dk>
2020-07-08 15:23:44 +0200

    Makes URI endpoints work

--- appserver/neo4japp/blueprints/entity_resources.py
+++ appserver/neo4japp/blueprints/entity_resources.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from neo4japp.blueprints.auth import auth
 from neo4japp.models import AnnotationStyle, DomainULRsMap
@@ -22,11 +22,19 @@ def get_all_styles():
 @bp.route('/uri', methods=['POST'])
 @auth.login_required
 def get_uri():
-    uri = DomainULRsMap.query.filter_by(domain=domain)
-    return {'uri': uri.base_URL + identifier}
+    payload = request.json
+
+    uri = DomainULRsMap.query.filter_by(domain=payload['domain'])[0]
+    return {'uri': uri.base_URL + payload['identifier']}
 
 
-@bp.route('/uri', methods=['GET'])
+@bp.route('/uri/batch', methods=['POST'])
 @auth.login_required
-def get_uri_batch(params): #TODO
-    pass
+def get_uri_batch():
+    uris = []
+    payload = request.json
+    for entry in payload['batch']:
+        uri = DomainULRsMap.query.filter_by(domain=entry['domain'])[0]
+        uris.append({'uri': uri.base_URL + entry['identifier']})
+
+    return {'batch': uris}
