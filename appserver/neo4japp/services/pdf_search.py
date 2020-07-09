92a45c7f1cea3419cae69ec84b443579c1766070
Miguel Rueda <miguerueda86@yahoo.es>
2020-07-09 13:01:34 +0200

    fix: linting issues

--- appserver/neo4japp/services/pdf_search.py
+++ appserver/neo4japp/services/pdf_search.py
@@ -42,7 +42,8 @@ class PDFSearchResult:
 
         for highlight in self.highlight[field]:
             untagged = highlight.replace(start_tag, '').replace(end_tag, '')
-            tagged = highlight.replace(start_tag, '<strong class="highlight">').replace(end_tag, '</strong>')
+            tagged = highlight.replace(start_tag, '<strong class="highlight">')\
+                .replace(end_tag, '</strong>')
             bkp_data = '<br>'.join((bkp_data, tagged)) if bkp_data else tagged
             data = data.replace(untagged, tagged)
 
