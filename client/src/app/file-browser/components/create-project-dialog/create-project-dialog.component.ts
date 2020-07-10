3ef5652dce29456684c2fb190d7df1d39a05365b
Mustafa Ahmed Khan III <makman09@gmail.com>
2020-07-10 08:38:41 -0700

    add error handler for create project

--- client/src/app/file-browser/components/create-project-dialog/create-project-dialog.component.ts
+++ client/src/app/file-browser/components/create-project-dialog/create-project-dialog.component.ts
@@ -4,6 +4,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { CommonFormDialogComponent } from 'app/shared/components/dialog/common-form-dialog.component';
 import { MessageDialog } from 'app/shared/services/message-dialog.service';
+import { ProjectSpaceService } from 'app/file-browser/services/project-space.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-project-dialog',
@@ -16,17 +18,23 @@ export class CreateProjectDialogComponent extends CommonFormDialogComponent impl
     description: new FormControl('')
   });
 
-  isInvalid = false;
-
   subscription: Subscription;
 
-  constructor(modal: NgbActiveModal, messageDialog: MessageDialog) {
+  errors = {
+    duplicateProjectName: 'A project with that name already exists'
+  };
+
+  constructor(
+    modal: NgbActiveModal,
+    messageDialog: MessageDialog,
+    private projSpace: ProjectSpaceService
+  ) {
     super(modal, messageDialog);
   }
 
   ngOnInit() {
     this.subscription = this.form.valueChanges.subscribe(
-      resp => this.isInvalid = false
+      () => {}
     );
   }
 
@@ -40,9 +48,15 @@ export class CreateProjectDialogComponent extends CommonFormDialogComponent impl
 
   submit() {
     if (this.form.valid) {
-      super.submit();
+      this.projSpace.createProject(this.getValue())
+        .subscribe(
+          newProject => this.modal.close(newProject),
+          (err: HttpErrorResponse) => {
+            this.form.get('projectName')
+              .setErrors({duplicateProjectName: true});
+          }
+        );
     } else {
-      this.isInvalid = true;
     }
   }
 }
