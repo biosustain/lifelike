bdfe3e373e337d0dea98d6184543a7327959f608
Miguel Rueda <miguerueda86@yahoo.es>
2020-07-15 13:52:47 +0200

    fix: linting issues

--- appserver/neo4japp/services/indexing/index_pdf.py
+++ appserver/neo4japp/services/indexing/index_pdf.py
@@ -42,7 +42,7 @@ def populate_index():
         .join(FileContent, FileContent.id == Files.content_id) \
         .all()
     for filename, description, file_id, doi, creation_date, \
-        uploaded_url, user_id, file in entries:
+            uploaded_url, user_id, file in entries:
         encoded_pdf = base64.b64encode(file)
         email = db.session.query(AppUser.email).filter(user_id == AppUser.id).one_or_none()
         data = encoded_pdf.decode('utf-8')
