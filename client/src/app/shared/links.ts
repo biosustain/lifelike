40a8273a466b4bfe784f85aeac54dcb684de6153
Albert Pham <the.sk89q@gmail.com>
2020-07-15 14:30:07 -0700

    Bootstrap-ify PDF viewer dialogs.

---
+++ client/src/app/shared/links.ts
@@ -0,0 +1,15 @@
+import { Hyperlink } from '../drawing-tool/services/interfaces';
+
+export const SEARCH_LINKS: readonly Hyperlink[] = Object.freeze([{
+  domain: 'NCBI',
+  url: 'https://www.ncbi.nlm.nih.gov/gene/?query=%s',
+}, {
+  domain: 'UniProt',
+  url: 'https://www.uniprot.org/uniprot/?sort=score&query=%s',
+}, {
+  domain: 'Wikipedia',
+  url: 'https://www.google.com/search?q=site:+wikipedia.org+%s',
+}, {
+  domain: 'Google',
+  url: 'https://www.google.com/search?q=%s',
+}].map(item => Object.freeze(item)));
