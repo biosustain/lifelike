b075b7d1f0cb781e57b7de44f41bad53c065b8ed
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-07-15 09:43:13 -0700

    Add kg import module + some components
    Creates a "KG Import" module, replacing the original import tool. We may
    integrate the generic import prototyped earlier, but for now focusing on
    entity specific import tools.
    
    This commit also introduces the first steps towards a gene specific
    import wizard.

---
+++ client/src/app/kg-import/components/gene-import-config/gene-import-config.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gene-import-config',
+  templateUrl: './gene-import-config.component.html',
+  styleUrls: ['./gene-import-config.component.scss']
+})
+export class GeneImportConfigComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
