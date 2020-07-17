56246780bdaf3d860c5c0bd5ef39ef8a5e552f55
Albert Pham <the.sk89q@gmail.com>
2020-07-17 16:54:05 -0700

    Initial overhaul of the project space.

--- appserver/neo4japp/blueprints/projects.py
+++ appserver/neo4japp/blueprints/projects.py
@@ -61,7 +61,6 @@ def get_projects():
 @bp.route('/', methods=['POST'])
 @auth.login_required
 def add_projects():
-
     data = request.get_json()
     user = g.current_user
 
@@ -128,7 +127,6 @@ def get_project_collaborators(project_name: str = ''):
 @auth.login_required
 @requires_project_role('project-admin')
 def add_collaborator(username: str, project_name: str = ''):
-
     proj_service = get_projects_service()
 
     data = request.get_json()
@@ -167,7 +165,6 @@ def add_collaborator(username: str, project_name: str = ''):
 @auth.login_required
 @requires_project_role('project-admin')
 def edit_collaborator(username: str, project_name: str):
-
     proj_service = get_projects_service()
 
     data = request.get_json()
@@ -202,7 +199,6 @@ def edit_collaborator(username: str, project_name: str):
 @auth.login_required
 @requires_project_role('project-admin')
 def remove_collaborator(username: str, project_name: str = ''):
-
     proj_service = get_projects_service()
 
     user = g.current_user
@@ -225,31 +221,6 @@ def remove_collaborator(username: str, project_name: str = ''):
     proj_service.remove_collaborator(new_collaborator, projects)
 
     yield jsonify(dict(result='success')), 200
-
-
-@bp.route('/<string:project_name>/directories', methods=['GET'])
-@auth.login_required
-@requires_project_permission(AccessActionType.READ)
-def get_top_level_directories(project_name: str = ''):
-    proj_service = get_projects_service()
-    projects = Projects.query.filter(
-        Projects.project_name == project_name
-    ).one_or_none()
-
-    if projects is None:
-        raise RecordNotFoundException(f'No such projects: {project_name}')
-
-    user = g.current_user
-
-    yield user, projects
-    root_dir = proj_service.get_root_dir(projects)
-    child_dirs = proj_service.get_immediate_child_dirs(projects, root_dir)
-    contents = DirectoryContent(
-        child_directories=[c.to_dict() for c in child_dirs],
-        files=[f.to_dict() for f in root_dir.files],
-        maps=[m.to_dict() for m in root_dir.project],
-    )
-    yield jsonify(dict(result=contents.to_dict()))
 
 
 @bp.route('/<string:project_name>/directories', methods=['POST'])
@@ -277,6 +248,7 @@ def add_directory(project_name: str = ''):
     yield jsonify(dict(results=new_dir.to_dict()))
 
 
+@bp.route('/<string:project_name>/directories', methods=['GET'], defaults={'current_dir_id': None})
 @bp.route('/<string:project_name>/directories/<int:current_dir_id>', methods=['GET'])
 @auth.login_required
 @requires_project_permission(AccessActionType.READ)
@@ -293,11 +265,40 @@ def get_child_directories(current_dir_id: int, project_name: str = ''):
     user = g.current_user
 
     yield user, projects
-    current_dir = Directory.query.get(current_dir_id)
-    child_dirs = proj_service.get_immediate_child_dirs(projects, current_dir)
+
+    if current_dir_id:
+        dir = Directory.query.get(current_dir_id)
+    else:
+        dir = proj_service.get_root_dir(projects)
+
+    parents = proj_service.get_absolute_dir_path(projects, dir)
+    child_dirs = proj_service.get_immediate_child_dirs(projects, dir)
+
     contents = DirectoryContent(
-        child_directories=[c.to_dict() for c in child_dirs],
-        files=[f.to_dict() for f in current_dir.files],
-        maps=[m.to_dict() for m in current_dir.project],
+        dir=dir.to_dict(),
+        path=[{
+            'id': d[0],
+            'name': d[1],
+            'directoryParentId': d[2],
+            'projectsId': d[3],  # TODO: get_absolute_dir_path() should return Directory[]
+        } for d in reversed(parents)],
+        objects=[
+            *[{
+                'type': 'dir',
+                'name': c.name,
+                'creator': None,
+                'data': c.to_dict(),
+            } for c in child_dirs],
+            *[{
+                'type': 'file',
+                'name': f.filename,
+                'data': f.to_dict(),
+            } for f in dir.files],
+            *[{
+                'type': 'map',
+                'name': m.label,
+                'data': m.to_dict(),
+            } for m in dir.project],
+        ],
     )
     yield jsonify(dict(result=contents.to_dict()))
--- client/src/app/file-browser/file-browser.module.ts
+++ client/src/app/file-browser/file-browser.module.ts
@@ -2,28 +2,28 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
 import { FileBrowserComponent } from './components/file-browser.component';
 import { FileEditDialogComponent } from './components/file-edit-dialog.component';
-import { FileDeleteDialogComponent } from './components/file-delete-dialog.component';
-import { FileUploadDialogComponent } from './components/file-upload-dialog.component';
+import { ObjectDeleteDialogComponent } from './components/object-delete-dialog.component';
+import { ObjectUploadDialogComponent } from './components/object-upload-dialog.component';
 import { FileViewComponent } from './components/file-view.component';
 import { PdfViewerLibModule } from '../pdf-viewer/pdf-viewer-lib.module';
 import { ProjectBrowserComponent } from './components/project-browser.component';
 import { ProjectTitleAcronymPipe } from './services/project-title-acronym.pipe';
 import { ProjectEditDialogComponent } from './components/project-edit-dialog.component';
 import { ProjectCreateDialogComponent } from './components/project-create-dialog.component';
-import { ContentAddDialogComponent } from './components/content-add-dialog.component';
+import { DirectoryCreateDialogComponent } from './components/directory-create-dialog.component';
 
 @NgModule({
   declarations: [
     FileEditDialogComponent,
-    FileDeleteDialogComponent,
-    FileUploadDialogComponent,
+    ObjectDeleteDialogComponent,
+    ObjectUploadDialogComponent,
     FileBrowserComponent,
     FileViewComponent,
     ProjectBrowserComponent,
     ProjectTitleAcronymPipe,
     ProjectEditDialogComponent,
     ProjectCreateDialogComponent,
-    ContentAddDialogComponent,
+    DirectoryCreateDialogComponent,
   ],
   imports: [
     SharedModule,
@@ -31,12 +31,12 @@ import { ContentAddDialogComponent } from './components/content-add-dialog.compo
   ],
   entryComponents: [
     FileEditDialogComponent,
-    FileDeleteDialogComponent,
-    FileUploadDialogComponent,
+    ObjectDeleteDialogComponent,
+    ObjectUploadDialogComponent,
     FileViewComponent,
     ProjectCreateDialogComponent,
     ProjectEditDialogComponent,
-    ContentAddDialogComponent,
+    DirectoryCreateDialogComponent,
   ],
   exports: [],
 })
