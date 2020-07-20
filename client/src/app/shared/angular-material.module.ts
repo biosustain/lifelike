23b27c2b274de4d4348bd975630d270d18ba39fc
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2020-07-20 03:56:33 +0200

    fix: improve material imports based on stackblitz examples

--- client/src/app/shared/angular-material.module.ts
+++ client/src/app/shared/angular-material.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
-
+import { A11yModule } from '@angular/cdk/a11y';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PortalModule } from '@angular/cdk/portal';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
@@ -8,6 +14,8 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -16,61 +24,31 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatNativeDateModule , MatRippleModule} from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSortModule } from '@angular/material/sort';
-import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ANIMATION_MODULE_TYPE } from '@angular/platform-browser/animations';
 
 
 @NgModule({
-    imports: [
-        MatAutocompleteModule,
-        MatBadgeModule,
-        MatBottomSheetModule,
-        MatButtonModule,
-        MatButtonToggleModule,
-        MatCardModule,
-        MatCheckboxModule,
-        MatChipsModule,
-        MatDialogModule,
-        MatDividerModule,
-        MatExpansionModule,
-        MatGridListModule,
-        MatIconModule,
-        MatInputModule,
-        MatListModule,
-        MatMenuModule,
-        MatPaginatorModule,
-        MatProgressBarModule,
-        MatProgressSpinnerModule,
-        MatRadioModule,
-        MatSelectModule,
-        MatSidenavModule,
-        MatSlideToggleModule,
-        MatSnackBarModule,
-        MatSortModule,
-        MatStepperModule,
-        MatTableModule,
-        MatTabsModule,
-        MatToolbarModule,
-        MatTooltipModule,
-        MatTreeModule,
-    ],
-    // exported modules are visible to modules that import this one
     exports: [
+        A11yModule,
+        CdkStepperModule,
+        CdkTableModule,
+        CdkTreeModule,
+        DragDropModule,
         MatAutocompleteModule,
         MatBadgeModule,
         MatBottomSheetModule,
@@ -79,6 +57,7 @@ import { ANIMATION_MODULE_TYPE } from '@angular/platform-browser/animations';
         MatCardModule,
         MatCheckboxModule,
         MatChipsModule,
+        MatDatepickerModule,
         MatDialogModule,
         MatDividerModule,
         MatExpansionModule,
@@ -87,14 +66,17 @@ import { ANIMATION_MODULE_TYPE } from '@angular/platform-browser/animations';
         MatInputModule,
         MatListModule,
         MatMenuModule,
+        MatNativeDateModule,
         MatPaginatorModule,
         MatProgressBarModule,
         MatProgressSpinnerModule,
         MatRadioModule,
+        MatRippleModule,
         MatSelectModule,
-        MatSnackBarModule,
         MatSidenavModule,
+        MatSliderModule,
         MatSlideToggleModule,
+        MatSnackBarModule,
         MatSortModule,
         MatStepperModule,
         MatTableModule,
@@ -102,7 +84,8 @@ import { ANIMATION_MODULE_TYPE } from '@angular/platform-browser/animations';
         MatToolbarModule,
         MatTooltipModule,
         MatTreeModule,
+        PortalModule,
+        ScrollingModule,
     ],
 })
-
 export class AngularMaterialModule {}
