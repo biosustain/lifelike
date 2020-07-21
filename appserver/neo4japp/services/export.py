67727f0ec4d24ba0cf1db8e3bf799243ee6a7068
Anna Sirunian <ansir@biosustain.dtu.dk>
2020-07-21 17:12:58 +0200

    refactor: use with statement while working with the writer

--- appserver/neo4japp/services/export.py
+++ appserver/neo4japp/services/export.py
@@ -10,19 +10,16 @@ class ExcelExportService:
 
     def get_bytes(self, data: List[Dict[str, str]]) -> bytes:
         output = BytesIO()
-        writer = ExcelWriter(output)
         dataframe = DataFrame(data)
-        dataframe.to_excel(writer, sheet_name='Sheet1', index=False)
+        with ExcelWriter(output, engine='xlsxwriter') as writer:
+            dataframe.to_excel(writer, sheet_name='Sheet1', index=False)
 
-        # adapt width of the columns
-        worksheet = writer.sheets['Sheet1']
-        for i, col in enumerate(dataframe.columns):
-            column_len = dataframe[col].astype(str).str.len().max()
-            column_len = max(column_len, len(col)) + 2
-            worksheet.set_column(i, i, column_len)
-
-        writer.save()
-        writer.close()
+            # adapt width of the columns
+            worksheet = writer.sheets['Sheet1']
+            for i, col in enumerate(dataframe.columns):
+                column_len = dataframe[col].astype(str).str.len().max()
+                column_len = max(column_len, len(col)) + 2
+                worksheet.set_column(i, i, column_len)
         return output.getvalue()
 
     def get_filename(self, filename: str) -> str:
