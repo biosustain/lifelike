9dc81b9dcd37cecb7ff2bc31a7e2d33cf3cd46b2
Binh Vu <binhvu.dev@gmail.com>
2020-07-27 08:41:14 -0700

    Changed docker-compose install command to get latest.

--- deployment/bin/kibana.sh
+++ deployment/bin/kibana.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# sudo apt-get update \
-#     && sudo apt-get install docker-compose -y
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o \
+    # /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
 cd /srv
 sudo mkdir lmdb
