6f3fb4069d4a110d81d154971672f7c3f8cebecf
Albert Pham <the.sk89q@gmail.com>
2020-07-30 15:14:20 -0700

    Use new windows for the info panel.

--- client/src/app/drawing-tool/components/map-editor/info-panel.component.ts
+++ client/src/app/drawing-tool/components/map-editor/info-panel.component.ts
@@ -12,7 +12,6 @@ import { MessageType } from '../../../interfaces/message-dialog.interface';
 @Component({
   selector: 'app-info-panel',
   templateUrl: './info-panel.component.html',
-  styleUrls: ['./info-panel.component.scss'],
 })
 export class InfoPanelComponent {
   @Input() selected: GraphEntity | undefined;
