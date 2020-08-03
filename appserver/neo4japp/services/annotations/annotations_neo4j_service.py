429eac39cc7406495527de7570727600acb8f797
Binh Vu <binhvu.dev@gmail.com>
2020-08-03 13:38:03 -0700

    LL-1326: For gene matching in lmdb, be more strict and check if there is an exact case match.

--- appserver/neo4japp/services/annotations/annotations_neo4j_service.py
+++ appserver/neo4japp/services/annotations/annotations_neo4j_service.py
@@ -71,5 +71,5 @@ class AnnotationsNeo4jService(RDBMSBaseDao):
 
         return postgres_result
 
-    def get_organisms_from_synonyms(self, synonyms: List[str]) -> List[str]:
-        return self.neo4j.get_organisms_from_synonyms(synonyms)
+    def get_organisms_from_with_ids(self, tax_ids: List[str]) -> List[str]:
+        return self.neo4j.get_organisms_from_with_ids(tax_ids)
