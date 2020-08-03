0ffeee284cb586e0f6b8e84c3acc9205f0e3929e
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-08-03 15:07:50 -0700

    Fix snippet terms sometimes not being highlighted
    There's currently an issue with some of the snippet data where
    multi-word terms are joined by an underscore in the sentence, but not in
    the entry text. This led to a bug in the snippet view where some of the
    terms would not be highlighted.

--- client/src/app/shared/components/highlight-snippet.component.ts
+++ client/src/app/shared/components/highlight-snippet.component.ts
@@ -26,7 +26,6 @@ export class HighlightSnippetComponent implements OnChanges {
 
     ngOnChanges() {
         const entry1BackgroundColor = this.legend.get(this.entry1Type)[0];
-
         const entry1StyleString = `
             background-color: ${hexToRGBA(entry1BackgroundColor, 0.3)};
             display: inline-block;
@@ -34,6 +33,7 @@ export class HighlightSnippetComponent implements OnChanges {
             text-align: center;
             vertical-align: middle;
         `;
+        const entry1TextJoinedByUnderscore = this.entry1Text.split(' ').join('_');
 
         const entry2BackgroundColor = this.legend.get(this.entry2Type)[0];
         const entry2StyleString = `
@@ -43,14 +43,21 @@ export class HighlightSnippetComponent implements OnChanges {
             text-align: center;
             vertical-align: middle;
         `;
+        const entry2TextJoinedByUnderscore = this.entry2Text.split(' ').join('_');
 
         const styleMap = {};
         styleMap[this.entry1Text] = `<div style="${entry1StyleString}">${this.entry1Text}</div>`;
         styleMap[this.entry2Text] = `<div style="${entry2StyleString}">${this.entry2Text}</div>`;
+        styleMap[entry1TextJoinedByUnderscore] = `<div style="${entry1StyleString}">${this.entry1Text}</div>`;
+        styleMap[entry2TextJoinedByUnderscore] = `<div style="${entry2StyleString}">${this.entry2Text}</div>`;
 
-        this.highlightedSnippet = this.snippet.replace(new RegExp(`\\b${this.entry1Text}|${this.entry2Text}\\b`, 'g'), match => {
-            return styleMap[match];
-        });
+
+        this.highlightedSnippet = this.snippet.replace(
+            new RegExp(`\\b${this.entry1Text}|${this.entry2Text}|${entry1TextJoinedByUnderscore}|${entry2TextJoinedByUnderscore}\\b`, 'g'),
+            match => {
+                return styleMap[match];
+            }
+        );
 
         // We have to be VERY careful using this! It could expose our site to XSS attacks if we aren't cautious.
         this.trustedHTML = this.domSanitizer.bypassSecurityTrustHtml(this.highlightedSnippet);
