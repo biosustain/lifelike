cedc2b9ac00eb5fe7e09ab9a8fb08c158733f381
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-08-03 13:34:32 -0700

    Fix component specs

--- client/src/app/kg-import/containers/gene-import-wizard/gene-import-wizard.component.spec.ts
+++ client/src/app/kg-import/containers/gene-import-wizard/gene-import-wizard.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { configureTestSuite } from 'ng-bullet';
 
@@ -29,7 +30,8 @@ describe('GeneImportWizardComponent', () => {
             imports: [
                 BrowserAnimationsModule,
                 RootStoreModule,
-                SharedModule
+                SharedModule,
+                RouterTestingModule,
             ]
         });
     });
