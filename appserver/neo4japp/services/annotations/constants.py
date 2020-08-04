0433a8a7a94399403394e4214feaac53d95c3f39
Binh Vu <binhvu.dev@gmail.com>
2020-08-04 15:02:23 -0700

    Added the file of annotation stop words to database and query from there.

--- appserver/neo4japp/services/annotations/constants.py
+++ appserver/neo4japp/services/annotations/constants.py
@@ -19,40 +19,40 @@ NLP_ENDPOINT = 'http://nlpapi:5001/infer/v1'
 LOWERCASE_FIRST_LETTER_UPPERCASE_LAST_LETTER_GENE_LENGTH = 4
 PDF_NEW_LINE_THRESHOLD = .30
 PDF_CHARACTER_SPACING_THRESHOLD = .325
-COMMON_TWO_LETTER_WORDS = {
-    'of', 'to', 'in', 'it', 'is', 'be', 'as', 'at',
-    'so', 'we', 'he', 'by', 'or', 'on', 'do', 'if',
-    'me', 'my', 'up', 'an', 'go', 'no', 'us', 'am',
-    'et', 'vs',
-}
-COMMON_THREE_LETTER_WORDS = {
-    'the', 'and', 'for', 'are', 'but', 'not', 'you', 'all',
-    'any', 'can', 'had', 'her', 'was', 'one', 'our', 'out',
-    'day', 'get', 'has', 'him', 'his', 'how', 'man', 'new',
-    'now', 'old', 'see', 'two', 'way', 'who', 'boy', 'did',
-    'its', 'let', 'put', 'say', 'she', 'too', 'use', 'end',
-    'min', 'far', 'set', 'key', 'tag', 'pdf', 'raw', 'low',
-    'med', 'men', 'led', 'add',
-}
-COMMON_FOUR_LETTER_WORDS = {
-    'that', 'with', 'have', 'this', 'will', 'your', 'from',
-    'name', 'they', 'know', 'want', 'been', 'good', 'much',
-    'some', 'time', 'none', 'link', 'bond', 'acid', 'role',
-    'them', 'even',
-}
+# COMMON_TWO_LETTER_WORDS = {
+#     'of', 'to', 'in', 'it', 'is', 'be', 'as', 'at',
+#     'so', 'we', 'he', 'by', 'or', 'on', 'do', 'if',
+#     'me', 'my', 'up', 'an', 'go', 'no', 'us', 'am',
+#     'et', 'vs',
+# }
+# COMMON_THREE_LETTER_WORDS = {
+#     'the', 'and', 'for', 'are', 'but', 'not', 'you', 'all',
+#     'any', 'can', 'had', 'her', 'was', 'one', 'our', 'out',
+#     'day', 'get', 'has', 'him', 'his', 'how', 'man', 'new',
+#     'now', 'old', 'see', 'two', 'way', 'who', 'boy', 'did',
+#     'its', 'let', 'put', 'say', 'she', 'too', 'use', 'end',
+#     'min', 'far', 'set', 'key', 'tag', 'pdf', 'raw', 'low',
+#     'med', 'men', 'led', 'add',
+# }
+# COMMON_FOUR_LETTER_WORDS = {
+#     'that', 'with', 'have', 'this', 'will', 'your', 'from',
+#     'name', 'they', 'know', 'want', 'been', 'good', 'much',
+#     'some', 'time', 'none', 'link', 'bond', 'acid', 'role',
+#     'them', 'even',
+# }
 
-COMMON_MISC_WORDS = {
-    'patch', 'membrane', 'walker', 'group', 'cluster',
-    'protein', 'transporter', 'toxin', 'molecule', 'vitamin',
-    'light', 'mixture', 'solution', 'other', 'unknown', 'damage',
-}
+# COMMON_MISC_WORDS = {
+#     'patch', 'membrane', 'walker', 'group', 'cluster',
+#     'protein', 'transporter', 'toxin', 'molecule', 'vitamin',
+#     'light', 'mixture', 'solution', 'other', 'unknown', 'damage',
+# }
 
-COMMON_WORDS = set.union(*[
-    COMMON_TWO_LETTER_WORDS,
-    COMMON_THREE_LETTER_WORDS,
-    COMMON_FOUR_LETTER_WORDS,
-    COMMON_MISC_WORDS,
-])
+# COMMON_WORDS = set.union(*[
+#     COMMON_TWO_LETTER_WORDS,
+#     COMMON_THREE_LETTER_WORDS,
+#     COMMON_FOUR_LETTER_WORDS,
+#     COMMON_MISC_WORDS,
+# ])
 
 CHEMICAL_EXCLUSION = {'aa', 'same'}
 COMPOUND_EXCLUSION = {'aa', 'same'}  # should this be the same as chemical?
