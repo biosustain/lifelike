acd769d105b9eab11209fa186511886194cb4962
Miguel Rueda <miguerueda86@yahoo.es>
2020-08-04 12:06:07 +0200

    feat: adding backend changes to support snippet search

--- appserver/neo4japp/services/pdf_search.py
+++ appserver/neo4japp/services/pdf_search.py
@@ -15,15 +15,22 @@ class PDFSearchResult:
         self.uploaded_date = ''
         self.highlight = data.get('highlight', {})
         self.preview_text_size = 400
+        self.external_url = ''
+        self.email = ''
+        self.description = ''
         self.parse_pdf_entries(data)
 
     def parse_pdf_entries(self, data):
         source = data['_source']
         self.file_id = source['internal_link']
-        self.doi = source['doi'] if source['doi'] is not None else 'N/A'
+        self.doi = source['doi']
         self.preview_text = source['data']['content']
         self.preview_text = self.parse_highlight('data.content', self.preview_text)
         self.uploaded_date = source['uploaded_date']
+        self.external_url = source['external_link']
+        self.email = source['email']
+        self.description = source['description']
+        self.filename = source['filename']
 
     def parse_highlight(self, field, data):
         start_tag = '<highlight>'
@@ -35,21 +42,20 @@ class PDFSearchResult:
 
         for highlight in self.highlight[field]:
             untagged = highlight.replace(start_tag, '').replace(end_tag, '')
-            tagged = highlight.replace(start_tag, '<strong>').replace(end_tag, '</strong>')
-            if bkp_data:
-                bkp_data = '<br>'.join((bkp_data, tagged))
-            else:
-                bkp_data = tagged
+            tagged = highlight.replace(
+                start_tag,
+                '<strong class="highlight">'
+            ).replace(end_tag, '</strong>')
+            bkp_data = '<br>'.join((bkp_data, tagged)) if bkp_data else tagged
             data = data.replace(untagged, tagged)
 
         if not data:
             data = bkp_data
 
         first_highlight = data.find('<strong')
-        if first_highlight < len(data) - self.preview_text_size:
-            data = '...' + data[first_highlight - 20:self.preview_text_size]
-        else:
-            data = '...' + data[-self.preview_text_size:]
+        data = '...' + data[first_highlight - 20:first_highlight + self.preview_text_size] \
+            if first_highlight < len(data) - self.preview_text_size \
+            else '...' + data[-self.preview_text_size:]
         return data
 
     def to_json(self):
@@ -58,7 +64,10 @@ class PDFSearchResult:
             'file_id': self.file_id,
             'doi': self.doi,
             'preview_text': self.preview_text,
-            'uploaded_date': self.uploaded_date
+            'uploaded_date': self.uploaded_date,
+            'external_url': self.external_url,
+            'email': self.email,
+            'description': self.description
         }
 
     def __str__(self):
