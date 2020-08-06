3a1ca4832b276a0fe648a9201d6a7a49c0c337a9
andrewhuangwp <andrewhuangwp@gmail.com>
2020-08-06 15:01:55 -0700

    Add project version model, project version get and add endpoint

--- appserver/neo4japp/models/drawing_tool.py
+++ appserver/neo4japp/models/drawing_tool.py
@@ -25,6 +25,7 @@ class Project(RDBMSBase):
     dir_id = db.Column(db.Integer, db.ForeignKey('directory.id'), nullable=False)
     hash_id = db.Column(db.String(50), unique=True)
     search_vector = db.Column(TSVectorType('label'))
+    creation_date = db.Column(db.DateTime)
 
     def set_hash_id(self):
         """ Assign hash based on project id with salt
@@ -35,6 +36,36 @@ class Project(RDBMSBase):
             "{} {}".format(self.id, salt).encode()
         )
         self.hash_id = h.hexdigest()
+
+
+class ProjectVersion(RDBMSBase):
+    """ Model representation of a version of a project drawing in a
+        network graph networking tool
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.Text)
+    date_modified = db.Column(db.DateTime)
+    graph = db.Column(db.JSON)
+    author = db.Column(db.String(240), nullable=False)
+    public = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('appuser.id'), nullable=False)
+    dir_id = db.Column(db.Integer, db.ForeignKey('directory.id'), nullable=False)
+    hash_id = db.Column(db.String(50), unique=True)
+    search_vector = db.Column(TSVectorType('label'))
+    creation_date = db.Column(db.DateTime)
+    version_name = db.Column(db.String(250))
+    version_hash_id = db.Column(db.String(50), unique=True)
+
+    def set_version_hash_id(self):
+        """ Assign hash based on project id with salt
+        """
+        salt = "i am man"
+
+        h = hashlib.md5(
+            "{} {}".format(self.id, salt).encode()
+        )
+        self.version_hash_id = h.hexdigest()
 
 
 class ProjectSchema(ma.ModelSchema):  # type: ignore
