3a641a57e33fb74f2720b485193e2be68e2f6cff
Binh Vu <binhvu.dev@gmail.com>
2020-08-07 12:54:37 -0700

    Reduced word limit in schema for annotation_stop_words.

--- appserver/neo4japp/models/annotations.py
+++ appserver/neo4japp/models/annotations.py
@@ -4,4 +4,4 @@ from neo4japp.models.common import RDBMSBase
 
 class AnnotationStopWords(RDBMSBase):
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    word = db.Column(db.String(200), nullable=False)
+    word = db.Column(db.String(80), nullable=False)
