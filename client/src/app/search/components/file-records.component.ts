7d18a65eaefa6751dec20bf9b33a43eca9eec426
Miguel Rueda <miguerueda86@yahoo.es>
2020-08-12 15:01:20 +0200

    feat: initial work under snnipet card

--- client/src/app/search/components/file-records.component.ts
+++ client/src/app/search/components/file-records.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {PDFResult} from "../../interfaces";
 
 @Component({
   selector: 'app-file-records',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./file-records.component.scss']
 })
 export class FileRecordsComponent implements OnInit {
+
+  @Input() results: PDFResult;
 
   constructor() {
   }
