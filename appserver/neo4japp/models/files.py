4d095fe303fbb79a3a98475155fbdd6aa19a427c
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-08-12 11:38:05 -0700

    Make usage of db.Integer consistent across backend

--- appserver/neo4japp/models/files.py
+++ appserver/neo4japp/models/files.py
@@ -10,7 +10,7 @@ from neo4japp.models.common import RDBMSBase
 
 class FileContent(RDBMSBase):
     __tablename__ = 'files_content'
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     raw_file = db.Column(db.LargeBinary, nullable=False)
     checksum_sha256 = db.Column(db.Binary(32), nullable=False, index=True, unique=True)
     creation_date = db.Column(db.DateTime, nullable=False, default=db.func.now())
@@ -18,7 +18,7 @@ class FileContent(RDBMSBase):
 
 class Files(RDBMSBase):  # type: ignore
     __tablename__ = 'files'
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     file_id = db.Column(db.String(36), unique=True, nullable=False)
     filename = db.Column(db.String(200), nullable=False)
     description = db.Column(db.String(2048), nullable=True)
@@ -29,7 +29,7 @@ class Files(RDBMSBase):  # type: ignore
     creation_date = db.Column(db.DateTime, default=db.func.now())
     annotations = db.Column(postgresql.JSONB, nullable=True, server_default='[]')
     annotations_date = db.Column(TIMESTAMP(timezone=True), nullable=True)
-    project = db.Column(db.Integer(), db.ForeignKey('projects.id'), nullable=False)
+    project = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable=False)
     custom_annotations = db.Column(postgresql.JSONB, nullable=True, server_default='[]')
     dir_id = db.Column(db.Integer, db.ForeignKey('directory.id'), nullable=False)
     doi = db.Column(db.String(1024), nullable=True)
@@ -88,7 +88,7 @@ class Directory(RDBMSBase):
 # we also don't currently have a home in the UI for managing these worksheets.
 class Worksheet(RDBMSBase):  # type: ignore
     __tablename__ = 'worksheets'
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     filename = db.Column(db.String(200), nullable=False)
     sheetname = db.Column(db.String(200), nullable=False)
     neo4j_node_id = db.Column(db.Integer, nullable=False)
