db2f0459ec4d448941c8550811541c5988467fbe
Miguel Rueda <miguerueda86@yahoo.es>
2020-08-12 17:08:24 +0200

    feat: finalize first phase of the component

--- client/src/app/search/components/file-records.component.ts
+++ client/src/app/search/components/file-records.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {PDFResult} from "../../interfaces";
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {PDFResult} from '../../interfaces';
 
 @Component({
   selector: 'app-file-records',
   templateUrl: './file-records.component.html',
-  styleUrls: ['./file-records.component.scss']
+  styleUrls: ['./file-records.component.scss'],
+  styles: ['.highlight {border: 2px solid red}'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FileRecordsComponent implements OnInit {
 
