6fe3489eece1fba864b66bef78ef3b82fe5bd74d
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-08-25 16:31:58 -0700

    Fix multi-directional clusters not showing bug
    Needed to take into account the direction of new clusters when creating
    them, previously we were just looking at the relationship description
    when deciding whether to replace.

--- client/src/app/visualization/components/visualization-canvas/visualization-canvas.component.ts
+++ client/src/app/visualization/components/visualization-canvas/visualization-canvas.component.ts
@@ -859,20 +859,21 @@ export class VisualizationCanvasComponent implements OnInit, AfterViewInit {
         } as ReferenceTableDataRequest;
 
         this.visService.getReferenceTableData(referenceTableDataRequest).subscribe(result => {
-            // Remove any existing clusters connected to the origin node on this relationship first. Any
+            const {referenceTableRows, direction} = result;
+            const url = this.createClusterSvg(referenceTableRows);
+
+            // Remove any existing clusters connected to the origin node on this relationship/direction first. Any
             // nodes within should have been included in the duplicateNodeEdgePairs array sent to the appserver.
             this.networkGraph.getConnectedNodes(originNode).forEach(nodeId => {
                 if (this.networkGraph.isCluster(nodeId)) {
-                    if (this.clusters.get(nodeId).relationship === relationship) {
+                    const cluster = this.clusters.get(nodeId);
+                    if (cluster.relationship === relationship && cluster.direction === direction) {
                         this.destroyCluster(nodeId);
                     }
                 }
             });
 
             this.updateGraphWithDuplicates(duplicateNodeEdgePairs);
-
-            const referenceTableRows = result.referenceTableRows;
-            const url = this.createClusterSvg(referenceTableRows);
 
             // TODO: Would be nice to have some indication that the cluster has been selected.
             // A bit tricky, since clusters are SVGs, but maybe this can be done.
@@ -896,7 +897,7 @@ export class VisualizationCanvasComponent implements OnInit, AfterViewInit {
                 },
                 processProperties: (clusterOptions) => {
                     const newClusterId = `cluster:${uuidv4()}`;
-                    this.clusters.set(newClusterId, {referenceTableRows, relationship});
+                    this.clusters.set(newClusterId, {referenceTableRows, relationship, direction});
                     return {...clusterOptions, id: newClusterId};
                 }
             });
