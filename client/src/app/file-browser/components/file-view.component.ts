833de6215c5757f63b983dbf949a17265f87a1b0
Anna Sirunian <ansir@biosustain.dtu.dk>
2020-08-27 15:54:15 +0200

    refactor: add enums for annotation and database types

--- client/src/app/file-browser/components/file-view.component.ts
+++ client/src/app/file-browser/components/file-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, OnDestroy, Output, ViewChild } from '@angular/core';
 import { combineLatest, Subject, Subscription, BehaviorSubject } from 'rxjs';
 import { PdfFilesService } from 'app/shared/services/pdf-files.service';
-import { Hyperlink, SearchLink } from 'app/shared/constants';
+import { Hyperlink, DatabaseType, AnnotationType } from 'app/shared/constants';
 
 import { PdfAnnotationsService } from '../../drawing-tool/services';
 
@@ -458,17 +458,17 @@ export class FileViewComponent implements OnDestroy, ModuleAwareComponent {
 
   generateHyperlink(ann: Annotation): string {
     switch (ann.meta.idType) {
-      case 'CHEBI':
+      case DatabaseType.Chebi:
         return this.buildUrl(Hyperlink.Chebi, ann.meta.id);
-      case 'MESH':
+      case DatabaseType.Mesh:
         // prefix 'MESH:' should be removed from the id in order for search to work
         return this.buildUrl(Hyperlink.Mesh, ann.meta.id.substring(5));
-      case 'UNIPROT':
+      case DatabaseType.Uniprot:
         return this.buildUrl(Hyperlink.Uniprot, ann.meta.id);
-      case 'NCBI':
-        if (ann.meta.type === 'Gene') {
+      case DatabaseType.Ncbi:
+        if (ann.meta.type === AnnotationType.Gene) {
           return this.buildUrl(Hyperlink.NcbiGenes, ann.meta.id);
-        } else if (ann.meta.type === 'Species') {
+        } else if (ann.meta.type === AnnotationType.Species) {
           return this.buildUrl(Hyperlink.NcbiSpecies, ann.meta.id);
         }
         return '';
@@ -477,7 +477,7 @@ export class FileViewComponent implements OnDestroy, ModuleAwareComponent {
     }
   }
 
-  private buildUrl(provider: Hyperlink | SearchLink, query: string): string {
+  private buildUrl(provider: Hyperlink, query: string): string {
     return provider + query;
   }
 
