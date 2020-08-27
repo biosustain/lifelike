7968c3a9ee83d4bccb00c5f5731e10a63850c2f8
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-08-27 16:08:58 -0700

    Add triggers for modified_date columns

--- appserver/migrations/versions/bd32fb61927a_add_creation_modified_date_columns_to_.py
+++ appserver/migrations/versions/bd32fb61927a_add_creation_modified_date_columns_to_.py
@@ -103,22 +103,68 @@ def data_upgrades():
     session = Session(op.get_bind())
 
     # appuser -> creation/modified
+    appuser_update_modified_date_proc_query = """
+        CREATE OR REPLACE FUNCTION appuser_update_modified_date() RETURNS trigger AS $appuser_update_modified_date$
+        BEGIN
+            NEW.modified_date := current_timestamp;
+            RETURN NEW;
+        END;
+        $appuser_update_modified_date$ LANGUAGE plpgsql;
+    """
+    session.execute(appuser_update_modified_date_proc_query)
+
+    appuser_update_modified_date_trigger_query = """
+        CREATE TRIGGER appuser_modified_date_stamp BEFORE INSERT OR UPDATE ON appuser
+            FOR EACH ROW EXECUTE PROCEDURE appuser_update_modified_date();
+    """
+    session.execute(appuser_update_modified_date_trigger_query)
+
     session.execute(
         t_appuser.update().values(
             creation_date=db.func.now(),
-            modified_date=db.func.now()
         )
     )
 
-    # directpry -> creation/modified
+    # directory -> creation/modified
+    directory_update_modified_date_proc_query = """
+        CREATE OR REPLACE FUNCTION directory_update_modified_date() RETURNS trigger AS $directory_update_modified_date$
+        BEGIN
+            NEW.modified_date := current_timestamp;
+            RETURN NEW;
+        END;
+        $directory_update_modified_date$ LANGUAGE plpgsql;
+    """
+    session.execute(directory_update_modified_date_proc_query)
+
+    directory_update_modified_date_trigger_query = """
+        CREATE TRIGGER directory_modified_date_stamp BEFORE INSERT OR UPDATE ON directory
+            FOR EACH ROW EXECUTE PROCEDURE directory_update_modified_date();
+    """
+    session.execute(directory_update_modified_date_trigger_query)
+
     session.execute(
         t_directory.update().values(
             creation_date=db.func.now(),
-            modified_date=db.func.now()
         )
     )
 
     # files -> modified
+    files_update_modified_date_proc_query = """
+        CREATE OR REPLACE FUNCTION files_update_modified_date() RETURNS trigger AS $files_update_modified_date$
+        BEGIN
+            NEW.modified_date := current_timestamp;
+            RETURN NEW;
+        END;
+        $files_update_modified_date$ LANGUAGE plpgsql;
+    """
+    session.execute(files_update_modified_date_proc_query)
+
+    files_update_modified_date_trigger_query = """
+        CREATE TRIGGER files_modified_date_stamp BEFORE INSERT OR UPDATE ON files
+            FOR EACH ROW EXECUTE PROCEDURE files_update_modified_date();
+    """
+    session.execute(files_update_modified_date_trigger_query)
+
     session.execute(
         t_files.update().values(
             modified_date=db.func.now()
@@ -126,30 +172,91 @@ def data_upgrades():
     )
 
     # global_list -> creation/modified
+    global_list_update_modified_date_proc_query = """
+        CREATE OR REPLACE FUNCTION global_list_update_modified_date() RETURNS trigger AS $global_list_update_modified_date$
+        BEGIN
+            NEW.modified_date := current_timestamp;
+            RETURN NEW;
+        END;
+        $global_list_update_modified_date$ LANGUAGE plpgsql;
+    """
+    session.execute(global_list_update_modified_date_proc_query)
+
+    global_list_update_modified_date_trigger_query = """
+        CREATE TRIGGER global_list_modified_date_stamp BEFORE INSERT OR UPDATE ON global_list
+            FOR EACH ROW EXECUTE PROCEDURE global_list_update_modified_date();
+    """
+    session.execute(global_list_update_modified_date_trigger_query)
+
     session.execute(
         t_global_list.update().values(
             creation_date=db.func.now(),
-            modified_date=db.func.now()
         )
     )
 
     # project -> creation
+    project_update_date_modified_proc_query = """
+        CREATE OR REPLACE FUNCTION project_update_date_modified() RETURNS trigger AS $project_update_date_modified$
+        BEGIN
+            NEW.date_modified := current_timestamp;
+            RETURN NEW;
+        END;
+        $project_update_date_modified$ LANGUAGE plpgsql;
+    """
+    session.execute(project_update_date_modified_proc_query)
+
+    project_update_date_modified_trigger_query = """
+        CREATE TRIGGER project_date_modified_stamp BEFORE INSERT OR UPDATE ON project
+            FOR EACH ROW EXECUTE PROCEDURE project_update_date_modified();
+    """
+    session.execute(project_update_date_modified_trigger_query)
+
     session.execute(
         t_project.update().values(
             creation_date=db.func.now(),
-            date_modified=db.func.now()
         )
     )
 
     # project_backup -> creation
+    project_backup_update_date_modified_proc_query = """
+        CREATE OR REPLACE FUNCTION project_backup_update_date_modified() RETURNS trigger AS $project_backup_update_date_modified$
+        BEGIN
+            NEW.date_modified := current_timestamp;
+            RETURN NEW;
+        END;
+        $project_backup_update_date_modified$ LANGUAGE plpgsql;
+    """
+    session.execute(project_backup_update_date_modified_proc_query)
+
+    project_backup_update_date_modified_trigger_query = """
+        CREATE TRIGGER project_backup_date_modified_stamp BEFORE INSERT OR UPDATE ON project_backup
+            FOR EACH ROW EXECUTE PROCEDURE project_backup_update_date_modified();
+    """
+    session.execute(project_backup_update_date_modified_trigger_query)
+
     session.execute(
         t_project_backup.update().values(
             creation_date=db.func.now(),
-            date_modified=db.func.now()
         )
     )
 
     # projects -> modified
+    projects_update_modified_date_proc_query = """
+        CREATE OR REPLACE FUNCTION projects_update_modified_date() RETURNS trigger AS $projects_update_modified_date$
+        BEGIN
+            NEW.modified_date := current_timestamp;
+            RETURN NEW;
+        END;
+        $projects_update_modified_date$ LANGUAGE plpgsql;
+    """
+    session.execute(projects_update_modified_date_proc_query)
+
+    projects_update_modified_date_trigger_query = """
+        CREATE TRIGGER projects_modified_date_stamp BEFORE INSERT OR UPDATE ON projects
+            FOR EACH ROW EXECUTE PROCEDURE projects_update_modified_date();
+    """
+    session.execute(projects_update_modified_date_trigger_query)
+
     session.execute(
         t_projects.update().values(
             modified_date=db.func.now()
@@ -157,6 +264,22 @@ def data_upgrades():
     )
 
     # worksheets -> modified
+    worksheets_update_modified_date_proc_query = """
+        CREATE OR REPLACE FUNCTION worksheets_update_modified_date() RETURNS trigger AS $worksheets_update_modified_date$
+        BEGIN
+            NEW.modified_date := current_timestamp;
+            RETURN NEW;
+        END;
+        $worksheets_update_modified_date$ LANGUAGE plpgsql;
+    """
+    session.execute(worksheets_update_modified_date_proc_query)
+
+    worksheets_update_modified_date_trigger_query = """
+        CREATE TRIGGER worksheets_modified_date_stamp BEFORE INSERT OR UPDATE ON worksheets
+            FOR EACH ROW EXECUTE PROCEDURE worksheets_update_modified_date();
+    """
+    session.execute(worksheets_update_modified_date_trigger_query)
+
     session.execute(
         t_worksheets.update().values(
             modified_date=db.func.now()
