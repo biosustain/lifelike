1140f8645c33db4398e0fba7fdfe601a26a6cd24
Albert Pham <the.sk89q@gmail.com>
2020-08-27 16:51:05 -0700

    Add support for multiple selection on the map editor. Fixes LL-1562

--- client/src/app/graph-viewer/renderers/canvas/behaviors/selectable-entity.ts
+++ client/src/app/graph-viewer/renderers/canvas/behaviors/selectable-entity.ts
@@ -2,7 +2,8 @@ import * as d3 from 'd3';
 
 import { CanvasGraphView } from '../canvas-graph-view';
 import { AbstractCanvasBehavior, BehaviorResult } from '../../behaviors';
-import { GraphEntity } from '../../../../drawing-tool/services/interfaces';
+import { GraphEntity, UniversalGraphNode } from '../../../../drawing-tool/services/interfaces';
+import { isCtrlOrMetaPressed, isShiftPressed } from '../../../../shared/utils';
 
 export class SelectableEntity extends AbstractCanvasBehavior {
   constructor(private readonly graphView: CanvasGraphView) {
@@ -20,8 +21,28 @@ export class SelectableEntity extends AbstractCanvasBehavior {
 
   dragEnd(event: MouseEvent): BehaviorResult {
     const subject: GraphEntity | undefined = d3.event.subject;
-    this.graphView.selection.replace(subject ? [subject] : []);
-    this.graphView.dragging.replace([]);
+    if (isCtrlOrMetaPressed(event) || isShiftPressed(event)) {
+      if (subject != null) {
+        const selection = [...this.graphView.selection.get()];
+        let found = false;
+        for (let i = 0; i < selection.length; i++) {
+          if (selection[i].entity === subject.entity) {
+            found = true;
+            selection.splice(i, 1);
+            break;
+          }
+        }
+        if (!found) {
+          selection.push(subject);
+        }
+        this.graphView.selection.replace(selection);
+        this.graphView.dragging.replace(selection);
+        this.graphView.invalidateEntity(subject);
+      }
+    } else {
+      this.graphView.selection.replace(subject ? [subject] : []);
+      this.graphView.dragging.replace(subject ? [subject] : []);
+    }
     this.graphView.requestRender(); // TODO: Don't call unless needed
     return BehaviorResult.Continue;
   }
