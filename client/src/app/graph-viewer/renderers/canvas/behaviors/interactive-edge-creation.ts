5fe177c393597ada3a4cefe25a0f9f7bfbb6010a
Albert Pham <the.sk89q@gmail.com>
2020-08-27 13:48:35 -0700

    Preliminary new node connection UX.

--- client/src/app/graph-viewer/renderers/canvas/behaviors/interactive-edge-creation.ts
+++ client/src/app/graph-viewer/renderers/canvas/behaviors/interactive-edge-creation.ts
@@ -5,24 +5,99 @@ import { CanvasGraphView } from '../canvas-graph-view';
 import { AbstractCanvasBehavior, BehaviorResult } from '../../behaviors';
 import { Arrowhead } from '../../../utils/canvas/line-heads/arrow';
 import { EdgeCreation } from '../../../actions/edges';
-import { isCtrlOrMetaPressed } from '../../../../shared/utils';
+import { PlacedNode } from '../../../styles/styles';
+import { AbstractNodeHandleBehavior, Handle } from '../../../utils/behaviors/abstract-node-handle-behavior';
+import { isAltOrOptionPressed } from '../../../../shared/utils';
+
+const HANDLE_BEHAVIOR_KEY = '_interactive-edge-creation/handle';
+const HELPER_BEHAVIOR_KEY = '_interactive-edge-creation/helper';
 
 export class InteractiveEdgeCreation extends AbstractCanvasBehavior {
+  /**
+   * Subscription for when the selection changes.
+   */
+  private selectionChangeSubscription;
+
   constructor(private readonly graphView: CanvasGraphView) {
     super();
   }
 
-  doubleClick(event: MouseEvent): BehaviorResult {
-    const subject = this.graphView.getEntityAtMouse();
-    if (subject && subject.type === GraphEntityType.Node) {
-      const node = subject.entity as UniversalGraphNode;
-      this.graphView.behaviors.add('interactive-edge', new ActiveEdgeCreation(this.graphView, node), 10);
+  setup() {
+    this.selectionChangeSubscription = this.graphView.selection.changeObservable.subscribe(
+      ([newSelection, oldSelection]) => {
+        if (newSelection.length === 1 && newSelection[0].type === GraphEntityType.Node) {
+          this.graphView.behaviors.delete(HANDLE_BEHAVIOR_KEY);
+          this.graphView.behaviors.add(HANDLE_BEHAVIOR_KEY,
+            new ActiveEdgeCreationHandle(this.graphView, newSelection[0].entity as UniversalGraphNode), 100);
+        } else {
+          this.graphView.behaviors.delete(HANDLE_BEHAVIOR_KEY);
+        }
+      },
+    );
+  }
+
+  click(event: MouseEvent): BehaviorResult {
+    if (isAltOrOptionPressed(event)) {
+      const subject = this.graphView.getEntityAtMouse();
+      if (subject && subject.type === GraphEntityType.Node) {
+        const node = subject.entity as UniversalGraphNode;
+        this.graphView.behaviors.delete(HELPER_BEHAVIOR_KEY);
+        this.graphView.behaviors.add(HELPER_BEHAVIOR_KEY, new ActiveEdgeCreationHelper(this.graphView, node), 10);
+      }
     }
     return BehaviorResult.Continue;
   }
 }
 
-class ActiveEdgeCreation extends AbstractCanvasBehavior {
+class ActiveEdgeCreationHandle extends AbstractNodeHandleBehavior<Handle> {
+  protected topOffset = 0;
+  protected leftOffset = 0;
+  protected size = 20;
+
+  constructor(graphView: CanvasGraphView,
+              target: UniversalGraphNode) {
+    super(graphView, target);
+  }
+
+  drawHandle(ctx: CanvasRenderingContext2D, transform: any, {minX, minY, maxX, maxY}: Handle) {
+    const noZoomScale = 1 / this.graphView.transform.scale(1).k;
+
+    const nodeRadius = this.size / 2 * noZoomScale;
+    const x = (maxX - minX) / 2 + minX;
+    const y = (maxY - minY) / 2 + minY;
+    ctx.moveTo(x, y);
+    ctx.arc(x, y, nodeRadius, 0, 2 * Math.PI);
+    ctx.strokeStyle = '#2B7CE9';
+    ctx.stroke();
+    ctx.fillStyle = '#97C2FC';
+    ctx.fill();
+  }
+
+  protected activeDragStart(event: MouseEvent, graphX: number, graphY: number) {
+    this.graphView.behaviors.delete(HELPER_BEHAVIOR_KEY);
+    this.graphView.behaviors.add(HELPER_BEHAVIOR_KEY, new ActiveEdgeCreationHelper(this.graphView, this.target), 10);
+  }
+
+  getHandleBoundingBoxes(placedNode: PlacedNode): Handle[] {
+    const bbox = placedNode.getBoundingBox();
+    const noZoomScale = 1 / this.graphView.transform.scale(1).k;
+    const size = this.size * noZoomScale;
+    const halfSize = size / 2;
+    const x = (bbox.maxX - bbox.minX) / 2 + bbox.minX;
+    const y = bbox.minY;
+    return [
+      // Top left
+      {
+        minX: x - halfSize + this.leftOffset,
+        minY: y - halfSize + this.topOffset,
+        maxX: x + halfSize + this.leftOffset,
+        maxY: y + halfSize + this.topOffset,
+      },
+    ];
+  }
+}
+
+class ActiveEdgeCreationHelper extends AbstractCanvasBehavior {
   private to: {
     data: {
       x, y
