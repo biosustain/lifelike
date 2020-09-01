33357d4954cf09d9b0f2567110383be99a492a6f
Miguel Rueda <miguerueda86@yahoo.es>
2020-09-01 15:46:23 +0200

    feat: file-records component to support snippet search

---
+++ client/src/app/search/components/file-records.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {PDFResult} from '../../interfaces';
+
+@Component({
+  selector: 'app-file-records',
+  templateUrl: './file-records.component.html',
+  styleUrls: ['./file-records.component.scss'],
+  styles: ['.highlight {border: 2px solid red}'],
+  encapsulation: ViewEncapsulation.None
+})
+export class FileRecordsComponent implements OnInit {
+
+  @Input() results: PDFResult;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
