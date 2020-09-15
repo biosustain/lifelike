7959bb4b1e300a956e1e945829c5251097633aa6
David Bour <dbour.developer@gmail.com>
2020-09-14 17:53:56 -0700

    Migration 2 of 2 for Sources
    - fix: the PDF file source now works with all older data. PDFs that the
      owner did not own before will now have a copy in the projects folder.

--- appserver/migrations/versions/1c3ac93bf8e7_.py
+++ appserver/migrations/versions/1c3ac93bf8e7_.py
@@ -7,6 +7,7 @@ Create Date: 2020-09-14 16:11:19.709898
 """
 import copy
 import json
+import uuid
 from alembic import context
 from alembic import op
 import sqlalchemy as sa
@@ -59,7 +60,8 @@ t_files = sa.Table(
     sa.Column('content_id', sa.Integer, sa.ForeignKey(
         'files_content.id', ondelete='CASCADE'), nullable=False),
     sa.Column('user_id', sa.Integer, sa.ForeignKey('appuser.id'), nullable=False),
-    sa.Column('creation_date', sa.DATETIME),
+    sa.Column('creation_date', sa.TIMESTAMP(timezone=True)),
+    sa.Column('modified_date', sa.TIMESTAMP(timezone=True)),
     sa.Column('annotations', postgresql.JSONB, nullable=False),
     sa.Column('annotations_date', sa.TIMESTAMP, nullable=True),
     sa.Column('project', sa.Integer, sa.ForeignKey('projects.id'), nullable=False),
@@ -96,26 +98,7 @@ t_projects = sa.Table(
     sa.Column('users', sa.ARRAY(sa.Integer), nullable=False)
 )
 
-""" Node JSON schema
-{
-    'data': {
-        'x': 47,
-        'y': -605,
-        'hyperlink': '',
-        'detail': '',
-        'source': '',
-        'search': [],
-        'subtype': '',
-        'hyperlinks': [],
-        'sources': [{'type': str, 'domain': str, 'url': str}]
-    },
-    'display_name': 'E. coli',
-    'hash': '84c5a886-8727-44fb-935e-5e9e01a039bb',
-    'shape': 'box',
-    'label': 'species',
-    'sub_labels': []
-}
-"""
+conn = op.get_bind()
 
 
 def upgrade():
@@ -140,39 +123,29 @@ def get_src_hash_id(source):
     return source.split('/')[3]
 
 
-def get_projects_name(dir_id):
-    """ Return the `Projects name` to which
+def get_projects(dir_id):
+    """ Return the `Projects name and id` to which
     the asset is located under """
-    conn = op.get_bind()
     return conn.execute(
         sa.select(
-            [t_projects.c.project_name]
+            [t_projects.c.id, t_projects.c.project_name]
         ).select_from(sa.join(
             t_projects,
             t_directory,
-            t_projects.c.id == dir_id
-        ))
-    ).fetchone()[0]
+            t_projects.c.id == t_directory.c.projects_id
+        )).where(t_directory.c.id == dir_id)
+    ).fetchone()
 
 
-def convert_map_source(node, projects_name):
-    """ Perform conversions if a map source is found """
-    node_copy = copy.deepcopy(node)
-    data = node_copy['data']
-    source = data.get('source')
-    sources = data.get('sources')
-
-    if source and get_src_type(source) == 'map':
-        data['source'] = map_source_conversion(source, projects_name)
-    if sources:
-        data['sources'] = [
-            map_source_conversion(s['url'], projects_name, True)
-            for s in sources if get_src_type(s['url']) == 'map']
-
-    return node_copy
+def is_new_format(source):
+    """ Checks if node/edge data already contains new format """
+    return source.find('/projects/') != -1
 
 
 def map_source_conversion(source, projects_name, new_structure=False):
+    if is_new_format(source):
+        return source
+    print('Converting map source')
     hash_id = get_src_hash_id(source)
     link = f'/projects/{projects_name}/maps/{hash_id}/edit'
     if new_structure:
@@ -180,16 +153,118 @@ def map_source_conversion(source, projects_name, new_structure=False):
     return link
 
 
+# already generated pdf copies
+new_pdf_copies = {}
+
+
+def pdf_source_conversion(source, projects_id, projects_name, user_id, dir_id, new_structure=False):
+    """
+    Old Format
+    /dt/pdf/16e703d8-fdb4-483e-93cf-b63b01a65d65/1/508.72679250633314/508.9869840578061/545.4304343746924/497.523753676678
+
+    New Format
+    /projects/Christine-Personal-Project/files/90d450a9-ac99-4c49-b074-badfcd161bc6#page=1&coords=49.5,603.7295,67.075,613.2295
+    """
+    if is_new_format(source):
+        return source
+    print('Converting pdf source')
+
+    hash_id = get_src_hash_id(source)
+    files_query = conn.execute(
+        sa.select(
+            [t_files]
+        ).where(
+            t_files.c.file_id == hash_id
+        )
+    ).fetchone()
+    if files_query is not None:
+        query_user_id = files_query[5]
+    else:
+        query_user_id = None
+
+    src_split = source.split('/')
+    coordinates = f'coords={",".join(src_split[5:])}'
+    page = src_split[4]
+
+    # Check if the map owner is the same as the PDF owner
+    if user_id == query_user_id:
+        new_source = f'/projects/{projects_name}/files/{hash_id}#page={page}&{coordinates}'
+        if new_structure:
+            return {'type': '', 'domain': 'File Source', 'url': new_source}
+        else:
+            return new_source
+    elif query_user_id is None:
+        # query_user_id can be none, which means no file exists so we just return the source
+        return source
+    else:
+        # we make a copy of the PDF for the user and add it to their personal projects
+        # store pdf copies made so we don't make more copies than we need
+        unique_id = str(files_query[4]) + str(user_id)
+        if new_pdf_copies.get(unique_id):
+            if new_structure:
+                formatted = {'type': '', 'domain': 'File Source', 'url': new_pdf_copies[unique_id]}
+                return formatted
+            else:
+                return new_pdf_copies[unique_id]
+        else:
+            random_file_id = str(uuid.uuid4())
+            conn.execute(t_files.insert().values(
+                file_id=random_file_id,
+                filename=files_query[2],
+                description=files_query[3],
+                content_id=files_query[4],
+                user_id=user_id,
+                creation_date=files_query[6],
+                modified_date=files_query[7],
+                annotations=files_query[8],
+                annotations_date=files_query[9],
+                project=projects_id,
+                custom_annotations=files_query[11],
+                dir_id=dir_id,
+                doi=files_query[13],
+                upload_url=files_query[14],
+                excluded_annotations=files_query[15],
+            ))
+            new_source = f'/projects/{projects_name}/files/{random_file_id}#page={page}&{coordinates}'
+            new_pdf_copies[unique_id] = new_source
+            if new_structure:
+                formatted = {'type': '', 'domain': 'File Source', 'url': new_source}
+                return formatted
+            else:
+                return new_source
+
+
+def convert_source(component, projects_id, projects_name, user_id, dir_id):
+    """ Perform conversions if a map source is found """
+    component_copy = copy.deepcopy(component)
+    data = component_copy['data']
+    source = data.get('source')
+    sources = data.get('sources')
+
+    if source:
+        if get_src_type(source) == 'map':
+            data['source'] = map_source_conversion(source, projects_name)
+        else:
+            pdf_source_conversion(source, projects_id, projects_name, user_id, dir_id)
+    if sources:
+        data['sources'] = [
+            map_source_conversion(s['url'], projects_name, True)
+            if get_src_type(s['url']) == 'map'
+            else pdf_source_conversion(s['url'], projects_id, projects_name, user_id, dir_id, True)
+            for s in sources
+        ]
+    return component_copy
+
+
 def data_upgrades():
     """Add optional data upgrade migrations here"""
-    conn = op.get_bind()
     project = conn.execute(sa.select([t_project])).fetchall()
     for proj_id, _, _, _, graph, _, _, user_id, dir_id, _, _ in project:
         nodes = graph['nodes']
         edges = graph['edges']
-        projects_name = get_projects_name(dir_id)
-        nodes = [convert_map_source(node, projects_name) for node in nodes]
-        edges = [convert_map_source(edge, projects_name) for edge in edges]
+        projects_id, projects_name = get_projects(dir_id)
+        nodes = [convert_source(node, projects_id, projects_name, user_id, dir_id) for node in nodes]
+        edges = [convert_source(edge, projects_id, projects_name, user_id, dir_id) for edge in edges]
         graph['nodes'] = nodes
         graph['edges'] = edges
         conn.execute(t_project.update().where(t_project.c.id == proj_id).values(graph=graph))
