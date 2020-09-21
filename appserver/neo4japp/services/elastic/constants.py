60de4c6d032926c09cd42cbcf51d1a22cc42fea2
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-09-21 15:35:51 -0700

    Refactor elastic file search class/implementation
    Previous document search was pdf specific, which doesn't make sense now
    that we've refactored the index to use both pdf and maps. Refactors the
    old elastic search query code in the ElasticService, and also updates
    the 'pdf-search' endpoint to filter out any maps/pdfs the user doesn't
    have access to.

--- appserver/neo4japp/services/elastic/constants.py
+++ appserver/neo4japp/services/elastic/constants.py
@@ -2,6 +2,7 @@ import os
 
 directory = os.path.realpath(os.path.dirname(__file__))
 
+# Indexing constants
 FILE_INDEX_ID = 'file'
 FILE_INDEX_DEFINITION_PATH = os.path.join(directory, './mappings/document_idx.json')
 ATTACHMENT_PIPELINE_ID = 'attachment'
@@ -16,3 +17,7 @@ ELASTIC_INDEX_SEED_PAIRS = [
 ELASTIC_PIPELINE_SEED_PAIRS = [
     (ATTACHMENT_PIPELINE_ID, ATTACHMENT_PIPELINE_DEFINITION_PATH),
 ]
+
+# Search constants
+FRAGMENT_SIZE = 2147483647
+WILDCARD_MIN_LEN = 3
