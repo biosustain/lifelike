0412ffcfbba45235073eb0ddb41dceb1efb32c5b
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-09-22 12:36:26 -0700

    Add esanche06 tweaks to ws viewer and generic tabl

--- client/src/app/file-browser/components/generic-table.component.ts
+++ client/src/app/file-browser/components/generic-table.component.ts
@@ -1,43 +1,40 @@
 import { Component, Input } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { CommonDialogComponent } from '../../shared/components/dialog/common-dialog.component';
-import { MessageDialog } from '../../shared/services/message-dialog.service';
-import { DirectoryObject } from '../../interfaces/projects.interface';
 
 @Component({
   selector: 'app-generic-table',
   templateUrl: './generic-table.component.html',
   styleUrls: ['./generic-table.component.scss']
 })
-export class GenericTableComponent{
+export class GenericTableComponent {
   HEADER: TableHeader[][];
+
+  // Number of columns can be inferred from the headers
   numColumns: number[];
   entries: string[][];
-  constructor() {
-    console.log("reached");
-    
-  }
 
-  @Input() 
-  set header(header: TableHeader[][]){
+  constructor() {}
+
+  // Probably don't need setters for all of these
+  @Input()
+  set header(header: TableHeader[][]) {
     this.HEADER = header;
   }
   @Input()
-  set columns(num: number){
+  set columns(num: number) {
     this.numColumns = new Array(num);
   }
   @Input()
-  set rows(entries: string[][]){
+  set rows(entries: string[][]) {
     this.entries = entries;
-    console.log(this.entries);
   }
 }
 
-export class TableHeader{
+// Should probably be an interface rather than a class
+export class TableHeader {
   name: string;
   span: string;
 
-  constructor(name: string, span: string){
+  constructor(name: string, span: string) {
     this.name = name;
     this.span = span;
   }
--- client/src/app/file-browser/components/worksheet-viewer.component.ts
+++ client/src/app/file-browser/components/worksheet-viewer.component.ts
@@ -1,9 +1,6 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { CommonDialogComponent } from '../../shared/components/dialog/common-dialog.component';
-import { MessageDialog } from '../../shared/services/message-dialog.service';
-import { DirectoryObject } from '../../interfaces/projects.interface';
-import { GenericTableComponent, TableHeader} from './generic-table.component';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { TableHeader } from './generic-table.component';
 
 @Component({
   selector: 'app-worksheet-viewer',
@@ -12,21 +9,57 @@ import { GenericTableComponent, TableHeader} from './generic-table.component';
 })
 export class WorksheetViewerComponent implements OnInit, OnDestroy {
   tableHeader: TableHeader[][] = [
-    [new TableHeader("Gene", "1"), new TableHeader("Regulon", "1"), new TableHeader("Uniprot", "2"),
-    new TableHeader("String", "2"), new TableHeader("Go Enrichment", "6"), new TableHeader("Ecocyc", "2")],
-    [new TableHeader("", "1"), new TableHeader("", "1"), new TableHeader("", "1"), new TableHeader("", "1"),
-    new TableHeader("", "1"), new TableHeader("", "1"), new TableHeader("Molecular Function", "2"),
-    new TableHeader("Biological Process", "2"), new TableHeader("Cellular Componenet", "2"), new TableHeader("", "1"),new TableHeader("", "1"),]
+    // Primary headers
+    [
+      new TableHeader('Gene', '1'),
+      new TableHeader('Regulon', '1'),
+      new TableHeader('Uniprot', '1'),
+      new TableHeader('String', '1'),
+      new TableHeader('Go Enrichment', '3'),
+      new TableHeader('Ecocyc', '1')
+    ],
+    // Secondary headers
+    [
+      new TableHeader('', '1'),
+      new TableHeader('', '1'),
+      new TableHeader('', '1'),
+      new TableHeader('', '1'),
+      new TableHeader('Molecular Function', '1'),
+      new TableHeader('Biological Process', '1'),
+      new TableHeader('Cellular Componenet', '1'),
+      new TableHeader('', '1'),
+    ]
   ];
-  numColumns: number = 14;
-  entries: string[][] = [["placeholder", "placeholder", "", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder"]];;
-  constructor() {
-    console.log("reached");
-    
-  }
-  ngOnInit() {
-  }
-  ngOnDestroy() {
+  numColumns = 8;
 
-  }
+  loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel ex tellus. Fusce vestibulum erat pharetra bibendum malesuada. Suspendisse potenti. Mauris ac metus metus. Nunc non enim vel purus iaculis iaculis. Etiam magna lorem, faucibus eu libero quis, ultrices vestibulum enim. Nam quam lorem, dictum a fermentum non, blandit sed nisl.';
+
+  entries: string[][] = [
+    [
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+    ],
+    [
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+      this.loremIpsum,
+    ]
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngOnDestroy() {}
 }
