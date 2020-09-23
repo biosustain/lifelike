3bdbbaea00d06765f6566c8410930a690c090005
Miguel Rueda <miguerueda86@yahoo.es>
2020-09-23 23:01:18 +0200

    feat: adding word cloud implementation

--- client/src/app/word-cloud/word-cloud.component.ts
+++ client/src/app/word-cloud/word-cloud.component.ts
@@ -1,37 +1,41 @@
-import {Component, OnInit} from '@angular/core';
-import {Subscription} from 'rxjs';
+import {Component, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {Observable, of, Subscription} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
-import {CloudData, CloudOptions} from 'angular-tag-cloud-module';
+import {CloudData, CloudOptions, TagCloudComponent} from 'angular-tag-cloud-module';
+import {WordCloudService} from './services/word-cloud.service';
 
 @Component({
   selector: 'app-word-cloud',
   templateUrl: './word-cloud.component.html',
   styleUrls: ['./word-cloud.component.scss']
 })
+
 export class WordCloudComponent implements OnInit {
+  @ViewChild(TagCloudComponent, {static: false}) tagCloudComponent: TagCloudComponent;
   paramsSubscription: Subscription;
   projectName: string;
   fileId: string;
+  annotations: any[] = [];
+
+  // please refer to this documentation: https://github.com/d-koppenhagen/angular-tag-cloud-module \
+  // to see different options
+
   options: CloudOptions = {
     width: 0.98,
     height: 500,
-    overflow: false
+    overflow: false,
+    realignOnResize: true,
+    randomizeAngle: true,
+    step: 5,
+    font: 'italic bold 14px "Georgia", cursive'
   };
-  data: CloudData[] = [
-    {text: 'Weight-9-link', weight: 10},
-    {text: 'Weight-10-link', weight: 9},
-    {text: 'Weight-11-link', weight: 8},
-    {text: 'Weight-12-link', weight: 7},
-    {text: 'Weight-13-link', weight: 6},
-    {text: 'Weight-14-link', weight: 5},
-    {text: 'Weight-15-link', weight: 4},
-    {text: 'Weight-16-link', weight: 3},
-    {text: 'Weight-17-link', weight: 2},
-    {text: 'Weight-18-link', weight: 1},
-    {text: 'Weight-19-link', weight: 0}
-  ];
 
-  constructor(readonly route: ActivatedRoute) {
+  data: CloudData[] = [];
+
+  constructor(
+      readonly route: ActivatedRoute,
+      readonly wordCloudService: WordCloudService
+  ) {
     this.paramsSubscription = this.route.params.subscribe(param => {
       this.projectName = param.project_name;
       this.fileId = param.file_id;
@@ -39,6 +43,31 @@ export class WordCloudComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getAnnotationsForFile();
+  }
+
+  getAnnotationsForFile() {
+    this.wordCloudService.getCombinedAnnotations(this.projectName, this.fileId)
+        .subscribe(data => {
+          const list = data.split('\n');
+          const newData: CloudData[] = [];
+          list.forEach(e => this.annotations.push(e));
+          // remove the headers from tsv response
+          this.annotations.shift();
+          // remove empty line at the end of the tsv response
+          this.annotations.pop();
+          this.annotations.forEach(e => {
+            const annotation = e.split('\t');
+            newData.push({text: annotation[2], weight: parseInt(annotation[3], 10)});
+
+          });
+          // NOTE: You can remove the limit to show all words but this make completely unreadable the word cloud
+          this.setData(newData.slice(0, 51));
+        });
+  }
+
+  private setData(data: CloudData[]) {
+    this.data = data;
   }
 
 }
