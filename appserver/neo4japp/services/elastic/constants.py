8cfeedb48b5a90ee98bd6143e0d80c5a21df582a
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-09-23 16:17:02 -0700

    Move elastic file idx constant to global constants
    Couldn't keep it in the elastic-specific constants because of circular
    import issues, and in any case it makes more sense to put it in the
    global constants file because it's used, well, globally.

--- appserver/neo4japp/services/elastic/constants.py
+++ appserver/neo4japp/services/elastic/constants.py
@@ -1,9 +1,10 @@
 import os
 
+from neo4japp.constants import FILE_INDEX_ID
+
 directory = os.path.realpath(os.path.dirname(__file__))
 
 # Indexing constants
-FILE_INDEX_ID = 'file'
 FILE_INDEX_DEFINITION_PATH = os.path.join(directory, './mappings/document_idx.json')
 ATTACHMENT_PIPELINE_ID = 'attachment'
 ATTACHMENT_PIPELINE_DEFINITION_PATH = os.path.join(
@@ -19,5 +20,4 @@ ELASTIC_PIPELINE_SEED_PAIRS = [
 ]
 
 # Search constants
-FRAGMENT_SIZE = 2147483647
 WILDCARD_MIN_LEN = 3
