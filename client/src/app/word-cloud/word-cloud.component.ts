7470379b563963d47dd0dc1fe9a0de9f5b39804d
Miguel Rueda <miguerueda86@yahoo.es>
2020-09-23 15:46:27 +0200

    feat: adding route with parameters

--- client/src/app/word-cloud/word-cloud.component.ts
+++ client/src/app/word-cloud/word-cloud.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-word-cloud',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./word-cloud.component.scss']
 })
 export class WordCloudComponent implements OnInit {
+  paramsSubscription: Subscription;
+  projectName: string;
+  fileId: string;
 
-  constructor() { }
+  constructor(readonly route: ActivatedRoute) {
+    this.paramsSubscription = this.route.params.subscribe(param => {
+      this.projectName = param.project_name;
+      this.fileId = param.file_id;
+    });
+  }
 
   ngOnInit() {
   }
