13696a2d0e5d177a0f7485c516fed50964847f2e
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-09-28 13:30:37 -0700

    Address final comments on word cloud
    Removes the leftover code that was pointed out and adds some comments to
    the word cloud component.

--- client/src/app/word-cloud/word-cloud.module.ts
+++ client/src/app/word-cloud/word-cloud.module.ts
@@ -1,8 +1,9 @@
-import {NgModule} from '@angular/core';
-import {SharedModule} from '../shared/shared.module';
-import {WordCloudComponent} from './word-cloud.component';
-import {TagCloudModule} from 'angular-tag-cloud-module';
-import {WordCloudService} from './services/word-cloud.service';
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from 'app/shared/shared.module';
+
+import { WordCloudComponent } from './word-cloud.component';
+import { WordCloudService } from './services/word-cloud.service';
 
 
 @NgModule({
@@ -11,7 +12,6 @@ import {WordCloudService} from './services/word-cloud.service';
   ],
   imports: [
     SharedModule,
-    TagCloudModule
   ],
   providers: [
     WordCloudService
