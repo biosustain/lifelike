e1ca6ff397ae658ff5b0dfd0c4820a07b20ab990
andrewhuangwp <andrewhuangwp@gmail.com>
2020-10-01 15:27:48 -0700

    Add links

--- client/src/app/file-browser/components/generic-table.component.ts
+++ client/src/app/file-browser/components/generic-table.component.ts
@@ -20,16 +20,22 @@ export class GenericTableComponent {
     const num = Math.max.apply(null, header.map(x => x.reduce((a,b) => a + parseInt(b.span), 0)))
     this.numColumns = new Array(num)
   }
-  @Input() entries: string[][];
+  @Input() entries: TableCell[][];
+}
+
+export interface TableCell {
+  text: string;
+  singleLink?: TableLink;
+  multiLink?: TableLink[];
+}
+
+export interface TableLink {
+  link: string;
+  linkText: string
 }
 
 // Should probably be an interface rather than a class
-export class TableHeader {
+export interface TableHeader {
   name: string;
   span: string;
-
-  constructor(name: string, span: string) {
-    this.name = name;
-    this.span = span;
-  }
 }
