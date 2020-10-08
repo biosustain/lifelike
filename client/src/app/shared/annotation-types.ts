8110f0d470cde089e53dfd673f74d83753d03c3d
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-10-08 14:45:49 -0700

    Add food and anatomy entity types to frontend

--- client/src/app/shared/annotation-types.ts
+++ client/src/app/shared/annotation-types.ts
@@ -8,17 +8,19 @@ export class EntityType {
 }
 
 export const ENTITY_TYPES = [
-  new EntityType('Gene', '#8f7cbf'),
+  new EntityType('Gene', '#673ab7'),
   new EntityType('Protein', '#bcbd22'),
-  new EntityType('Disease', '#fae0b8'),
-  new EntityType('Species', '#3177b8'),
-  new EntityType('Companies', '#ff7f7f'),
-  new EntityType('Mutations', '#8b5d2e'),
-  new EntityType('Chemical', '#cee5cb'),
+  new EntityType('Disease', '#ff9800'),
+  new EntityType('Species', '#0277bd'),
+  new EntityType('Companies', '#d62728'),
+  new EntityType('Mutations', '#5d4037'),
+  new EntityType('Chemical', '#4caf50'),
   new EntityType('Phenotype', '#edc949'),
-  new EntityType('Pathways', '#90eebf'),
+  new EntityType('Pathways', '#e377c2'),
   new EntityType('Entities', '#7f7f7f'),
-  new EntityType('Compound', '#cee5cb'),
+  new EntityType('Compound', '#4caf50'),
+  new EntityType('Food', '#8eff69'),
+  new EntityType('Anatomy', '#0202bd')
 ];
 
 export const ENTITY_TYPE_MAP = ENTITY_TYPES.reduce((map, item) => {
