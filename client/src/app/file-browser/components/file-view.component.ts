f19dce242eb137ce3c005be042c2d4904f85af50
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-10-08 15:48:03 -0700

    Set word cloud to open in opposite pane

--- client/src/app/file-browser/components/file-view.component.ts
+++ client/src/app/file-browser/components/file-view.component.ts
@@ -591,6 +591,6 @@ export class FileViewComponent implements OnDestroy, ModuleAwareComponent {
 
   openWordCloudPane() {
     const url = `/word-cloud/${this.projectName}/${this.pdfFile.file_id}`;
-    this.workSpaceManager.openTabByUrl('left', url);
+    this.workSpaceManager.navigateByUrl(url, {sideBySide: true, newTab: true});
   }
 }
