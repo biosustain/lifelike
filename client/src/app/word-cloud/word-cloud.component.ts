a36e805b0dbd76380477bdeacc9b6a9bc7da449a
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-10-09 14:30:40 -0700

    Add min/max frequency filters to word cloud

--- client/src/app/word-cloud/word-cloud.component.ts
+++ client/src/app/word-cloud/word-cloud.component.ts
@@ -1,7 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
-
 
 import { uniqueId } from 'lodash';
 
@@ -18,6 +23,7 @@ import { WordCloudService } from './services/word-cloud.service';
 
 import * as d3 from 'd3';
 import * as cloud from 'd3.layout.cloud';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-word-cloud',
@@ -26,12 +32,13 @@ import * as cloud from 'd3.layout.cloud';
 })
 
 export class WordCloudComponent {
-  @ViewChild('dropdown', {static: false, read: NgbDropdown}) dropdownComponent: NgbDropdown;
   id = uniqueId('WordCloudComponent-');
 
   projectName: string;
   fileId: string;
   fileName: string;
+
+  firstLoadComplete: boolean;
 
   loadTask: BackgroundTask<[], [PdfFile, NodeLegend, string]>;
   annotationsLoadedSub: Subscription;
@@ -43,7 +50,13 @@ export class WordCloudComponent {
 
   legend: Map<string, string>;
 
+  minimumFrequencyInputId: string;
+  maximumFrequencyInputId: string;
+
+  filtersForm: FormGroup;
+
   NOT_SHOWN_TOOLTIP = 'Could not fit this term in the cloud. Try expanding the window or filtering other terms.';
+  WORD_CLOUD_MARGIN = 10;
 
   constructor(
       readonly route: ActivatedRoute,
@@ -53,12 +66,33 @@ export class WordCloudComponent {
   ) {
     this.projectName = this.route.snapshot.params.project_name;
     this.fileId = this.route.snapshot.params.file_id;
+    this.fileName = '';
 
-    this.legend = new Map<string, string>();
-    this.annotationData = [];
+    this.firstLoadComplete = false;
+
     this.wordVisibilityMap = new Map<string, boolean>();
     this.wordVisibilityChanged = false;
-    this.fileName = '';
+
+    this.annotationData = [];
+
+    this.legend = new Map<string, string>();
+
+    this.minimumFrequencyInputId = `${this.id}-mininum-frequency-input`;
+    this.maximumFrequencyInputId = `${this.id}-maximum-frequency-input`;
+
+    this.filtersForm = new FormGroup(
+      // Form controls
+      {
+        minimumFrequency: new FormControl(
+          0, [Validators.required, Validators.min(0), Validators.pattern(/^-?[0-9][^\.]*$/)]
+        ),
+        maximumFrequency: new FormControl(
+          0, [Validators.required, Validators.min(0), Validators.pattern(/^-?[0-9][^\.]*$/)]
+        ),
+      },
+      // Form group validators
+      [this.minMaxFreqValidator()]
+    );
 
     this.loadTask = new BackgroundTask(() => {
       return combineLatest(
@@ -72,20 +106,21 @@ export class WordCloudComponent {
       result: [pdfFile, legend, annotationExport],
       value: [],
     }) => {
-      // Setup filename
+      // Reset filename
       this.fileName = pdfFile.filename;
 
-      // Setup legend
+      // Reset legend
       Object.keys(legend).forEach(label => {
         this.legend.set(label.toLowerCase(), legend[label].color);
       });
 
-      // Setup annotation data
-      const annotationList = annotationExport.split('\n');
+      // Reset annotation data
       this.annotationData = [];
       this.wordVisibilityMap.clear();
 
       const tempIdTypePairSet = new Map<string, number>();
+      const annotationList = annotationExport.split('\n');
+      let maximumPossibleFrequency = 0;
 
       // remove the headers from tsv response
       annotationList.shift();
@@ -108,24 +143,50 @@ export class WordCloudComponent {
           this.annotationData.push(annotation);
           tempIdTypePairSet.set(idTypePair, this.annotationData.length - 1);
         } else {
+          // Add the frequency of the synonym to the original word
           this.annotationData[tempIdTypePairSet.get(idTypePair)].value += parseInt(cols[3], 10);
           // TODO: In the future, we may want to show "synonyms" somewhere, or even allow the user to swap out the most frequent term for a
           // synonym
         }
+
+        // Need to keep track of the maximum possible frequency so we know what we should set the initial limit to
+        const frequencyOfThisEntity = this.annotationData[tempIdTypePairSet.get(idTypePair)].value;
+        if ( frequencyOfThisEntity > maximumPossibleFrequency) {
+          maximumPossibleFrequency = frequencyOfThisEntity;
+        }
       });
+
+      // If this is the first time we've loaded the data, set the filters to some default values
+      if (!this.firstLoadComplete) {
+        this.filtersForm.get('minimumFrequency').setValue(1);
+        this.filtersForm.get('maximumFrequency').setValue(maximumPossibleFrequency);
+      }
+
+      // Need to sort the data, since we may have squashed some terms down and messed with the order given by the API
+      this.annotationData = this.annotationData.sort((a, b) => b.value - a.value);
+
+      // Apply any existing filters and groupings, if any
+      this.applyFiltersAndGroupings();
+
+      // Need a slight delay between the data having been loaded and drawing the word cloud, seems like the BackgroundTask doesn't quite
+      // adhere to the normal change detection cycle.
       setTimeout(() => {
-        this.drawWordCloud(this.getAnnotationDataDeepCopy(), true);
+        this.drawWordCloud(this.getFilteredAnnotationDeepCopy(), true);
+
+        if (!this.firstLoadComplete) {
+          this.firstLoadComplete = true;
+        }
       }, 10);
     });
 
     this.getAnnotationsForFile();
   }
 
-  /**
-   * Closes the word cloud term dropdown widget.
-   */
-  closeFilterPopup() {
-    this.dropdownComponent.close();
+  submitFilterAndGroupingForm() {
+    if (this.filtersForm.valid) {
+      this.applyFiltersAndGroupings();
+      this.drawWordCloud(this.getFilteredAnnotationDeepCopy(), false);
+    }
   }
 
   /**
@@ -190,13 +251,24 @@ export class WordCloudComponent {
     this.wordVisibilityChanged = wordVisibilityChanged;
   }
 
+  /**
+   * Updates the 'shown' property of the words in annotationData. This property denotes whether the word cloud could draw the word within
+   * the given width/height/size constraints. To simplify, each word essentially has three states:
+   *
+   *   1. The word is visible in the cloud
+   *   2. The word is not visible in the cloud, because it was filtered and not considered in the layout algorithm
+   *   3. The word is not visible in the cloud, because it could not be drawn with the given size within the given width/height of the
+   *    container element
+   *
+   * This function determines the state of each input word.
+   * @param words list of objects representing word data
+   */
   private updateWordVisibility(words: Word[]) {
     const tempWordMap = new Map<string, Word>();
     words.forEach((word) => {
       tempWordMap.set(word.text, word);
     });
 
-    /* tslint:disable:prefer-for-of*/
     for (const word of this.annotationData) {
       // If the word was returned by the algorithm then it is not filtered and it is drawable
       if (tempWordMap.has(word.text)) {
@@ -213,26 +285,47 @@ export class WordCloudComponent {
     }
   }
 
+  /**
+   * Generates a copy of the annotation data. The reason we do this is the word cloud layout algorithm actually mutates the input data. To
+   * keep our API response data pure, we deep copy it and give the copy to the layout algorithm instead.
+   */
   private getAnnotationDataDeepCopy() {
     return JSON.parse(JSON.stringify(this.annotationData)) as Word[];
   }
 
+  /**
+   * Gets a filtered copy of the annotation data. Any word not mapped to 'true' in the wordVisibilityMap will be filtered out.
+   */
   private getFilteredAnnotationDeepCopy() {
     return this.getAnnotationDataDeepCopy().filter(annotation => this.wordVisibilityMap.get(annotation.text));
   }
 
-  private getWrapperDimensions() {
-    const margin = {top: 10, right: 10, bottom: 10, left: 10};
+
+  /**
+   * Generates the width/height for the word cloud svg element. Uses the size of the wrapper element, minus a fixed margin. For example, if
+   * the parent is 600px x 600px, and our margin is 10px, the size of the word cloud svg will be 580px x 580px.
+   */
+  private getCloudSvgDimensions() {
+    const margin = {
+      top: this.WORD_CLOUD_MARGIN,
+      right: this.WORD_CLOUD_MARGIN,
+      bottom: this.WORD_CLOUD_MARGIN,
+      left: this.WORD_CLOUD_MARGIN
+    };
     const width = (document.getElementById(`${this.id}cloud-wrapper`).offsetWidth) - margin.left - margin.right;
     const height = (document.getElementById(`${this.id}cloud-wrapper`).offsetHeight) - margin.top - margin.bottom;
 
     return {width, height};
   }
 
+  /**
+   * Creates the word cloud svg and related elements. Also creates 'text' elements for each value in the 'words' input.
+   * @param words list of objects representing terms and their position info as decided by the word cloud layout algorithm
+   */
   private createInitialWordCloudElements(words: Word[]) {
     this.updateWordVisibility(words);
 
-    const {width, height} = this.getWrapperDimensions();
+    const {width, height} = this.getCloudSvgDimensions();
 
     // Append the svg element to the wrapper, append the grouping element to the svg, and create initial words
     d3.select(`#${this.id}cloud-wrapper`)
@@ -258,11 +351,17 @@ export class WordCloudComponent {
         .duration(1000);
   }
 
+  /**
+   * Updates the word cloud svg and related elements. Distinct from createInitialWordCloudElements in that it finds the existing elements
+   * and updates them if possible. Any existing words will be re-scaled and moved to their new positions, removed words will be removed,
+   * and added words will be drawn.
+   * @param words list of objects representing terms and their position info as decided by the word cloud layout algorithm
+   */
   private updateWordCloudElements(words: Word[]) {
     this.updateWordVisibility(words);
 
     // Set the dimensions and margins of the graph
-    const {width, height} = this.getWrapperDimensions();
+    const {width, height} = this.getCloudSvgDimensions();
 
     // Get the svg element and update
     const svg = d3.select(`#${this.id}cloud-wrapper`)
@@ -304,7 +403,7 @@ export class WordCloudComponent {
    */
   private drawWordCloud(data: Word[], initial: boolean) {
     // Reference for this code: https://www.d3-graph-gallery.com/graph/wordcloud_basic
-    const {width, height} = this.getWrapperDimensions();
+    const {width, height} = this.getCloudSvgDimensions();
     const maximumCount = Math.max(...data.map(annotation => annotation.value as number));
 
     // Constructs a new cloud layout instance (it runs the algorithm to find the position of words)
@@ -315,8 +414,76 @@ export class WordCloudComponent {
       // max ~86px, min ~12px
       .fontSize((d) => ((d.value / maximumCount) * 74) + 12)
       /* tslint:disable:no-bitwise*/
-      .rotate(() => (~~(Math.random() * 8) - 3) * 15)
-      .on('end', (words) => { initial ? this.createInitialWordCloudElements(words) : this.updateWordCloudElements(words); });
+      .rotate(() => 0)
+      // TODO: Maybe in the future we can allow the user to define their own rotation intervals,
+      // but for now just keep it simple and don't rotate the words
+      // .rotate(() => (~~(Math.random() * 8) - 3) * 15)
+      .on('end', words => initial ? this.createInitialWordCloudElements(words) : this.updateWordCloudElements(words));
     layout.start();
+  }
+
+  /**
+   * Sets visibility to false for all entities that are not within the range specified by the user. This DOES NOT redraw the cloud! The
+   * calling function should bre responsible for the redraw.
+   */
+  private filterByFrequency() {
+    const minimumFrequency = this.filtersForm.get('minimumFrequency').value;
+    const maximumFrequency = this.filtersForm.get('maximumFrequency').value;
+
+    for (const annotation of this.annotationData) {
+      this.wordVisibilityMap.set(annotation.text,  minimumFrequency <= annotation.value && annotation.value <= maximumFrequency);
+    }
+    this.invalidateWordVisibility();
+  }
+
+  /**
+   * Helper function for applying all filter and grouping methods simultaneously. Used by the filter form submission function.
+   */
+  private applyFiltersAndGroupings() {
+    this.filterByFrequency();
+  }
+
+  /**
+   * Validation function used by the filter form group to check validity of the maximum and minimum frequency values.
+   */
+  private minMaxFreqValidator(): ValidatorFn {
+    return (fg: FormGroup): ValidationErrors => {
+      const minFreqControl = fg.get('minimumFrequency');
+      const maxFreqControl = fg.get('maximumFrequency');
+
+      if (minFreqControl.value > maxFreqControl.value) {
+        minFreqControl.setErrors({ ...minFreqControl.errors, badMinMax: true });
+        maxFreqControl.setErrors({ ...maxFreqControl.errors, badMinMax: true });
+      } else {
+        let minFreqControlErrors = minFreqControl.errors;
+        let maxFreqControlErrors = maxFreqControl.errors;
+
+        // Need to remove the 'badMinMax' property entirely from the errors object; as long as the property exists, the error is assumed to
+        // exist
+        if (!isNullOrUndefined(minFreqControlErrors)) {
+          delete minFreqControlErrors.badMinMax;
+
+          // If there are no more properties in the errors object, we need to set errors to null in order for it to be recognized as valid
+          // (an empty object will still mark the control as invalid)
+          if (Object.keys(minFreqControlErrors).length === 0) {
+            minFreqControlErrors = null;
+          }
+        }
+
+        // Do the same for the max frequency control
+        if (!isNullOrUndefined(maxFreqControlErrors)) {
+          delete maxFreqControlErrors.badMinMax;
+
+          if (Object.keys(maxFreqControlErrors).length === 0) {
+            maxFreqControlErrors = null;
+          }
+        }
+
+        minFreqControl.setErrors(minFreqControlErrors);
+        maxFreqControl.setErrors(maxFreqControlErrors);
+      }
+
+      return fg.errors;
+    };
   }
 }
