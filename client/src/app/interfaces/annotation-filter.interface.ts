3be5e17fcd2871c0fd462e7f3c7bfb192ea8f4b7
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-10-13 12:14:08 -0700

    Add grouping inputs to annotation filter
    Interacting with the grouping inputs doesn't do anything yet.

--- client/src/app/interfaces/annotation-filter.interface.ts
+++ client/src/app/interfaces/annotation-filter.interface.ts
@@ -9,3 +9,19 @@ export interface WordCloudAnnotationFilterEntity extends AnnotationFilterEntity 
   shown: boolean;
   synonyms?: string[];
 }
+
+export enum DefaultGroupByOptions {
+  NONE = 'None',
+  ENTITY_TYPE = 'Entity Type',
+  ANNOTATION_TYPE = 'Annotation Type (Manual vs. Automatic)'
+}
+
+export enum DefaultOrderByOptions {
+  FREQUENCY = 'Frequency',
+  FILTERED = 'Filtered',
+}
+
+export enum OrderDirection {
+  ASCENDING = 'Ascending',
+  DESCENDING = 'Descending'
+}
