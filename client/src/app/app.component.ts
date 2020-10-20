5f0ae4b1733a5446c3bf9ce56189afed6a00ecc7
David Bour <dbour.developer@gmail.com>
2020-10-20 09:26:36 -0700

    - fix: fix download which was not getting the blob data

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -71,7 +71,7 @@ export class AppComponent {
   downloadManual() {
     this.storage.getUserManual().subscribe(resp => {
       const filename = resp.headers.get('content-disposition').split('=')[1];
-      downloader(resp, 'application/pdf', filename);
+      downloader(resp.body, 'application/pdf', filename);
     });
   }
 }
