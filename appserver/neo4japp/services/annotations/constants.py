fd59bde1c94a81e6bbdab9bf4b421adec4579d3a
Anna Sirunian <ansir@biosustain.dtu.dk>
2020-10-21 11:26:32 +0200

    feat: add new search links for automatic annotations

--- appserver/neo4japp/services/annotations/constants.py
+++ appserver/neo4japp/services/annotations/constants.py
@@ -161,10 +161,15 @@ class ManualAnnotationType(Enum):
 # these links are used in annotations and custom annotations
 # first are search links
 # then entity hyperlinks
-NCBI_LINK = 'https://www.ncbi.nlm.nih.gov/gene/?term='
-UNIPROT_LINK = 'https://www.uniprot.org/uniprot/?sort=score&query='
-WIKIPEDIA_LINK = 'https://www.google.com/search?q=site:+wikipedia.org+'
-GOOGLE_LINK = 'https://www.google.com/search?q='
+SEARCH_LINKS = {
+    'ncbi': 'https://www.ncbi.nlm.nih.gov/gene/?term=',
+    'uniprot': 'https://www.uniprot.org/uniprot/?sort=score&query=',
+    'mesh': 'https://www.ncbi.nlm.nih.gov/mesh/?term=',
+    'chebi': 'https://www.google.com/search?q=site:ebi.ac.uk/+',
+    'pubchem': 'https://www.google.com/search?q=site:ncbi.nlm.nih.gov/+',
+    'wikipedia': 'https://www.google.com/search?q=site:+wikipedia.org+',
+    'google': 'https://www.google.com/search?q=',
+}
 ENTITY_HYPERLINKS = {
     DatabaseType.CHEBI.value: 'https://www.ebi.ac.uk/chebi/searchId.do?chebiId=',
     DatabaseType.MESH.value: 'https://www.ncbi.nlm.nih.gov/mesh/',
@@ -174,5 +179,5 @@ ENTITY_HYPERLINKS = {
         EntityType.SPECIES.value: 'https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=',
     },
     DatabaseType.BIOCYC.value: 'https://biocyc.org/compound?orgid=META&id=',
-    DatabaseType.CUSTOM.value: GOOGLE_LINK
+    DatabaseType.CUSTOM.value: SEARCH_LINKS['google'],
 }
