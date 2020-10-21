4f58117a7e4a26b27cd1ea56cbad10340e0e92b9
Albert Pham <the.sk89q@gmail.com>
2020-10-21 11:11:00 -0700

    Add basic 'More...' button for highlights.

--- client/src/app/file-browser/components/file-info.component.ts
+++ client/src/app/file-browser/components/file-info.component.ts
@@ -8,4 +8,17 @@ import { DirectoryObject } from '../../interfaces/projects.interface';
 export class FileInfoComponent {
   @Input() object: DirectoryObject | undefined;
   @Output() objectEdit = new EventEmitter<DirectoryObject>();
+  highlightLimit = 5;
+
+  get shownHighlights() {
+    return this.object.highlight.slice(0, this.highlightLimit);
+  }
+
+  get reachedHighlightLimit() {
+    return this.highlightLimit >= this.object.highlight.length;
+  }
+
+  displayMoreHighlights() {
+    this.highlightLimit += 5;
+  }
 }
