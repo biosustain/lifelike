e7cafbf06323d84fd19e527ad0b45f33c05d8028
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-10-20 17:00:35 -0700

    Clean up UI in file navigator

--- client/src/app/file-navigator/file-navigator.component.ts
+++ client/src/app/file-navigator/file-navigator.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { combineLatest, Subscription } from 'rxjs';
+
+import { PdfFile } from 'app/interfaces/pdf-files.interface';
+import { BackgroundTask } from 'app/shared/rxjs/background-task';
+import { PdfFilesService } from 'app/shared/services/pdf-files.service';
 
 @Component({
   selector: 'app-file-navigator',
   templateUrl: './file-navigator.component.html',
   styleUrls: ['./file-navigator.component.scss']
 })
-export class FileNavigatorComponent implements OnInit {
+export class FileNavigatorComponent {
 
-  constructor() { }
+  loadTask: BackgroundTask<[], [PdfFile]>;
+  fileLoadedSub: Subscription;
 
-  ngOnInit() {
+  projectName: string;
+  fileId: string;
+  fileName: string;
+
+  constructor(
+    readonly route: ActivatedRoute,
+    private pdf: PdfFilesService,
+  ) {
+    this.projectName = this.route.snapshot.params.project_name;
+    this.fileId = this.route.snapshot.params.file_id;
+
+    this.loadTask = new BackgroundTask(() => {
+      return combineLatest(
+        this.pdf.getFileMeta(this.fileId, this.projectName),
+      );
+    });
+
+    this.fileLoadedSub = this.loadTask.results$.subscribe(({
+      result: [pdfFile],
+      value: []
+    }) => {
+      this.fileName = pdfFile.filename;
+    });
+
+    this.loadTask.update([]);
   }
-
 }
