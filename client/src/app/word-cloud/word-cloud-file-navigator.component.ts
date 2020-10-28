4820f4de31e0456a6df120999cd1dc2a4f94ccc2
Albert Pham <the.sk89q@gmail.com>
2020-10-28 10:45:59 -0700

    Implement entity search from the entity cloud. Fixes LL-1988

--- client/src/app/word-cloud/word-cloud-file-navigator.component.ts
+++ client/src/app/word-cloud/word-cloud-file-navigator.component.ts
@@ -1,35 +1,72 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BackgroundTask } from 'app/shared/rxjs/background-task';
 
 import { LegendService } from 'app/shared/services/legend.service';
 import { PdfFilesService } from 'app/shared/services/pdf-files.service';
-import { combineLatest } from 'rxjs';
+import { combineLatest, Subscription } from 'rxjs';
 
 import { WordCloudService } from './services/word-cloud.service';
 import { WordCloudComponent } from './word-cloud.component';
+import { WordCloudAnnotationFilterEntity } from '../interfaces/annotation-filter.interface';
+import { FileViewComponent } from '../file-browser/components/file-view.component';
+import { WorkspaceManager } from '../shared/workspace-manager';
+import { escapeRegExp } from 'lodash';
 
 @Component({
   selector: 'app-word-cloud-file-navigator',
   templateUrl: './word-cloud-file-navigator.component.html',
   styleUrls: ['./word-cloud.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class WordCloudFileNavigatorComponent extends WordCloudComponent {
+export class WordCloudFileNavigatorComponent extends WordCloudComponent
+    implements OnInit, OnDestroy {
+
+  clickableWords = true;
+  private wordOpenSubscription: Subscription;
 
   constructor(
-    readonly route: ActivatedRoute,
-    public pdf: PdfFilesService,
-    public wordCloudService: WordCloudService,
-    public legendService: LegendService,
+      readonly route: ActivatedRoute,
+      readonly router: Router,
+      public pdf: PdfFilesService,
+      public wordCloudService: WordCloudService,
+      public legendService: LegendService,
+      public workspaceManager: WorkspaceManager,
   ) {
     super(route, pdf, wordCloudService, legendService);
+  }
+
+  ngOnInit() {
+    this.wordOpenSubscription = this.wordOpen.subscribe((annotation: WordCloudAnnotationFilterEntity) => {
+      const url = [
+        '/projects',
+        encodeURIComponent(this.projectName),
+        'files',
+        encodeURIComponent(this.fileId),
+      ].join('/');
+
+      this.workspaceManager.navigateByUrl(`${url}#annotation=${encodeURIComponent(annotation.id)}`, {
+        newTab: true,
+        sideBySide: true,
+        matchExistingTab: `^/*${escapeRegExp(url)}.*`,
+        shouldReplaceTab: component => {
+          const fileViewComponent = component as FileViewComponent;
+          fileViewComponent.highlightAnnotation(annotation.id);
+          return false;
+        },
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.wordOpenSubscription.unsubscribe();
   }
 
   initDataFetch() {
     this.loadTask = new BackgroundTask(() => {
       return combineLatest(
-        this.legendService.getAnnotationLegend(),
-        this.wordCloudService.getCombinedAnnotations(this.projectName, this.fileId),
+          this.legendService.getAnnotationLegend(),
+          this.wordCloudService.getCombinedAnnotations(this.projectName, this.fileId),
       );
     });
   }
@@ -37,9 +74,9 @@ export class WordCloudFileNavigatorComponent extends WordCloudComponent {
   initWordCloud() {
     this.initDataFetch();
     this.annotationsLoadedSub = this.loadTask.results$.subscribe(({
-      result: [legend, annotationExport],
-      value: [],
-    }) => {
+                                                                    result: [legend, annotationExport],
+                                                                    value: [],
+                                                                  }) => {
       this.windowTitle = 'Word Cloud';
 
       // Reset legend
