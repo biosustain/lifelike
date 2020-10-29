b06d5709885cd8d4c170c8cdb913f4e90e6d9c10
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-10-29 13:32:32 -0700

    Add skeleton shortest-path component

---
+++ client/src/app/shortest-path/containers/shortest-path.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shortest-path',
+  templateUrl: './shortest-path.component.html',
+  styleUrls: ['./shortest-path.component.scss']
+})
+export class ShortestPathComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
