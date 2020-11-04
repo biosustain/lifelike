18df5b4569aa0756bec1ccc06838f88e25a278c8
David Bour <dbour.developer@gmail.com>
2020-11-04 14:50:25 -0800

    - refactor: GlobalAnnotation API now returns only the fields necessary   for the client instead of the entire payload. - refactor: GlobalAnnotation API uses its own pagination; there was   an issue attempting to integrate with the util pagination function   because of the synthetic label. TODO: fix this - refactor: client now uses the GlobalAnnotation API data directory   without needing to transform on the fly.

--- client/src/app/interfaces/annotation.ts
+++ client/src/app/interfaces/annotation.ts
@@ -10,30 +10,6 @@ export interface InclusionMeta {
     type: string;
 }
 
-export interface GlobalInclusion {
-    inclusion_date: string;
-    keywords: string[];
-    meta: InclusionMeta;
-    pageNumber: number;
-    rects: number[][];
-    user_id: number;
-    uuid: string;
-}
-
-export interface GlobalExclusion {
-    comment: string;
-    excludeGlobally: boolean;
-    exclusion_date: string;
-    id: string;
-    idHyperlink: string;
-    pageNumber: number;
-    reason: string;
-    rects: number[][];
-    text: string;
-    type: string;
-    user_id: number;
-}
-
 export interface GlobalAnnotation {
     id: number;
     file_id: string;
@@ -44,16 +20,7 @@ export interface GlobalAnnotation {
     approved: boolean;
     creationDate: string;
     modifiedDate: string;
-    annotation: GlobalExclusion | GlobalInclusion;
-}
-
-export interface GlobalAnnotationRow {
-    pid: number;
     text: string;
-    type: string;
-    filename: string;
-    addedBy: string;
-    creationDate: string;
     reason: string;
     entityType: string;
     annotationId: string;
