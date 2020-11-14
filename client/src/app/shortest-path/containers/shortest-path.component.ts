48831a2ae54668f03cdaaba371e62eb8f5e84661
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-11-13 16:08:32 -0800

    Consolidate shortest path query endpoints

--- client/src/app/shortest-path/containers/shortest-path.component.ts
+++ client/src/app/shortest-path/containers/shortest-path.component.ts
@@ -29,67 +29,11 @@ export class ShortestPathComponent implements OnInit {
 
   loadNewQuery(query: number) {
     this.graphData = null;
-    switch (query) {
-      // 3-hydroxyisobutyric Acid to pykF Using ChEBI
-      case 0: {
-        this.shortestPathService.threeHydroxyisobutyricAcidToPykfChebi().subscribe((result) => {
-          this.graphData = {
-            nodes: result.nodes,
-            edges: result.edges,
-          };
-        });
-        break;
-      }
-      // 3-hydroxyisobutyric Acid to pykF using BioCyc
-      case 1: {
-        this.shortestPathService.threeHydroxyisobutyricAcidToPykfBiocyc().subscribe((result) => {
-          this.graphData = {
-            nodes: result.nodes,
-            edges: result.edges,
-          };
-        });
-        break;
-      }
-      // icd to rhsE
-      case 2: {
-        this.shortestPathService.icdToRhse().subscribe((result) => {
-          this.graphData = {
-            nodes: result.nodes,
-            edges: result.edges,
-          };
-        });
-        break;
-      }
-      // SIRT5 to NFE2L2 Using Literature Data
-      case 3: {
-        this.shortestPathService.sirt5ToNfe2l2Literature().subscribe((result) => {
-          this.graphData = {
-            nodes: result.nodes,
-            edges: result.edges,
-          };
-        });
-        break;
-      }
-      // CTNNB1 to Diarrhea Using Literature Data
-      case 4: {
-        this.shortestPathService.ctnnb1ToDiarrheaLiterature().subscribe((result) => {
-          this.graphData = {
-            nodes: result.nodes,
-            edges: result.edges,
-          };
-        });
-        break;
-      }
-      // Two pathways using BioCyc
-      case 5: {
-        this.shortestPathService.twoPathwaysBiocyc().subscribe((result) => {
-          this.graphData = {
-            nodes: result.nodes,
-            edges: result.edges,
-          };
-        });
-        break;
-      }
-    }
+    this.shortestPathService.getShortestPathQueryResult(query).subscribe((result) => {
+      this.graphData = {
+        nodes: result.nodes,
+        edges: result.edges,
+      };
+    });
   }
 }
