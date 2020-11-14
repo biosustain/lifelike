412cfc4c7ba3468983ec7f25aed888e90d103987
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-11-13 16:08:32 -0800

    Consolidate shortest path query endpoints

--- client/src/app/shortest-path/services/shortest-path.service.ts
+++ client/src/app/shortest-path/services/shortest-path.service.ts
@@ -17,10 +17,9 @@ export class ShortestPathService extends AbstractService {
     super(auth, http);
   }
 
-  // 3-hydroxyisobutyric Acid to pykF Using ChEBI
-  threeHydroxyisobutyricAcidToPykfChebi(): Observable<any> {
+  getShortestPathQueryResult(queryId: number): Observable<any> {
     return this.http.get<{result: any}>(
-      `${this.kgAPI}/shortest-path-queries/three-hydroxisobuteric-acid-to-pykf-chebi`, {
+      `${this.kgAPI}/shortest-path-query/${queryId}`, {
         ...this.getHttpOptions(true),
       }
     ).pipe(
@@ -28,54 +27,9 @@ export class ShortestPathService extends AbstractService {
     );
   }
 
-  // 3-hydroxyisobutyric Acid to pykF using BioCyc
-  threeHydroxyisobutyricAcidToPykfBiocyc(): Observable<any> {
-    return this.http.get<{result: any}>(
-      `${this.kgAPI}/shortest-path-queries/three-hydroxisobuteric-acid-to-pykf-biocyc`, {
-        ...this.getHttpOptions(true),
-      }
-    ).pipe(
-      map((resp: any) => resp.result),
-    );
-  }
-
-  // icd to rhsE
-  icdToRhse(): Observable<any> {
-    return this.http.get<{result: any}>(
-      `${this.kgAPI}/shortest-path-queries/icd-to-rhse`, {
-        ...this.getHttpOptions(true),
-      }
-    ).pipe(
-      map((resp: any) => resp.result),
-    );
-  }
-
-  // SIRT5 to NFE2L2 Using Literature Data
-  sirt5ToNfe2l2Literature(): Observable<any> {
-    return this.http.get<{result: any}>(
-      `${this.kgAPI}/shortest-path-queries/sirt5-to-nfe2l2-literature`, {
-        ...this.getHttpOptions(true),
-      }
-    ).pipe(
-      map((resp: any) => resp.result),
-    );
-  }
-
-  // CTNNB1 to Diarrhea Using Literature Data
-  ctnnb1ToDiarrheaLiterature(): Observable<any> {
-    return this.http.get<{result: any}>(
-      `${this.kgAPI}/shortest-path-queries/ctnnb1-to-diarrhea-literature`, {
-        ...this.getHttpOptions(true),
-      }
-    ).pipe(
-      map((resp: any) => resp.result),
-    );
-  }
-
-  // Two pathways using BioCyc
-  twoPathwaysBiocyc(): Observable<any> {
-    return this.http.get<{result: any}>(
-      `${this.kgAPI}/shortest-path-queries/two-pathways-biocyc`, {
+  getShortestPathQueryList(): Observable<any> {
+    return this.http.get<{result: Map<number, string>}>(
+      `${this.kgAPI}/shortest-path-query-list`, {
         ...this.getHttpOptions(true),
       }
     ).pipe(
