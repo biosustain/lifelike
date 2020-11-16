7c69040b578b936b397161092580dfa26e6ae20b
Albert Pham <the.sk89q@gmail.com>
2020-11-16 14:20:33 -0800

    'Upload URL' => 'External URL'. Fixes LL-2132
    Ticket: LL-2132

--- client/src/app/shared/components/sources/sources.component.ts
+++ client/src/app/shared/components/sources/sources.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Hyperlink } from 'app/drawing-tool/services/interfaces';
 
 @Component({
   selector: 'app-sources',
   templateUrl: './sources.component.html',
-  styleUrls: ['./sources.component.scss']
+  styleUrls: ['./sources.component.scss'],
 })
 export class SourcesComponent implements OnInit {
 
   @Input() links: Hyperlink[] | undefined;
   @Output() executeFileSource = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+  normalizeDomainDisplay(domain: string) {
+    if (domain === 'Upload URL') {
+      return 'External URL';
+    } else {
+      return domain;
+    }
+  }
+
 }
