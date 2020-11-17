a9827f2c17d18cc4616bdb721f7bb4493ab58bf6
Ethan Sanchez <ethan.dsanch@gmail.com>
2020-11-16 16:36:01 -0800

    Add api error handling to shortest path gui

--- client/src/app/shortest-path/components/route-builder.component.ts
+++ client/src/app/shortest-path/components/route-builder.component.ts
@@ -1,5 +1,9 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
+import { combineLatest, Subscription } from 'rxjs';
+
+import { BackgroundTask } from 'app/shared/rxjs/background-task';
+
 import { ShortestPathService } from '../services/shortest-path.service';
 
 import { DisplayType } from './route-display.component';
@@ -12,6 +16,9 @@ import { DisplayType } from './route-display.component';
 export class RouteBuilderComponent implements OnInit {
   @Output() loadNewQuery: EventEmitter<number>;
   @Output() changeDisplayType: EventEmitter<string>;
+
+  loadShortestPathQueries: BackgroundTask<[], any>;
+  queriesLoadedsub: Subscription;
 
   routeBuilderContainerClass: string;
 
@@ -29,15 +36,24 @@ export class RouteBuilderComponent implements OnInit {
 
     this.loadNewQuery = new EventEmitter<number>();
     this.changeDisplayType = new EventEmitter<DisplayType>();
+
+    this.loadShortestPathQueries = new BackgroundTask(() => {
+      return combineLatest(
+        this.shortestPathService.getShortestPathQueryList(),
+      );
+    });
+    this.queriesLoadedsub = this.loadShortestPathQueries.results$.subscribe(({
+      result: [shortestPathQueries],
+      value: [],
+    }) => {
+      this.queries = shortestPathQueries;
+    });
   }
 
   ngOnInit() {
     this.loadNewQuery.emit(0);
     this.changeDisplayType.emit('NETWORK');
-
-    this.shortestPathService.getShortestPathQueryList().subscribe(queryMap => {
-      this.queries = queryMap;
-    });
+    this.loadShortestPathQueries.update([]);
   }
 
   toggleRouteBuilderOpen() {
