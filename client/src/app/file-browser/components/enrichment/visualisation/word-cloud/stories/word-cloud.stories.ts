f24c215f6484f9c70c5bdd52070ce3e50bc82384
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2020-11-30 18:04:46 +0100

    clean word cloud abstract

---
+++ client/src/app/file-browser/components/enrichment/visualisation/word-cloud/stories/word-cloud.stories.ts
@@ -0,0 +1,57 @@
+import {moduleMetadata, Story} from '@storybook/angular';
+import {withKnobs} from '@storybook/addon-knobs';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {WordCloudComponent} from "../word-cloud.component";
+
+// This exports the Stories group for this component
+export default {
+  // The title defines the name and where in the structure of
+  // Storybook's menu this is going to be placed.
+  // Here we add it to a "Components" section under "Link"
+  title: 'Enrichment/Visualisation/WordCloudComponent',  // The component related to the Stories
+  component: WordCloudComponent, decorators: [
+    withKnobs,
+    // The necessary modules for the component to work on Storybook
+    moduleMetadata({
+      declarations: [
+        WordCloudComponent
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        NgbModule
+      ]
+    }),
+  ]
+};// This creates a Story for the component
+
+const Template: Story<WordCloudComponent> = (args) => ({
+  component: WordCloudComponent,
+  props: args,
+  template: `
+    <app-word-cloud
+        style='display:block;height:100vh;'
+        [data]="data"
+    >
+    </app-word-cloud>
+  `,
+});
+
+export const Default = Template.bind({});// Other stories could be added here as well, all you have to do is export them along!
+Default.args = {
+  data: [
+    {
+      text: "sfagrsg",
+      frequency: 1
+    },
+    {
+      text: "sfafwefwgrsg",
+      frequency: 16
+    },
+    {
+      // text: "Sdrgerg",
+      frequency: 12
+    }
+  ]
+};
