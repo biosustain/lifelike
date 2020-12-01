8329698907bec4d40865bdffc998e87bd55d3c7d
Albert Pham <the.sk89q@gmail.com>
2020-11-30 16:50:29 -0800

    Organize file navigator module.

--- client/src/app/file-navigator/file-navigator.module.ts
+++ client/src/app/file-navigator/file-navigator.module.ts
@@ -5,7 +5,7 @@ import { SharedModule } from 'app/shared/shared.module';
 import { WordCloudModule } from 'app/word-cloud/word-cloud.module';
 
 import { ObjectNavigatorComponent } from './components/object-navigator.component';
-import { AssociatedMapsComponent } from './components/associated-maps/associated-maps.component';
+import { AssociatedMapsComponent } from './components/associated-maps.component';
 import { FileBrowserModule } from '../file-browser/file-browser.module';
 
 
