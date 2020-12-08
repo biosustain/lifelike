ec0a0efd01aa81ba2dffcea7bdd7efe955349a13
Albert Pham <the.sk89q@gmail.com>
2020-12-08 15:08:57 -0800

    Add a warning if other users are editing a map. Fixes LL-2230

--- client/src/app/file-browser/services/filesystem.service.ts
+++ client/src/app/file-browser/services/filesystem.service.ts
@@ -7,22 +7,29 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ProgressDialog } from '../../shared/services/progress-dialog.service';
 import { ErrorHandler } from '../../shared/services/error-handler.service';
 import { ProjectPageService } from './project-page.service';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, throwError } from 'rxjs';
 import { PdfFile } from '../../interfaces/pdf-files.interface';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ApiService } from '../../shared/services/api.service';
+import { ResultList } from '../../shared/schemas/common';
+import { ObjectLockData } from '../schema';
+import { ObjectLock } from '../models/object-lock';
 
 @Injectable()
 export class FilesystemService {
   protected lmdbsDates = new BehaviorSubject<object>({});
 
-  constructor(private readonly filesService: PdfFilesService,
-              private readonly router: Router,
-              private readonly snackBar: MatSnackBar,
-              private readonly modalService: NgbModal,
-              private readonly progressDialog: ProgressDialog,
-              private readonly errorHandler: ErrorHandler,
-              private readonly route: ActivatedRoute,
-              private readonly projectPageService: ProjectPageService) {
+  constructor(protected readonly filesService: PdfFilesService,
+              protected readonly router: Router,
+              protected readonly snackBar: MatSnackBar,
+              protected readonly modalService: NgbModal,
+              protected readonly progressDialog: ProgressDialog,
+              protected readonly errorHandler: ErrorHandler,
+              protected readonly route: ActivatedRoute,
+              protected readonly projectPageService: ProjectPageService,
+              protected readonly http: HttpClient,
+              protected readonly apiService: ApiService) {
     this.filesService.getLMDBsDates().subscribe(lmdbsDates => {
       this.lmdbsDates.next(lmdbsDates);
     });
@@ -30,8 +37,8 @@ export class FilesystemService {
 
   get(locator: PathLocator): Observable<FilesystemObject> {
     return this.projectPageService.getDirectory(
-        locator.projectName,
-        locator.directoryId,
+      locator.projectName,
+      locator.directoryId,
     ).pipe(map(result => {
       const object = new FilesystemObject();
       object.type = 'dir';
@@ -79,14 +86,61 @@ export class FilesystemService {
 
   private generateTooltipContent(file: PdfFile): string {
     const outdated = Array
-        .from(Object.entries(this.lmdbsDates))
-        .filter(([, date]: [string, string]) => Date.parse(date) >= Date.parse(file.annotations_date));
+      .from(Object.entries(this.lmdbsDates))
+      .filter(([, date]: [string, string]) => Date.parse(date) >= Date.parse(file.annotations_date));
     if (outdated.length === 0) {
       return '';
     }
     return outdated.reduce(
-        (tooltip: string, [name, date]: [string, string]) => `${tooltip}\n- ${name}, ${new Date(date).toDateString()}`,
-        'Outdated:',
+      (tooltip: string, [name, date]: [string, string]) => `${tooltip}\n- ${name}, ${new Date(date).toDateString()}`,
+      'Outdated:',
     );
+  }
+
+  getLocks(hashId: string): Observable<ObjectLock[]> {
+    return this.http.get<ResultList<ObjectLockData>>(
+      `/api/filesystem/objects/${encodeURIComponent(hashId)}/locks`, {
+        ...this.apiService.getHttpOptions(true),
+      },
+    ).pipe(
+      map(data => {
+        return data.results.map(itemData => new ObjectLock().update(itemData));
+      }),
+    );
+  }
+
+  putLock(hashId: string, options: { own: true }): Observable<ObjectLock[]> {
+    return this.http.put<ResultList<ObjectLockData>>(
+      `/api/filesystem/objects/${encodeURIComponent(hashId)}/locks?own=true`,
+      {},
+      this.apiService.getHttpOptions(true),
+    ).pipe(
+      map(data => {
+        return data.results.map(itemData => new ObjectLock().update(itemData));
+      }),
+      catchError(e => {
+        if (e instanceof HttpErrorResponse) {
+          if (e.status === 409) {
+            const otherLocks = e.error.results.map(itemData => new ObjectLock().update(itemData));
+            return throwError(new LockError(otherLocks));
+          }
+        }
+
+        return throwError(e);
+      }),
+    );
+  }
+
+  deleteLock(hashId: string, options: { own: true }): Observable<any> {
+    return this.http.delete<unknown>(
+      `/api/filesystem/objects/${encodeURIComponent(hashId)}/locks?own=true`, {
+        ...this.apiService.getHttpOptions(true),
+      },
+    ).pipe(map(() => ({})));
+  }
+}
+
+export class LockError {
+  constructor(public readonly locks: ObjectLock[]) {
   }
 }
