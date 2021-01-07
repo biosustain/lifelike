916ebfa1eb64af7af87b5c12c8e898f81f23b6e0
Albert Pham <the.sk89q@gmail.com>
2021-01-07 14:35:25 -0800

    Abstract new file creation through ObjectTypeService.

--- client/src/app/file-browser/providers/directory-type-provider.ts
+++ client/src/app/file-browser/providers/directory-type-provider.ts
@@ -1,16 +1,24 @@
-import { ObjectTypeProvider } from '../services/object-type.service';
+import {
+  AbstractObjectTypeProvider,
+  CreateActionOptions,
+  CreateDialogAction,
+} from '../services/object-type.service';
 import { DIRECTORY_MIMETYPE, FilesystemObject } from '../models/filesystem-object';
 import { ComponentFactory, ComponentFactoryResolver, Injectable, Injector } from '@angular/core';
 import { FilesystemService } from '../services/filesystem.service';
 import { map } from 'rxjs/operators';
 import { DirectoryPreviewComponent } from '../components/directory-preview.component';
+import { RankedItem } from '../../shared/schemas/common';
+import { ObjectCreationService } from '../services/object-creation.service';
 
 @Injectable()
-export class DirectoryTypeProvider implements ObjectTypeProvider {
+export class DirectoryTypeProvider extends AbstractObjectTypeProvider {
 
   constructor(protected readonly componentFactoryResolver: ComponentFactoryResolver,
               protected readonly injector: Injector,
-              protected readonly filesystemService: FilesystemService) {
+              protected readonly filesystemService: FilesystemService,
+              protected readonly objectCreationService: ObjectCreationService) {
+    super();
   }
 
   handles(object: FilesystemObject): boolean {
@@ -26,6 +34,25 @@ export class DirectoryTypeProvider implements ObjectTypeProvider {
       instance.objects = newObject.children;
       return componentRef;
     }));
+  }
+
+  getCreateDialogOptions(): RankedItem<CreateDialogAction>[] {
+    return [{
+      rank: 100,
+      item: {
+        label: 'Folder',
+        openSuggested: false,
+        create: (options?: CreateActionOptions) => {
+          const object = new FilesystemObject();
+          object.filename = 'New Folder';
+          object.mimeType = DIRECTORY_MIMETYPE;
+          object.parent = options.parent;
+          return this.objectCreationService.openCreateDialog(object, {
+            title: 'New Folder',
+          });
+        },
+      },
+    }];
   }
 
 }
--- client/src/app/file-browser/services/object-type.service.ts
+++ client/src/app/file-browser/services/object-type.service.ts
@@ -1,21 +1,78 @@
 import { ComponentRef, Injectable, InjectionToken, Injector } from '@angular/core';
 import { FilesystemObject } from '../models/filesystem-object';
 import { Observable, of } from 'rxjs';
+import { RankedItem } from '../../shared/schemas/common';
+import { CreateDialogOptions } from './object-creation.service';
 
 export const TYPE_PROVIDER = new InjectionToken<ObjectTypeProvider[]>('objectTypeProvider');
 
+export interface CreateActionOptions {
+  parent?: FilesystemObject;
+  createDialog?: Omit<CreateDialogOptions, 'request'>;
+}
+
+export interface CreateDialogAction {
+  /**
+   * Whether the object should be opened afterwards.
+   */
+  openSuggested: boolean;
+  /**
+   * A description of the object type in Word Case.
+   */
+  label: string;
+
+  create(options?: CreateActionOptions): Promise<FilesystemObject>;
+}
+
 /**
- * A type provider provides services for different file types.
+ * A file type provider knows how to handle a certain or set of object types. Instances
+ * are used by the application to discover operations on objects stored within Lifelike.
  */
 export interface ObjectTypeProvider {
 
+  /**
+   * Test whether this provider is for the given type of object.
+   * @param object the object
+   */
   handles(object: FilesystemObject): boolean;
 
+  /**
+   * Create a component to preview the given object, although null can be returned
+   * for the observable if the file type cannot be previewed.
+   * @param object the object
+   */
   createPreviewComponent(object: FilesystemObject): Observable<ComponentRef<any> | undefined>;
+
+  /**
+   * Get a list of options for creating this type of file.
+   *
+   * @return a list of actions, with ranking, where the highest number ranks appear first
+   */
+  getCreateDialogOptions(): RankedItem<CreateDialogAction>[];
 
 }
 
-export class DefaultObjectTypeProvider implements ObjectTypeProvider {
+/**
+ * A base class for object type providers.
+ */
+export abstract class AbstractObjectTypeProvider implements ObjectTypeProvider {
+  abstract handles(object: FilesystemObject): boolean;
+
+  createPreviewComponent(object: FilesystemObject): Observable<ComponentRef<any> | undefined> {
+    return of(null);
+  }
+
+  getCreateDialogOptions(options?: CreateDialogOptions) {
+    return [];
+  }
+
+}
+
+/**
+ * A generic file type provider that is returned when we don't know what type of object
+ * it is or we don't support it.
+ */
+export class DefaultObjectTypeProvider extends AbstractObjectTypeProvider {
 
   handles(object: FilesystemObject): boolean {
     return true;
@@ -27,6 +84,9 @@ export class DefaultObjectTypeProvider implements ObjectTypeProvider {
 
 }
 
+/**
+ * The object type service returns object type providers for given objects.
+ */
 @Injectable()
 export class ObjectTypeService {
   private defaultProvider = new DefaultObjectTypeProvider();
@@ -34,6 +94,11 @@ export class ObjectTypeService {
   constructor(protected readonly injector: Injector) {
   }
 
+  /**
+   * Get the provider for the given file.
+   * @param object the object
+   * @return  a provider, which may be the default one
+   */
   get(object: FilesystemObject): Observable<ObjectTypeProvider> {
     const providers = this.injector.get(TYPE_PROVIDER);
     for (const provider of providers) {
@@ -42,6 +107,13 @@ export class ObjectTypeService {
       }
     }
     return of(this.defaultProvider);
+  }
+
+  /**
+   * Load all providers.
+   */
+  all(): Observable<ObjectTypeProvider[]> {
+    return of(this.injector.get(TYPE_PROVIDER));
   }
 
 }
