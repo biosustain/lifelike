f75c31c98d492f4b62de6c46cba66b038a1df901
Albert Pham <the.sk89q@gmail.com>
2021-01-08 13:08:37 -0800

    Update enrichment tables to use the new unified file schema. Fixes LL-2312

--- appserver/neo4japp/services/file_types/service.py
+++ appserver/neo4japp/services/file_types/service.py
@@ -10,11 +10,19 @@ class BaseFileTypeProvider:
     A file type provider knows how to handle a certain or set of file types. Instances
     are used by the application to discover operations on files stored within Lifelike.
     """
+
+    # The first entry in the mime_types tuple is the "canonical" mime type that will
+    # typically be used when storing the file in the database. Make sure all entries
+    # in this list are lowercase
     mime_types = ('application/octet-stream',)
 
     def handles(self, file: Files) -> bool:
         """
         Test whether this provider is for the given type of file.
+
+        Most implementations should just compare the file's mime type and generally you
+        should not override this method.
+
         :param file: the file
         :return: whether this provide should be used
         """
@@ -27,6 +35,13 @@ class BaseFileTypeProvider:
         indicate a higher confidence and negative numbers are supported, while
         a zero indicates a neutral position. If the provided buffer is
         definitely not of this file type, None should be returned.
+
+        This method is called when the user uploads a file from their computer and
+        we need to figure out what kind of file type it is. This method does not have
+        to be implemented by a file type, but that means that the user cannot upload
+        those types of files (as of writing) because there's no way to select the
+        file type when uploading a file (as of writing).
+
         :param buffer: the file buffer
         :return: a confidence level or None
         """
@@ -35,6 +50,11 @@ class BaseFileTypeProvider:
     def can_create(self) -> bool:
         """
         Returns whether this files of this type can be created or uploaded by the user.
+
+        This method exists because we may implement file types that can be created by
+        the system but cannot be created by the user. This method is called if the user
+        uploads a file (or hacks the API to create a new copy) of this format.
+
         :return: true if the type can be created
         """
         return False
@@ -43,17 +63,26 @@ class BaseFileTypeProvider:
         """
         Validate the contents of the given buffer to see if it is correct for
         this given file type.
+
+        You MUST validate the data if possible, especially if it's our
+        own format, especially if it's JSON that we generate!
+
         :param buffer: the file's contents
         :raises ValueError: raised if the content is invalid
         """
-        raise NotImplementedError()
+        # Be sure to implement JSON validation (if applicable)!
+        # See the map and enrichment table formats for examples
+        raise ValueError('format cannot be validated')
 
     def extract_doi(self, buffer: BytesIO) -> Optional[str]:
         """
         Attempt to extract a DOI from the file.
+
         :param buffer: the file's contents
         :return: a DOI string or None
         """
+        # In the PDF implementation as of writing, we do a regex on the file's
+        # contents to look for the DOI
         return None
 
     def to_indexable_content(self, buffer: BytesIO):
@@ -62,9 +91,13 @@ class BaseFileTypeProvider:
         some file formats, this operation may return a whole different type of file
         with just the keywords that need to be indexed from the original. By default,
         this method returns an empty byte buffer.
+
         :param buffer: the file's contents
         :return: a new file to be indexed
         """
+        # Files of this file type cannot be indexed until you override this method
+        # You can actually just return a blob of text (encoded in UTF-8)
+        # with all the relevant keywords
         return BytesIO()
 
     def should_highlight_content_text_matches(self) -> bool:
@@ -73,15 +106,19 @@ class BaseFileTypeProvider:
         to the user. For some formats, we may give Elasticsearch a file that can be indexed
         by Elasticsearch (in :func:`to_indexable_content()`) but may not be useful for
         display.
-        :return:
+
+        :return: whether highlights should be shown
         """
+        # If to_indexable_content() returns something like JSON, return False here because
+        # the highlights will look like garbage to the user
         return False
 
     def generate_export(self, file: Files, format: str) -> FileExport:
         """
         Generate an export for this file of the provided format. If the format is not
-        supported, then an exception should be raised. The .content field of the
-        provided file is available.
+        supported, then an exception should be raised. The file.content field of the
+        provided file is available and may (or may not) have been eager loaded.
+
         :param file: the file
         :param format: the format
         :return: an export
@@ -95,6 +132,8 @@ class DefaultFileTypeProvider(BaseFileTypeProvider):
     A generic file type provider that is returned when we don't know what
     type of file it is or we don't support it.
     """
+    mime_types = ('application/octet-stream',)
+
     def handles(self, file: Files) -> bool:
         return True
 
