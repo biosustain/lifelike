9a06ff82e69d40314ecd96d9e2fad03b5c4a0f58
David Bour <dbour.developer@gmail.com>
2021-01-11 15:59:41 -0800

    - refactor: remove the seed elastic and seed lmdb from the startup   script. We do this to since we will want to explicit when we want to   perform these actions such as only when the application is ran for the   first time.
    - refactor: re-added the step to add the parent LMDB directory for
      volume mounting.

--- appserver/bin/startup.sh
+++ appserver/bin/startup.sh
@@ -16,22 +16,10 @@ if [ "${FLASK_ENV}" = "development" ]; then
     ${__dir__}/dev-db-setup
     # Start in server directory
     cd "${__dir__}/.."
-    echo "### Downloading LMDB files & updating respective LMDB database tables ###"
-    # Load lmdb data
-    flask load-lmdb &
     # Mark ready
     touch .READY
-    echo "### Setting up elastic indices ###"
-    # Seed elastic indices
-    flask seed-elastic &
     flask run --host 0.0.0.0
 elif [ "${FLASK_ENV}" = "production" ]; then
-    echo "### Setting up production environment ###"
-    # Load lmdb data
-    flask load-lmdb &
-    echo "### Setting up elastic indices ###"
-    # Seed elastic indices
-    flask seed-elastic &
     gunicorn -b 0.0.0.0:5000 -w 4 app:app --timeout 1200
 else
     echo "No environment setup for ${FLASK_ENV}"
