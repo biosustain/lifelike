c564d5f30932df0ce7306b773d7c08c0a8bcb063
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-01-13 15:26:56 -0800

    Add snippet display to entity table view
    Still some UX questions that need to be answered here...It's not
    entirely clear how useful this feature will be when there are
    potentially thousands of associated entities of the given type, and then
    thousands of snippets for each entity.

--- appserver/neo4japp/request_schemas/visualizer.py
+++ appserver/neo4japp/request_schemas/visualizer.py
@@ -6,3 +6,10 @@ from neo4japp.database import ma
 class NodeAssociatedTypesRequest(ma.Schema):
     node_id = ma.Integer(required=True)
     to_label = ma.String(required=True)
+
+
+class GetSnippetsForNodePairRequest(ma.Schema):
+    from_id = ma.Integer(required=True)
+    to_id = ma.Integer(required=True)
+    page = ma.Integer(required=True)
+    limit = ma.Integer(required=True)
