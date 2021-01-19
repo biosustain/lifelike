048cf84bd5671c499c666cf22d124a24c9d06085
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-01-19 14:50:40 +0100

    reduce changes

--- appserver/neo4japp/blueprints/annotations.py
+++ appserver/neo4japp/blueprints/annotations.py
@@ -1,9 +1,9 @@
+import sqlalchemy as sa
+from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
 from enum import Enum
 from typing import Dict, List, Optional, Tuple
 
-import numpy as np
-import sqlalchemy as sa
 from flask import (
     Blueprint,
     current_app,
@@ -12,24 +12,23 @@ from flask import (
     request,
     jsonify,
 )
-from flask_apispec import use_kwargs
-from pandas import DataFrame
-from sqlalchemy import and_
-from sqlalchemy.exc import SQLAlchemyError
 
-import neo4japp.models.files_queries as files_queries
+from flask_apispec import use_kwargs
+
 from neo4japp.blueprints.auth import auth
 from neo4japp.blueprints.permissions import (
     requires_role,
     requires_project_permission
 )
 from neo4japp.constants import TIMEZONE
-from neo4japp.data_transfer_objects.common import ResultList
 from neo4japp.database import (
     db,
     get_excel_export_service,
     get_manual_annotation_service,
 )
+from neo4japp.data_transfer_objects.common import ResultList
+from neo4japp.models.files import FileAnnotationsVersion, AnnotationChangeCause
+from neo4japp.request_schemas.annotations import GlobalAnnotationsDeleteSchema
 from neo4japp.exceptions import (
     AnnotationError,
     RecordNotFoundException
@@ -43,9 +42,7 @@ from neo4japp.models import (
     Projects,
     FallbackOrganism
 )
-from neo4japp.models.files import FileAnnotationsVersion, AnnotationChangeCause
-from neo4japp.request_schemas.annotations import GlobalAnnotationsDeleteSchema
-from neo4japp.services.annotations import AnnotationGraphService
+import neo4japp.models.files_queries as files_queries
 from neo4japp.services.annotations.constants import (
     AnnotationMethod,
     EntityType,
@@ -62,15 +59,18 @@ from neo4japp.util import (
 )
 from neo4japp.utils.logger import UserEventLog
 
+from neo4japp.services.annotations import AnnotationGraphService
+
+
 bp = Blueprint('annotations', __name__, url_prefix='/annotations')
 
 
 def annotate(
-        doc: Files,
-        cause: AnnotationChangeCause,
-        specified_organism: Optional[FallbackOrganism] = None,
-        annotation_method: str = AnnotationMethod.RULES.value,  # default to Rules Based
-        user_id: int = None,
+    doc: Files,
+    cause: AnnotationChangeCause,
+    specified_organism: Optional[FallbackOrganism] = None,
+    annotation_method: str = AnnotationMethod.RULES.value,  # default to Rules Based
+    user_id: int = None,
 ) -> Tuple[Dict, Dict]:
     annotations_json = create_annotations(
         annotation_method=annotation_method,
@@ -146,7 +146,7 @@ class FrequencySA(SortedAnnotation):
     @staticmethod
     def get_annotations(annotation_service, project_id) -> Dict[Tuple[str, str, str], float]:
         combined_annotations = annotation_service.get_combined_annotations_in_project(project_id)
-        distinct_annotations = {}
+        distinct_annotations: Dict[Tuple[str, str, str], float] = {}
         for annotation in combined_annotations:
             annotation_data = (
                 annotation['meta']['id'],
@@ -187,7 +187,10 @@ class MannWhitneyUSA(SortedAnnotation):
         res2 = res.size()
         distinct_annotations = {}
         for annotation, group in res2.groupby('annotation'):
-            distinct_annotations[annotation] = -np.log(mannwhitneyu(group, res2[res2.index.get_level_values('annotation') != annotation]).pvalue)
+            distinct_annotations[annotation] = -np.log(mannwhitneyu(
+                group,
+                res2[res2.index.get_level_values('annotation') != annotation]
+            ).pvalue)
         return distinct_annotations
 
 
