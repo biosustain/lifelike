4c4a2b60cbe1bab8b4c8ecfce44abc969b70c8e8
Albert Pham <the.sk89q@gmail.com>
2021-01-21 16:27:15 -0800

    Add more get/patch file tests.

---
+++ appserver/tests/api/filesystem/conftest.py
@@ -0,0 +1,104 @@
+import pytest
+
+from neo4japp.models import AppUser, Projects, projects_collaborator_role, Files, FileContent
+from neo4japp.services import AccountService
+from neo4japp.services.file_types.providers import DirectoryTypeProvider, MapTypeProvider
+
+
+@pytest.fixture(scope='function')
+def login_password() -> str:
+    return 'some password'
+
+
+@pytest.fixture(scope='function')
+def project_owner_user(
+        request,
+        session,
+        account_user: AccountService,
+        login_password: str) -> AppUser:
+    user = AppUser(
+        username='project owner',
+        email=f'somebody@***ARANGO_DB_NAME***.bio',
+        first_name='joe',
+        last_name='taylor',
+    )
+    user.set_password(login_password)
+    session.add(user)
+    session.flush()
+    return user
+
+
+@pytest.fixture(scope='function')
+def project(
+        session,
+        project_owner_user: AppUser) -> Projects:
+    ***ARANGO_USERNAME***_dir = Files(
+        mime_type=DirectoryTypeProvider.MIME_TYPE,
+        filename='/',
+        user=project_owner_user,
+    )
+    project = Projects(
+        name='my-life-work',
+        description='random stuff',
+        ***ARANGO_USERNAME***=***ARANGO_USERNAME***_dir,
+    )
+    session.add(***ARANGO_USERNAME***_dir)
+    session.add(project)
+    session.flush()
+    return project
+
+
+@pytest.fixture(scope='function')
+def user_with_project_roles(
+        request,
+        session,
+        account_user: AccountService,
+        login_password: str,
+        project: Projects) -> AppUser:
+    user = AppUser(
+        username='user_with_project_roles',
+        email=f'somehow@***ARANGO_DB_NAME***.bio',
+        first_name='erica',
+        last_name='samuel',
+    )
+    user.set_password(login_password)
+    user.roles.extend([account_user.get_or_create_role(role_name)
+                       for role_name in request.param[0]])
+    session.add(user)
+    session.flush()
+
+    for role_name in request.param[1]:
+        session.execute(
+            projects_collaborator_role.insert(),
+            [{
+                'appuser_id': user.id,
+                'app_role_id': account_user.get_or_create_role(role_name).id,
+                'projects_id': project.id,
+            }]
+        )
+    session.flush()
+    return user
+
+
+@pytest.fixture(scope='function')
+def map_in_project(
+        request,
+        session,
+        project: Projects,
+        project_owner_user: AppUser) -> Files:
+    content = FileContent()
+    content.raw_file_utf8 = '{}'
+
+    file = Files(
+        mime_type=MapTypeProvider.MIME_TYPE,
+        filename='a map',
+        description='desc',
+        user=project_owner_user,
+        content=content,
+        parent=project.***ARANGO_USERNAME***,
+        **request.param,
+    )
+    session.add(content)
+    session.add(file)
+    session.flush()
+    return file
---
+++ appserver/tests/api/filesystem/objects_test.py
@@ -0,0 +1,269 @@
+import hashlib
+from urllib.parse import quote
+
+import pytest
+
+from neo4japp.models import AppUser, Files, Projects
+from tests.helpers.api import generate_jwt_headers
+from tests.helpers.assertions import assert_project_response, assert_file_response
+
+
+@pytest.mark.parametrize(
+    'map_in_project, user_with_project_roles, status_code, readable, writable, commentable', [
+        # Private file
+        (dict(public=False), ([], []), 403, False, False, False),
+        (dict(public=False), (['private-data-access'], []), 200, True, True, True),
+        (dict(public=False), ([], ['project-read']), 200, True, False, False),
+        (dict(public=False), ([], ['project-write']), 200, True, True, True),
+        (dict(public=False), ([], ['project-admin']), 200, True, True, True),
+        (dict(public=False), ([], ['project-write', 'project-admin']), 200, True, True, True),
+        (dict(public=False), ([], ['project-read', 'project-write']), 200, True, True, True),
+        (dict(public=False), ([], ['project-read', 'project-admin']), 200, True, True, True),
+        (dict(public=False), ([], ['project-read', 'project-write', 'project-admin']), 200, True,
+         True, True),
+        # Public file
+        (dict(public=True), ([], []), 200, True, False, False),
+        (dict(public=True), (['private-data-access'], []), 200, True, True, True),
+        (dict(public=True), ([], ['project-read']), 200, True, False, False),
+        (dict(public=True), ([], ['project-write']), 200, True, True, True),
+        (dict(public=True), ([], ['project-admin']), 200, True, True, True),
+        (dict(public=True), ([], ['project-write', 'project-admin']), 200, True, True, True),
+        (dict(public=True), ([], ['project-read', 'project-write']), 200, True, True, True),
+        (dict(public=True), ([], ['project-read', 'project-admin']), 200, True, True, True),
+        (dict(public=True), ([], ['project-read', 'project-write', 'project-admin']), 200, True,
+         True, True),
+    ],
+    indirect=['map_in_project', 'user_with_project_roles'],
+    ids=str,
+)
+def test_get_file(
+        client,
+        login_password: str,
+        user_with_project_roles: AppUser,
+        status_code: int,
+        readable: bool,
+        writable: bool,
+        commentable: bool,
+        project: Projects,
+        map_in_project: Files):
+    login_resp = client.login_as_user(user_with_project_roles.email, login_password)
+    headers = generate_jwt_headers(login_resp['access_jwt'])
+
+    resp = client.get(
+        f'/filesystem/objects/{quote(map_in_project.hash_id)}',
+        headers=headers,
+        content_type='application/json'
+    )
+
+    assert resp.status_code == status_code
+
+    if status_code == 200:
+        resp_data = resp.get_json()
+        resp_file = resp_data['result']
+        assert_file_response(resp_file, map_in_project)
+        assert_project_response(resp_file['project'], project)
+        assert resp_file['privileges']['readable'] is readable
+        assert resp_file['privileges']['writable'] is writable
+        assert resp_file['privileges']['commentable'] is commentable
+
+
+@pytest.mark.parametrize(
+    'user_with_project_roles', [
+        ([], []),
+    ],
+    indirect=['user_with_project_roles'],
+    ids=str,
+)
+def test_get_file_missing(
+        client,
+        login_password: str,
+        user_with_project_roles: AppUser):
+    login_resp = client.login_as_user(user_with_project_roles.email, login_password)
+    headers = generate_jwt_headers(login_resp['access_jwt'])
+
+    resp = client.get(
+        f'/filesystem/objects/test_get_file_missing',
+        headers=headers,
+        content_type='application/json'
+    )
+
+    assert resp.status_code == 404
+
+
+@pytest.mark.parametrize(
+    'user_with_project_roles, status_code', [
+        (([], []), 403),
+        ((['private-data-access'], []), 200),
+        (([], ['project-read']), 403),
+        (([], ['project-write']), 200),
+        (([], ['project-admin']), 200),
+        (([], ['project-write', 'project-admin']), 200),
+        (([], ['project-read', 'project-write']), 200),
+        (([], ['project-read', 'project-admin']), 200),
+        (([], ['project-read', 'project-write', 'project-admin']), 200),
+    ],
+    indirect=['user_with_project_roles'],
+    ids=str
+)
+@pytest.mark.parametrize(
+    'map_in_project', [
+        dict(public=False),
+        dict(public=True),
+    ],
+    indirect=['map_in_project'],
+    ids=str,
+)
+def test_patch_file_permitted(
+        request,
+        session,
+        client,
+        login_password: str,
+        user_with_project_roles: AppUser,
+        status_code: int,
+        map_in_project: Files):
+    login_resp = client.login_as_user(user_with_project_roles.email, login_password)
+    headers = generate_jwt_headers(login_resp['access_jwt'])
+
+    original_filename = session.query(Files.filename) \
+        .filter(Files.id == map_in_project.id) \
+        .one()[0]
+    new_filename = hashlib.sha1(request.node.name.encode('utf-8')).hexdigest()
+    assert original_filename != new_filename
+
+    resp = client.patch(
+        f'/filesystem/objects/{quote(map_in_project.hash_id)}',
+        headers=headers,
+        json={
+            'filename': new_filename,
+        },
+    )
+
+    assert resp.status_code == status_code
+
+    # Let's make sure the database did or did not change!
+    updated_filename = session.query(Files.filename) \
+        .filter(Files.id == map_in_project.id) \
+        .one()[0]
+    if status_code == 200:
+        assert updated_filename == new_filename
+    else:
+        assert updated_filename != new_filename
+
+
+@pytest.mark.parametrize(
+    'user_with_project_roles', [
+        ([], []),
+    ],
+    indirect=['user_with_project_roles'],
+    ids=str,
+)
+def test_patch_file_missing(
+        client,
+        login_password: str,
+        user_with_project_roles: AppUser):
+    login_resp = client.login_as_user(user_with_project_roles.email, login_password)
+    headers = generate_jwt_headers(login_resp['access_jwt'])
+
+    resp = client.patch(
+        f'/filesystem/objects/test_get_file_missing',
+        headers=headers,
+        json={
+            'filename': 'whatever, man',
+        },
+    )
+
+    assert resp.status_code == 404
+
+
+@pytest.mark.parametrize(
+    'user_with_project_roles, status_code', [
+        (([], []), 403),
+        ((['private-data-access'], []), 200),
+        (([], ['project-read']), 403),
+        (([], ['project-write']), 200),
+        (([], ['project-admin']), 200),
+        (([], ['project-write', 'project-admin']), 200),
+        (([], ['project-read', 'project-write']), 200),
+        (([], ['project-read', 'project-admin']), 200),
+        (([], ['project-read', 'project-write', 'project-admin']), 200),
+    ],
+    indirect=['user_with_project_roles'],
+    ids=str
+)
+@pytest.mark.parametrize(
+    'map_in_project', [
+        dict(public=False),
+        dict(public=True),
+    ],
+    indirect=['map_in_project'],
+    ids=str,
+)
+def test_bulk_patch_files_permitted(
+        request,
+        session,
+        client,
+        login_password: str,
+        user_with_project_roles: AppUser,
+        status_code: int,
+        map_in_project: Files):
+    login_resp = client.login_as_user(user_with_project_roles.email, login_password)
+    headers = generate_jwt_headers(login_resp['access_jwt'])
+
+    original_filename = session.query(Files.filename) \
+        .filter(Files.id == map_in_project.id) \
+        .one()[0]
+    new_filename = hashlib.sha1(request.node.name.encode('utf-8')).hexdigest()
+    assert original_filename != new_filename
+
+    resp = client.patch(
+        f'/filesystem/objects',
+        headers=headers,
+        json={
+            'hashIds': [
+                map_in_project.hash_id,
+            ],
+            'filename': new_filename,
+        },
+    )
+
+    assert resp.status_code == status_code
+
+    updated_filename = session.query(Files.filename) \
+        .filter(Files.id == map_in_project.id) \
+        .one()[0]
+    if status_code == 200:
+        assert updated_filename == new_filename
+    else:
+        assert updated_filename != new_filename
+
+
+@pytest.mark.parametrize(
+    'user_with_project_roles', [
+        ([], []),
+    ],
+    indirect=['user_with_project_roles'],
+    ids=str,
+)
+def test_bulk_patch_files_missing(
+        client,
+        login_password: str,
+        user_with_project_roles: AppUser):
+    login_resp = client.login_as_user(user_with_project_roles.email, login_password)
+    headers = generate_jwt_headers(login_resp['access_jwt'])
+
+    resp = client.patch(
+        f'/filesystem/objects',
+        headers=headers,
+        json={
+            'hashIds': [
+                'test_bulk_patch_files_missing',
+            ],
+            'filename': 'punk isn\'t dead.gif',
+        },
+    )
+
+    assert resp.status_code == 200
+
+    resp_data = resp.get_json()
+    assert resp_data['mapping'] == dict()
+    assert resp_data['missing'] == ['test_bulk_patch_files_missing']
