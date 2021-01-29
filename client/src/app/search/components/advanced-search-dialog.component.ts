0924274e6b5390e69f1ace5b6bf60ba716676d86
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-01-29 11:00:16 -0800

    Update advanced search component layout

--- client/src/app/search/components/advanced-search-dialog.component.ts
+++ client/src/app/search/components/advanced-search-dialog.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { ContentSearchOptions, TYPES } from '../content-search';
+import { SearchType } from '../shared';
 
 @Component({
   selector: 'app-advanced-search-dialog',
@@ -8,6 +12,20 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./advanced-search-dialog.component.scss']
 })
 export class AdvancedSearchDialogComponent {
+  @Input() set params(params: ContentSearchOptions) {
+    this.form.setValue({
+      ...this.form.value,
+      q: params.q,
+      types: params.types,
+    });
+  }
+
+  typeChoices: SearchType[] = TYPES.concat().sort((a, b) => a.name.localeCompare(b.name));
+
+  form = new FormGroup({
+    q: new FormControl('', [Validators.required, this.whitespaceValidator]),
+    types: new FormControl([]),
+  });
 
   constructor(
     private readonly modal: NgbActiveModal,
@@ -19,7 +37,19 @@ export class AdvancedSearchDialogComponent {
   }
 
   close() {
-    this.modal.close();
+    if (this.form.valid) {
+      this.modal.close(this.form.value);
+    } else {
+      this.form.markAsDirty();
+    }
   }
 
+  choiceLabel(choice) {
+    return choice.name;
+  }
+
+  whitespaceValidator(control: AbstractControl): {[key: string]: any} | null {
+    const val =  control.value as string;
+    return val.length > 0 && val.match(/.*\S.*/) === null ? {whitespace: {value: control.value}} : null;
+  }
 }
