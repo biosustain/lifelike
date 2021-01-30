487885b18a88d9ddff93d42bb7f5893b0057a42a
David Bour <dbour.developer@gmail.com>
2021-01-29 17:28:36 -0800

    - chore: add API unit tests for logging feature

--- appserver/neo4japp/schemas/errors.py
+++ appserver/neo4japp/schemas/errors.py
@@ -3,10 +3,10 @@ from neo4japp.schemas.base import CamelCaseSchema
 
 
 class ClientErrorSchema(CamelCaseSchema):
-    title = fields.String()
+    title = fields.String(required=True)
     message = fields.String()
     detail = fields.String(allow_none=True)
-    transaction_id = fields.String()
+    transaction_id = fields.String(required=True)
     url = fields.String()
     label = fields.String()
     expected = fields.Boolean()
