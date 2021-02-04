aa6d131d529f4b3440fddf880d6c391638b278da
Albert Pham <the.sk89q@gmail.com>
2021-02-04 15:41:52 -0800

    Catch object creation error.

--- client/src/app/file-browser/components/object-browser.component.ts
+++ client/src/app/file-browser/components/object-browser.component.ts
@@ -228,8 +228,9 @@ export class ObjectBrowserComponent implements OnInit, OnDestroy {
           newTab: true,
         });
       }
-    }, () => {
-    });
+    }, this.errorHandler.createCallback({
+      label: 'Create action',
+    }));
   }
 
 }
