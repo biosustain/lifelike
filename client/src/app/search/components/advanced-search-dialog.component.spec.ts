b32bbaca5707004c1d3a051cff4abcf626797d61
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-02-05 11:51:27 -0800

    Fix test issues

--- client/src/app/search/components/advanced-search-dialog.component.spec.ts
+++ client/src/app/search/components/advanced-search-dialog.component.spec.ts
@@ -1,11 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
 import { configureTestSuite } from 'ng-bullet';
 
+import { RootStoreModule } from 'app/root-store';
 import { SharedModule } from 'app/shared/shared.module';
 
 import { AdvancedSearchDialogComponent } from './advanced-search-dialog.component';
+import { ContentSearchService } from '../services/content-search.service';
 
 describe('AdvancedSearchDialogComponent', () => {
   let component: AdvancedSearchDialogComponent;
@@ -14,10 +18,15 @@ describe('AdvancedSearchDialogComponent', () => {
   configureTestSuite(() => {
     TestBed.configureTestingModule({
       imports: [
+        RootStoreModule,
         SharedModule,
         BrowserAnimationsModule,
       ],
-      declarations: [ AdvancedSearchDialogComponent ]
+      declarations: [ AdvancedSearchDialogComponent ],
+      providers: [
+        ContentSearchService,
+        NgbActiveModal,
+      ]
     });
   });
 
