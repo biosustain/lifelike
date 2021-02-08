b9f725f7d4fcc1f51b1f1906bbef89560767f2c3
Albert Pham <the.sk89q@gmail.com>
2021-02-08 14:05:08 -0800

    Add user name to file annotation history. Fixes LL-2251

--- client/src/app/file-browser/models/file-annotation-history.ts
+++ client/src/app/file-browser/models/file-annotation-history.ts
@@ -9,6 +9,7 @@ import { CollectionModel } from '../../shared/utils/collection-model';
 import { AnnotationChangeExclusionMeta, Meta } from '../../pdf-viewer/annotation-type';
 import { startCase } from 'lodash';
 import { ModelList } from '../../shared/models';
+import { AppUser } from '../../interfaces';
 
 class AnnotationChange {
   action: 'added' | 'removed';
@@ -57,6 +58,7 @@ export class AnnotationExclusionChange extends AnnotationChange {
  */
 export class FileAnnotationChange {
   date: string;
+  user: AppUser;
   cause: 'user' | 'user_reannotation' | 'sys_reannotation';
   inclusionChanges: AnnotationInclusionChange[];
   exclusionChanges: AnnotationExclusionChange[];
@@ -77,8 +79,15 @@ export class FileAnnotationChange {
     }
   }
 
+  get isReannotation() {
+    // Use type checker to catch added types
+    const cause: 'user' | 'user_reannotation' | 'sys_reannotation' = this.cause;
+    return cause === 'user_reannotation' || cause === 'sys_reannotation';
+  }
+
   update(data: FileAnnotationChangeData): FileAnnotationChange {
     this.date = data.date;
+    this.user = data.user;
     this.cause = data.cause;
     this.inclusionChanges = data.inclusionChanges.map(
       itemData => new AnnotationInclusionChange().update(itemData));
