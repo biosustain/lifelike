9e31ac6165425bc97c27518f1cc2f27dc920935f
Albert Pham <the.sk89q@gmail.com>
2021-02-16 16:52:47 -0800

    Global error handler should log the error but not show it (causes double errors). Fixes LL-2534

--- client/src/app/global-error-handler.ts
+++ client/src/app/global-error-handler.ts
@@ -8,14 +8,18 @@ import { ErrorHandler as ErrorHandlerService } from 'app/shared/services/error-h
  * errors that only occur client side.
  */
 export class GlobalErrorHandler implements ErrorHandler {
-    constructor(private errorHandlerService: ErrorHandlerService) {}
+  constructor(private errorHandlerService: ErrorHandlerService) {
+  }
 
-    // Used to prevent error dialogs for specific HTTP codes
-    KNOWN_HTTP_ERROR_CODES = [0, 401];
+  // Used to prevent error dialogs for specific HTTP codes
+  KNOWN_HTTP_ERROR_CODES = [0, 401];
 
-    handleError(error: Error | HttpErrorResponse) {
-        if (!(error instanceof HttpErrorResponse && this.KNOWN_HTTP_ERROR_CODES.includes(error.status))) {
-            this.errorHandlerService.showError(error, {label: 'Uncaught exception', expected: false});
-        }
+  handleError(error: Error | HttpErrorResponse) {
+    if (!(error instanceof HttpErrorResponse && this.KNOWN_HTTP_ERROR_CODES.includes(error.status))) {
+      this.errorHandlerService.logError(error, {label: 'Uncaught exception', expected: false});
+
+      // We are logging the error but are not changing how errors are handled
+      throw error;
     }
+  }
 }
