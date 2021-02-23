b1db1715ce72391c808f347e7d063e27d9182854
Albert Pham <the.sk89q@gmail.com>
2021-02-23 12:52:44 -0800

    Try injecting the error handler later.

--- client/src/app/global-error-handler.ts
+++ client/src/app/global-error-handler.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { ErrorHandler, Injectable } from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { ErrorHandler as ErrorHandlerService } from 'app/shared/services/error-handler.service';
 
 @Injectable()
@@ -8,16 +8,18 @@ import { ErrorHandler as ErrorHandlerService } from 'app/shared/services/error-h
  * errors that only occur client side.
  */
 export class GlobalErrorHandler implements ErrorHandler {
-  constructor(private errorHandlerService: ErrorHandlerService) {
+  constructor(protected readonly errorHandlerService: ErrorHandlerService,
+              protected readonly injector: Injector) {
   }
 
   // Used to prevent error dialogs for specific HTTP codes
   KNOWN_HTTP_ERROR_CODES = [0, 401];
 
   handleError(error: Error | HttpErrorResponse) {
+    const errorHandlerService = this.injector.get(ErrorHandlerService);
     try {
       if (!(error instanceof HttpErrorResponse && this.KNOWN_HTTP_ERROR_CODES.includes(error.status))) {
-        this.errorHandlerService.logError(error, {label: 'Uncaught exception', expected: false});
+        errorHandlerService.logError(error, {label: 'Uncaught exception', expected: false});
       }
     } catch (e) {
       console.error('Failed to log Lifelike error', e);
