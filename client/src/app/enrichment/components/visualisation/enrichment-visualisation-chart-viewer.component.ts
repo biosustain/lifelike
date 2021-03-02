f102fb8dece339890b02afa6367073ac2aec85d4
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-03-02 14:44:31 +0100

    added missing tooltips in graphs

--- client/src/app/enrichment/components/visualisation/enrichment-visualisation-chart-viewer.component.ts
+++ client/src/app/enrichment/components/visualisation/enrichment-visualisation-chart-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,9 +6,23 @@ import { Component, Input } from '@angular/core';
   templateUrl: './enrichment-visualisation-chart-viewer.component.html',
   styleUrls: ['./enrichment-visualisation-viewer.component.scss'],
 })
-export class EnrichmentVisualisationChartViewerComponent {
+export class EnrichmentVisualisationChartViewerComponent implements OnInit, OnChanges {
   @Input() data: { gene: string, 'p-value': number }[];
   @Input() showMore;
+
+  slicedData;
+
+  slice() {
+    this.slicedData = this.showMore ? this.data.slice(0, 50) : this.data.slice(0, 25);
+  }
+
+  ngOnInit() {
+    this.slice();
+  }
+
+  ngOnChanges(change) {
+    this.slice();
+  }
 }
 
 export interface EnrichmentVisualisationData {
