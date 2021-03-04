0ffffe14b2713a37ae410ea2e22587abbf347d2b
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-03-04 18:40:15 +0100

    cleanup

--- .run/prep_for_debuging.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker-compose build appserver
-docker-compose -f /Users/dommas/Projects/kg-prototypes/docker-compose.yml -f /Users/dommas/Projects/kg-prototypes/docker-compose.override.yml -f /Users/dommas/Projects/kg-prototypes/.run/assets/docker-compose.yml -f /Users/dommas/Library/Caches/JetBrains/PyCharm2020.3/tmp/docker-compose.override.3.yml build builder appserver
