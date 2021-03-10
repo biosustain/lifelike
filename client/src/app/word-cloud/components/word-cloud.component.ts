201f99c1832048ff034a70c6f7514f383041fe73
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-03-10 13:26:09 +0100

    missed import

--- client/src/app/word-cloud/components/word-cloud.component.ts
+++ client/src/app/word-cloud/components/word-cloud.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnDestroy, Output, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output, ViewEncapsulation, OnInit } from '@angular/core';
 
 import { uniqueId } from 'lodash';
 
