b52de335c7bcba362d1f51e369208c65059f7bcc
David Bour <dbour.developer@gmail.com>
2021-03-15 09:21:43 -0700

    - refactor: update the SSL check in the redis connection

--- appserver/neo4japp/database.py
+++ appserver/neo4japp/database.py
@@ -61,7 +61,7 @@ def connect_to_neo4j():
             name=current_app.config.get('NEO4J_DATABASE'),
             host=current_app.config.get('NEO4J_HOST'),
             auth=current_app.config.get('NEO4J_AUTH').split('/'),
-            secure=secure.find('s') > -1,  # Any 's' will be considered a secure protocol
+            secure=secure in ['bolts', 'bolt+s', 'bolt+ssc', 'https', 'http+s', 'http+ssc'],  # Any 's' will be considered a secure protocol
             port=current_app.config.get('NEO4J_PORT'),
             scheme=current_app.config.get('NEO4J_SCHEME')
         )
