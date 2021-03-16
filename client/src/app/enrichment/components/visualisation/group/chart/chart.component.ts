bc5983c753c426be4dd7f076a3b768b114b31924
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-03-16 20:45:15 +0100

    covered rest of the tasks

--- client/src/app/enrichment/components/visualisation/group/chart/chart.component.ts
+++ client/src/app/enrichment/components/visualisation/group/chart/chart.component.ts
@@ -21,40 +21,24 @@ export class ChartComponent implements OnInit, OnChanges {
     responsive: true,
     maintainAspectRatio: false,
     scales: {
-      xAxes: [
-        {
-          ticks: {
-            suggestedMin: 0,
-            stepSize: 1,
-            // callback: value => value
-          },
-          gridLines: {
-            drawOnChartArea: false
-          },
-          offset: true,
-          type: 'logarithmic',
-          scaleLabel: {
-            display: true,
-            labelString: '-log10 p-value'
-          }
+    xAxes: [
+      {
+        ticks: {
+          suggestedMin: 0,
+          stepSize: 1,
+          // callback: value => value
+        },
+        gridLines: {
+          drawOnChartArea: false
+        },
+        offset: true,
+        type: 'logarithmic',
+        scaleLabel: {
+          display: true,
+          labelString: '-log10 p-value'
         }
-      ],
-      yAxes: [
-        {
-          ticks: {
-            reverse: true,
-            // suggestedMin: -0.5,
-            beginAtZero: true,
-            stepSize: 1,
-            callback: (value, index) =>
-              index in this.slicedData ? this.slicedData[value].gene : '',
-          },
-          offset: true,
-          gridLines: {
-            drawOnChartArea: false
-          }
-        }
-      ]
+      }
+    ],
     },
     plugins: {
       // Change options for ALL labels of THIS CHART
@@ -72,22 +56,22 @@ export class ChartComponent implements OnInit, OnChanges {
       }
     }
   };
-  public chartType: ChartType = 'bubble';
+  public chartType: ChartType = 'horizontalBar';
   legend = false;
   public chartData = [];
   @Input() showMore;
   @Input() data;
 
   slicedData;
+  labels;
 
   parseData() {
-    const data = this.showMore ? this.data.slice(0, 50) : this.data.slice(0, 25);
+    const data = this.showMore ? this.data.slice(0, 50) : this.data.slice(0, 10);
     this.slicedData = data.map((d: any, i) => ({
       ...d,
-      x: -Math.log10(d['p-value']),
-      y: i,
-      // r: 3.75 + 3.75 * Math.log10(d["geneNames"].length),
+      x: -Math.log10(d['p-value'])
     }));
+    this.labels = data.map(({gene})=>gene);
   }
 
   ngOnInit() {
