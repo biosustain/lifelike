bb20adb820dc407a4223a88a5660affb6a333f97
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-03-18 12:55:17 +0100

    whole process without catching in 4.5s - not so bad

--- client/src/app/enrichment/components/visualisation/word-cloud/cloud-viewer.component.ts
+++ client/src/app/enrichment/components/visualisation/word-cloud/cloud-viewer.component.ts
@@ -12,33 +12,11 @@ import { BackgroundTask } from '../../../../shared/rxjs/background-task';
   selector: 'app-cloud-viewer',
   templateUrl: './cloud-viewer.component.html'
 })
-export class CloudViewerComponent implements OnInit, OnChanges {
+export class CloudViewerComponent {
   loadingData: boolean;
 
   data: { text: any; frequency: any }[] = [];
 
   loadTask: BackgroundTask<string, any>;
   loadSubscription: Subscription;
-
-  constructor(protected readonly enrichmentService: EnrichmentVisualisationService) {
-    this.loadTask = new BackgroundTask(() =>
-      this.enrichmentService.getGOSignificance(),
-    );
-
-    this.loadSubscription = this.loadTask.results$.subscribe((result) => {
-      // tslint:disable-next-line:no-string-literal
-      this.data = result.result.map(d => ({text: d['gene'], frequency: d['n_related_GO_terms']}));
-      this.loadingData = false;
-    });
-  }
-
-  ngOnInit() {
-    this.loadingData = true;
-    this.loadTask.update();
-  }
-
-  ngOnChanges() {
-    this.loadingData = true;
-    this.loadTask.update();
-  }
 }
