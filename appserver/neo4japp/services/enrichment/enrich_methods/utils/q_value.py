7c48039ca1ae260f98a9099a4e3a4711832aefdb
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-03-18 15:39:33 +0100

    polishments

--- appserver/neo4japp/services/enrichment/enrich_methods/utils/q_value.py
+++ appserver/neo4japp/services/enrichment/enrich_methods/utils/q_value.py
@@ -2,6 +2,7 @@ from statsmodels.stats.multitest import fdrcorrection
 import pandas as pd
 import numpy as np
 
+
 def add_q_value(df, related_go_terms_count, inplace=True):
     p_values = df['p-value']
     extended_p_values = p_values.append(
