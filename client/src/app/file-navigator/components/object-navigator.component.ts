75f15747cf684037d92fd2a68df87242e4ad2db8
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-03-22 16:59:50 -0700

    Add enrichment table find from word cloud

--- client/src/app/file-navigator/components/object-navigator.component.ts
+++ client/src/app/file-navigator/components/object-navigator.component.ts
@@ -1,16 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { combineLatest, Subscription } from 'rxjs';
-import { BackgroundTask } from 'app/shared/rxjs/background-task';
-import { FilesystemObject} from '../../file-browser/models/filesystem-object';
-import { FilesystemService } from '../../file-browser/services/filesystem.service';
-import { WordCloudAnnotationFilterEntity } from '../../interfaces/annotation-filter.interface';
-import { WorkspaceManager } from '../../shared/workspace-manager';
 import { escapeRegExp } from 'lodash';
-import { FileViewComponent } from '../../pdf-viewer/components/file-view.component';
-import { ModuleAwareComponent, ModuleProperties } from '../../shared/modules';
-import { PDF_MIMETYPE } from '../../pdf-viewer/providers/pdf-type-provider';
+
+import { combineLatest, Subscription } from 'rxjs';
+
+import { EnrichmentTableViewerComponent } from 'app/enrichment/components/table/enrichment-table-viewer.component';
+import { ENRICHMENT_TABLE_MIMETYPE } from 'app/enrichment/providers/enrichment-table.type-provider';
+import { FilesystemObject} from 'app/file-browser/models/filesystem-object';
+import { FilesystemService } from 'app/file-browser/services/filesystem.service';
+import { WordCloudAnnotationFilterEntity } from 'app/interfaces/annotation-filter.interface';
+import { FileViewComponent } from 'app/pdf-viewer/components/file-view.component';
+import { PDF_MIMETYPE } from 'app/pdf-viewer/providers/pdf-type-provider';
+import { ModuleAwareComponent, ModuleProperties } from 'app/shared/modules';
+import { BackgroundTask } from 'app/shared/rxjs/background-task';
+import { WorkspaceManager } from 'app/shared/workspace-manager';
+
 
 @Component({
   selector: 'app-object-navigator',
@@ -60,6 +65,24 @@ export class ObjectNavigatorComponent implements ModuleAwareComponent {
           shouldReplaceTab: component => {
             const fileViewComponent = component as FileViewComponent;
             fileViewComponent.highlightAnnotation(annotation.id);
+            return false;
+          },
+        },
+      );
+    } else if (this.object.mimeType === ENRICHMENT_TABLE_MIMETYPE) {
+      const query = useKeyword ? annotation.keyword : annotation.primaryName;
+      const url = this.object.getURL();
+      this.workspaceManager.navigateByUrl(
+        `${url}#query=${encodeURIComponent(query)}`, {
+          newTab: true,
+          sideBySide: true,
+          matchExistingTab: `^/*${escapeRegExp(url)}.*`,
+          shouldReplaceTab: component => {
+            const enrichmentTableViewerComponent = component as EnrichmentTableViewerComponent;
+            enrichmentTableViewerComponent.findController.query = query;
+            // If the tab is already open and loaded, execute the find. If it's not loaded, the enrichment table viewer will need to figure
+            // out when to actually start the find.
+            enrichmentTableViewerComponent.findController.nextOrStart();
             return false;
           },
         },
