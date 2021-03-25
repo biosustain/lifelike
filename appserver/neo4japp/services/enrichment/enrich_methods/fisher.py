f341897aaad27b5c9c58254df47f7132d1ab1976
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-03-25 13:16:56 +0100

    done

--- appserver/neo4japp/services/enrichment/enrich_methods/fisher.py
+++ appserver/neo4japp/services/enrichment/enrich_methods/fisher.py
@@ -1,7 +1,9 @@
-import numpy as np
+from time import time
+
 import pandas as pd
-from .utils.q_value import add_q_value
 from scipy.stats.distributions import hypergeom
+
+from .utils.q_value import add_q_value
 
 
 def fisher_p(k, M, n, N):
@@ -26,14 +28,21 @@ def fisher(geneNames, GOterms, related_go_terms_count):
     M = df["geneNames"].explode().nunique()
     N = len(query)
 
+    fisher_time = 0
+
     def f(go):
         matching_gene_names = list(set(go["geneNames"]).intersection(query))
+        s = time()
         go['p-value'] = fisher_p(len(matching_gene_names), M, len(go["geneNames"]), N)
+        nonlocal fisher_time
+        fisher_time += time() - s
         go['gene'] = f"{go['goTerm']} ({go['goId']})"
         go["geneNames"] = matching_gene_names
         return go
 
     df = df.apply(f, axis=1).sort_values(by='p-value')
+    print(f"\t\tFisher calls took: {fisher_time}s")
+    print(f"\t\tOne term fisher computation took on average: {fisher_time / len(df)}s")
 
     add_q_value(df, related_go_terms_count)
     return df.to_json(orient='records')
