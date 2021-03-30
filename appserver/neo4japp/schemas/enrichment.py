e8b6984200edbfde213a2fe170bd7fb517a7af63
Binh Vu <binhvu.dev@gmail.com>
2021-03-29 20:48:37 -0700

    Removed extra API call to the KG to create the enrichment JSON.
    The JSON is already saved in file_content table, so no need to recreate it. Also changed the edit parameters to
    save instead of refreshing. This will allow the file to update the file_content. The downside is the user
    will now have to manually click refresh data. Also moved all the enrichment text mapping work to the backend.

--- appserver/neo4japp/schemas/enrichment.py
+++ appserver/neo4japp/schemas/enrichment.py
@@ -5,17 +5,17 @@ from neo4japp.schemas.base import CamelCaseSchema
 
 class EnrichmentValue(CamelCaseSchema):
     text = fields.String(required=True)
-    annotated_text = fields.String(allow_none=True)
+    annotatedText = fields.String(allow_none=True)
     link = fields.String(required=True)
 
 
 class EnrichedGene(CamelCaseSchema):
     imported = fields.String(allow_none=True)
     matched = fields.String(allow_none=True)
-    full_name = fields.String(allow_none=True)
-    annotated_imported = fields.String(allow_none=True)
-    annotated_matched = fields.String(allow_none=True)
-    annotated_full_name = fields.String(allow_none=True)
+    fullName = fields.String(allow_none=True)
+    annotatedImported = fields.String(allow_none=True)
+    annotatedMatched = fields.String(allow_none=True)
+    annotatedFullName = fields.String(allow_none=True)
     link = fields.String(allow_none=True)
     domains = fields.Dict(
         keys=fields.String(), values=fields.Dict(
@@ -26,21 +26,16 @@ class DomainInfo(CamelCaseSchema):
     labels = fields.List(fields.String())
 
 
-class EnrichmentTextMapping(CamelCaseSchema):
-    text = fields.String(required=True)
-    row = fields.Integer(required=True)
-    domain = fields.String(allow_none=True)
-    label = fields.String(allow_none=True)
-    matched = fields.Boolean(allow_none=True)
-    imported = fields.Boolean(allow_none=True)
-    full_name = fields.Boolean(allow_none=True)
+class EnrichmentResult(CamelCaseSchema):
+    version = fields.String(required=True)
+    domainInfo = fields.Dict(
+        keys=fields.String(), values=fields.Nested(DomainInfo), required=True)
+    genes = fields.List(fields.Nested(EnrichedGene), required=True)
 
 
 # Requests
 # ----------------------------------------
 
 class EnrichmentTableSchema(CamelCaseSchema):
-    version = fields.String(required=True)
-    domain_info = fields.Dict(
-        keys=fields.String(), values=fields.Nested(DomainInfo), required=True)
-    genes = fields.List(fields.Nested(EnrichedGene), required=True)
+    data = fields.String(required=True)
+    result = fields.Nested(EnrichmentResult, required=True)
