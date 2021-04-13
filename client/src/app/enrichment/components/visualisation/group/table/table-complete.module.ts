f5028ffde6dfaec9a973a450bf46e8f081c434e0
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-04-13 18:28:18 +0200

    limit changes

--- client/src/app/enrichment/components/visualisation/group/table/table-complete.module.ts
+++ client/src/app/enrichment/components/visualisation/group/table/table-complete.module.ts
@@ -2,23 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { SortableTableHeaderDirective } from '../../../../../shared/directives/table-sortable-header.directive';
 import { TableCompleteComponent } from './table-complete.component';
-import { SharedModule } from 'app/shared/shared.module';
-import { CloudViewerModule } from '../word-cloud/cloud-viewer.module';
 import { LinkModule } from '../../components/link/link.module';
 
 @NgModule({
   imports: [
-    SharedModule,
     BrowserModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    NgbModule,
-    SharedModule,
     LinkModule
   ],
   declarations: [TableCompleteComponent, SortableTableHeaderDirective],
