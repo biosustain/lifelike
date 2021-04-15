3894f0df54d34517d75146a4c83a6b8a9473b177
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-04-15 13:17:24 -0700

    Convert viz search queries to n4j driver

--- appserver/neo4japp/models/neo4j.py
+++ appserver/neo4japp/models/neo4j.py
@@ -1,5 +1,7 @@
 from flask import json
+from neo4j.graph import Node as N4jDriverNode
 from py2neo import Node, Relationship
+
 from neo4japp.models.common import NEO4JBase
 from neo4japp.util import snake_to_camel_dict
 
@@ -47,6 +49,26 @@ class GraphNode(NEO4JBase):
         display_name = None if not display_fn else display_fn(node)
         url = None if not url_fn else url_fn(node)
         return cls(node.identity, primary_label, domain_labels, data, labels, display_name, url)
+
+    @classmethod
+    def from_neo4j_driver(
+        cls,
+        node: N4jDriverNode,
+        prop_filter_fn=None,
+        primary_label_fn=None,
+        domain_labels_fn=None,
+        display_fn=None,
+        url_fn=None
+    ):
+        labels = [l for l in node.labels]
+        prop_filter_fn = prop_filter_fn or (lambda x: x)
+        primary_label = labels[0] if not primary_label_fn else primary_label_fn(node)
+        domain_labels = [] if not domain_labels_fn else domain_labels_fn(node)
+        data = prop_filter_fn({k: v for k, v in dict(node).items()})
+        data = snake_to_camel_dict(data, {})
+        display_name = None if not display_fn else display_fn(node)
+        url = None if not url_fn else url_fn(node)
+        return cls(node.id, primary_label, domain_labels, data, labels, display_name, url)
 
 
 class GraphRelationship(NEO4JBase):
