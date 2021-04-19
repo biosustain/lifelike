e7f95ab9fffbb31a57fbea4284a5105142c4960b
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-04-19 16:43:34 +0200

    fixed

--- client/src/app/enrichment/components/visualisation/group/clustergram/clustergram.component.ts
+++ client/src/app/enrichment/components/visualisation/group/clustergram/clustergram.component.ts
@@ -3,6 +3,12 @@ import { annotationTypesMap } from '../../../../../shared/annotation-styles';
 import { EnrichWithGOTermsResult, EnrichmentVisualisationService } from '../../../../services/enrichment-visualisation.service';
 import { KeyValue } from '@angular/common';
 
+interface GeneRow {
+  values: boolean[];
+  frequency: number;
+  others: number;
+}
+
 @Component({
   selector: 'app-clustergram',
   templateUrl: './clustergram.component.html',
@@ -12,15 +18,16 @@ export class ClustergramComponent implements OnChanges {
   @Input() data: EnrichWithGOTermsResult[];
   @Input() showMore: boolean;
 
-  genes = new Map<string, boolean[]>();
+  genes = new Map<string, GeneRow>();
+  others: GeneRow;
   goTerms: EnrichWithGOTermsResult[] = [];
   geneColor: string = annotationTypesMap.get('gene').color;
 
   constructor(readonly enrichmentService: EnrichmentVisualisationService) {
   }
 
-  rowOrder(a: KeyValue<string, boolean[]>, b: KeyValue<string, boolean[]>) {
-    return b.value.filter(d => d).length - a.value.filter(d => d).length;
+  rowOrder(a: KeyValue<string, GeneRow>, b: KeyValue<string, GeneRow>) {
+    return b.value.frequency - a.value.frequency;
   }
 
   columnOrder(a: EnrichWithGOTermsResult, b: EnrichWithGOTermsResult) {
@@ -28,23 +35,35 @@ export class ClustergramComponent implements OnChanges {
   }
 
   ngOnChanges() {
-    const data = (this.showMore ?
-      this.data.slice(0, 50)
-      : this.data.slice(0, 25))
-      .sort(this.columnOrder);
-    const genes = new Map<string, boolean[]>();
+    const data = this.data.sort(this.columnOrder);
+    const sliceSize = Math.min(data.length, this.showMore ? 50 : 25);
+    const genes = new Map<string, GeneRow>();
+    let others;
     const {importGenes} = this.enrichmentService.enrichmentDocument;
-    data.forEach(({geneNames}, goIndex) => {
+    const goTerms = data.slice(0, sliceSize);
+    data.slice(0, sliceSize).forEach(({geneNames}, goIndex) => {
       importGenes.filter(value => geneNames.includes(value)).forEach(g => {
         let geneRow = genes.get(g);
         if (!geneRow) {
-          geneRow = new Array(data.length);
+          geneRow = {values: new Array(sliceSize), frequency: 0, others: 0} as GeneRow;
           genes.set(g, geneRow);
         }
-        geneRow[goIndex] = true;
+        geneRow.frequency++;
+        geneRow.values[goIndex] = true;
+      });
+    });
+    data.slice(sliceSize).forEach(({geneNames}, goIndex) => {
+      importGenes.filter(value => geneNames.includes(value)).forEach(g => {
+        let geneRow = genes.get(g);
+        if (!geneRow) {
+          others = geneRow = others || {values: new Array(sliceSize), frequency: 0, others: 0} as GeneRow;
+        }
+        geneRow.frequency++;
+        geneRow.others++;
       });
     });
     this.genes = genes;
-    this.goTerms = data;
+    this.others = others;
+    this.goTerms = goTerms;
   }
 }
