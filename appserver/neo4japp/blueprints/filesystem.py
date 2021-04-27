c3fc0ae7af402e3b585b03bbfbab35963eccd20a
David Bour <dbour.developer@gmail.com>
2021-04-27 14:12:38 -0700

    - refactor: allow update enrichment button to enrich all enrichment   tables.

--- appserver/neo4japp/blueprints/filesystem.py
+++ appserver/neo4japp/blueprints/filesystem.py
@@ -72,7 +72,7 @@ bp = Blueprint('filesystem', __name__, url_prefix='/filesystem')
 # - The project that the files are in may be recycled
 
 
-# TODO: Deprecate me after LL-2840
+# TODO: Deprecate me after LL-3006
 @bp.route('/enrichment-tables', methods=['GET'])
 @auth.login_required
 def get_all_enrichment_tables():
@@ -80,15 +80,7 @@ def get_all_enrichment_tables():
     if is_admin is False:
         raise NotAuthorized(message='You do not have sufficient privileges.', code=400)
 
-    query = db.session.query(Files.hash_id).filter(
-        and_(
-            Files.mime_type == 'vnd.lifelike.document/enrichment-table',
-            or_(
-                Files.annotation_configs.is_(None),
-                Files.fallback_organism_id.is_(None)
-            )
-        )
-    )
+    query = db.session.query(Files.hash_id).filter(Files.mime_type == 'vnd.lifelike.document/enrichment-table')
     results = [hash_id[0] for hash_id in query.all()]
     return jsonify(dict(result=results)), 200
 
