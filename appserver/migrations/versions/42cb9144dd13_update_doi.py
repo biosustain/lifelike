56ee00488164c01ad0642bb51389677630b4d9e2
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-04-27 19:11:03 +0200

    works, but is it correct migration?

--- appserver/migrations/versions/42cb9144dd13_update_doi.py
+++ appserver/migrations/versions/42cb9144dd13_update_doi.py
@@ -5,8 +5,11 @@ Revises: bc9d080502da
 Create Date: 2021-04-27 13:50:40.622862
 
 """
+import io
+
 from alembic import context
 from alembic import op
+from sqlalchemy.orm.session import Session
 
 # revision identifiers, used by Alembic.
 from neo4japp.models import Files, FileContent
@@ -17,27 +20,34 @@ down_revision = 'bc9d080502da'
 branch_labels = None
 depends_on = None
 
+
 def recalculate_doi_based_on_current_algorithm():
+    session = Session(op.get_bind())
     pdf_type_provider = PDFTypeProvider()
-    for file in op.session.query(Files)\
-        .filter(Files.mime_type == 'application/pdf')\
-        .join(Files.content)\
-        .with_entities(Files.filename, Files.doi, FileContent.raw_file):
+    dois = []
+    for file in session.query(Files) \
+            .filter(Files.mime_type == 'application/pdf') \
+            .join(Files.content) \
+            .with_entities(Files.id, Files.doi, FileContent.raw_file):
 
         buffer = io.BytesIO(file.raw_file)
-        file.doi = pdf_type_provider.extract_doi(buffer)
+        extracted_doi = pdf_type_provider.extract_doi(buffer)
 
-        file.save()
-        print(f'saved {file}')
+        dois.append(extracted_doi)
+
+        session.query(Files)\
+            .filter(Files.id == file.id)\
+            .update(dict(doi=extracted_doi))
+
 
 def upgrade():
-    pass
     if context.get_x_argument(as_dictionary=True).get('data_migrate', None):
         data_upgrades()
 
 
 def downgrade():
-    pass
+    if context.get_x_argument(as_dictionary=True).get('data_migrate', None):
+        data_downgrades()
     # NOTE: In practice perfect downgrades are difficult and in some cases
     # impossible! It is more practical to use database backups/snapshots to
     # "downgrade" the database. Changes to the database that we intend to
