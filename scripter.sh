c685c843946d966b4e0346b21c4b5a327d2715de
David Bour <dbour.developer@gmail.com>
2021-05-03 15:25:13 -0700

    - chore: change neo4j to use ip address - chore: piplock file

--- scripter.sh
+++ scripter.sh
@@ -1,4 +1,4 @@
-git tag --delete qa
-git push --delete origin qa
-git tag qa
-git push origin qa
+git tag --delete qa-azure
+git push --delete origin qa-azure
+git tag qa-azure
+git push origin qa-azure
