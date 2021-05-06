1a1dcfb1ee83d404e713bd5143090fbfd21cddd5
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-05-06 16:37:57 -0700

    Cleanup elastic code + update index on proj rename
    Lots of code cleanup in the elastic document create/update/delete
    pipeline. Also adds an event listener to projects so that all files in
    the project are updated in elastic when the project name changes. This
    is to enable a consistent filepath field in elastic later.

--- appserver/neo4japp/utils/sqlalchemy.py
+++ appserver/neo4japp/utils/sqlalchemy.py
@@ -1,4 +1,5 @@
 import sqlalchemy
+from sqlalchemy import inspect
 from sqlalchemy_searchable import inspect_search_vectors, search_manager
 
 
@@ -24,3 +25,35 @@ def ft_search(query, search_query, vector=None, regconfig=None):
     ).label('rank'))
 
     return query.params(term=search_query)
+
+
+def get_model_changes(model):
+    """
+    Return a dictionary containing changes made to the model since it was
+    fetched from the database.
+
+    The dictionary is of the form {'property_name': [old_value, new_value]}
+
+    Example:
+      user = get_user_by_id(402)
+      >>> '<User id=402 email="business_email@gmail.com">'
+      get_model_changes(user)
+      >>> {}
+      user.email = 'new_email@who-dis.biz'
+      get_model_changes(user)
+      >>> {'email': ['business_email@gmail.com', 'new_email@who-dis.biz']}
+    """
+
+    state = inspect(model)
+    changes = {}
+    for attr in state.attrs:
+        hist = state.get_history(attr.key, True)
+
+        if not hist.has_changes():
+            continue
+
+        old_value = hist.deleted[0] if hist.deleted else None
+        new_value = hist.added[0] if hist.added else None
+        changes[attr.key] = [old_value, new_value]
+
+    return changes
