b278e180e6474f6ef244b99499016a39961ab56b
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-05-11 15:52:40 +0200

    corr calc with others grouping

--- client/src/app/enrichment/components/visualisation/group/clustergram/clustergram.component.ts
+++ client/src/app/enrichment/components/visualisation/group/clustergram/clustergram.component.ts
@@ -19,7 +19,7 @@ class GeneRow {
 
 interface ColumnHeader {
   x: number;
-  goId?: string;
+  goId?: number;
   goTerm: string;
 }
 
@@ -30,9 +30,11 @@ interface RowHeader {
 
 interface Match {
   x: number;
-  y: number;
+  y: number | string;
   value?: number;
 }
+
+const OTHERS = 'others';
 
 @Component({
   selector: 'app-clustergram',
@@ -40,19 +42,6 @@ interface Match {
   styleUrls: ['./clustergram.component.scss']
 })
 export class ClustergramComponent implements OnChanges {
-  @Input() data: EnrichWithGOTermsResult[];
-  @Input() showMore: boolean;
-  @Input() show: boolean;
-
-  matches: Match[];
-  genes: RowHeader[];
-  goTerms: ColumnHeader[] = [];
-  geneColor: string = annotationTypesMap.get('gene').color;
-
-
-  @HostBinding('style') private size;
-
-  _itemSize;
   get itemSize() {
     return this._itemSize;
   }
@@ -72,97 +61,125 @@ export class ClustergramComponent implements OnChanges {
     this.itemSize = [35, 35];
   }
 
+  @Input() data: EnrichWithGOTermsResult[];
+  @Input() showMore: boolean;
+  @Input() show: boolean;
+
+  cells: Match[];
+  rowHeaders: RowHeader[];
+  columnHeaders: ColumnHeader[] = [];
+  geneColor: string = annotationTypesMap.get('gene').color;
+
+
+  @HostBinding('style') private size;
+
+  _itemSize;
+
+  private static _valueMapToSortedList(valueMap: Map<string, number>): [string, number][] {
+    return [...valueMap].sort((a, b) => b[1] - a[1]);
+  }
+
+  private static _sortedListToIndexMap(sortedList: [string, number][]): Map<string, number> {
+    return new Map(
+      sortedList
+        .map(([k, v], i) => [k, i])
+    );
+  }
+
+  private static _indexMapToRowHeaders(indexMap: Map<string, number>): RowHeader[] {
+    return [...indexMap].map(([key, i]) => ({key, y: i} as RowHeader));
+  }
+
+  private static _goTermsToColumnHeaders(goTerms: EnrichWithGOTermsResult[]): ColumnHeader[] {
+    return goTerms.map(({goId, goTerm}, i) => ({goId, goTerm, x: i} as ColumnHeader));
+  }
+
+  private static _mapRowNameToIndex(data: any[], indexMap: Map<string, number>) {
+    return data.map(({y, ...rest}) => ({y: indexMap.get(y), ...rest}));
+  }
+
+  private static _valueMapToColumn(valueMap, columnIndex): Match[] {
+    return [...valueMap].map(([y, value]) => ({y, x: columnIndex, value}));
+  }
+
   rowOrder(a: KeyValue<string, GeneRow>, b: KeyValue<string, GeneRow>) {
     return b.value.frequency - a.value.frequency;
   }
 
+  // sort by number of related gene names in each go term
   columnOrder(a: EnrichWithGOTermsResult, b: EnrichWithGOTermsResult) {
     return b.geneNames.length - a.geneNames.length;
   }
 
   ngOnChanges() {
     if (this.show) {
-      const data = [...this.data].sort(this.columnOrder).map((o, i) => ({...o, x: i}));
-      let genes = new Map<string, number>();
-      let matches;
-      let goTerms;
+      // Sort copy of data
+      const data: EnrichWithGOTermsResult[] = [...this.data].sort(this.columnOrder);
+      const rowMentions = new Map<string, number>();
+      let columnHeaders, geneIndex;
+      // Iterate through columns and nested rows collecting points and number of ref per row
+      let matches: Match[] = data.reduce((o1, {geneNames}, goIndex) =>
+          geneNames.reduce((o2, g) => {
+            const frequency = rowMentions.get(g);
+            rowMentions.set(g, (frequency || 0) + 1);
+            o2.push({
+              x: goIndex,
+              y: g
+            });
+            return o2;
+          }, o1)
+        , [] as Match[]);
+      // get gene order
+      const sortedGeneList = ClustergramComponent._valueMapToSortedList(rowMentions);
       if (this.showMore) {
-        goTerms = data;
-        matches = data.reduce((o1, {geneNames}, goIndex) =>
-            geneNames.reduce((o2, g) => {
-              let frequency = genes.get(g);
-              if (!frequency) {
-                genes.set(g, 1);
-              } else {
-                genes.set(g, ++frequency);
-              }
-              o2.push({
-                x: goIndex,
-                y: g
-              });
-              return o2;
-            }, o1)
-          , []);
+        columnHeaders = ClustergramComponent._goTermsToColumnHeaders(data);
+        // resolve row index for each gene
+        geneIndex = ClustergramComponent._sortedListToIndexMap(sortedGeneList);
       } else {
         const sliceSize = Math.min(data.length, 25);
-        // @ts-ignore
-        goTerms = data.slice(0, sliceSize).concat([{
-          goTerm: 'others',
-          x: sliceSize
-        } as ColumnHeader]) as ColumnHeader;
-        const others = new Map<string, any>();
-        matches = data.reduce((o1, {geneNames}, goIndex) => {
-          if (goIndex < sliceSize) {
-            return geneNames.reduce((o2, g) => {
-              let frequency = genes.get(g);
-              if (!frequency) {
-                genes.set(g, 1);
-              } else {
-                genes.set(g, ++frequency);
-              }
-              o2.push({
-                x: goIndex,
-                y: g
-              });
-              return o2;
-            }, o1);
-          } else {
-            return geneNames.reduce((o2, g) => {
-              let frequency = genes.get(g);
-              if (frequency) {
-                genes.set(g, ++frequency);
-              }
-              if (!genes.has(g)) {
-                g = 'others';
-                genes.set(g, 0);
-              }
-              let cell = others.get(g);
-              if (!cell) {
-                cell = {
-                  y: g,
-                  x: sliceSize,
-                  value: 1
-                };
-                others.set(g, cell);
-                o2.push(cell);
-              } else {
-                cell.value++;
-              }
-              return o2;
-            }, o1);
-          }
-        }, []);
+        if (data.length > sliceSize) {
+          const {matches: slicedMatches, shown, others} = matches.reduce((o, n) => {
+            if (n.x < sliceSize) {
+              // show all cells within column limit
+              o.matches.push(n);
+              // and save which rows are being shown
+              o.shown.add(n.y);
+            } else {
+              // if row has been shown accumulate 'others' for it
+              // otherwise dump to row/column 'others' common cell
+              const rowId = o.shown.has(n.y) ? n.y : OTHERS;
+              const othersNumber = o.others.get(rowId);
+              o.others.set(rowId, (othersNumber || 0) + 1);
+            }
+            return o;
+          }, {
+            matches: [] as Match[],
+            shown: new Set(),
+            others: new Map(),
+          });
+          // add column of others at index equal to sliced size
+          const othersCells = ClustergramComponent._valueMapToColumn(others, sliceSize);
+          matches = slicedMatches.concat(othersCells);
+          columnHeaders = ClustergramComponent._goTermsToColumnHeaders(
+            data.slice(0, sliceSize)
+          );
+          columnHeaders.push({
+            goTerm: OTHERS,
+            x: sliceSize
+          } as ColumnHeader);
+          // resolve row index for each shown gene
+          const sortedShownGenes = sortedGeneList.filter(d => shown.has(d[0]));
+          // add others row
+          sortedShownGenes.push([OTHERS, 0]);
+          geneIndex = ClustergramComponent._sortedListToIndexMap(sortedShownGenes);
+        }
       }
-      // get row order
-      genes = new Map(
-        [...genes]
-          .sort((a, b) => b[1] - a[1])
-          .map(([k, v], i) => [k, i])
-      );
-      this.genes = [...genes].map(([key, i]) => ({key, y: i})) as RowHeader[];
-      this.matches = matches
-        .map(({y, ...rest}) => ({y: genes.get(y), ...rest})) as Match[];
-      this.goTerms = goTerms as ColumnHeader[];
+      // generate rows headers
+      this.rowHeaders = ClustergramComponent._indexMapToRowHeaders(geneIndex);
+      // generate cells
+      this.cells = ClustergramComponent._mapRowNameToIndex(matches, geneIndex);
+      // generate column headers
+      this.columnHeaders = columnHeaders;
     }
   }
 }
