766c582deee761019d67c8220a26f020a79658be
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-05-17 10:52:08 -0700

    Update wikipedia links to search the site directly

--- client/src/app/shared/links.ts
+++ client/src/app/shared/links.ts
@@ -22,7 +22,7 @@ export const SEARCH_LINKS: readonly Hyperlink[] = Object.freeze([{
   isDatabase: true,
 }, {
   domain: 'Wikipedia',
-  url: 'https://www.google.com/search?q=site:+wikipedia.org+%s',
+  url: 'https://en.wikipedia.org/w/index.php?search=%s',
   isDatabase: false,
 }, {
   domain: 'Google',
