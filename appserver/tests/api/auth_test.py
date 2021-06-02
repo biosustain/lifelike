87f493f9f6f83bbfc34195118fa921cc4045b94f
Maciek <mgrzelczak97@gmail.com>
2021-06-02 16:37:27 +0200

    removed unnecessery variable from auth_test

--- appserver/tests/api/auth_test.py
+++ appserver/tests/api/auth_test.py
@@ -12,7 +12,6 @@ def user_factory(uid):
         'lastName': f'lastname-{uid}',
         'email': f'appuser-{uid}@***ARANGO_DB_NAME***.bio',
         'roles': ['user'],
-        'failed_login_count': 0
     }
 
 
