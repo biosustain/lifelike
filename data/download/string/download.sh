898feb5643bfb4666d0fb0dfb72805a17cf63ef0
rcai92130 <rcai@ucsd.edu>
2021-06-02 08:51:49 -0700

    add download data scripts

---
+++ data/download/string/download.sh
@@ -0,0 +1,4 @@
+curl -o protein.info.txt.gz https://stringdb-static.org/download/protein.info.v11.0.txt.gz
+curl -o all_organism_uniprot2string.tsv.gz https://string-db.org/mapping_files/uniprot/all_organisms.uniprot_2_string.2018.tsv.gz
+
+
---
+++ data/download/uniprot/download.sh
@@ -0,0 +1,2 @@
+curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.dat.gz
+curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping_selected.tab.gz
