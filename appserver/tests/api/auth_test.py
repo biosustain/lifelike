52fb35a5013bec56993215bcc960db248a245535
Maciek <mgrzelczak97@gmail.com>
2021-06-02 16:48:11 +0200

    Added additional info in auth_test

--- appserver/tests/api/auth_test.py
+++ appserver/tests/api/auth_test.py
@@ -12,6 +12,8 @@ def user_factory(uid):
         'lastName': f'lastname-{uid}',
         'email': f'appuser-{uid}@***ARANGO_DB_NAME***.bio',
         'roles': ['user'],
+        'failed_login_count': 0,
+        'forced_password_reset': False
     }
 
 
