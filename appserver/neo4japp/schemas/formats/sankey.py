47841b627b2997485e0072c95f8296c25da5cc8a
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-06-03 18:25:31 +0200

    not clear coding but somehow works

--- appserver/neo4japp/schemas/formats/sankey.py
+++ appserver/neo4japp/schemas/formats/sankey.py
@@ -6,6 +6,6 @@ import fastjsonschema
 from .. import formats
 
 # noinspection PyTypeChecker
-with resources.open_text(formats, 'sankey.json') as f:
+with resources.open_text(formats, 'sankey_v2.json') as f:
     # Use this method to validate the content of an enrichment table
     validate_sankey = fastjsonschema.compile(json.load(f))
