20836e570dce1921ac92231fc37cdf617fa9e772
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-06-03 12:21:26 +0200

    fix linter

--- client/src/app/sankey-viewer/components/sankey-view.component.ts
+++ client/src/app/sankey-viewer/components/sankey-view.component.ts
@@ -18,108 +18,7 @@ import { uuidv4 } from '../../shared/utils';
   styleUrls: ['./sankey-view.component.scss'],
 })
 export class SankeyViewComponent implements OnDestroy, ModuleAwareComponent {
-  @Output() requestClose: EventEmitter<any> = new EventEmitter();
 
-  paramsSubscription: Subscription;
-  returnUrl: string;
-
-  loadTask: any;
-  openSankeySub: Subscription;
-  ready = false;
-  object?: FilesystemObject;
-  // Type information coming from interface sankeySource at:
-  // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/sankeyjs-dist/index.d.ts
-  sankeyData;
-  sankeyFileLoaded = false;
-  modulePropertiesChange = new EventEmitter<ModuleProperties>();
-  private currentFileId: any;
-  filtersPanelOpened;
-  filteredSankeyData;
-  filter;
-
-  resolveFilteredNodesLinks(nodes) {
-    let newLinks = [];
-    let oldLinks = [];
-    nodes.forEach(node => {
-      oldLinks = oldLinks.concat(node.sourceLinks, node.targetLinks);
-      const nodeNewLinks = node.sourceLinks.reduce((newLinks, sl, sIter) => {
-        const targetNode = sl.target;
-        const targetIndex = targetNode.targetLinks.findIndex(l => l === sl);
-        targetNode.targetLinks.splice(targetIndex, 1);
-        return node.targetLinks.reduce((newLinks, tl, tIter) => {
-          // used for link initial position after creation
-          // const templateLink = sIter % 2 ? sl : tl;
-          const sourceNode = tl.source;
-          const newLink = {
-            // ...templateLink,
-            folded: true,
-            id: uuidv4(),
-            source: sourceNode,
-            target: targetNode,
-            value: ((sl.value + tl.value) / 2) || 1,
-            path: `${tl.path} => ${node.displayName} => ${sl.path}`
-          };
-          newLinks.push(newLink);
-          if (!tIter) {
-            const sourceIndex = sourceNode.sourceLinks.findIndex(l => l === tl);
-            sourceNode.sourceLinks.splice(sourceIndex, 1);
-          }
-          sourceNode.sourceLinks.push(newLink);
-          targetNode.targetLinks.push(newLink);
-          return newLinks;
-        }, newLinks);
-      }, []);
-      newLinks = newLinks.concat(nodeNewLinks);
-      // corner case - starting or ending node
-      if (!nodeNewLinks.length) {
-        // console.log(newLinks, oldLinks);
-      }
-    });
-    return {
-      newLinks,
-      oldLinks
-    };
-  }
-
-  changeFilter(filter = d => d) {
-    this.filter = filter;
-    const {nodes, links, ...data} = this.sankeyData;
-    const [filteredNodes, filteredOutNodes] = nodes.reduce(([filteredNodes, filteredOutNodes], n) => {
-      if (filter(n).hidden) {
-        filteredOutNodes.push(n);
-      } else {
-        filteredNodes.push(n);
-      }
-      return [filteredNodes, filteredOutNodes];
-    }, [[], []]);
-    console.log('calc start');
-    const {newLinks, oldLinks} = this.resolveFilteredNodesLinks(filteredOutNodes);
-    console.log('calc stop');
-    console.table({
-      'link diff': oldLinks.length - newLinks.length,
-      'initial links': links.length,
-      'final links': links.filter(link => !oldLinks.includes(link)).concat(newLinks).length
-    });
-    let filteredLinks = links.concat(newLinks).filter(link => !oldLinks.includes(link));
-    if (links.length - oldLinks.length + newLinks.length !== filteredLinks.length) {
-      const r = {
-        filtfromlinks: links.filter(value => oldLinks.includes(value)),
-        filtfromnew: newLinks.filter(value => oldLinks.find(d => d.path === value.path))
-      };
-      const r2 = oldLinks.filter(value => !r.filtfromlinks.includes(value) && !r.filtfromnew.includes(value));
-      filteredLinks = filteredLinks.filter(value => r2.find(v => v.path === value.path));
-    }
-    this.filteredSankeyData = {
-      ...data,
-      nodes: filteredNodes,
-      // filter after concat newLinks and oldLinks are not mutually exclusive
-      links: filteredLinks
-    };
-  };
-
-  toggleFiltersPanel() {
-    this.filtersPanelOpened = !this.filtersPanelOpened;
-  }
 
   constructor(
     protected readonly filesystemService: FilesystemService,
@@ -154,9 +53,110 @@ export class SankeyViewComponent implements OnDestroy, ModuleAwareComponent {
 
     this.loadFromUrl();
   }
+  @Output() requestClose: EventEmitter<any> = new EventEmitter();
+
+  paramsSubscription: Subscription;
+  returnUrl: string;
+
+  loadTask: any;
+  openSankeySub: Subscription;
+  ready = false;
+  object?: FilesystemObject;
+  // Type information coming from interface sankeySource at:
+  // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/sankeyjs-dist/index.d.ts
+  sankeyData;
+  sankeyFileLoaded = false;
+  modulePropertiesChange = new EventEmitter<ModuleProperties>();
+  private currentFileId: any;
+  filtersPanelOpened;
+  filteredSankeyData;
+  filter;
 
   paths = new Set();
   nodesColorMap = new Map();
+
+  resolveFilteredNodesLinks(nodes) {
+    let newLinks = [];
+    let oldLinks = [];
+    nodes.forEach(node => {
+      oldLinks = oldLinks.concat(node.sourceLinks, node.targetLinks);
+      const nodeNewLinks = node.sourceLinks.reduce((inewLinks, sl, sIter) => {
+        const targetNode = sl.target;
+        const targetIndex = targetNode.targetLinks.findIndex(l => l === sl);
+        targetNode.targetLinks.splice(targetIndex, 1);
+        return node.targetLinks.reduce((iinewLinks, tl, tIter) => {
+          // used for link initial position after creation
+          const templateLink = sIter % 2 ? sl : tl;
+          const sourceNode = tl.source;
+          const newLink = {
+            ...templateLink,
+            folded: true,
+            id: uuidv4(),
+            source: sourceNode,
+            target: targetNode,
+            value: ((sl.value + tl.value) / 2) || 1,
+            path: `${tl.path} => ${node.displayName} => ${sl.path}`
+          };
+          iinewLinks.push(newLink);
+          if (!tIter) {
+            const sourceIndex = sourceNode.sourceLinks.findIndex(l => l === tl);
+            sourceNode.sourceLinks.splice(sourceIndex, 1);
+          }
+          sourceNode.sourceLinks.push(newLink);
+          targetNode.targetLinks.push(newLink);
+          return iinewLinks;
+        }, inewLinks);
+      }, []);
+      newLinks = newLinks.concat(nodeNewLinks);
+      // corner case - starting or ending node
+      if (!nodeNewLinks.length) {
+        // console.log(newLinks, oldLinks);
+      }
+    });
+    return {
+      newLinks,
+      oldLinks
+    };
+  }
+
+  changeFilter(filter = d => d) {
+    this.filter = filter;
+    const {nodes, links, ...data} = this.sankeyData;
+    const [filteredNodes, filteredOutNodes] = nodes.reduce(([ifilteredNodes, ifilteredOutNodes], n) => {
+      if (filter(n).hidden) {
+        ifilteredOutNodes.push(n);
+      } else {
+        ifilteredNodes.push(n);
+      }
+      return [ifilteredNodes, ifilteredOutNodes];
+    }, [[], []]);
+    console.log('calc start');
+    const {newLinks, oldLinks} = this.resolveFilteredNodesLinks(filteredOutNodes);
+    console.log('calc stop');
+    console.table({
+      'link diff': oldLinks.length - newLinks.length,
+      'initial links': links.length,
+      'final links': links.filter(link => !oldLinks.includes(link)).concat(newLinks).length
+    });
+    let filteredLinks = links.concat(newLinks).filter(link => !oldLinks.includes(link));
+    if (links.length - oldLinks.length + newLinks.length !== filteredLinks.length) {
+      const r = {
+        filtfromlinks: links.filter(value => oldLinks.includes(value)),
+        filtfromnew: newLinks.filter(value => oldLinks.find(d => d.path === value.path))
+      };
+      const r2 = oldLinks.filter(value => !r.filtfromlinks.includes(value) && !r.filtfromnew.includes(value));
+      filteredLinks = filteredLinks.filter(value => r2.find(v => v.path === value.path));
+    }
+    this.filteredSankeyData = {
+      ...data,
+      nodes: filteredNodes,
+      // filter after concat newLinks and oldLinks are not mutually exclusive
+      links: filteredLinks
+    };
+  }
+  toggleFiltersPanel() {
+    this.filtersPanelOpened = !this.filtersPanelOpened;
+  }
 
   parseData({links, graph, nodes, ...data}) {
     const pathIdAccessor = path => nodes.find(n => n.id === path[0]).name[0];
