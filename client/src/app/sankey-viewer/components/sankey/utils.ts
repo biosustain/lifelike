594783622b9adec517659bbcf0197bdc19764e1d
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-06-09 01:05:47 +0200

    ready

--- client/src/app/sankey-viewer/components/sankey/utils.ts
+++ client/src/app/sankey-viewer/components/sankey/utils.ts
@@ -165,4 +165,4 @@ export const christianColors = [
   '#BF5650', '#E83000', '#66796D', '#DA007C', '#FF1A59', '#8ADBB4', '#1E0200', '#5B4E51',
   '#C895C5', '#320033', '#FF6832', '#66E1D3', '#CFCDAC', '#D0AC94', '#7ED379', '#012C58'];
 
-export const representativePositiveNumber = clamp(1e-6, 1e36);
+export const representativePositiveNumber = clamp(1e-4, 1e4);
