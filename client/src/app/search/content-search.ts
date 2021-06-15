3945360766b58673c86e05e98a008ad6feb4ada0
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-06-15 14:45:00 -0700

    Remove phrase and wildcard params from search qry
    These haven't really been necessary for quite a while, since we don't
    have explicit inputs for either of them anymore.

--- client/src/app/search/content-search.ts
+++ client/src/app/search/content-search.ts
@@ -4,6 +4,4 @@ import { StandardRequestOptions } from 'app/shared/schemas/common';
 export interface ContentSearchOptions extends StandardRequestOptions {
   types?: SearchType[];
   projects?: string[];
-  phrase?: string;
-  wildcards?: string;
 }
