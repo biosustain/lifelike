0b07c0ef9ab2dcb8a67a432396c2ff43ccf5a379
Eren Yagdiran <erenyagdiran@gmail.com>
2021-06-17 14:12:11 +0200

    dont process json files for bioc viewer

--- appserver/neo4japp/services/file_types/providers.py
+++ appserver/neo4japp/services/file_types/providers.py
@@ -285,7 +285,7 @@ class BiocTypeProvider(BaseFileTypeProvider):
     MIME_TYPE = 'vnd.***ARANGO_DB_NAME***.document/bioc'
     SHORTHAND = 'BioC'
     mime_types = (MIME_TYPE,)
-    ALLOWED_TYPES = ['.json', '.xml', '.bioc']
+    ALLOWED_TYPES = ['.xml', '.bioc']
 
     def handles(self, file: Files) -> bool:
         ext = os.path.splitext(file.filename)[1].lower()
