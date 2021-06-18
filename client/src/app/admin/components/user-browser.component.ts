7835c8cb7e7d4a2f540db2c7c7409a38257af813
Maciek <mgrzelczak97@gmail.com>
2021-06-18 22:04:16 +0200

    LL-3258: Update current user info only if current user was the one being updated

--- client/src/app/admin/components/user-browser.component.ts
+++ client/src/app/admin/components/user-browser.component.ts
@@ -23,6 +23,7 @@ import { State } from '../../***ARANGO_USERNAME***-store';
   templateUrl: 'user-browser.component.html',
 })
 export class UserBrowserComponent implements OnInit, OnDestroy {
+  currentUser: AppUser;
   users: AppUser[];
   shownUsers: AppUser[] = [];
   filterQuery = '';
@@ -43,7 +44,7 @@ export class UserBrowserComponent implements OnInit, OnDestroy {
       this.users = data.results;
       this.updateFilter();
     });
-
+    this.accountService.currentUser().subscribe(user => this.currentUser = user);
     this.refresh();
   }
 
@@ -138,9 +139,13 @@ export class UserBrowserComponent implements OnInit, OnDestroy {
             .pipe(this.errorHandler.create({label: 'Update user'}))
             .subscribe(() => {
               progressDialogRef.close();
-              this.store.dispatch(AuthActions.userUpdated(
+              if (this.currentUser.hashId === selectedUser.hashId) {
+                  updatedUser = Object.assign(this.currentUser, updatedUser);
+                  this.store.dispatch(AuthActions.userUpdated(
                       {user: updatedUser},
                     ));
+                  this.currentUser = updatedUser;
+              }
               this.accountService.getUserList();
               this.refresh();
               this.snackBar.open(
