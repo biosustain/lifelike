6242ad39f3d8e7edada5c89a457ae05394d05ef0
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-06-23 14:09:32 +0200

    custom prescalers

---
+++ client/src/app/sankey-viewer/components/algorithms/prescalers.ts
@@ -0,0 +1,54 @@
+interface Prescaler {
+  name: string;
+  description: string;
+  fn: (v: number) => number;
+}
+
+
+const prescalers: ArrayWithDefault<Prescaler> = [
+  {
+    name: 'None',
+    description: 'No transformation',
+    fn: (v: number) => v
+  } as Prescaler,
+  {
+    name: 'ln',
+    description: 'Natural logarithm',
+    fn: Math.log
+  } as Prescaler,
+  {
+    name: 'ln(x + 1)',
+    description: 'Natural logarithm moved by one',
+    fn: v => Math.log1p(v)
+  },
+  {
+    name: 'log2(x + 1)',
+    description: 'Base-2 logarithm moved by one',
+    fn: v => Math.log2(v + 1)
+  },
+  {
+    name: 'log10(x + 1)',
+    description: 'Base-10 logarithm moved by one',
+    fn: v => Math.log10(v + 1)
+  },
+  {
+    name: 'sqrt',
+    description: 'square root',
+    fn: Math.sqrt
+  },
+  {
+    name: '1/x',
+    description: 'Value multiplicative inverse',
+    fn: v => 1 / v
+  } as Prescaler
+];
+
+prescalers.default = prescalers[0];
+
+interface ArrayWithDefault<T> extends Array<T> {
+  default?: T;
+}
+
+export default prescalers;
+
+
