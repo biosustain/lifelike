5679a3dedcf5c542ba15ef07f6ee2b2de8dfac56
Maciek <mgrzelczak97@gmail.com>
2021-07-05 17:08:10 +0200

    LL-3300: Added dialog allowing to fix missing roles

--- client/src/app/admin/components/missing-roles-dialog.component.ts
+++ client/src/app/admin/components/missing-roles-dialog.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonFormDialogComponent } from '../../shared/components/dialog/common-form-dialog.component';
-import { AppUser } from '../../interfaces';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AppUser, UserUpdateRequest } from '../../interfaces';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { MessageDialog } from '../../shared/services/message-dialog.service';
+import { BehaviorSubject } from 'rxjs';
+import { Progress } from '../../interfaces/common-dialog.interface';
+import { ProgressDialog } from '../../shared/services/progress-dialog.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ErrorHandler } from '../../shared/services/error-handler.service';
+import { AccountService } from '../../users/services/account.service';
+
 
 @Component({
   selector: 'app-missing-roles-dialog',
@@ -11,24 +17,70 @@ import { MessageDialog } from '../../shared/services/message-dialog.service';
 })
 export class MissingRolesDialogComponent extends CommonFormDialogComponent {
 
-  usernames: string[];
-  readonly form: FormGroup = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
-    username: new FormControl('', Validators.required),
-    roles: new FormControl('')
+  users: AppUser[];
+  modified = false;
 
-  });
-
-  constructor(modal: NgbActiveModal, messageDialog: MessageDialog) {
+  constructor(modal: NgbActiveModal,
+              messageDialog: MessageDialog,
+              private readonly accountService: AccountService,
+              private readonly progressDialog: ProgressDialog,
+              private readonly snackBar: MatSnackBar,
+              private readonly errorHandler: ErrorHandler) {
     super(modal, messageDialog);
+    this.accountService = accountService;
   }
 
-  getValue() {
-      return true;
+  setUsers(users: AppUser[]) {
+    this.users = users;
   }
 
-  setUsers(usernames: string[]) {
-    this.usernames = usernames;
+  updateAll() {
+    for (const user of this.users) {
+      this.fixMissingUserRole(user);
+    }
+  }
+
+  close() {
+    this.modal.close(this.getValue());
+  }
+
+  fixMissingUserRole(user: AppUser) {
+    const updateRequest: UserUpdateRequest = {hashId: user.hashId, roles: ['user']};
+    const progressDialogRef = this.progressDialog.display({
+            title: `Updating User`,
+            progressObservable: new BehaviorSubject<Progress>(new Progress({
+              status: 'Updating user...',
+            })),
+          });
+    this.accountService.updateUser(updateRequest)
+    .pipe(this.errorHandler.create({label: 'Update user role'}))
+    .subscribe(() => {
+      progressDialogRef.close();
+      this.modified = true;
+      const index = this.users.indexOf(user);
+      this.users.splice(index, 1);
+      if (this.users.length === 0) {
+      this.snackBar.open(
+        `All roles fixed!`,
+        'close',
+        {duration: 5000},
+      );
+      this.close();
+    } else {
+        this.snackBar.open(
+        `User ${user.username} role fixed!`,
+        'close',
+        {duration: 5000},
+      );
+    }
+    }, () => {
+      progressDialogRef.close();
+    });
+
+
+  }
+
+  getValue(): boolean {
+    return this.modified;
   }
 }
