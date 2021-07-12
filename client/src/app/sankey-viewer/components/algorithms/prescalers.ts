4e872a9a0958265c98a7db67ce167f6cfc7ffe21
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-07-12 19:15:20 +0200

    Revert "still does not scale"
    This reverts commit 3112dd9bf6f88be29d221d84a9f1e7e3d71921e8.

--- client/src/app/sankey-viewer/components/algorithms/prescalers.ts
+++ client/src/app/sankey-viewer/components/algorithms/prescalers.ts
@@ -1,53 +1,4 @@
 import { Prescaler, ArrayWithDefault } from '../interfaces';
-import { computeNodeLinks } from './d3-sankey/d3-sankey';
-
-const calcNormalisationFactor = (nodeValue, linkValues, log) => {
-  const sumOfLinks = linkValues.reduce((v, l) => v + l, 0);
-  const sumOfLogLinks = linkValues.reduce((v, l) => v + log(l), 0);
-  const valBias = nodeValue - sumOfLinks;
-  return log(nodeValue) / (sumOfLogLinks + log(valBias < 0 ? 0 : valBias));
-};
-
-function normalize(data, key, accessor = d => d) {
-  const values = data.map(d => accessor(d)[key]);
-  const [min, max] = values.reduce((o, d) => [Math.min(o[0] || 0, d), Math.max(o[1] || 0, d)], [0, 0]);
-  data.forEach(d => {
-    accessor(d)[key] = (accessor(d)[key] - min) / (max - min);
-  });
-}
-
-const logScale = (log = Math.log) => data => {
-  computeNodeLinks(data);
-
-  data.nodes.forEach(n => {
-    n.sn =
-      calcNormalisationFactor(
-        n.fixedValue,
-        n.sourceLinks.map(({multiple_values, value}) => multiple_values ? multiple_values[0] : value),
-        log
-      );
-    n.tn =
-      calcNormalisationFactor(
-        n.fixedValue,
-        n.targetLinks.map(({multiple_values, value}) => multiple_values ? multiple_values[1] : value),
-        log
-      );
-    n.fixedValue = log(n.fixedValue);
-  });
-
-  data.links.forEach(l => {
-    l.multiple_values = [
-      log(l.multiple_values[0]) * l.source.sn,
-      log(l.multiple_values[1]) * l.target.tn
-    ];
-  });
-
-
-  normalize(data.nodes, 'fixedValue');
-  normalize(data.links, 'value');
-  normalize(data.links, 0, d => d.multiple_values);
-  normalize(data.links, 1, d => d.multiple_values);
-};
 
 const prescalers: ArrayWithDefault<Prescaler> = [
   {
@@ -58,17 +9,17 @@ const prescalers: ArrayWithDefault<Prescaler> = [
   {
     name: 'ln',
     description: 'Natural logarithm',
-    process: logScale(Math.log)
+    fn: Math.log
   },
   {
     name: 'log2',
     description: 'Base-2 logarithm',
-    process: logScale(Math.log2)
+    fn: v => Math.log2(v + 1)
   },
   {
     name: 'log10',
     description: 'Base-10 logarithm',
-    process: logScale(Math.log10)
+    fn: v => Math.log10(v + 1)
   },
   {
     name: 'sqrt',
