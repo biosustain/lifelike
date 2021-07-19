3f07d9307a60b7237f7dc322065f1b91f52d8db4
Binh Vu <binhvu.dev@gmail.com>
2021-07-19 11:52:21 -0700

    Updated enrichment JSON schema to allow empty strings, and set full name to empty string if null

--- appserver/neo4japp/schemas/formats/enrichment_tables.py
+++ appserver/neo4japp/schemas/formats/enrichment_tables.py
@@ -6,6 +6,6 @@ import fastjsonschema
 from .. import formats
 
 # noinspection PyTypeChecker
-with resources.open_text(formats, 'enrichment_tables_v4.json') as f:
+with resources.open_text(formats, 'enrichment_tables_v5.json') as f:
     # Use this method to validate the content of an enrichment table
     validate_enrichment_table = fastjsonschema.compile(json.load(f))
