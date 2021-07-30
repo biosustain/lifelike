294c99d4a7f68c96abd4254834fbc39db78c5065
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-07-30 10:07:56 -0700

    Fix default sorting of annotation entity types
    The list is now sorted by default. Any new entity types should be added
    in dictionary order. Also adds manual sorting of the list in the synonym
    search just in case the list is not sorted.

--- client/src/app/shared/annotation-types.ts
+++ client/src/app/shared/annotation-types.ts
@@ -22,22 +22,23 @@ export class EntityType {
 }
 
 export const ENTITY_TYPES = [
-  new EntityType('Gene', annotationTypesMap.get('gene').color, [DatabaseType.NCBI_GENE, DatabaseType.NONE]),
-  new EntityType('Protein', annotationTypesMap.get('protein').color, [DatabaseType.UNIPROT, DatabaseType.NONE]),
-  new EntityType('Disease', annotationTypesMap.get('disease').color, [DatabaseType.MESH, DatabaseType.NONE]),
-  new EntityType('Species', annotationTypesMap.get('species').color, [DatabaseType.NCBI_TAXONOMY, DatabaseType.NONE]),
-  new EntityType('Company', annotationTypesMap.get('company').color, [DatabaseType.NONE]),
-  new EntityType('Mutation', annotationTypesMap.get('mutation').color, [DatabaseType.NONE]),
+  new EntityType('Anatomy', annotationTypesMap.get('anatomy').color, [DatabaseType.MESH, DatabaseType.NONE]),
   new EntityType('Chemical', annotationTypesMap.get('chemical').color, [DatabaseType.CHEBI, DatabaseType.PUBCHEM, DatabaseType.NONE]),
+  new EntityType('Company', annotationTypesMap.get('company').color, [DatabaseType.NONE]),
+  new EntityType('Compound', annotationTypesMap.get('compound').color, [DatabaseType.BIOCYC, DatabaseType.NONE]),
+  new EntityType('Disease', annotationTypesMap.get('disease').color, [DatabaseType.MESH, DatabaseType.NONE]),
+  new EntityType('Entity', annotationTypesMap.get('entity').color, [DatabaseType.NONE]),
+  new EntityType('Food', annotationTypesMap.get('food').color, [DatabaseType.MESH, DatabaseType.NONE]),
+  new EntityType('Gene', annotationTypesMap.get('gene').color, [DatabaseType.NCBI_GENE, DatabaseType.NONE]),
+  new EntityType('Lab Sample', annotationTypesMap.get('lab sample').color, [DatabaseType.NONE]),
+  new EntityType('Lab Strain', annotationTypesMap.get('lab strain').color, [DatabaseType.NONE]),
+  new EntityType('Mutation', annotationTypesMap.get('mutation').color, [DatabaseType.NONE]),
+  new EntityType('Pathway', annotationTypesMap.get('pathway').color, [DatabaseType.NONE]),
   new EntityType('Phenomena', annotationTypesMap.get('phenomena').color, [DatabaseType.MESH, DatabaseType.NONE]),
   new EntityType('Phenotype', annotationTypesMap.get('phenotype').color, [DatabaseType.MESH, DatabaseType.NONE]),
-  new EntityType('Pathway', annotationTypesMap.get('pathway').color, [DatabaseType.NONE]),
-  new EntityType('Entity', annotationTypesMap.get('entity').color, [DatabaseType.NONE]),
-  new EntityType('Compound', annotationTypesMap.get('compound').color, [DatabaseType.BIOCYC, DatabaseType.NONE]),
-  new EntityType('Food', annotationTypesMap.get('food').color, [DatabaseType.MESH, DatabaseType.NONE]),
-  new EntityType('Anatomy', annotationTypesMap.get('anatomy').color, [DatabaseType.MESH, DatabaseType.NONE]),
-  new EntityType('Lab Strain', annotationTypesMap.get('lab strain').color, [DatabaseType.NONE]),
-  new EntityType('Lab Sample', annotationTypesMap.get('lab sample').color, [DatabaseType.NONE])
+  new EntityType('Protein', annotationTypesMap.get('protein').color, [DatabaseType.UNIPROT, DatabaseType.NONE]),
+  new EntityType('Species', annotationTypesMap.get('species').color, [DatabaseType.NCBI_TAXONOMY, DatabaseType.NONE]),
+
 ];
 
 export const ENTITY_TYPE_MAP = ENTITY_TYPES.reduce((map, item) => {
