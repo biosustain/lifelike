95080550f1a6fe5eca617004859c6fc6a3204593
Ethan Sanchez <ethan.dsanch@gmail.com>
2021-08-10 18:08:44 -0700

    Fix misc. bugs in content search
    Refactors how the query string is managed in the component and its
    children. Also fixes a bug where there was whitespace between the query
    and synonyms when they are added. Finally, fixes a bug where selecting a
    folder and closing the advanced search component was not consistent with
    other fields.

--- client/src/app/search/components/content-search-form.component.ts
+++ client/src/app/search/components/content-search-form.component.ts
@@ -1,46 +1,35 @@
-import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-
-import { Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { MessageDialog } from 'app/shared/services/message-dialog.service';
 import { FormComponent } from 'app/shared/components/base/form.component';
 
 import { ContentSearchOptions } from '../content-search';
+import { SearchableRequestOptions } from 'app/shared/schemas/common';
 
 @Component({
   selector: 'app-content-search-form',
   templateUrl: './content-search-form.component.html',
 })
-export class ContentSearchFormComponent extends FormComponent<ContentSearchOptions> implements OnDestroy {
-  @Input() set queryString(queryString: string) {
-    super.params = {
-      q: queryString,
-    };
-  }
-  @Output() formResult = new EventEmitter<ContentSearchOptions>();
-  @Output() formChange = new EventEmitter<ContentSearchOptions>();
+export class ContentSearchFormComponent extends FormComponent<ContentSearchOptions> {
+  @Input() queryString: string;
+  @Output() queryStringChange = new EventEmitter<string>();
+  @Output() formResult = new EventEmitter<SearchableRequestOptions>();
 
   form = new FormGroup({
     q: new FormControl(''),
   });
 
-  formChangesSub: Subscription;
-
   constructor(messageDialog: MessageDialog) {
     super(messageDialog);
-
-    // We want to be listening for changes immediately, so subscribe in the constructor. If we were to subscribe later (e.g. in `ngOnInit`)
-    // we may not capture the very first change, which happens when the input `params` changes the first time.
-    this.formChangesSub = this.form.valueChanges.pipe(
-      debounceTime(250)
-    ).subscribe(() => {
-      this.formChange.emit({...this.form.value});
-    });
   }
 
-  ngOnDestroy() {
-    this.formChangesSub.unsubscribe();
+  queryChanged() {
+    this.queryStringChange.emit(this.queryString);
+  }
+
+  submit() {
+    this.form.patchValue({q: this.queryString});
+    super.submit();
   }
 }
