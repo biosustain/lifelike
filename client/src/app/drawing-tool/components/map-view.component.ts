aca84656bfefc116875c32b1d338da8148d8ebc2
Bob Zhang <chz026@ucsd.edu>
2021-08-11 17:17:17 -0700

    added jszip-utils, TODO: test new save/load map

--- client/src/app/drawing-tool/components/map-view.component.ts
+++ client/src/app/drawing-tool/components/map-view.component.ts
@@ -2,7 +2,6 @@ import { AfterViewInit, Component, Input, NgZone, OnDestroy } from '@angular/cor
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Observable, Subscription } from 'rxjs';
-import { firstValueFrom } from 'rxjs';
 import { ModuleAwareComponent } from 'app/shared/modules';
 import { ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -22,7 +21,6 @@ import { DataTransferDataService } from '../../shared/services/data-transfer-dat
 import { MapImageProviderService } from '../services/map-image-provider.service';
 import { first } from 'rxjs/operators';
 import JSZip from 'jszip';
-import { ConsoleReporter } from 'jasmine';
 
 @Component({
   selector: 'app-map-view',
@@ -85,104 +83,20 @@ export class MapViewComponent<ExtraResult = void> extends MapComponent<ExtraResu
                    * zip the map w/ images
                    * unzip the map, populate image from `image_id`
                    */
-    /*
-    let image_hashes = new Set();
-    for (const node of this.graphCanvas.getGraph().nodes) {
-      if (node.image_id !== undefined) { // is image node
-        image_hashes.add(node.image_id);
-        let oneImage$: Observable<CanvasImageSource> = this.mapImageProviderService.get(node.image_id)
-        oneImage$.pipe(first()) // only take the first element
-          .subscribe({
-            next(img) {
-              img = img as HTMLImageElement;
-              console.log("image src is: " + img.src);
-              // one way to get the original image blob. It's a PROMISE though
-              let blob = fetch(img.src).then(r => r.blob());
-              console.log("image blob is:");
-              console.log(blob);
-              console.log("END image blob");
-            },
-            error(msg) {
-              console.error("error with subscriber: " + msg)
-            }
-          });
-      }
-    }
-    console.log(image_hashes)
-    if (image_hashes.size !== 0) {
-      // TODO: save images. Otherwise no images so move on with regular save
-    }
-    */
-
-    /*
-    const contentValue = new Blob([JSON.stringify(this.graphCanvas.getGraph())], {
-      type: MAP_MIMETYPE,
-    }); // change this blob to include images (and zip it)
-    */
-
-    /** an example that might be working
-     * var zip = new JSZip();
-     * zip.file("Hello.txt", "Hello World\n");
-     * var img = zip.folder("images");
-     * img.file("smile.gif", imgData, {base64: true});
-     * var content = zip.generate();
-     * location.href="data:application/zip;base64,"+content;
-     */
-
     const contentValue = new JSZip();
-    const imgs = contentValue.folder("images");
-    const imageNodeObservables = []
-    for (const node of this.graphCanvas.getGraph().nodes) {
-      if (node.image_id !== undefined) {
-        imageNodeObservables.push(this.mapImageProviderService.get(node.image_id));
-      }
-    }
-    console.log(imageNodeObservables);
-    const mapLoop = async _ => {
-      const promises = imageNodeObservables.map(async ob => {
-        return await firstValueFrom(ob);
-        // CF: https://indepth.dev/posts/1287/rxjs-heads-up-topromise-is-being-deprecated
-        // TODO: figure out `rxjs` version, use `toPromise()` if needed
+    contentValue.file("graph.json", JSON.stringify(this.graphCanvas.getGraph()));
+    contentValue.generateAsync({ type: "blob" })
+      .then(function (content) {
+        this.filesystemService.save([this.locator], { content })
+          .pipe(this.errorHandler.create({label: 'Update map'}))
+          .subscribe(() => {
+            this.unsavedChanges$.next(false);
+            this.emitModuleProperties();
+            this.snackBar.open('Map saved.', null, {
+              duration: 2000,
+            });
+          });
       });
-      const imageBlobs = await Promise.all(promises);
-      console.log(imageBlobs);
-    }
-
-    // contentValue.generateAsync({ type: "base64" }).then(function (base64) {
-    //   location.href = "data:application/zip;base64," + base64;
-    // });
-
-    // zip all images
-    // look for zipping byte array or input streams, or make a tmp dir and zip from that
-
-    // TODO: how to zip the map with ref to images?
-    // contentValue.generateAsync({type: MAP_SHORTHAND})
-    //   .then((contentValue) => {
-    //     this.filesystemService.save([this.locator], {contentValue})
-    //       .pipe(this.errorHandler.create({label: "Update Map"}))
-    //       .subscribe(() => {
-    //         this.unsavedChanges$.next(false);
-    //         this.emitModuleProperties();
-    //         this.snackBar.open('Map saved.', null, {
-    //           duration: 2000,
-    //         });
-    //       })
-    //   })
-      /*
-
-      // Push to backend to save
-      this.filesystemService.save([this.locator], {
-        contentValue,
-      })
-      .pipe(this.errorHandler.create({label: 'Update map'}))
-      .subscribe(() => {
-        this.unsavedChanges$.next(false);
-        this.emitModuleProperties();
-        this.snackBar.open('Map saved.', null, {
-          duration: 2000,
-        });
-      });
-      */
   }
 
   openCloneDialog() {
