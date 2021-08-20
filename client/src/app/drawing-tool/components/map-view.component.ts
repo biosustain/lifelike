97441ac1b1b8df6fec8bc49ab424e8eb8f83925a
Bob Zhang <chz026@ucsd.edu>
2021-08-20 11:40:52 -0700

    got json text in initMap, TODO: load it to graph

--- client/src/app/drawing-tool/components/map-view.component.ts
+++ client/src/app/drawing-tool/components/map-view.component.ts
@@ -88,7 +88,6 @@ export class MapViewComponent<ExtraResult = void> extends MapComponent<ExtraResu
     zip.file("graph.json", JSON.stringify(this.graphCanvas.getGraph()));
     zip.generateAsync({ type: "blob" })
       .then(function (content) {
-        // location.href = "data:application/zip;base64," + content; // how to download the zip
         console.log("here's content");
         console.log(content);
         filesystemService.save([locator], { contentValue: content })
