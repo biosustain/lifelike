2a41caaef8c7d8dc07e9d910dd3f59844ce91507
Bob Zhang <chz026@ucsd.edu>
2021-08-26 17:08:28 -0700

    added file-saver package, updated json verify schema. saving map with images as zip should be fine. TODO: load them upon refresh

--- client/src/app/drawing-tool/services/map-image-provider.service.ts
+++ client/src/app/drawing-tool/services/map-image-provider.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ResourceProvider } from '../../graph-viewer/utils/resource/resource-manager';
-import { Observable, of, Subject } from 'rxjs';
+import { Observable, of, Subject, from } from 'rxjs';
 
 @Injectable()
 export class MapImageProviderService implements ResourceProvider<string, CanvasImageSource> {
@@ -24,6 +24,16 @@ export class MapImageProviderService implements ResourceProvider<string, CanvasI
       };
       image.src = preloadedUrl;
       return subject;
+    } else {
+      // TODO: Return an error to be handled
+      return of(null);
+    }
+  }
+
+  getBlob(id: string): Observable<Blob> {
+    const preloadedUrl = this.preloadedUrls.get(id);
+    if (preloadedUrl != null) {
+      return from(fetch(preloadedUrl).then(r => r.blob()));
     } else {
       // TODO: Return an error to be handled
       return of(null);
