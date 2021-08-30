f42209e57c2884c8aa6e9975fceee13636e2e9b2
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-08-30 20:00:06 +0200

    encapsulate tree view

--- client/src/app/trace-viewer/components/tree-view.component.ts
+++ client/src/app/trace-viewer/components/tree-view.component.ts
@@ -10,8 +10,7 @@ interface TreeNode {
 @Component({
   selector: 'app-tree-view',
   templateUrl: './tree-view.component.html',
-  styleUrls: ['./tree-view.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./tree-view.component.scss']
 })
 export class TreeViewComponent {
   @Input() dataSource;
