1af3ddc43c7dc3a456948ac715417a03c82e3b80
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-09-03 14:11:04 +0200

    provide abstract version of tree-view component

--- client/src/app/shared/components/tree-view/tree-view.component.ts
+++ client/src/app/shared/components/tree-view/tree-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, TemplateRef, OnChanges, SimpleChanges, } from '@angular/core';
+import { Component, Input, TemplateRef, OnChanges, SimpleChanges, ViewEncapsulation, } from '@angular/core';
 import { NestedTreeControl } from '@angular/cdk/tree';
 
 interface TreeNode {
@@ -10,14 +10,41 @@ interface TreeNode {
 @Component({
   selector: 'app-tree-view',
   templateUrl: './tree-view.component.html',
-  styleUrls: ['./tree-view.component.scss']
+  styleUrls: ['./tree-view.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TreeViewComponent implements OnChanges {
+  /**
+   * @params:
+   * - dataSource - list of nodes to show
+   * - getChildren - node children accessor
+   * - hasChild - fast check for node children (without parsing them)
+   * - treeNode - template to render leaf node
+   * - nestTreeNode - template to render node with nested values
+   * Usage example:
+   *   <app-tree-view [dataSource]="dataSource"
+   *                 [getChildren]="getChildren"
+   *                 [hasChild]="hasChild"
+   *                 [treeNode]="treeNode"
+   *                 [nestedTreeNode]="nestTreeNode"
+   *  >
+   *      <!-- This is the tree node template for leaf nodes -->
+   *      <ng-template #treeNode let-node>
+   *          {{ node.label }}
+   *      </ng-template>
+   *      <!-- This is the tree node template for expandable nodes -->
+   *      <ng-template #nestTreeNode let-node>
+   *          {{ node.label }}
+   *          <!-- Optionally inject counter in desired part of templates -->
+   *          <span [attr.data-counter]="true"></span>
+   *      </ng-template>
+   *  </app-tree-view>
+   */
   @Input() dataSource;
   @Input() treeNode: TemplateRef<any>;
   @Input() nestedTreeNode: TemplateRef<any>;
   @Input() getChildren: (node: any) => Array<any> | undefined;
-  @Input() hasChild: (node: any) => boolean;
+  @Input() hasChild: (index: number, node: any) => boolean;
   treeControl;
 
   ngOnChanges({getChildren, hasChild}: SimpleChanges) {
@@ -25,7 +52,7 @@ export class TreeViewComponent implements OnChanges {
       this.treeControl = new NestedTreeControl<TreeNode>(getChildren.currentValue);
     }
     if (hasChild) {
-      this.hasChild = n => hasChild.currentValue(undefined, n);
+      this.hasChild = hasChild.currentValue;
     }
   }
 }
