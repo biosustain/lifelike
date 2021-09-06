a32ce211107793e468ff546157999bb6c90435a1
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-09-06 16:01:40 +0200

    added popups in advanced menu

---
+++ client/src/app/sankey-viewer/components/advanced-panel/advanced-panel-option.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, EventEmitter, Output, } from '@angular/core';
+import { SankeyAdvancedOptions } from '../interfaces';
+import { uuidv4 } from '../../../shared/utils';
+
+
+@Component({
+  selector: 'app-sankey-advanced-panel',
+  templateUrl: './advanced-panel.component.html',
+  styleUrls: ['./advanced-panel.component.scss'],
+})
+export class SankeyAdvancedPanelComponent {
+  @Input() options!: SankeyAdvancedOptions;
+  @Output() optionsChange = new EventEmitter<SankeyAdvancedOptions>();
+
+  uuid: string;
+
+  constructor() {
+    this.uuid = uuidv4();
+  }
+
+  update() {
+    this.optionsChange.emit(this.options);
+  }
+
+  customSizingUpdate() {
+    this.options.selectedPredefinedValueAccessor = {
+      description: 'Customised'
+    };
+    this.update();
+  }
+}
