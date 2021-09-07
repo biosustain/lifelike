6f5ff9c896c3187b9f8c4b0e2461d93c67810a53
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-09-07 10:48:58 +0200

    clean up advanced menu code

--- client/src/app/sankey-viewer/components/advanced-panel/advanced-panel-option.component.ts
+++ client/src/app/sankey-viewer/components/advanced-panel/advanced-panel-option.component.ts
@@ -1,31 +1,12 @@
-import { Component, Input, EventEmitter, Output, } from '@angular/core';
-import { SankeyAdvancedOptions } from '../interfaces';
-import { uuidv4 } from '../../../shared/utils';
+import { Component, Input, ViewEncapsulation, } from '@angular/core';
 
 
 @Component({
-  selector: 'app-sankey-advanced-panel',
-  templateUrl: './advanced-panel.component.html',
-  styleUrls: ['./advanced-panel.component.scss'],
+  selector: 'app-sankey-advanced-panel-option',
+  templateUrl: './advanced-panel-option.component.html',
+  styleUrls: ['./advanced-panel-option.component.scss']
 })
-export class SankeyAdvancedPanelComponent {
-  @Input() options!: SankeyAdvancedOptions;
-  @Output() optionsChange = new EventEmitter<SankeyAdvancedOptions>();
-
-  uuid: string;
-
-  constructor() {
-    this.uuid = uuidv4();
-  }
-
-  update() {
-    this.optionsChange.emit(this.options);
-  }
-
-  customSizingUpdate() {
-    this.options.selectedPredefinedValueAccessor = {
-      description: 'Customised'
-    };
-    this.update();
-  }
+export class SankeyAdvancedPanelOptionComponent {
+  @Input() title!: string;
+  @Input() moreInfo: string;
 }
