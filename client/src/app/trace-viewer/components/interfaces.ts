b7e50e1c5fb44c7094496d25c44c0a72b0345fc2
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-09-13 14:51:31 +0200

    improve docs; use implicit types as intermidiates in trace view

--- client/src/app/trace-viewer/components/interfaces.ts
+++
@@ -1,9 +0,0 @@
-import visNetwork from 'vis-network';
-import { SankeyNode } from '../../sankey-viewer/components/interfaces';
-
-export interface LinkedNode {
-  fromEdges: Array<any>;
-  toEdges: Array<any>;
-}
-
-export type IntermediateNodeType = visNetwork.Node & SankeyNode & LinkedNode;
