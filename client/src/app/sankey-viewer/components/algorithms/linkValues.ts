98452297dd7f37b57db9ddc6ba4aee9e6c1a4be2
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-09-15 18:00:49 +0200

    fix input count and network trace dropdown regression

--- client/src/app/sankey-viewer/components/algorithms/linkValues.ts
+++ client/src/app/sankey-viewer/components/algorithms/linkValues.ts
@@ -1,6 +1,7 @@
 import * as d3Sankey from 'd3-sankey-circular';
 import { representativePositiveNumber } from '../utils';
 import { DirectedTraversal } from '../../services/directed-traversal';
+import { CustomisedSankeyLayoutService } from '../../services/customised-sankey-layout.service';
 
 export const fixedValue = value => ({links}) => {
   links.forEach(l => {
@@ -73,70 +74,35 @@ export const fractionOfFixedNodeValue = ({links, nodes}) => {
     }
   };
 };
+
 export const inputCount = ({links, nodes, _inNodes, _outNodes}: any) => {
-  // region TODO: once layout service supports circular rel it should be used instead
-  // this is temporary workaround
   links.forEach(l => {
-    l.value = 1;
-    l.s = l.source;
-    l.t = l.target;
+    l._value = 1;
   });
-  d3Sankey.sankeyCircular()
-    .nodeId(n => n.id)
-    .nodePadding(1)
-    .nodePaddingRatio(0.5)
-    .nodeAlign(d3Sankey.sankeyRight)
-    .nodeWidth(10)
-    ({nodes, links});
-  nodes.forEach(n => {
-    n._sourceLinks = n.sourceLinks;
-    n._targetLinks = n.targetLinks;
-  });
-  // endregion
+  // @ts-ignore
+  const layout = new CustomisedSankeyLayoutService();
+  layout.computeNodeLinks({nodes, links});
+  layout.identifyCircles({nodes, links});
+  layout.computeNodeValues({nodes, links});
+  layout.computeNodeDepths({nodes, links});
   const dt = new DirectedTraversal([_inNodes, _outNodes]);
   dt.reverse();
   [...nodes].sort(dt.depthSorter()).forEach(n => {
     if (dt.startNodes.includes(n.id)) {
-      n.value = 1;
+      n._value = 1;
     } else {
-      n.value = 0;
+      n._value = 0;
     }
-    n.value = dt.prevLinks(n).reduce((a, l) => a + l.value, n.value || 0);
-    const outFrac = n.value / dt.nextLinks(n).length;
+    n._value = dt.prevLinks(n).reduce((a, l) => a + l._value, n._value || 0);
+    const outFrac = n._value / dt.nextLinks(n).length;
     dt.nextLinks(n).forEach(l => {
-      l.value = outFrac;
+      l._value = outFrac;
     });
   });
   return {
     nodes: nodes
-      .filter(n => n.sourceLinks.length + n.targetLinks.length > 0)
-      .map(({
-              value,
-              depth,
-              index,
-              height,
-              sourceLinks,
-              targetLinks,
-              _sourceLinks,
-              _targetLinks,
-              layer,
-              x0, x1,
-              y0, y1,
-              ...node
-            }) => ({
-        ...node
-      })),
-    links: links.map(({
-                        value = 0.001,
-                        y0, y1,
-                        s, t,
-                        ...link
-                      }) => ({
-      ...link,
-      _value: value,
-      source: s,
-      target: t
-    })),
+      .filter(n => n._sourceLinks.length + n._targetLinks.length > 0),
+    links,
     _sets: {
       link: {
         _value: true
@@ -144,6 +110,7 @@ export const inputCount = ({links, nodes, _inNodes, _outNodes}: any) => {
     }
   };
 };
+
 export const byProperty = property => ({links}) => {
   links.forEach(l => {
     l._value = representativePositiveNumber(l[property]);
