5882e19fffacfa42320e0edfb7be8f3df9c8099a
Binh Vu <binhvu.dev@gmail.com>
2021-09-16 12:24:18 -0700

    Update MeSH query to exclude GlobalInclusion labels cause those are most likely db_Lifelike nodes. Also replaced id(n) with n.eid cause id(n) is not the same across different graphs.

--- src/create_data_file.py
+++ src/create_data_file.py
@@ -21,7 +21,7 @@ def create_data_file(db: Database, filepath: str, query: str):
     with open(filepath, 'w', newline='\n') as tsvfile:
         results = db.get_data(query)
         writer = csv.writer(tsvfile, delimiter='\t', quotechar='"')
-        writer.writerow(['node_id', 'entity_type'])
+        writer.writerow(['entity_id', 'entity_type'])
 
         # a node could have 2+ entity labels (rare)
         # but at the same time, a global inclusion could've
@@ -33,22 +33,22 @@ def create_data_file(db: Database, filepath: str, query: str):
         node_id_labels = {}
         node_id_edge_entity_type = defaultdict(set)
         for index, row in results.iterrows():
-            node_id_global[row.node_id].add(row.is_global)
-            node_id_labels[row.node_id] = row.node_labels
-            node_id_edge_entity_type[row.node_id] = node_id_edge_entity_type[row.node_id].union(
+            node_id_global[row.entity_id].add(row.is_global)
+            node_id_labels[row.entity_id] = row.node_labels
+            node_id_edge_entity_type[row.entity_id] = node_id_edge_entity_type[row.entity_id].union(
                 set(entity_type for entity_type in row.edge_entity_types))
 
         del results
 
-        for node_id, global_set in node_id_global.items():
+        for entity_id, global_set in node_id_global.items():
             if True not in global_set:
-                writer.writerow([node_id, ','.join(label for label in node_id_labels[node_id])])
+                writer.writerow([entity_id, ','.join(label for label in node_id_labels[entity_id])])
             else:
-                if len(node_id_labels[node_id]) == 1:
-                    writer.writerow([node_id, ','.join(label for label in node_id_labels[node_id])])
+                if len(node_id_labels[entity_id]) == 1:
+                    writer.writerow([entity_id, ','.join(label for label in node_id_labels[entity_id])])
                 else:
-                    writer.writerow([node_id, ','.join(
-                        label for label in set(node_id_labels[node_id]) - node_id_edge_entity_type[node_id])])
+                    writer.writerow([entity_id, ','.join(
+                        label for label in set(node_id_labels[entity_id]) - node_id_edge_entity_type[entity_id])])
 
 
 def azure_upload(filepath: str, filename: str, zip_filename: str, zip_filepath: str):
@@ -72,16 +72,17 @@ def azure_upload(filepath: str, filename: str, zip_filename: str, zip_filepath: 
 
 if __name__ == '__main__':
     db = get_database()
-    filename = 'jira-LL-3625-add-entity-type-array-protein.tsv'
+    filename = 'jira-LL-3625-add-entity-type-array-mesh.tsv'
     filepath = os.path.join(get_data_dir(), filename)
-    zip_filename = 'jira-LL-3625-add-entity-type-array-protein.zip'
+    zip_filename = 'jira-LL-3625-add-entity-type-array-mesh.zip'
     zip_filepath = os.path.join(get_data_dir(), zip_filename)
     query = """
-    MATCH (n:Protein)-[r:HAS_SYNONYM]-(s:Synonym)
-    WITH n, r, s, [l IN labels(n) WHERE NOT l starts WITH 'db_' AND NOT l IN ['Complex', 'GlobalInclusion']] as entityLabel
+    MATCH (n:db_MESH)-[r:HAS_SYNONYM]-(s:Synonym)
+    WHERE NOT n:GlobalInclusion
+    WITH n, r, [l IN labels(n) WHERE NOT l IN ['db_MESH', 'TopicalDescriptor', 'TreeNumber']] AS entityLabel
     WHERE size(entityLabel) >= 1
-    RETURN DISTINCT id(n) AS node_id, entityLabel AS node_labels,
-        exists(r.global_inclusion) AS is_global, collect(DISTINCT r.entity_type) AS edge_entity_types
+    RETURN DISTINCT n.eid AS entity_id, entityLabel AS node_labels,
+    exists(r.global_inclusion) AS is_global, collect(DISTINCT r.entity_type) AS edge_entity_types
     """
     create_data_file(db, filepath, query)
     azure_upload(filepath, filename, zip_filename, zip_filepath)
