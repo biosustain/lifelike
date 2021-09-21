e21dbdd6c643e40d77829df0a9af8c0e851a05ca
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-09-21 15:27:55 +0200

    reverse polyfill modification

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -59,7 +59,8 @@ import '@angular/localize/init';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'hammerjs';
 import 'web-animations-js';
 
 /***************************************************************************************************
