5ccb2fe007474681b8e794dea6077b42dcbff058
Binh Vu <binhvu.dev@gmail.com>
2021-09-21 14:58:30 -0700

    Moved all files to new graph-db folder to prep for merging into kg-prototypes repo...

--- migrations/java/src/main/java/edu/ucsd/sbrg/liquibase/storage/Storage.java
+++
@@ -1,8 +0,0 @@
-package edu.ucsd.sbrg.liquibase.storage;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
-public interface Storage {
-    public OutputStream download(String fileName) throws IOException;
-}
