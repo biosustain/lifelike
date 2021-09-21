b7ffb91ebdfa2c4e41997510b8f8270bc2e34b6d
Binh Vu <binhvu.dev@gmail.com>
2021-09-21 11:05:02 -0700

    Updated app.py to parse kegg and produce TSV files, upload them to Azure with the new AzureCloudStorage.

--- src/app.py
+++ src/app.py
@@ -9,6 +9,7 @@ import coloredlogs
 import biocyc.biocyc_parser as biocyc_parser
 import chebi.chebi_parser as chebi_parser
 import go.go_parser as go_parser
+import kegg.kegg_parser as kegg_parser
 import mesh.add_disease_synonyms_by_pruning_disease as add_disease_synonyms_by_pruning_disease
 import mesh.mesh_annotations as mesh_annotations
 import ncbi.ncbi_gene_parser as ncbi_gene_parser
@@ -16,20 +17,21 @@ import ncbi.ncbi_taxonomy_parser as ncbi_taxonomy_parser
 import regulondb.regulondb_parser as regulondb_parser
 import uniprot.uniprot_parser as uniprot_parser
 
-_LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
+_LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
 _LOG_MAX_SIZE = 1024 * 1024
 _LOG_MAX_FILES = 5
 
 DOMAIN_PARSERS = {
-    "biocyc": biocyc_parser,
-    "chebi": chebi_parser,
-    "go": go_parser,
-    "mesh-add-disease-synonyms": add_disease_synonyms_by_pruning_disease,
-    "mesh-annotations": mesh_annotations,
-    "ncbi-gene": ncbi_gene_parser,
-    "ncbi-taxonomy": ncbi_taxonomy_parser,
-    "regulondb": regulondb_parser,
-    "uniprot": uniprot_parser,
+    'biocyc': biocyc_parser,
+    'chebi': chebi_parser,
+    'go': go_parser,
+    'kegg': kegg_parser,
+    'mesh-add-disease-synonyms': add_disease_synonyms_by_pruning_disease,
+    'mesh-annotations': mesh_annotations,
+    'ncbi-gene': ncbi_gene_parser,
+    'ncbi-taxonomy': ncbi_taxonomy_parser,
+    'regulondb': regulondb_parser,
+    'uniprot': uniprot_parser,
 }
 
 
@@ -41,33 +43,34 @@ def parse_args(argv):
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--log-file",
-        help="Append log messages to file; files are rotated at 1MB",
+        '--log-file',
+        help='Append log messages to file; files are rotated at 1MB',
         type=Path,
     )
     parser.add_argument(
-        "--log-level",
-        default="INFO",
-        choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
+        '--log-level',
+        default='INFO',
+        choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'),
     )
 
-    subparser = parser.add_subparsers(dest="domain", required=True)
-    biocyc_parser = subparser.add_parser("biocyc")
+    subparser = parser.add_subparsers(dest='domain', required=True)
+    biocyc_parser = subparser.add_parser('biocyc')
     biocyc_parser.add_argument(
-        "--data-sources",
-        nargs="*",
-        help="A list of data sources to load, e.g. PseudomonasCyc YeastCyc EcoCyc HumanCyc",
+        '--data-sources',
+        nargs='*',
+        help='A list of data sources to load, e.g. PseudomonasCyc YeastCyc EcoCyc HumanCyc',
     )
 
     # parsers with no additional arguments
-    subparser.add_parser("chebi")
-    subparser.add_parser("go")
-    subparser.add_parser("mesh-add-disease-synonyms")
-    subparser.add_parser("mesh-annotations")
-    subparser.add_parser("ncbi-gene")
-    subparser.add_parser("ncbi-taxonomy")
-    subparser.add_parser("regulondb")
-    subparser.add_parser("uniprot")
+    subparser.add_parser('chebi')
+    subparser.add_parser('go')
+    subparser.add_parser('kegg')
+    subparser.add_parser('mesh-add-disease-synonyms')
+    subparser.add_parser('mesh-annotations')
+    subparser.add_parser('ncbi-gene')
+    subparser.add_parser('ncbi-taxonomy')
+    subparser.add_parser('regulondb')
+    subparser.add_parser('uniprot')
 
     return parser.parse_args(argv)
 
@@ -90,12 +93,12 @@ def main(argv):
 
     setup_logging(args)
 
-    logger = logging.getLogger("main")
+    logger = logging.getLogger('main')
     logger.info(
-        "Executing "
+        'Executing '
         + __file__
-        + " with arguments: "
-        + ", ".join(["%s=%s" % (key, value) for (key, value) in args.__dict__.items()])
+        + ' with arguments: '
+        + ', '.join(['%s=%s' % (key, value) for (key, value) in args.__dict__.items()])
     )
 
     # get parser function using args.domain
@@ -103,11 +106,11 @@ def main(argv):
 
     # call main function and pass arguments
 
-    if args.domain == "biocyc":
+    if args.domain == 'biocyc':
         parser.main(args)
     else:
         parser.main()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1:])
