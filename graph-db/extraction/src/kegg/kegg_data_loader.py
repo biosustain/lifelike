b6740072651bb11a836d0c7b19fc0be24f97b7aa
rcai92130 <rcai@ucsd.edu>
2021-09-22 15:02:17 -0700

    Fix kegg parser and data loader

--- graph-db/extraction/src/kegg/kegg_data_loader.py
+++ graph-db/extraction/src/kegg/kegg_data_loader.py
@@ -1,13 +1,12 @@
 from common.base_data_loader import BaseDataLoader
 from common.constants import *
 from common.database import *
-from kegg_parser import KeggParser
-
+from common.utils import get_data_dir
 
 class KeggDataLoader(BaseDataLoader):
     def __init__(self, basedir):
-        BaseDataLoader.__init__(DB_KEGG.lower(), basedir)
-        parser = KeggParser(basedir)
+        BaseDataLoader.__init__(self, DB_KEGG.lower(), basedir)
+        self.logger = logging.getLogger(__name__)
 
     def create_indexes(self, database: Database):
         database.create_constraint(NODE_KEGG, PROP_ID, "constraint_kegg_id")
@@ -20,53 +19,67 @@ class KeggDataLoader(BaseDataLoader):
 
     def load_data_to_neo4j(self, database: Database):
         # load pathway
-        logging.info('load pathways')
+        self.logger.info('load pathways')
         df = pd.read_csv(os.path.join(self.output_dir, 'pathway_data.tsv'), sep='\t', dtype={PROP_ID: str})
         df[PROP_DATA_SOURCE] = 'KEGG'
         cols = [PROP_ID, PROP_NAME, PROP_DATA_SOURCE]
         query = get_create_nodes_query(NODE_KEGG, PROP_ID, cols, [NODE_PATHWAY])
+        self.logger.info(query)
         database.load_data_from_dataframe(df, query)
 
         # load ko
-        logging.info('load ko')
+        self.logger.info('load ko')
         df = pd.read_csv(os.path.join(self.output_dir, 'ko_data.tsv'), sep='\t')
         df[PROP_DATA_SOURCE] = 'KEGG'
         cols = [PROP_ID, PROP_NAME, PROP_DEF, PROP_DATA_SOURCE]
         query = get_create_nodes_query(NODE_KEGG, PROP_ID, cols, [NODE_KO])
+        self.logger.info(query)
         database.load_data_from_dataframe(df, query)
 
-        logging.info('load gene')
+        self.logger.info('load gene')
         df = pd.read_csv(os.path.join(self.output_dir, 'gene_data.tsv'), sep='\t', dtype={PROP_GENE_ID: str})
         df[PROP_DATA_SOURCE] = 'KEGG'
-        cols = [PROP_ID, PROP_GENOME, PROP_DATA_SOURCE]
+        cols = [PROP_ID, PROP_GENOME, PROP_GENE_ID, PROP_DATA_SOURCE]
         query = get_create_nodes_query(NODE_KEGG, PROP_ID, cols, [NODE_GENE])
+        self.logger.info(query)
         database.load_data_from_dataframe(df, query, 5000)
-        logging.info('load gene-ncbi gene relationships')
+        self.logger.info('load gene-ncbi gene relationships')
         query = get_create_relationships_query(NODE_KEGG, PROP_ID, PROP_ID, NODE_GENE, PROP_ID, PROP_GENE_ID,
                                                REL_IS)
+        self.logger.info(query)
         database.load_data_from_dataframe(df, query, 5000)
 
-        logging.info('load gene-ko relationships')
+        self.logger.info('load gene-ko relationships')
         cols = ['gene', 'ko']
         query = get_create_relationships_query(NODE_KEGG, PROP_ID, 'gene', NODE_KO, PROP_ID, 'ko', 'HAS_KO')
+        self.logger.info(query)
         database.load_csv_file(os.path.join(self.output_dir, 'gene2ko_data.tsv'), cols, query, 1, '\t', 5000)
 
-        logging.info('load ko-pathway relationships')
+        self.logger.info('load ko-pathway relationships')
         cols = ['ko', 'pathway']
         query = get_create_relationships_query(NODE_KO, PROP_ID, 'ko', NODE_PATHWAY, PROP_ID, 'pathway', 'IN_PATHWAY')
+        self.logger.info(query)
         database.load_csv_file(os.path.join(self.output_dir, 'ko2pathway_data.tsv'), cols, query, 1, '\t', 5000,
                                ['pathway'])
 
-        logging.info('load genome-pathway association')
+        self.logger.info('load genome-pathway association')
         df = pd.read_csv(os.path.join(self.output_dir, 'genome2pathway_data.tsv'), sep='\t', dtype={'pathway': str})
         df[PROP_DATA_SOURCE] = 'KEGG'
         df_genome = df[['genome', PROP_DATA_SOURCE]].drop_duplicates()
-        query = get_create_nodes_query(NODE_GENOME, PROP_ID, 'genome', [PROP_DATA_SOURCE], [NODE_KEGG])
+        df_genome.columns = [PROP_ID, PROP_DATA_SOURCE]
+        query = get_create_nodes_query(NODE_GENOME, PROP_ID, [PROP_DATA_SOURCE], [NODE_KEGG])
+        self.logger.info(query)
         database.load_data_from_dataframe(df_genome, query, 5000)
         cols = ['genome', 'pathway']
         query = get_create_relationships_query(NODE_GENOME, PROP_ID, 'genome', NODE_PATHWAY, PROP_ID, 'pathway', REL_HAS_PATHWAY)
+        self.logger.info(query)
         database.load_data_from_dataframe(df, query, 5000)
 
 
-
+def main():
+    database = get_database()
+    loader = KeggDataLoader(get_data_dir())
+    loader.create_indexes(database)
+    loader.load_data_to_neo4j(database)
+    database.close()
 
