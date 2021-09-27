90372e4a66237b65c28b36372c39118528848e58
Maciek <mgrzelczak97@gmail.com>
2021-09-27 20:12:29 +0200

    LL-1926: readd removed imports

--- client/src/app/drawing-tool/components/map-editor/map-editor.component.ts
+++ client/src/app/drawing-tool/components/map-editor/map-editor.component.ts
@@ -30,6 +30,8 @@ import { MimeTypes } from 'app/shared/constants';
 import { DeleteKeyboardShortcutBehavior } from 'app/graph-viewer/renderers/canvas/behaviors/delete-keyboard-shortcut.behavior';
 import { PasteKeyboardShortcutBehavior } from 'app/graph-viewer/renderers/canvas/behaviors/paste-keyboard-shortcut.behavior';
 import { HistoryKeyboardShortcutsBehavior } from 'app/graph-viewer/renderers/canvas/behaviors/history-keyboard-shortcuts.behavior';
+import {ImageUploadBehavior} from 'app/graph-viewer/renderers/canvas/behaviors/image-upload.behavior';
+import {DragDropEntityBehavior} from 'app/graph-viewer/renderers/canvas/behaviors/drag-drop-entity.behavior';
 
 import { KnowledgeMap, UniversalGraph } from '../../services/interfaces';
 import { MapViewComponent } from '../map-view.component';
