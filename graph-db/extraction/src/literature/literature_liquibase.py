5bb69289703109e95ccee977e53e8fc479d7af1b
Binh Vu <binhvu.dev@gmail.com>
2021-10-15 16:27:56 -0700

    Try splitting the cyphers into two parts, create literature, and create snippet-pub.
    If revert due to no performance improvement, keep the MERGE (n1:LiteratureEntity {eid:row.entry1_id}) part - NOTE need to use eid not id.

--- graph-db/extraction/src/literature/literature_liquibase.py
+++ graph-db/extraction/src/literature/literature_liquibase.py
@@ -26,6 +26,7 @@ class LiteratureChangeLog(ChangeLog):
         if initial_load:
             self.add_index_change_set()
         self.load_literature_model()
+        self.load_literature_snippet_pub_rels()
         self.load_literature_chemical_mapped_rels()
         self.load_literature_disease_mapped_rels()
         self.load_literature_gene_mapped_rels()
@@ -42,6 +43,21 @@ class LiteratureChangeLog(ChangeLog):
                 id = f'{self.id_prefix} {id}'
             comment = ''
             query = LiteratureDataParser.get_create_literature_query(entity1_type, entity2_type)
+            changeset = CustomChangeSet(id, self.author, comment, query, f'{self.file_prefix}{filename}')
+            self.change_sets.append(changeset)
+
+    def load_literature_snippet_pub_rels(self):
+        for filename, (entity1_type, entity2_type) in [
+            (ZENODO_CHEMICAL2DISEASE_FILE, (NODE_CHEMICAL, NODE_DISEASE)),
+            (ZENODO_CHEMICAL2GENE_FILE, (NODE_CHEMICAL, NODE_GENE)),
+            (ZENODO_GENE2DISEASE_FILE, (NODE_GENE, NODE_DISEASE)),
+            (ZENODO_GENE2GENE_FILE, (NODE_GENE, NODE_GENE))
+        ]:
+            id = f'Zenodo literature snippet data {entity1_type}-{entity2_type} on date {self.date_tag}'
+            if self.id_prefix:
+                id = f'{self.id_prefix} {id}'
+            comment = f'Link snippets for {entity1_type}-{entity2_type} to publication'
+            query = LiteratureDataParser.get_create_literature_snippet_pub_query()
             changeset = CustomChangeSet(id, self.author, comment, query, f'{self.file_prefix}{filename}')
             self.change_sets.append(changeset)
 
@@ -103,6 +119,7 @@ class LiteratureChangeLog(ChangeLog):
         queries.append(get_create_constraint_query(NODE_LITERATURE_DISEASE, PROP_ID, 'constraint_literaturedisease_id') + ';')
         queries.append(get_create_constraint_query(NODE_LITERATURE_CHEMICAL, PROP_ID, 'constraint_literaturechemical_id') + ';')
         queries.append(get_create_constraint_query(NODE_LITERATURE_GENE, PROP_ID, 'constraint_literaturegene_id') + ';')
+        queries.append(get_create_constraint_query(NODE_PUBLICATION, PROP_PUBLICATION_ID, 'constraint_publication_pmid') + ';')
         return queries
 
     def add_index_change_set(self):
