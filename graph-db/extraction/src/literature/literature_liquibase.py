4c0f0882fd667c63b6385bfad5b77b54594f86f6
Binh Vu <binhvu.dev@gmail.com>
2021-11-01 18:07:37 -0700

    Updated the query again, need to add display_name cause that is used by visualizer.
    It is not ideal to have this property, because it is a duplicate data property and is at risk for being outdated.

--- graph-db/extraction/src/literature/literature_liquibase.py
+++ graph-db/extraction/src/literature/literature_liquibase.py
@@ -42,7 +42,10 @@ class LiteratureChangeLog(ChangeLog):
             if self.id_prefix:
                 id = f'{self.id_prefix} {id}'
             comment = f'Split creation of {entity1_type}-{entity2_type} nodes and assocation from snippet creation; seem to be faster instead of one giant cypher' \
-                '; Need to use foreach because if some entities can have two entity type so the on create will not set the second, and cannot use normal set cause it could add a duplicate'
+                '. Need to use foreach because if some entities can have two entity type so the on create will not set the second, and cannot use normal set cause it could add a duplicate.\n' \
+                'The display_name property is a duplicate data property, and only used for the visualizer. Not sure why this decision was made, cause the information is' \
+                ' available in the (n)-[MAPPED_TO]-(m) in m.name, the extra relationship does not seem to make any difference, in fact it is faster since the current visualizer query returns the entire node.\n' \
+                'Need to also consider the fact the property could be at risk for being outdated.'
             query = LiteratureDataParser.get_create_literature_query(entity1_type, entity2_type)
             changeset = CustomChangeSet(id, self.author, comment, query, f'{self.file_prefix}{filename}')
             self.change_sets.append(changeset)
@@ -71,11 +74,11 @@ class LiteratureChangeLog(ChangeLog):
         query = """
         CALL apoc.periodic.iterate(
         'MATCH (n:db_Literature:LiteratureChemical) WHERE n.eid CONTAINS "MESH:" RETURN n',
-        'MERGE (n)-[:MAPPED_TO]->(c:db_MESH {eid:split(n.eid, ":")[1]}) FOREACH (item IN CASE WHEN NOT "Chemical" IN labels(c) THEN [1] ELSE [] END | SET c:Chemical)',
+        'MERGE (c:db_MESH {eid:split(n.eid, ":")[1]}) WITH c,n MERGE (n)-[:MAPPED_TO]->(c) FOREACH (item IN CASE WHEN NOT "Chemical" IN labels(c) THEN [1] ELSE [] END | SET c:Chemical)',
         {batchSize:10000});
         CALL apoc.periodic.iterate(
         'MATCH (n:db_Literature:LiteratureChemical) WHERE n.eid CONTAINS "CHEBI:" RETURN n',
-        'MERGE (n)-[:MAPPED_TO]->(c:db_CHEBI {eid:split(n.eid, ":")[1]}) FOREACH (item IN CASE WHEN NOT "Chemical" IN labels(c) THEN [1] ELSE [] END | SET c:Chemical)',
+        'MERGE (c:db_CHEBI {eid:split(n.eid, ":")[1]}) WITH c,n MERGE (n)-[:MAPPED_TO]->(c) FOREACH (item IN CASE WHEN NOT "Chemical" IN labels(c) THEN [1] ELSE [] END | SET c:Chemical)',
         {batchSize:10000});
         """
         changeset = ChangeSet(id, self.author, comment, query)
@@ -89,11 +92,11 @@ class LiteratureChangeLog(ChangeLog):
         query = """
         CALL apoc.periodic.iterate(
         'MATCH (n:db_Literature:LiteratureDisease) WHERE n.eid CONTAINS "MESH:" RETURN n',
-        'MERGE (n)-[:MAPPED_TO]->(d:db_MESH {eid:split(n.eid, ":")[1]}) FOREACH (item IN CASE WHEN NOT "Disease" IN labels(d) THEN [1] ELSE [] END | SET d:Disease)',
+        'MERGE (d:db_MESH {eid:split(n.eid, ":")[1]}) WITH d,n MERGE (n)-[:MAPPED_TO]->(d) FOREACH (item IN CASE WHEN NOT "Disease" IN labels(d) THEN [1] ELSE [] END | SET d:Disease)',
         {batchSize:10000});
         CALL apoc.periodic.iterate(
         'MATCH (n:db_Literature:LiteratureDisease) WHERE NOT n.eid CONTAINS "MESH:" RETURN n',
-        'MERGE (n)-[:MAPPED_TO]->(d:Disease {eid:n.eid})',
+        'MERGE (d:Disease {eid:n.eid}) WITH d,n MERGE (n)-[:MAPPED_TO]->(d)',
         {batchSize:10000});
         """
         changeset = ChangeSet(id, self.author, comment, query)
@@ -107,7 +110,7 @@ class LiteratureChangeLog(ChangeLog):
         query = """
         CALL apoc.periodic.iterate(
         'MATCH (n:db_Literature:LiteratureGene) RETURN n',
-        'MERGE (n)-[:MAPPED_TO]->(g:db_NCBI:Gene {eid:n.eid})',
+        'MERGE (g:db_NCBI:Gene {eid:n.eid}) WITH g,n MERGE (n)-[:MAPPED_TO]->(g)',
         {batchSize:10000});
         """
         changeset = ChangeSet(id, self.author, comment, query)
