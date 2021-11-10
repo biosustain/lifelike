4ff1d333dd7ca9fea378a00ae168d7ee01465347
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2021-11-10 11:36:18 +0100

    ngbTooltip version

--- client/src/app/shared/directives/text-truncate.directive.ts
+++ client/src/app/shared/directives/text-truncate.directive.ts
@@ -1,4 +1,18 @@
-import { Directive, ElementRef, Renderer2, Input, AfterViewInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  Input,
+  AfterViewInit,
+  Injector,
+  ComponentFactoryResolver,
+  ViewContainerRef,
+  NgZone,
+  ChangeDetectorRef,
+  ApplicationRef
+} from '@angular/core';
+
+import { NgbTooltip, NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
 
 /**
  * Show tooltip only if text offloads
@@ -7,21 +21,39 @@ import { Directive, ElementRef, Renderer2, Input, AfterViewInit } from '@angular
   // tslint:disable-next-line:directive-selector
   selector: '.text-truncate'
 })
-export class TextTruncateDirective implements AfterViewInit {
+// @ts-ignore
+export class TextTruncateDirective extends NgbTooltip implements AfterViewInit {
   constructor(
-    private readonly renderer: Renderer2,
-    private readonly element: ElementRef
+    protected _elementRef: ElementRef<HTMLElement>,
+    protected _renderer: Renderer2,
+    protected injector: Injector,
+    protected componentFactoryResolver: ComponentFactoryResolver,
+    protected viewContainerRef: ViewContainerRef,
+    protected config: NgbTooltipConfig,
+    protected _ngZone: NgZone,
+    protected _changeDetector: ChangeDetectorRef,
+    protected applicationRef: ApplicationRef
   ) {
+    super(
+      _elementRef,
+      _renderer,
+      injector,
+      componentFactoryResolver,
+      viewContainerRef,
+      config,
+      _ngZone,
+      document,
+      _changeDetector,
+      applicationRef
+    );
   }
 
   @Input() title: string;
 
   ngAfterViewInit() {
-    const {nativeElement} = this.element;
-    if (nativeElement.scrollWidth > nativeElement.offsetWidth) {
-      this.renderer.setAttribute(nativeElement, 'title', this.title || nativeElement.innerText);
-    } else {
-      this.renderer.removeAttribute(nativeElement, 'title');
-    }
+    const {nativeElement: {scrollWidth, offsetWidth}} = this._elementRef;
+    this.ngbTooltip = this.title || this._elementRef.nativeElement.innerText;
+    this.disableTooltip = scrollWidth <= offsetWidth;
+    this.container = 'body';
   }
 }
