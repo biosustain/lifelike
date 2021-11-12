9c44120ba1ea05ed78936bb77c328c7c0cce8cc7
Binh Vu <binhvu.dev@gmail.com>
2021-11-12 08:24:29 -0800

    Added a download in chunk implementation to handle large files.

--- graph-db/migration/liquibase-src/src/main/java/edu/ucsd/sbrg/storage/AzureCloudStorage.java
+++ graph-db/migration/liquibase-src/src/main/java/edu/ucsd/sbrg/storage/AzureCloudStorage.java
@@ -1,15 +1,19 @@
 package edu.ucsd.sbrg.storage;
 
+import com.azure.core.util.Context;
 import com.azure.storage.file.share.ShareDirectoryClient;
 import com.azure.storage.file.share.ShareFileClient;
 import com.azure.storage.file.share.ShareFileClientBuilder;
+import com.azure.storage.file.share.models.ShareFileRange;
+import edu.ucsd.sbrg.extract.FileExtract;
 
-import java.io.IOException;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.time.Duration;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 public class AzureCloudStorage extends CloudStorage {
@@ -29,12 +33,90 @@ public class AzureCloudStorage extends CloudStorage {
         return this.storageClient;
     }
 
+    /**
+     * Download the entire file blob from the cloud.
+     *
+     * @param fileName file to download.
+     * @return
+     * @throws IOException
+     */
     @Override
     public OutputStream download(String fileName) throws IOException {
         ShareFileClient fileClient = this.initStorageClient().getFileClient(fileName);
         OutputStream out = new ByteArrayOutputStream();
         fileClient.download(out);
         return out;
+    }
+
+    /**
+     * Download the file in chunks from the cloud.
+     *
+     * @param fileExtract FileExtract class, contains filename, etc
+     * @throws IOException
+     */
+    @Override
+    public void downloadToFile(FileExtract fileExtract) throws IOException {
+        ShareFileClient fileClient = this.initStorageClient().getFileClient(fileExtract.getFileName());
+
+        // chunk download
+        int chunkSize = 1024*1024*100; // 100MB
+        long totalFileSize = fileClient.getProperties().getContentLength();
+        long remainingChunks = totalFileSize;
+        long startPosition = 0;
+        String localZip = fileExtract.getFilePath().replace(fileExtract.getFileExtension(), ".zip");
+        new File(localZip).createNewFile();
+
+        do {
+            ByteArrayOutputStream bao = new ByteArrayOutputStream();
+            fileClient.downloadWithResponse(bao, new ShareFileRange(startPosition, startPosition + chunkSize),
+                    false, Duration.ofSeconds(60), Context.NONE);
+
+            try (FileOutputStream out = new FileOutputStream(localZip, true)) {
+                FileChannel ch = out.getChannel();
+                ch.write(ByteBuffer.wrap(bao.toByteArray()));
+            }
+            bao.close();
+            startPosition += chunkSize + 1;
+            remainingChunks -= chunkSize;
+            System.out.printf("Downloaded %s / %s total file size\n", totalFileSize - remainingChunks, totalFileSize);
+        } while (remainingChunks > 0);
+
+        this.extractFile(fileExtract.getFileDir(), fileExtract.getFileName());
+    }
+
+    private void extractZip(ZipFile zip, String localSaveDir) throws IOException {
+        FileOutputStream out;
+        byte[] buffer = new byte[1024];
+        int read;
+        ZipEntry entry;
+
+        Enumeration<? extends ZipEntry> entries = zip.entries();
+
+        while (entries.hasMoreElements()) {
+            entry = entries.nextElement();
+            String filePath = localSaveDir + "/" + entry.getName();
+            out = new FileOutputStream(filePath);
+            InputStream is = zip.getInputStream(entry);
+            while ((read = is.read(buffer, 0, buffer.length)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            is.close();
+            out.close();
+        }
+        zip.close();
+    }
+
+    /**
+     * Extract a zip file.
+     *
+     * @param localSaveDir local directory containing zip file.
+     * @param filename name of zip file.
+     * @throws IOException
+     */
+    @Override
+    public void extractFile(String localSaveDir, String filename) throws IOException {
+        ZipFile zip = new ZipFile(localSaveDir + "/" + filename);
+        this.extractZip(zip, localSaveDir);
     }
 
     /**
