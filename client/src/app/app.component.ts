113b830674483203e796d2e9576ed0af0f3b1cab
Maciek Grzelczak <mgrzelczak97@gmail.com>
2021-11-20 13:24:28 +0100

    Update client/src/app/app.component.ts
    Co-authored-by: Dominik Maszczyk <Skitionek@gmail.com>

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -28,7 +28,7 @@ export class AppComponent {
   readonly userRoles$: Observable<string[]>;
   readonly loggedIn$: Observable<boolean>;
   helpDeskUrl = 'https://sbrgsoftware.atlassian.net/servicedesk/customer/portal/1/group/1/create/9';
-  standAloneFileUrlRegex = /^\/(projects|folders)\/([^\/]+)\//;
+  inWorkspaceFileUrlRegex = /^\/workspace/local/;
 
   constructor(
     private readonly store: Store<State>,
