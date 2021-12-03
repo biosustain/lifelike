9898d644f01669302ed3b8139fbda2082e1ec074
David Regla <dreglad@gmail.com>
2021-12-03 09:33:22 -0600

    Add Elastic APM python agent to statistical-enrichment service

--- statistical-enrichment/statistical_enrichment/__init__.py
+++ statistical-enrichment/statistical_enrichment/__init__.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 from flask import Flask
@@ -7,6 +6,14 @@ from flask_marshmallow import Marshmallow
 app_name = os.environ.get('FLASK_APP', __name__)
 app = Flask(app_name)
 Marshmallow().init_app(app)
+
+# Initialize Elastic APM if configured
+if os.getenv('ELASTIC_APM_SERVER_URL'):
+    from elasticapm.contrib.flask import ElasticAPM
+    apm = ElasticAPM(
+        app,
+        service_name='lifelike-statistical-enrichment',
+        environment=os.getenv('FLASK_ENV'))
 
 from .views import *
 from .schemas import *
