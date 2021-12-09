74c40917cd144b58675cc23ce3221f111f826e17
Maciek <mgrzelczak97@gmail.com>
2021-12-09 15:36:16 +0100

    LL-3998: Fix imports

--- client/src/app/shared/validators.ts
+++ client/src/app/shared/validators.ts
@@ -1,4 +1,4 @@
-import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 import { toValidLink } from './utils/browser';
 
