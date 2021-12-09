169426de7d393e5b57ad5652e7a45f4fadbdfb06
Maciek <mgrzelczak97@gmail.com>
2021-12-09 17:07:15 +0100

    LL-4039: Clean-up the file

--- client/src/app/drawing-tool/services/map-image-provider.service.ts
+++ client/src/app/drawing-tool/services/map-image-provider.service.ts
@@ -18,30 +18,18 @@ export class MapImageProviderService implements ResourceProvider<string, CanvasI
 
   getDimensions(id: string): Observable<Dimensions> {
     const preloadedUrl = this.preloadedUrls.get(id);
-    return new Observable( subscriber => {
-     const img = new Image();
-     img.src = preloadedUrl;
-     img.onload = () => {
-       subscriber.next({height: img.height, width: img.width});
-     };
-   });
-
-
-
-    // const preloadedUrl = this.preloadedUrls.get(id);
-    // if (preloadedUrl != null) {
-    //   const image = new Image();
-    //   const result = from(image.onload = () => {
-    //       const height = image.height;
-    //       const width = image.width;
-    //       return {height, width};
-    //   });
-    //   image.src = preloadedUrl;
-    //   return result;
-    // } else {
-    //   // TODO: Return an error to be handled
-    //   return of(null);
-    // }
+    if (preloadedUrl != null) {
+      return new Observable( subscriber => {
+       const img = new Image();
+       img.src = preloadedUrl;
+       img.onload = () => {
+         subscriber.next({height: img.height, width: img.width});
+       };
+     });
+    } else {
+      // TODO: Return an error to be handled
+      return of(null);
+    }
   }
 
   get(id: string): Observable<CanvasImageSource> {
