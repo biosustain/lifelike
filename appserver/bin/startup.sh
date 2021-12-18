d0465b2c084a4d1af3e8d090ef6b39ce6b7ccc24
David Regla <dreglad@gmail.com>
2021-12-18 14:32:35 -0600

    Simplify appserver startup script

--- appserver/bin/startup.sh
+++ appserver/bin/startup.sh
@@ -1,26 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -o errexit                  # exit on command failure; use <cmd> || true to allow for exception
-set -o nounset                  # exit when script tries to use undeclared variables
-
-if [ "${FLASK_ENV}" = "development" ] && [ "${FLASK_APP_CONFIG}" = "Development" ]; then
-    echo "### Starting up development environment ###"
-    __dir__="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    # wait for postgres
-    timeout 300 ${__dir__}/wait-for-postgres
-    # wait for neo4j
-    timeout 300 ${__dir__}/wait-for-neo4j
-    #wait for elastic
-    timeout 300 ${__dir__}/wait-for-elastic
-    # setup db
-    ${__dir__}/dev-db-setup
-    # Start in server directory
-    cd "${__dir__}/.."
-    # Mark ready
-    touch .READY
-    flask run --host 0.0.0.0
-elif [ "${FLASK_APP_CONFIG}" = "Production" ] || [ "${FLASK_APP_CONFIG}" = "Staging" ] || [ "${FLASK_APP_CONFIG}" = "QA" ]; then
-    gunicorn -b 0.0.0.0:5000 --workers=9 --threads=10 --timeout 1200 --max-requests 200 app:app
+if [ "${FLASK_ENV}" = "development" ]; then
+    flask run \
+      --host 0.0.0.0 \
+      --port ${PORT:-5000}
 else
-    echo "No environment setup for ${FLASK_ENV}"
+    gunicorn \
+      -b 0.0.0.0:${PORT:-5000} \
+      --workers=${GUNICORN_WORKERS:-9} \
+      --threads=${GUNICORN_THREADS:-10} \
+      --timeout=${GUNICORN_TIMEOUT:-1200} \
+      --max-requests=${GUNICORN_MAX_REQUESTS:-120} \
+      app:app
 fi
