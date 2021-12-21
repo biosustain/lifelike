6308a218da0870ee275debd19931efd5550f3310
David Regla <dreglad@gmail.com>
2021-12-20 21:02:43 -0600

    [cache-invalidator] Lint main.py module

--- cache-invalidator/main.py
+++ cache-invalidator/main.py
@@ -1,98 +1,110 @@
 import json
 import logging
 import os
-import redis
 import time
-
 from collections import defaultdict
+
+import redis
 from neo4j import GraphDatabase, basic_auth
 
+LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING")
 
-logger = logging.getLogger(__name__)
-log_level = os.environ.get('LOG_LEVEL')
-if log_level:
-    log_level = getattr(logging, log_level.upper())
-else:
-    log_level = logging.WARNING
+NEO4J_HOST = os.getenv("NEO4J_HOST", "localhost")
+NEO4J_PORT = os.getenv("NEO4J_PORT", "7687")
+NEO4J_AUTH = os.getenv("NEO4J_AUTH", "neo4j/password")
+NEO4J_SCHEME = os.getenv("NEO4J_SCHEME", "bolt")
+NEO4J_DATABASE = os.getenv("NEO4J_DATABASE", "neo4j")
 
-logger.setLevel(log_level)
-print(f'Set log level to {log_level}')
-logging.debug('Debug Logs')
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")
+REDIS_DB = os.getenv("REDIS_DB", "0")
+REDIS_SSL = os.getenv("REDIS_SSL", "").lower()
 
-SUCCESSFUL_SLEEP_TIME = 3600 * 24       # refresh cached data this often
-ERROR_INITIAL_SLEEP_TIME = 60           # if error occurs, try again sooner
-ERROR_SLEEP_TIME_MULTIPLIER = 2         # on subsequent errors, sleep longer
-ERROR_MAX_SLEEP_TIME = 3600 * 6         # but not longer than this
-CACHE_EXPIRATION_TIME = 3600 * 24 * 14  # expire cached data
+CACHE_TTL = int(os.getenv("CACHE_TTL", "3600"))  # expire cached data
+SUCCESSFUL_SLEEP_TIME = CACHE_TTL / 6  # refresh cached data this often
+ERROR_INITIAL_SLEEP_TIME = 60  # if error occurs, try again sooner
+ERROR_SLEEP_TIME_MULTIPLIER = 2  # on subsequent errors, sleep longer
+ERROR_MAX_SLEEP_TIME = 3600 * 6  # but not longer than this
 
-REDIS_HOST = os.environ.get('REDIS_HOST')
-REDIS_PORT = os.environ.get('REDIS_PORT')
-REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
-REDIS_SSL = os.environ.get('REDIS_SSL', 'false').lower()
-connection_prefix = 'rediss' if REDIS_SSL == 'true' else 'redis'
-connection_url = f'{connection_prefix}://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0'
 
-# Establish Redis Connection
+logging.basicConfig(level=LOG_LEVEL)
+logger = logging.getLogger("cache-invalidator")
+
+# Redis connection
+redis_schema = "rediss" if REDIS_SSL in ["true", "1"] else "redis"
+redis_url = f"{redis_schema}://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 redis_server = redis.Redis(
-    connection_pool=redis.BlockingConnectionPool.from_url(connection_url))
+    connection_pool=redis.BlockingConnectionPool.from_url(redis_url)
+)
 
-# Establish Neo4j Connection
-host = os.getenv('NEO4J_HOST', '0.0.0.0')
-scheme = os.getenv('NEO4J_SCHEME', 'bolt')
-port = os.getenv('NEO4J_PORT', '7687')
-url = f'{scheme}://{host}:{port}'
-username, password = os.getenv('NEO4J_AUTH', 'neo4j/password').split('/')
-neo4j_driver = GraphDatabase.driver(url, auth=basic_auth(username, password))
+# Neo4j connection
+neo4j_url = f"{NEO4J_SCHEME}://{NEO4J_HOST}:{NEO4J_PORT}/{NEO4J_DATABASE}"
+neo4j_auth = basic_auth(*NEO4J_AUTH.split("/", 1))
+neo4j_driver = GraphDatabase.driver(neo4j_url, auth=neo4j_auth)
+
 
 def main():
+    logger.info("Starting cache-invalitation loop...")
     next_error_sleep_time = ERROR_INITIAL_SLEEP_TIME
     while True:
         try:
-            statistics = get_kg_statistics()
-            cache_data("kg_statistics", statistics)
+            cache_data("kg_statistics", get_kg_statistics())
             next_error_sleep_time = ERROR_INITIAL_SLEEP_TIME
-            logger.debug(f'Going to sleep for {SUCCESSFUL_SLEEP_TIME} seconds...')
+            logger.debug(f"Going to sleep for {SUCCESSFUL_SLEEP_TIME} seconds...")
         except Exception as err:
-            logger.debug(f"Error occured, will try again in {next_error_sleep_time} seconds: {err}")
+            logger.error(
+                f"Error occured, will try again in {next_error_sleep_time} seconds: {err}"
+            )
             time.sleep(next_error_sleep_time)
-            next_error_sleep_time = min(ERROR_MAX_SLEEP_TIME, next_error_sleep_time * ERROR_SLEEP_TIME_MULTIPLIER)
+            next_error_sleep_time = min(
+                ERROR_MAX_SLEEP_TIME,
+                next_error_sleep_time * ERROR_SLEEP_TIME_MULTIPLIER,
+            )
         finally:
             try:
                 precalculateGO()
                 next_error_sleep_time = ERROR_INITIAL_SLEEP_TIME
-                logger.debug(f'Going to sleep for {SUCCESSFUL_SLEEP_TIME} seconds...')
+                logger.info(f"Going to sleep for {SUCCESSFUL_SLEEP_TIME} seconds...")
             except Exception as err:
-                logger.debug(f"Error occured, will try again in {next_error_sleep_time} seconds: {err}")
+                logger.error(
+                    f"Error occured, will try again in {next_error_sleep_time} seconds: {err}"
+                )
                 time.sleep(next_error_sleep_time)
-                next_error_sleep_time = min(ERROR_MAX_SLEEP_TIME, next_error_sleep_time * ERROR_SLEEP_TIME_MULTIPLIER)
+                next_error_sleep_time = min(
+                    ERROR_MAX_SLEEP_TIME,
+                    next_error_sleep_time * ERROR_SLEEP_TIME_MULTIPLIER,
+                )
             else:
                 time.sleep(SUCCESSFUL_SLEEP_TIME)
 
 
-
 def get_kg_statistics():
-    logger.debug("Getting Kg Statistics")
+    logger.info("Getting Kg Statistics")
     graph = neo4j_driver.session()
+
     logger.debug("Kg Statistics Query start...")
-    results = graph.read_transaction(lambda tx: tx.run('call db.labels()').data())
+    results = graph.read_transaction(lambda tx: tx.run("CALL db.labels()").data())
     logger.debug("Kg Statistics Query finished")
+
     domain_labels = []
     entity_labels = []
     for row in results:
-        label = row['label']
-        if label.startswith('db_'):
+        label = row["label"]
+        if label.startswith("db_"):
             domain_labels.append(label)
-        elif label != 'Synonym':
+        elif label != "Synonym":
             entity_labels.append(label)
+
     statistics = defaultdict(lambda: defaultdict())
     for domain in domain_labels:
         for entity in entity_labels:
-            query = f'MATCH (:`{domain}`:`{entity}`) RETURN count(*) as count'
-            logger.debug(f"NEO4J QUERY: {query}")
+            query = f"MATCH (:`{domain}`:`{entity}`) RETURN count(*) AS count"
+            logger.debug(f"Neorj query: {query}")
             result = graph.read_transaction(lambda tx: tx.run(query).data())
-            count = result[0]['count']
+            count = result[0]["count"]
             if count != 0:
-                statistics[domain.replace('db_', '', 1)][entity] = count
+                statistics[domain.replace("db_", "", 1)][entity] = count
     graph.close()
     return statistics
 
@@ -106,39 +118,45 @@ def precalculateGO():
         return tx.run(
             """
             MATCH (g:Gene)-[:GO_LINK {tax_id:$id}]-(go:db_GO)
-            WITH go, collect(distinct g) as genes
-            RETURN go.eid as goId,
-                go.name as goTerm,
-                [lbl in labels(go) where lbl <> 'db_GO'] as goLabel,
-                [g in genes |g.name] as geneNames
+            WITH go, collect(distinct g) AS genes
+            RETURN
+                go.eid AS goId,
+                go.name AS goTerm,
+                [lbl IN labels(go) WHERE lbl <> 'db_GO'] AS goLabel,
+                [g IN genes |g.name] AS geneNames
             """,
-            id=organism['id']
+            id=organism["id"],
         ).data()
 
     organisms = graph.read_transaction(
-            lambda tx: tx.run(
-                """
-                MATCH (t:Taxonomy)-[:HAS_TAXONOMY]-(:Gene)-[:GO_LINK]-(go:db_GO)
-                with t, count(go) as c
-                where c > 0
-                RETURN t.eid as id, t.name as name
-                """
-            ).data()
+        lambda tx: tx.run(
+            """
+            MATCH (t:Taxonomy)-[:HAS_TAXONOMY]-(:Gene)-[:GO_LINK]-(go:db_GO)
+            WITH t, count(go) AS c
+            WHERE c > 0
+            RETURN
+                t.eid AS id,
+                t.name AS name
+            """
+        ).data()
     )
 
     for organism in organisms:
-        logging.debug(f'Catching data for organism: {organism}')
-        cache_data(f"GO_for_{organism['id']}", graph.read_transaction(fetch_organism_go_query, organism))
+        logger.debug(f"Caching data for organism: {organism}")
+        cache_data(
+            f"GO_for_{organism['id']}",
+            graph.read_transaction(fetch_organism_go_query, organism),
+        )
     graph.close()
 
 
 def cache_data(key, value):
     try:
         redis_server.set(key, json.dumps(value))
-        redis_server.expire(key, CACHE_EXPIRATION_TIME)
+        redis_server.expire(key, CACHE_TTL)
     finally:
         redis_server.connection_pool.disconnect()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
