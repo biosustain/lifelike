1da61aa1f4cc718ec60f33c35563de6758c540c9
David Regla <dreglad@gmail.com>
2021-12-20 21:54:50 -0600

    [statistical-enrichment] Refactor Dockerfile

--- statistical-enrichment/bin/startup.sh
+++ statistical-enrichment/bin/startup.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -o errexit                  # exit on command failure; use <cmd> || true to allow for exception
-set -o nounset                  # exit when script tries to use undeclared variables
-
-if [ "${FLASK_ENV}" = "development" ] && [ "${FLASK_APP_CONFIG}" = "Development" ]; then
-    flask run --host 0.0.0.0 -p 5010
-elif [ "${FLASK_APP_CONFIG}" = "Production" ] || [ "${FLASK_APP_CONFIG}" = "Staging" ] || [ "${FLASK_APP_CONFIG}" = "QA" ]; then
-    gunicorn -b 0.0.0.0:5010 --workers=2 --max-requests=200 statistical_enrichment:app --timeout 1200
+if [ "${FLASK_ENV}" = "development" ]; then
+    flask run \
+      --host 0.0.0.0 \
+      --port ${PORT:-5000}
 else
-    echo "No environment setup for ${FLASK_ENV}"
+    gunicorn \
+      -b 0.0.0.0:${PORT:-5000} \
+      --workers=${GUNICORN_WORKERS:-2} \
+      --threads=${GUNICORN_THREADS:-2} \
+      --timeout=${GUNICORN_TIMEOUT:-1200} \
+      --max-requests=${GUNICORN_MAX_REQUESTS:-120} \
+      wsgi:app
 fi
