5a579cc48203a735886cde24ef92dd810fcac4ac
David Regla <dreglad@gmail.com>
2021-12-21 00:45:13 -0600

    [statistical-enrichment] Add ENVIRONMENT_NAME env variable

--- statistical-enrichment/statistical_enrichment/__init__.py
+++ statistical-enrichment/statistical_enrichment/__init__.py
@@ -29,6 +29,10 @@ def create_app(test_config: Dict = None):
     if os.getenv("ELASTIC_APM_SERVER_URL"):
         from elasticapm.contrib.flask import ElasticAPM
 
-        ElasticAPM(app, service_name=app.name)
+        ElasticAPM(
+            app,
+            service_name=app.name,
+            environment=os.getenv("ENVIRONMENT_NAME", app.config["FLASK_ENV"]),
+        )
 
     return app
