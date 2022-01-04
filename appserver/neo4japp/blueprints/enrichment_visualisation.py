29b5d07829780ba0f84555de6fa24c5e737711cd
David Regla <dreglad@gmail.com>
2022-01-03 18:00:39 -0600

    Undo code style changes and any non-essential for open source preparations

--- appserver/neo4japp/blueprints/enrichment_visualisation.py
+++ appserver/neo4japp/blueprints/enrichment_visualisation.py
@@ -3,74 +3,69 @@ import logging
 import os
 
 import requests
-from flask import Blueprint, Response, request
-from requests.exceptions import ConnectionError
+from flask import (
+    Blueprint, request, Response
+)
 
 from neo4japp.blueprints.auth import auth
 from neo4japp.exceptions import StatisticalEnrichmentError
+from requests.exceptions import ConnectionError
 
-SE_URL = os.getenv("STATISTICAL_ENRICHMENT_URL", "http://statistical-enrichment:5000")
+bp = Blueprint('enrichment-visualisation-api', __name__, url_prefix='/enrichment-visualisation')
 
-bp = Blueprint(
-    "enrichment-visualisation-api", __name__, url_prefix="/enrichment-visualisation"
-)
-
-
-@bp.route("/enrich-with-go-terms", methods=["POST"])
-@auth.login_required
-def enrich_go():
-    return _proxy_se_request("/enrich-with-go-terms")
+host = os.getenv('SE_HOST', 'statistical-enrichment')
+port = os.getenv('SE_PORT', '5010')
 
 
-def _proxy_se_request(resource):
+def forward_request():
+    host_port = f'{host}:{port}'
+    url = f'{request.scheme}://{request.path.replace(bp.url_prefix, host_port)}'
     try:
         resp = requests.request(
-            url=f"{SE_URL}{resource}",
-            method=request.method,
-            headers={key: value for (key, value) in request.headers if key != "Host"},
-            data=request.get_data(),
-            allow_redirects=False,
-            timeout=60,
+                method=request.method,
+                url=url,
+                headers={key: value for (key, value) in request.headers if key != 'Host'},
+                data=request.get_data(),
+                cookies=request.cookies,
+                allow_redirects=False
         )
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [
+            (name, value) for (name, value) in resp.raw.headers.items()
+            if name.lower() not in excluded_headers
+        ]
     except ConnectionError as e:
         raise StatisticalEnrichmentError(
-            "Unable to process request",
-            "An unexpected connection error occurred to statistical enrichment service.",
-            code=503,
+            'Unable to process request',
+            'An unexpected connection error occurred to statistical enrichment service.'
         )
 
     # 500 should contain message from service so we try to include it
     if resp.status_code == 500:
         try:
-            decoded_error_message = json.loads(resp.content)["message"]
+            decoded_error_message = json.loads(resp.content)['message']
         except Exception as e:
             # log and proceed so general error can be raised
             logging.exception(e)
         else:
             raise StatisticalEnrichmentError(
-                "Statistical enrichment error",
-                decoded_error_message,
-                code=500,
+                    'Statistical enrichment error',
+                    decoded_error_message,
+                    code=resp.status_code
             )
 
     # All errors including failure to parse internal error message
     if 400 <= resp.status_code < 600:
         raise StatisticalEnrichmentError(
-            "Unable to process request",
-            "An error of statistical enrichment service occurred.",
-            code=resp.status_code,
+                'Unable to process request',
+                'An internal error of statistical enrichment service occurred.',
+                code=resp.status_code
         )
 
-    excluded_headers = [
-        "content-encoding",
-        "content-length",
-        "transfer-encoding",
-        "connection",
-    ]
-    headers = [
-        (name, value)
-        for (name, value) in resp.raw.headers.items()
-        if name.lower() not in excluded_headers
-    ]
-
     return Response(resp.content, resp.status_code, headers)
+
+
+@bp.route('/enrich-with-go-terms', methods=['POST'])
+@auth.login_required
+def enrich_go():
+    return forward_request()
