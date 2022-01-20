3cea84542127e0c1e575b54acbfa9a6a5439db61
Dominik Maszczyk <Skitionek@gmail.com>
2022-01-20 16:29:23 +0100

    [LL-4092] Sankey views do not re-size to the full view instead shows zoomed view with right side cut off (#1494)
    * fix migration
    * fix search web-worker imports (do not import dom refs)
    * save viewport size while creating view
    * adjust zoom level when opening view

--- client/src/app/sankey-viewer/services/search-worker-actions.ts
+++ client/src/app/sankey-viewer/services/search-worker-actions.ts
@@ -1,3 +1,11 @@
+/* NOTE:
+    Be very carefull with adding imports as they cannot have any DOM references
+    since they are executed in a web worker enviroment.
+    Faulty import will prevent the worker from compiling, returning the error of type:
+     "document is undefined"
+     "window is undefined"
+     "alert is undefined"
+*/
 export enum WorkerActions {
   update,
   search,
