bc86ec7fb538582dc66f22fc7440f673286973ea
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-01-28 16:46:51 -0800

    Add Help & Info component

---
+++ client/src/app/help-and-info/help-and-info.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HelpAndInfoComponent } from './components/help-and-info.component';
+
+
+@NgModule({
+  declarations: [HelpAndInfoComponent],
+  imports: [
+    CommonModule,
+  ]
+})
+export class HelpAndInfoModule { }
