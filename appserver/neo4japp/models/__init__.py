b3d14d87062be8254c7901d99cf47fc468e50e54
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-02-01 11:34:53 -0800

    Add copyright infringement table

--- appserver/neo4japp/models/__init__.py
+++ appserver/neo4japp/models/__init__.py
@@ -8,4 +8,6 @@ from .neo4j import GraphNode, GraphRelationship
 from .projects import Projects, projects_collaborator_role
 from .annotations import AnnotationStopWords, GlobalList
 from .entity_resources import DomainURLsMap, AnnotationStyle
-from .files import Files, FileContent, FileVersion, FileBackup, file_collaborator_role
+from .files import Files, FileContent, FileVersion, FileBackup, Worksheet, \
+    file_collaborator_role, FallbackOrganism
+from .reports import CopyrightInfringementRequest
--- client/src/app/policies/components/copyright-infringement-policy.component.ts
+++ client/src/app/policies/components/copyright-infringement-policy.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-copyright-infringement-policy',
   templateUrl: './copyright-infringement-policy.component.html',
-  styleUrls: ['./copyright-infringement-policy.component.scss']
 })
 export class CopyrightInfringementPolicyComponent {
   constructor() { }
