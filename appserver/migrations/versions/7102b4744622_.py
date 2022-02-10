47389b71d1223aa4bf6d5268f2e312ca203b98c0
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-02-09 16:25:15 -0800

    Make subject column non-nullable
    Also fixes an error in the login logic. Using email as the basic auth
    subject was a mistake, since email is _not_ the subject for keycloak
    tokens. Better to make the two login methods more or less consistent.

--- appserver/migrations/versions/7102b4744622_.py
+++ appserver/migrations/versions/7102b4744622_.py
@@ -5,8 +5,14 @@ Revises: b49193a949a6
 Create Date: 2022-02-04 01:59:44.977808
 
 """
+from alembic import context
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import table, column
+from sqlalchemy.orm.session import Session
+
+from migrations.utils import window_chunk
+from neo4japp.models import AppUser
 
 # revision identifiers, used by Alembic.
 revision = '7102b4744622'
@@ -18,6 +24,16 @@ depends_on = None
 def upgrade():
     op.add_column('appuser', sa.Column('subject', sa.String(length=256), nullable=True))
 
+    if context.get_x_argument(as_dictionary=True).get('data_migrate', None):
+        data_upgrades()
+
+    op.alter_column(
+        'appuser',
+        'subject',
+        existing_type=sa.String(length=256),
+        nullable=False
+    )
+
 
 def downgrade():
     op.drop_column('appuser', 'subject')
@@ -25,7 +41,29 @@ def downgrade():
 
 def data_upgrades():
     """Add optional data upgrade migrations here"""
-    pass
+    conn = op.get_bind()
+    session = Session(conn)
+
+    t_appuser = table(
+        'appuser',
+        column('id', sa.Integer),
+        column('hash_id', sa.String))
+
+    files = conn.execution_options(stream_results=True).execute(sa.select([
+        t_appuser.c.id,
+        t_appuser.c.hash_id
+    ]))
+
+    for chunk in window_chunk(files, 20):
+        files_to_update = []
+        for id, hash_id in chunk:
+            files_to_update.append({'id': id, 'subject': hash_id})
+        try:
+            session.bulk_update_mappings(AppUser, files_to_update)
+            session.commit()
+        except Exception:
+            session.rollback()
+            raise
 
 
 def data_downgrades():
