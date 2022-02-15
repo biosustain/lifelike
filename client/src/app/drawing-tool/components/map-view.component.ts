43c115a45a082cc719884ef1e14f83235833bc20
Maciek Grzelczak <mgrzelczak97@gmail.com>
2022-02-15 15:07:03 +0100

    LL-4131: Front-end code for smarter map saving

--- client/src/app/drawing-tool/components/map-view.component.ts
+++ client/src/app/drawing-tool/components/map-view.component.ts
@@ -79,45 +79,28 @@ export class MapViewComponent<ExtraResult = void> extends MapComponent<ExtraResu
    * Save the current representation of knowledge model
    */
   save() {
-    const zip = new JSZip();
-    const imgs = zip.folder('images');
-    const imageIds: string[] = [];
     // Add a dummy observable to always fire the subscription below
-    const imageNodeObservables: Observable<Blob>[] = [];
-    for (const node of this.graphCanvas.getGraph().nodes) {
-      if (node.image_id !== undefined) { // is image
-        imageIds.push(node.image_id);
-        imageNodeObservables.push(this.mapImageProviderService.getBlob(node.image_id));
-      }
-    }
-    /**
-     * here we have an array of observables, they might emit value at any time
-     * so we use `combineLatest` to grab all of the values emitted by these observables
-     * in the callback function, `imageBlobs` is an array of blobs emitted
-     *     by these observables
-     * and they are in the SAME order as the imageNodeObservables, which is why iterating
-     *     through 2 arrays could work
-     * a better way might be to use HashMap (`Map` in typescript) but maps don't have
-     *     functions like `combineLatest`
-     */
-    zip.file('graph.json', JSON.stringify(this.graphCanvas.getGraph()));
+    const { newImages, deletedImages } = this.graphCanvas.getImageChanges();
+    const newImageBlobs = newImages.map(hash => this.mapImageProviderService.getBlob(hash));
+
+    const graphString = JSON.stringify(this.graphCanvas.getGraph());
+    const bytes = new TextEncoder().encode(graphString);
+    const content = new Blob([bytes], {
+      type: 'application/json;charset=utf-8'
+    });
     const hashesOfLinked = Array.from(this.graphCanvas.getHashesOfLinked());
     // DefaultIfEmpty ensures that we always call the subscription - even if there are no images
-    forkJoin(imageNodeObservables).pipe(defaultIfEmpty(null)).subscribe((imageBlobs: Blob[]) => {
-      for (let i = 0; i < imageIds.length; i++) {
-        imgs.file(imageIds[i] + '.png', imageBlobs[i]);
-      }
-      zip.generateAsync({ type: 'blob' }).then((content) => {
-        this.filesystemService.save([this.locator], { contentValue: content, hashesOfLinked})
-          .pipe(this.errorHandler.create({label: 'Update map'}))
-          .subscribe(() => {
-            this.unsavedChanges$.next(false);
-            this.emitModuleProperties(); // TODO: what does this do?
-            this.snackBar.open('Map saved.', null, {
-              duration: 2000,
-            });
+    forkJoin(newImageBlobs).pipe(defaultIfEmpty(null)).subscribe((imageBlobs: Blob[]) => {
+      this.filesystemService.save([this.locator], { contentValue: content, hashesOfLinked, newImages: imageBlobs, deletedImages})
+        .pipe(this.errorHandler.create({label: 'Update map'}))
+        .subscribe(() => {
+          this.unsavedChanges$.next(false);
+          this.graphCanvas.registerImages();
+          this.emitModuleProperties(); // TODO: what does this do?
+          this.snackBar.open('Map saved.', null, {
+            duration: 2000,
           });
-      });
+        });
     });
 
   }
