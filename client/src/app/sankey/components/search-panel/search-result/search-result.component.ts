43102b8077ac33d4b63a6ad7ed7db4bd05bb565a
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2022-02-15 10:02:56 -0800

    almost fully working search upgrade (there is issue with sankey hilight not going away)

---
+++ client/src/app/sankey/components/search-panel/search-result/search-result.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.scss']
+})
+export class SearchResultComponent {
+  constructor(protected element: ElementRef) {}
+  @Input('result') result!: any;
+  @Input('searchTokens') searchTokens!: any;
+  @Input('focused') focused: boolean;
+}
