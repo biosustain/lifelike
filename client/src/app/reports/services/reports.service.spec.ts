558f6fcaf3d5eec083ba4588bd540d9aeeb4384f
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-02-16 17:18:11 -0800

    Fix specs of policy related components

--- client/src/app/reports/services/reports.service.spec.ts
+++ client/src/app/reports/services/reports.service.spec.ts
@@ -1,12 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+
+import { configureTestSuite } from 'ng-bullet';
 
 import { ReportsService } from './reports.service';
 
 describe('ReportsService', () => {
   let service: ReportsService;
 
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientModule ]
+    });
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
     service = TestBed.inject(ReportsService);
   });
 
