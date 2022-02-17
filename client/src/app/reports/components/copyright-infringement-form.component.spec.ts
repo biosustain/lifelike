1b83829321ca8620a57b9a868f15da4d81286170
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-02-16 17:18:11 -0800

    Fix specs of policy related components

--- client/src/app/reports/components/copyright-infringement-form.component.spec.ts
+++ client/src/app/reports/components/copyright-infringement-form.component.spec.ts
@@ -1,17 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { configureTestSuite } from 'ng-bullet';
+
+import { RootStoreModule } from 'app/***ARANGO_USERNAME***-store';
+import { SharedModule } from 'app/shared/shared.module';
 
 import { CopyrightInfringementFormComponent } from './copyright-infringement-form.component';
+import { ReportsService } from '../services/reports.service';
 
 describe('CopyrightInfringementFormComponent', () => {
   let component: CopyrightInfringementFormComponent;
   let fixture: ComponentFixture<CopyrightInfringementFormComponent>;
 
-  beforeEach(async(() => {
+  configureTestSuite(() => {
     TestBed.configureTestingModule({
-      declarations: [ CopyrightInfringementFormComponent ]
-    })
-    .compileComponents();
-  }));
+      imports: [
+        HttpClientModule,
+        RootStoreModule,
+        RouterTestingModule,
+        SharedModule,
+      ],
+      declarations: [ CopyrightInfringementFormComponent ],
+      providers: [
+        ReportsService
+      ]
+    });
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CopyrightInfringementFormComponent);
