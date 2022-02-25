610b7c0c236e8852f54be57a478ea265b5a7ff65
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-02-25 14:07:46 -0800

    Fix typo in env.js files

--- client/src/environments/staging.js
+++ client/src/environments/staging.js
@@ -8,8 +8,8 @@
     window.__env.oauthEnabled = false;
 
     // OAuth issuer URL
-    window.__env.issuer = 'https://keycloak.apps.***ARANGO_DB_NAME***.cloud/auth/realms/master';
+    window.__env.oauthIssuer = 'https://keycloak.apps.***ARANGO_DB_NAME***.cloud/auth/realms/master';
 
     // Client ID of the OAuth application
-    window.__env.clientId = '***ARANGO_DB_NAME***-frontend';
+    window.__env.oauthClientId = '***ARANGO_DB_NAME***-frontend';
 }(this));
