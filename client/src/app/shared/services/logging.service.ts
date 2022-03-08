70b5400e75e88ee1b10d51fb2dc6a64f907fb896
David Regla <dreglad@gmail.com>
2022-03-07 23:15:38 -0600

    Debounce and deduplicate requests to logging endpoint

--- client/src/app/shared/services/logging.service.ts
+++ client/src/app/shared/services/logging.service.ts
@@ -1,19 +1,27 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 import { ErrorLog } from '../schemas/common';
 
-@Injectable({providedIn: '***ARANGO_USERNAME***'})
+const debounceDueTime = 1000;
+
+@Injectable({ providedIn: '***ARANGO_USERNAME***' })
 export class LoggingService {
+  readonly baseUrl = '/api/logging';
 
-    readonly baseUrl = '/api/logging';
+  loggingSubject = new Subject<ErrorLog>();
 
-    constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {
+    this.loggingSubject
+      .pipe(distinctUntilChanged(), debounceTime(debounceDueTime))
+      .subscribe((error) => http.post(this.baseUrl, error).toPromise().catch(console.error));
+  }
 
-    sendLogs(error: ErrorLog) {
-        return this.http.post(
-            `${this.baseUrl}/`,
-            error,
-        );
-    }
+  sendLogs(error: ErrorLog) {
+    this.loggingSubject.next(error);
+    return this.loggingSubject.asObservable();
+  }
 }
