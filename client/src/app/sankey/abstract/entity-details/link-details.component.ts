fd5fda3d2cff7995cdcd9cf22e1fdc6895157aa1
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2022-03-11 18:48:31 -0800

    fix merge

--- client/src/app/sankey/abstract/entity-details/link-details.component.ts
+++ client/src/app/sankey/abstract/entity-details/link-details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { SankeyEntityDetailsComponent } from './entity-details.component';
+import { ControllerService } from '../../services/controller.service';
+import { MultiLaneBaseControllerService } from '../../base-views/multi-lane/services/multi-lane-base-controller.service';
+import { SankeySingleLaneLink } from '../../base-views/single-lane/interfaces';
 
 @Component({
   selector: 'app-sankey-link-details'
 })
-export class SankeyLinkDetailsComponent extends SankeyEntityDetailsComponent {
+export class SankeyAbstractLinkDetailsComponent extends SankeyEntityDetailsComponent {
+  constructor(
+    protected common: ControllerService,
+    protected baseView: MultiLaneBaseControllerService,
+    protected readonly route: ActivatedRoute
+  ) {
+    super(common, route);
+  }
+
+  linkValueAccessors$ = this.common.linkValueAccessors$;
+  linkValueAccessor$ = this.baseView.linkValueAccessor$;
 }
 
