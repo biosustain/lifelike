cf596f3f6ae17c3d2f293ee6cdbb8e10a6b5f7fd
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2022-03-11 19:16:26 -0800

    fix building issues

--- client/src/app/sankey/abstract/entity-details/link-details.component.ts
+++ client/src/app/sankey/abstract/entity-details/link-details.component.ts
@@ -1,21 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { SankeyEntityDetailsComponent } from './entity-details.component';
-import { ControllerService } from '../../services/controller.service';
-import { MultiLaneBaseControllerService } from '../../base-views/multi-lane/services/multi-lane-base-controller.service';
-import { SankeySingleLaneLink } from '../../base-views/single-lane/interfaces';
+import { BaseControllerService } from '../../services/base-controller.service';
+import { SankeyBaseOptions, SankeyBaseState } from '../../base-views/interfaces';
 
 @Component({
   selector: 'app-sankey-link-details'
 })
 export class SankeyAbstractLinkDetailsComponent extends SankeyEntityDetailsComponent {
   constructor(
-    protected common: ControllerService,
-    protected baseView: MultiLaneBaseControllerService,
+    protected baseView: BaseControllerService<SankeyBaseOptions, SankeyBaseState>,
     protected readonly route: ActivatedRoute
   ) {
-    super(common, route);
+    super(baseView.common, route);
   }
 
   linkValueAccessors$ = this.common.linkValueAccessors$;
