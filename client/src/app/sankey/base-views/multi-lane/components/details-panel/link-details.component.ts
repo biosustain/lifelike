37f4891e59bc4936e02b60f878b98970f6f4add5
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2022-03-11 19:33:44 -0800

    finally compiles for prod

--- client/src/app/sankey/base-views/multi-lane/components/details-panel/link-details.component.ts
+++ client/src/app/sankey/base-views/multi-lane/components/details-panel/link-details.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { SankeyEntityDetailsComponent } from 'app/sankey/abstract/entity-details/entity-details.component';
-
-import { SankeyMultiLaneLink } from '../../interfaces';
-import { ControllerService } from '../../../../services/controller.service';
-import { MultiLaneBaseControllerService } from '../../services/multi-lane-base-controller.service';
 import { SankeyAbstractLinkDetailsComponent } from '../../../../abstract/entity-details/link-details.component';
+import { BaseControllerService } from '../../../../services/base-controller.service';
+import { SankeyBaseOptions, SankeyBaseState } from '../../../interfaces';
 
 @Component({
   selector: 'app-sankey-multi-lane-link-details',
   templateUrl: './link-details.component.html'
 })
 export class SankeyMultiLaneLinkDetailsComponent extends SankeyAbstractLinkDetailsComponent {
+  constructor(
+    protected baseView: BaseControllerService<SankeyBaseOptions, SankeyBaseState>,
+    protected readonly route: ActivatedRoute
+  ) {
+    super(baseView, route);
+  }
 }
 
