2f983234ec120b70385404d6557c395470a1274b
Maciek Grzelczak <mgrzelczak97@gmail.com>
2022-03-16 16:09:12 +0100

    LL-4164: Copy node edit behaviour for groups

--- client/src/app/drawing-tool/components/map-editor/group-form.component.ts
+++ client/src/app/drawing-tool/components/map-editor/group-form.component.ts
@@ -4,12 +4,29 @@ import { WorkspaceManager } from 'app/shared/workspace-manager';
 import { InternalSearchService } from 'app/shared/services/internal-search.service';
 
 import { NodeFormComponent } from './node-form.component';
+import { NodeGroup } from '../../services/interfaces';
+import { LINE_TYPES } from '../../services/line-types';
+import { BG_PALETTE_COLORS, PALETTE_COLORS } from '../../services/palette';
 
 @Component({
   selector: 'app-group-form',
   templateUrl: './group-form.component.html'
 })
 export class GroupFormComponent extends NodeFormComponent {
+
+  originalGroup: NodeGroup;
+  updatedGroup: NodeGroup;
+   lineTypeChoices = [
+    [null, {
+      name: '(Default)',
+    }],
+    ...LINE_TYPES.entries(),
+  ];
+  paletteChoices = [...PALETTE_COLORS];
+  bgPaletteChoices = [...BG_PALETTE_COLORS];
+  private ASSUMED_PANEL_HEIGHT = 450;
+
+
 
   constructor(protected readonly workspaceManager: WorkspaceManager,
               protected readonly internalSearch: InternalSearchService) {
