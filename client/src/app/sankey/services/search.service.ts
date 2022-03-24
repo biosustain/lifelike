443532999c4e065e7a585d67bf2c3a350b2389fd
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2022-03-24 11:25:51 -0700

    port quick fixes from todays presentation version

--- client/src/app/sankey/services/search.service.ts
+++ client/src/app/sankey/services/search.service.ts
@@ -49,7 +49,7 @@ export class SankeySearchService {
     // limit size of data we operate on
     map(({nodes, links, graph: {trace_networks}}) => ({
       nodes,
-      links,
+      links: links.map(link => ({_normalizer: false, ...link})),
       graph: {
         trace_networks
       }
