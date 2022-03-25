728fe93bec81c60d79a25545770a5ae89737e989
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2022-03-25 11:12:51 -0700

    fix enable prod mode bug

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,14 +2,18 @@ import '@angular/compiler';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import 'bootstrap/js/dist/index';
-
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+// Loading after init of prod mode so we don't hit the issue:
+// https://github.com/angular/angular-cli/issues/8340
+// ever again.
+// tslint:disable-next-line:ordered-imports
+import { AppModule } from './app/app.module';
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
