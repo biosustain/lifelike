ff83a4e1863f354c66de0de915c8583ddff0701b
Maciek Grzelczak <mgrzelczak97@gmail.com>
2022-04-23 19:42:10 +0200

    LL-4362: Fix double refactor of the preivous name

--- client/src/app/search/components/search-record-relationships.component.ts
+++ client/src/app/search/components/search-record-relationships.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 
 import { isNil } from 'lodash-es';
 
-import { UniversalGraphNode } from 'app/drawing-tool/services/interfaces';
 import { FTSReferenceRecord, GraphNode } from 'app/interfaces';
 import { PUBMED_URL } from 'app/shared/constants';
 import { stringToHex } from 'app/shared/utils';
@@ -62,12 +61,12 @@ export class SearchRecordRelationshipsComponent {
   constructor() {
   }
 
-  setChemicalDataStrings(chemical: UniversalGraphNode) {
+  setChemicalDataStrings(chemical: GraphNode) {
     this.chemicalDisplayName = isNil(chemical.displayName) ? '' : chemical.displayName;
     this.chemicalLabel = isNil(chemical.label) ? '' : chemical.label;
   }
 
-  setDiseaseDataStrings(disease: UniversalGraphNode) {
+  setDiseaseDataStrings(disease: GraphNode) {
     this.diseaseDisplayName = isNil(disease.displayName) ? '' : disease.displayName;
     this.diseaseLabel = isNil(disease.label) ? '' : disease.label;
   }
@@ -89,7 +88,7 @@ export class SearchRecordRelationshipsComponent {
           id: getLink(this.node),
         }],
       },
-    } as Partial<UniversalGraphNode>));
+    } as Partial<GraphNode>));
   }
 
   getVisualizerQueryParams(params) {
