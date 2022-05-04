c8f30947eb9949acb3c9237cfc736d6edf9bd7ad
Maciek Grzelczak <mgrzelczak97@gmail.com>
2022-05-04 18:42:35 +0200

    LL-3888: Remove old import

--- client/src/app/drawing-tool/components/map-editor/map-editor.component.ts
+++ client/src/app/drawing-tool/components/map-editor/map-editor.component.ts
@@ -8,7 +8,7 @@ import {
   ViewChild,
 } from '@angular/core';
 
-import { cloneDeep, partition } from 'lodash-es';
+import { cloneDeep } from 'lodash-es';
 import { from, Observable, of, Subscription, throwError } from 'rxjs';
 import { auditTime, catchError, finalize, switchMap } from 'rxjs/operators';
 
