f01c03f83835e124182bf9de3c00ede295059041
Dominik Maszczyk <Skitionek@gmail.com>
2022-05-05 13:28:37 -0700

    [LL-4310] Do not show dialog for copy events (#1624)
    * Do not use copy dialog anymore
    * Use Angular CDK for most part
    * fallback to old implementation
    * use error dialogs instead of alerts
    * reduce redundant named enum
    * normilise copy in pdfs as well
    * fix custom success message

--- client/src/app/interfaces/shared.interface.ts
+++ client/src/app/interfaces/shared.interface.ts
@@ -1,15 +1,3 @@
-export enum Browsers {
-  ANDROID = 'android',
-  BLINK = 'blink',
-  EDGE = 'edge',
-  FIREFOX = 'firefox',
-  IOS = 'ios',
-  SAFARI = 'safari',
-  TRIDENT = 'trident',
-  WEBKIT = 'webkit',
-  UNKNOWN = 'unknown',
-}
-
 export enum OperatingSystems {
   LINUX = 'linux',
   MAC = 'mac',
