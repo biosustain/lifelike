95c9608467fa6db59480a598688ab6cdd58acf78
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2022-05-30 14:03:54 +0200

    fixed node x position update pipe

--- client/src/app/sankey/services/layout.service.ts
+++ client/src/app/sankey/services/layout.service.ts
@@ -225,12 +225,12 @@ export class LayoutService<Base extends TypeContext> extends SankeyAbstractLayou
   }
 
   positionNodes(x) {
-    return switchMap(data => this.horizontal$.pipe(
-      // calculate width change ratio for repositioning of the nodes
-      startWith({} as any),
-      pairwise(),
-      switchMap(d => this.update.edited$.pipe(
-        switchMap(edited =>
+    return switchMap(data => this.update.edited$.pipe(
+      switchMap(edited => this.horizontal$.pipe(
+        // calculate width change ratio for repositioning of the nodes
+        startWith({} as any),
+        pairwise(),
+        switchMap(d =>
           iif(
             () => edited,
             of(d).pipe(
