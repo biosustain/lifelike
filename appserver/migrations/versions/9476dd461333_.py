0b13c5a6d078897a77e07e3f8d640b4786ab0892
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-06-14 16:12:21 -0700

    Add checks for str content and duplicates
    After running the migration again on the staging data, encountered some
    more edge cases that needed resolution. Still need to run it once again
    on prod to verify everything is still okay there too.

--- appserver/migrations/versions/9476dd461333_.py
+++ appserver/migrations/versions/9476dd461333_.py
@@ -35,6 +35,66 @@ def downgrade():
     pass
 
 
+DEFAULT_DOMAIN_INFO = {
+    "Regulon": {
+        "labels": [
+            "Regulator Family",
+            "Activated By",
+            "Repressed By"
+        ]
+    },
+    "UniProt": {
+        "labels": [
+            "Function"
+        ]
+    },
+    "String": {
+        "labels": [
+            "Annotation"
+        ]
+    },
+    "GO": {
+        "labels": [
+            "Annotation"
+        ]
+    },
+    "BioCyc": {
+        "labels": [
+            "Pathways"
+        ]
+    }
+}
+
+ORGANISM_MAP = {
+    '511145': 'Escherichia coli str. K-12 substr. MG1655',
+    '9606': 'Homo sapiens'
+}
+
+
+def _fix_str_annos(annos):
+    genes, tax_id, organism, sources = annos.split('/')
+    sources = sources.split(',')
+
+    if tax_id and organism == '':
+        organism = ORGANISM_MAP[tax_id]
+
+    if 'Biocyc' in sources:
+        sources[sources.index('Biocyc')] = 'BioCyc'
+
+    return {
+        'data': {
+            'genes': genes,
+            'taxId': tax_id,
+            'sources': sources,
+            'organism': organism
+        },
+        'result': {
+            'genes': [{'imported': gene} for gene in genes],
+            'domainInfo': DEFAULT_DOMAIN_INFO
+        }
+    }
+
+
 def _fix_annos_based_on_error(enrichment_annos: dict, error: str):
     if error == "data.result must not contain {'version'} properties":
         enrichment_annos['result'].pop('version')
@@ -129,8 +189,12 @@ def data_upgrades():
         'files_content',
         sa.column('id', sa.Integer),
         sa.column('raw_file', sa.LargeBinary),
+        sa.column('checksum_sha256', sa.Binary)
     )
 
+    # This will retrieve all enrichment tables that *have* annotations. The annotations and the
+    # content *might* be invalid. If the annotations are invalid, we will fix them and then
+    # replace the content with them.
     files = conn.execution_options(stream_results=True).execute(
         sa.select([
             t_files.c.id,
@@ -144,6 +208,8 @@ def data_upgrades():
         )
     )
 
+    # This will retrieve the *content* of all enrichment tables *without* annotations. If the
+    # content is invalid, we will fix it, and update the row with the valid content.
     files_content = conn.execution_options(stream_results=True).execute(
         sa.select([
             t_files_content.c.id,
@@ -151,7 +217,7 @@ def data_upgrades():
         ]).select_from(
             t_files_content.join(
                 t_files,
-                sa.and_ (
+                sa.and_(
                     t_files.c.content_id == t_files_content.c.id,
                     t_files.c.mime_type == FILE_MIME_TYPE_ENRICHMENT_TABLE,
                     t_files.c.enrichment_annotations.is_(None),
@@ -160,27 +226,32 @@ def data_upgrades():
         )
     )
 
-    for chunk in window_chunk(files_content, 25):
+    for chunk in window_chunk(files_content, 10):
         file_content_to_update = []
         for id, raw in chunk:
             try:
-                enrichment_annos = json.loads(raw.decode('utf-8'))
+                decoded_raw = raw.decode('utf-8')
+                enrichment_annos = json.loads(decoded_raw)
                 validate_enrichment_table(enrichment_annos)
+            except json.JSONDecodeError:
+                if type(decoded_raw) == str:
+                    enrichment_annos = _fix_str_annos(decoded_raw)
             except Exception as e:
                 enrichment_annos = _fix_annos_based_on_error(enrichment_annos, str(e))
 
-                # At this point, all schema issues should be resolved
-                validate_enrichment_table(enrichment_annos)
+            # At this point, all schema issues should be resolved
+            validate_enrichment_table(enrichment_annos)
 
-                # Add the FileContent update mapping to the list
-                new_content = json.dumps(enrichment_annos, separators=(',', ':')).encode('utf-8')
-                new_hash = hashlib.sha256(new_content).digest()
-                file_content_to_update.append({'id': id, 'raw_file': new_content, 'checksum_sha256': new_hash})  # noqa
+            # Add the FileContent update mapping to the list
+            new_content = json.dumps(enrichment_annos, separators=(',', ':')).encode('utf-8')
+            new_hash = hashlib.sha256(new_content).digest()
+            file_content_to_update.append({'id': id, 'raw_file': new_content, 'checksum_sha256': new_hash})  # noqa
 
         session.bulk_update_mappings(FileContent, file_content_to_update)
         session.commit()
 
-    for chunk in window_chunk(files, 25):
+    unique_content_map = {}
+    for chunk in window_chunk(files, 10):
         files_to_update = []
         file_content_to_update = []
         for id, content_id, enrichment_annos in chunk:
@@ -196,18 +267,33 @@ def data_upgrades():
                 # At this point, all schema issues should be resolved
                 validate_enrichment_table(enrichment_annos)
 
-                # Add the Files update mapping to the list
-                files_to_update.append({'id': id, 'enrichment_annotations': enrichment_annos})
-
                 # Do the same for FileContent, i.e., if the enrichment annotations were
                 # invalid, the raw content is too since they're more or less the same
                 new_content = json.dumps(enrichment_annos, separators=(',', ':')).encode('utf-8')
                 new_hash = hashlib.sha256(new_content).digest()
-                file_content_to_update.append({'id': content_id, 'raw_file': new_content, 'checksum_sha256': new_hash})  # noqa
+
+                existing_content = conn.execute(
+                    sa.select([
+                        t_files_content.c.id,
+                    ]).where(
+                        t_files_content.c.checksum_sha256 == new_hash,
+                    )
+                ).scalar()
+
+                file_content_id = existing_content
+                # If it's not a duplicate of an existing file content...
+                if existing_content is None:
+                    # And it's not a duplicate of a newly fixed file...
+                    if new_hash not in unique_content_map:
+                        unique_content_map[new_hash] = content_id
+                        file_content_to_update.append({'id': content_id, 'raw_file': new_content, 'checksum_sha256': new_hash})  # noqa
+                    file_content_id = unique_content_map[new_hash]
+                files_to_update.append({'id': id, 'enrichment_annotations': enrichment_annos, 'content_id': file_content_id})  # noqa
 
         session.bulk_update_mappings(Files, files_to_update)
         session.bulk_update_mappings(FileContent, file_content_to_update)
         session.commit()
+
 
 def data_downgrades():
     """Add optional data downgrade migrations here"""
