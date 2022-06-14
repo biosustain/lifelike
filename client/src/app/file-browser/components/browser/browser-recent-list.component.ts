2dca5acd9e082ff1906a6055a887189299c0a8c5
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-06-14 12:56:29 -0700

    Update community/recent lists to use table views

--- client/src/app/file-browser/components/browser/browser-recent-list.component.ts
+++ client/src/app/file-browser/components/browser/browser-recent-list.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { FilesystemObjectList } from 'app/file-browser/models/filesystem-object-list';
+import { BackgroundTask } from 'app/shared/rxjs/background-task';
 
 import { RecentFilesService } from '../../services/recent-files.service';
 
@@ -6,8 +12,35 @@ import { RecentFilesService } from '../../services/recent-files.service';
   selector: 'app-browser-recent-list',
   templateUrl: './browser-recent-list.component.html',
 })
-export class BrowserRecentListComponent {
-  constructor(
-    readonly recentFilesService: RecentFilesService
-  ) {}
+export class BrowserRecentListComponent implements OnInit, OnDestroy {
+  readonly loadTask: BackgroundTask<void, FilesystemObjectList> = new BackgroundTask(
+    () => this.recentFilesService.list.pipe(
+      map(data => {
+        const list = new FilesystemObjectList();
+        list.results.replace(data);
+        return list;
+      }),
+    )
+  );
+  private loadTaskSubscription: Subscription;
+
+  list: FilesystemObjectList = new FilesystemObjectList();
+
+  constructor(protected readonly recentFilesService: RecentFilesService) {}
+
+  ngOnInit() {
+    this.loadTaskSubscription = this.loadTask.results$.subscribe(({result: list}) => {
+      this.list = list;
+    });
+
+    this.refresh();
+  }
+
+  ngOnDestroy() {
+    this.loadTaskSubscription.unsubscribe();
+  }
+
+  refresh() {
+    this.loadTask.update();
+  }
 }
