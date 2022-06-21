a5b19fca5e926be089f5e6eba0e086d6534abed7
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2022-06-21 16:57:03 +0200

    add option of setting default base view

--- client/src/app/shared/providers/graph-type/interfaces.ts
+++ client/src/app/shared/providers/graph-type/interfaces.ts
@@ -97,8 +97,6 @@ export interface GraphTraceNetwork {
 
   method?: string;
 
-  default_sizing?: string;
-
   // by design all objects can have dynamic properties however this code should not be concerned about them
   // [key: string]: any;
 }
