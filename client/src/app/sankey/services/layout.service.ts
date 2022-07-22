dbaec2fa40ca982cce23c1963ce8f9e46d93b400
Dominik Maszczyk <Skitionek@gmail.com>
2022-07-22 15:19:51 +0200

    Update client/src/app/sankey/services/layout.service.ts

--- client/src/app/sankey/services/layout.service.ts
+++ client/src/app/sankey/services/layout.service.ts
@@ -63,7 +63,6 @@ interface LayerPlaceholder {
 
 function medianBy<T>(arr: T[], fn: (e: T) => number): number {
   const sortedArray = lodashMap(arr, fn).sort((a, b) => a - b);
-  const middleIndex = arr.length / 2;
   const middleIndex = sortedArray.length / 2;
   if (middleIndex % 1 === 0) {
       // Two middle numbers (e.g. [1, 2, 3, 4]), so to get the median take the average of the two
