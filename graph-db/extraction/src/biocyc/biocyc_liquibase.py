350b59bdc1a6aa5c42b96dfdc4b1e0844c9b9383
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2022-08-04 15:40:42 +0200

    correct files

--- graph-db/extraction/src/biocyc/biocyc_liquibase.py
+++ graph-db/extraction/src/biocyc/biocyc_liquibase.py
@@ -196,8 +196,9 @@ def generate_changelog_files(zip_datafile, biocyc_dbname, author):
 
 
 if __name__ == "__main__":
-    generate_changelog_files('EcoCyc-data-25.5.zip', DB_ECOCYC, 'rcai')
+    # generate_changelog_files('EcoCyc-data-25.5.zip', DB_ECOCYC, 'rcai')
     # generate_changelog_files('BsubCyc-data-47.zip', DB_BSUBCYC, 'rcai')
+    generate_changelog_files('PseudomonasAeruginosaPAO1-data-24.0.zip', DB_PSEUDOMONAS_AERUGINOSA_PAO_1, 'dommas')
 
 
 
