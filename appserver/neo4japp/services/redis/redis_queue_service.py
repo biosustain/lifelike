98397f6f1bd8f1c6f3e64ef01ddd9724be41d7f9
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-08-15 17:15:40 -0700

    Update redis queue logs

--- appserver/neo4japp/services/redis/redis_queue_service.py
+++ appserver/neo4japp/services/redis/redis_queue_service.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import current_app
 from rq import Queue, Retry, Worker
 from rq.command import send_shutdown_command
@@ -5,6 +6,7 @@ from rq.job import Job
 from rq.registry import FailedJobRegistry
 from typing import Iterable
 
+from neo4japp.constants import LogEventType
 from neo4japp.database import get_redis_connection
 
 
@@ -25,17 +27,26 @@ class RedisQueueService():
         retry = Retry(max=max_retry, interval=retry_interval) if max_retry is not None else None
 
         job = q.enqueue(f, *args, retry=retry, **kwargs)
-        current_app.logger.info(f'Job enqueued to redis: {job.to_dict()}')
+        current_app.logger.info(
+            f'Job enqueued to redis.',
+            extra={'event_type': LogEventType.REDIS, 'job': job.to_dict()}
+        )
         return job
 
     def empty_queue(self, queue: str) -> int:
         retval = self.get_queue(queue).empty()
-        current_app.logger.info(f'Redis queue {queue} emptied')
+        current_app.logger.info(
+            f'Redis queue {queue} emptied.',
+            extra={'event_type': LogEventType.REDIS}
+        )
         return retval
 
     def delete_queue(self, queue: str, delete_jobs=True):
         self.get_queue(queue).delete(delete_jobs)
-        current_app.logger.info(f'Redis queue {queue} deleted')
+        current_app.logger.info(
+            f'Redis queue {queue} deleted.',
+            extra={'event_type': LogEventType.REDIS}
+        )
 
     def delete_all_queues(self):
         for q in Queue.all(connection=self._redis_conn):
@@ -44,19 +55,28 @@ class RedisQueueService():
     def get_job_in_queue(self, queue: str, job_id) -> Job:
         job = self.get_queue(queue).fetch_job(job_id)
         if job is not None:
-            current_app.logger.info(f'Job fetched from queue {queue}: {job.to_dict()}')
+            current_app.logger.info(
+                f'Job fetched from queue {queue}.',
+                extra={'event_type': LogEventType.REDIS, 'job': job.to_dict()}
+            )
         return job
 
     def get_all_jobs_in_queue(self, queue: str) -> Iterable[Job]:
         jobs = self.get_queue(queue).jobs
         for job in jobs:
-            current_app.logger.info(f'Job fetched from queue {queue}: {job.to_dict()}')
+            current_app.logger.info(
+                f'Job fetched from queue {queue}.',
+                extra={'event_type': LogEventType.REDIS, 'job': job.to_dict()}
+            )
         return jobs
 
     def get_job(self, job_id) -> Job:
         job = Job.fetch(job_id, connection=self._redis_conn)
         if job is not None:
-            current_app.logger.info(f'Job fetched: {job.to_dict()}')
+            current_app.logger.info(
+                f'Job fetched.',
+                extra={'event_type': LogEventType.REDIS, 'job': job.to_dict()}
+            )
         return job
 
     def create_worker(self, queues, name, **kwargs) -> Worker:
@@ -66,18 +86,27 @@ class RedisQueueService():
             connection=self._redis_conn,
             **kwargs
         )
-        current_app.logger.info(f'Redis worker {worker.name} created')
+        current_app.logger.info(
+            f'Redis worker {worker.name} created.',
+            extra={'event_type': LogEventType.REDIS}
+        )
         return worker
 
     def get_worker(self, name) -> Worker:
         for w in Worker.all(connection=self._redis_conn):
             if w.name == name:
-                current_app.logger.info(f'Redis worker {w.name} fetched')
+                current_app.logger.info(
+                    f'Redis worker {w.name} fetched.',
+                    extra={'event_type': LogEventType.REDIS}
+                )
                 return w
 
     def get_all_workers(self) -> Iterable[Worker]:
         workers = Worker.all(connection=self._redis_conn)
-        current_app.logger.info(f'Redis workers fetched: {[w.name for w in workers]}')
+        current_app.logger.info(
+            f'Redis workers fetched: {[w.name for w in workers]}.',
+            extra={'event_type': LogEventType.REDIS}
+        )
         return workers
 
     def get_worker_count(self) -> int:
@@ -87,12 +116,16 @@ class RedisQueueService():
         q = self.get_queue(queue)
         queue_workers = Worker.all(queue=q)
         current_app.logger.info(
-            f'Redis queue {queue} workers fetched: {[w.name for w in queue_workers]}'
+            f'Redis queue {queue} workers fetched: {[w.name for w in queue_workers]}.',
+            extra={'event_type': LogEventType.REDIS}
         )
         return queue_workers
 
     def shutdown_worker(self, worker_name: str):
-        current_app.logger.info(f'Redis worker {worker_name} shut down')
+        current_app.logger.info(
+            f'Redis worker {worker_name} shut down.',
+            extra={'event_type': LogEventType.REDIS}
+        )
         send_shutdown_command(self._redis_conn, worker_name)
 
     def shutdown_all_workers(self):
@@ -111,25 +144,50 @@ class RedisQueueService():
 
     def log_failed_jobs(self, queue: str):
         for job in self.get_failed_jobs(queue):
-            current_app.logger.info(f'Failed Redis Job {job.id}: {job.exc_info}')
+            current_app.logger.info(
+                f'Failed Redis job {job.id}.',
+                extra={'event_type': LogEventType.REDIS, 'job': job.to_dict()}
+            )
 
     def retry_failed_jobs(self, queue: str):
         registry = self.get_failed_job_registry(queue)
         for job in self.get_failed_jobs(queue):
             registry.requeue(job)
-            current_app.logger.info(f'Failed Redis Job {job.id} requeued')
+            current_app.logger.info(
+                f'Failed Redis Job {job.id} requeued.',
+                extra={'event_type': LogEventType.REDIS, 'job': job.to_dict()}
+            )
 
     def cleanup_failed_job(self, queue: str, job_id: str):
         registry = self.get_failed_job_registry(queue)
-        registry.remove(self.get_job(job_id))
+        failed_job = self.get_job(job_id)
+        registry.remove(failed_job)
+
+        current_app.logger.info(
+            f'Failed Redis job {failed_job.id} removed.',
+            extra={'event_type': LogEventType.REDIS, 'job': failed_job.to_dict()}
+        )
 
     def cleanup_all_failed_jobs(self, queue: str):
         registry = self.get_failed_job_registry(queue)
         for job in self.get_failed_jobs(queue):
             registry.remove(job)
+            current_app.logger.info(
+                f'Failed Redis job {job.id} removed.',
+                extra={'event_type': LogEventType.REDIS, 'job': job.to_dict()}
+            )
 
     # Note that expired jobs are automatically cleaned up when calling certain helpers of the failed
     # job registry, the `get_job_ids` method for example.
     def cleanup_expired_failed_jobs(self, queue: str, timestamp=None):
         registry = self.get_failed_job_registry(queue)
         registry.cleanup(timestamp)
+
+        timestamp_str = (
+            timestamp if timestamp is not None
+            else datetime.now().strftime("%Y-%m-%d %I:%M %p")
+        )
+        current_app.logger.info(
+            f'Failed Redis jobs in queue {queue} expired before {timestamp_str} removed.',
+            extra={'event_type': LogEventType.REDIS}
+        )
