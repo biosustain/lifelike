e7b3c9e6e12cc3d213424913a46281acca0436c5
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-09-22 10:49:33 -0700

    Update query in map_link uq constraint migration

--- appserver/migrations/versions/55d9a626454f_add_unique_constrain_on_map_links.py
+++ appserver/migrations/versions/55d9a626454f_add_unique_constrain_on_map_links.py
@@ -8,13 +8,13 @@ Create Date: 2022-09-22 15:26:10.117758
 from alembic import context
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.orm import Session
 
 # revision identifiers, used by Alembic.
 revision = '55d9a626454f'
 down_revision = '580187b42c7b'
 branch_labels = None
 depends_on = None
+
 
 def upgrade():
     if context.get_x_argument(as_dictionary=True).get('data_migrate', None):
@@ -30,15 +30,25 @@ def downgrade():
 def data_upgrades():
     """Add optional data upgrade migrations here"""
     conn = op.get_bind()
-    session = Session(conn)
-    map_links = sa.table(
+
+    t_map_links = sa.table(
         'map_links',
         sa.column('entry_id', sa.Integer),
         sa.column('map_id', sa.Integer),
         sa.column('linked_id', sa.Integer)
     )
-    unique = session.query(sa.func.min(map_links.entry_id)).group_by(map_links.map_id, map_links.linked_id)
-    alias_unique = sa.aliased(unique)
-    session.delete(map_links) \
-        .where(~map_links.id.in_(alias_unique))
-    session.commit()
+
+    conn.execute(
+        sa.delete(
+            t_map_links
+        ).where(
+            ~t_map_links.c.entry_id.in_(
+                sa.select([
+                   sa.func.min(t_map_links.c.entry_id)
+                ]).group_by(
+                    t_map_links.c.map_id,
+                    t_map_links.c.linked_id
+                )
+            )
+        )
+    )
