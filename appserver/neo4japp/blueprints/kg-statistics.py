4086d76d7a2bc5a5bfb65ba94b23f562d5277813
David Regla <dreglad@gmail.com>
2022-10-24 06:05:23 +0000

    Send better status code when failing to query Redis cache

--- appserver/neo4japp/blueprints/kg-statistics.py
+++ appserver/neo4japp/blueprints/kg-statistics.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from http import HTTPStatus
 
 from neo4japp.services.rcache import redis_server
 from neo4japp.exceptions import ServerException
@@ -7,11 +8,12 @@ from neo4japp.exceptions import ServerException
 bp = Blueprint('kg-statistics-api', __name__, url_prefix='/kg-statistics')
 
 
-@bp.route('', methods=['GET'])
+@bp.route('/', methods=['GET'])
 def get_knowledge_graph_statistics():
     statistics = redis_server.get('kg_statistics')
     if statistics:
-        return statistics, 200
+        return statistics
     raise ServerException(
         title='Failed to get Statistics',
-        message='Knowledge Graph Statistics Not Available.')
+        message='Knowledge Graph Statistics Not Available.',
+        code=HTTPStatus.SERVICE_UNAVAILABLE)
