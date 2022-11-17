00da1b88503cadd5aea43dbdbeb1a16325550408
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-11-17 14:32:57 -0800

    Add arango container for local dev

---
+++ arangodb/bin/import_edges.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ./seed_data
+
+col=all_rels
+
+# Note that we explicitly use the "lifelike" database when creating the collection, and before importing!
+arangosh --server.password $ARANGO_ROOT_PASSWORD --server.database $LIFELIKE_DB_NAME --javascript.execute-string "db._createEdgeCollection('${col}')"
+
+for f in e_*.json;
+do
+echo ${f}
+time arangoimport --server.password $ARANGO_ROOT_PASSWORD --server.database $LIFELIKE_DB_NAME --file ${f} --type jsonl --collection ${col} &
+done
