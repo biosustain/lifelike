d4ef5e280d2490c7777cc89020ece16755511ef7
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-11-30 13:30:52 -0800

    Add David suggestions

--- arangodb/bin/import_edges.sh
+++ arangodb/bin/import_edges.sh
@@ -5,10 +5,10 @@ cd ./seed_data
 col=all_rels
 
 # Note that we explicitly use the "lifelike" database when creating the collection, and before importing!
-arangosh --server.password $ARANGO_ROOT_PASSWORD --server.database $LIFELIKE_DB_NAME --javascript.execute-string "db._createEdgeCollection('${col}')"
+arangosh --server.password "$ARANGO_ROOT_PASSWORD" --server.database $LIFELIKE_DB_NAME --javascript.execute-string "db._createEdgeCollection('${col}')"
 
 for f in e_*.json;
 do
 echo ${f}
-time arangoimport --server.password $ARANGO_ROOT_PASSWORD --server.database $LIFELIKE_DB_NAME --file ${f} --type jsonl --collection ${col} &
+time arangoimport --server.password "$ARANGO_ROOT_PASSWORD" --server.database $LIFELIKE_DB_NAME --file ${f} --type jsonl --collection ${col} &
 done
--- arangodb/bin/import_nodes.sh
+++ arangodb/bin/import_nodes.sh
@@ -8,6 +8,6 @@ col=`echo ${f} | sed 's/^c_*//' | sed 's/_nodes//' | sed 's/\.json$//'`
 echo ${col}
 
 # Note that we explicitly use the "lifelike" database when creating the collection, and before importing!
-arangosh --server.password $ARANGO_ROOT_PASSWORD --server.database $LIFELIKE_DB_NAME --javascript.execute-string "db._createDocumentCollection('${col}')"
-time arangoimport --server.password $ARANGO_ROOT_PASSWORD --server.database $LIFELIKE_DB_NAME --file ${f} --type jsonl --collection ${col} &
+arangosh --server.password "$ARANGO_ROOT_PASSWORD" --server.database $LIFELIKE_DB_NAME --javascript.execute-string "db._createDocumentCollection('${col}')"
+time arangoimport --server.password "$ARANGO_ROOT_PASSWORD" --server.database $LIFELIKE_DB_NAME --file ${f} --type jsonl --collection ${col} &
 done
