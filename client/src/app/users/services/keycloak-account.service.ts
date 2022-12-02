d712564c61815b163c653a5d79e12dd2fcf59026
David Regla <dreglad@gmail.com>
2022-12-01 20:09:39 -0600

    Padd TSLint import order linting rules

--- client/src/app/users/services/keycloak-account.service.ts
+++ client/src/app/users/services/keycloak-account.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
-
 import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
 
 import { KeycloakUserData } from '../interfaces';
 
