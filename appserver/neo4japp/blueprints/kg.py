de591e9234688beb6ad1560f79a9bbbcc6b8a38e
Ethan Sanchez <ethan.dsanch@gmail.com>
2022-12-19 12:30:56 -0800

    Update ET to use Arango

--- appserver/neo4japp/blueprints/kg.py
+++ appserver/neo4japp/blueprints/kg.py
@@ -1,37 +1,9 @@
-from flask import Blueprint, request, jsonify
-from pandas import DataFrame
+from flask import Blueprint, jsonify
 
-from neo4japp.constants import KGDomain
 from neo4japp.database import get_kg_service
-import numpy as np
+
 
 bp = Blueprint('kg-api', __name__, url_prefix='/knowledge-graph')
-
-
-@bp.route('/get-ncbi-nodes/enrichment-domains', methods=['POST'])
-def get_ncbi_enrichment_domains():
-    """ Find all domains matched to given node id, then return dictionary with all domains as
-        result. All domains should have matching indices e.g. regulon[1] should be data from
-        matching same node as uniprot[1].
-    """
-    # TODO: Validate incoming data using webargs + Marshmallow
-    data = request.get_json()
-    node_ids = data.get('nodeIds')
-    tax_id = data.get('taxID')
-    domains = data.get('domains')
-
-    if node_ids is not None and tax_id is not None:
-        kg = get_kg_service()
-        domain_nodes = {
-            domain.lower(): kg.get_genes(KGDomain(domain), node_ids, tax_id) for domain in domains
-        }
-        df = DataFrame(domain_nodes).replace({np.nan: None}).transpose()
-        # Redundant but just following old implementation
-        nodes = df.append(df.columns.to_series(name='node_id')).to_dict()
-    else:
-        nodes = {}
-
-    return jsonify({'result': nodes}), 200
 
 
 @bp.route('/shortest-path-query/<int:query_id>', methods=['GET'])
