41febfc7f4998188fb9952cfcd5c89859fbf63d7
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-01-04 16:53:54 -0800

    Update SE to use arango

---
+++ statistical-enrichment/statistical_enrichment/services/arangodb.py
@@ -0,0 +1,22 @@
+from arango import ArangoClient
+from arango.database import StandardDatabase
+from flask import current_app
+from typing import Any, List, Optional
+
+
+def get_db(
+    arango_client: ArangoClient,
+    name: Optional[str] = None,
+    username: Optional[str] = None,
+    password: Optional[str] = None
+):
+    return arango_client.db(
+        name=name or current_app.config.get('ARANGO_DB_NAME'),
+        username=username or current_app.config.get('ARANGO_USERNAME'),
+        password=password or current_app.config.get('ARANGO_PASSWORD'),
+    )
+
+
+def execute_arango_query(db: StandardDatabase, query: str, **bind_vars) -> List[Any]:
+    cursor = db.aql.execute(query, ttl=600, max_runtime=600, bind_vars=bind_vars)
+    return [row for row in cursor]
