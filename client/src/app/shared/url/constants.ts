6684942b15b0841948cc1331ea5b2aaf11a6f7a0
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-01-13 15:38:46 +0100

    fix test

--- client/src/app/shared/url/constants.ts
+++ client/src/app/shared/url/constants.ts
@@ -3,33 +3,33 @@ import { HttpURL } from '.';
 export const NCBI = Object.freeze({
   url: new HttpURL('https://www.ncbi.nlm.nih.gov/').freeze(),
   article: (id: string | number | boolean, file?: string | number | boolean) =>
-    new HttpURL(this.url, {
+    new HttpURL(NCBI.url, {
       pathSegments: ['pmc', 'articles', `PMC${id}`, ...(file ? ['bin', file] : [])],
     }),
   pubmed: (pmid: string | number | boolean) =>
-    new HttpURL(this.url, {
+    new HttpURL(NCBI.url, {
       pathSegments: ['pubmed', pmid],
     }),
   gene: (eid: string | number | boolean) =>
-    new HttpURL(this.url, {
+    new HttpURL(NCBI.url, {
       pathSegments: ['gene', eid],
     }),
   taxonomy: (id: string | number | boolean) =>
-    new HttpURL(this.url, {
+    new HttpURL(NCBI.url, {
       pathSegments: ['Taxonomy', 'Browser', 'wwwtax.cgi'],
       search: {id: String(id)},
     }),
   mesh: (term: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(NCBI.url, {
       pathSegments: ['mesh'],
       search: {term},
     }),
   pubtator: (id: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(NCBI.url, {
       pathSegments: ['research', 'pubtator'],
     }),
   pubtatorSearch: (searchTerm: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(NCBI.url, {
       pathSegments: ['research', 'pubtator'],
       search: {view: 'docsum', query: searchTerm},
     }),
@@ -38,7 +38,7 @@ export const NCBI = Object.freeze({
 export const UNIPROT = Object.freeze({
   url: new HttpURL('https://www.uniprot.org/uniprotkb').freeze(),
   search: (query: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(UNIPROT.url, {
       search: {query},
     }),
 });
@@ -46,11 +46,11 @@ export const UNIPROT = Object.freeze({
 export const GO = Object.freeze({
   url: new HttpURL('http://amigo.geneontology.org/').freeze(),
   id: (eid: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(GO.url, {
       pathSegments: ['amigo', 'term', `GO:${eid}`],
     }),
   search: (q: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(GO.url, {
       pathSegments: ['amigo', 'search', 'annotation'],
       search: {q},
     }),
@@ -59,17 +59,17 @@ export const GO = Object.freeze({
 export const BIOCYC = Object.freeze({
   url: new HttpURL('https://biocyc.org/').freeze(),
   id: (object: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(BIOCYC.url, {
       pathSegments: ['ECOLI', 'NEW-IMAGE'],
       search: {object},
     }),
   compound: (id: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(BIOCYC.url, {
       pathSegments: ['compound'],
       search: {orgid: 'META', id},
     }),
   gene: (id: string, orgid = 'PPUT160488') =>
-    new HttpURL(this.url, {
+    new HttpURL(BIOCYC.url, {
       pathSegments: ['gene'],
       search: {orgid, id},
     }),
@@ -78,7 +78,7 @@ export const BIOCYC = Object.freeze({
 export const REACTOME = Object.freeze({
   url: new HttpURL('https://reactome.org/').freeze(),
   id: (stId: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(REACTOME.url, {
       pathSegments: ['content', 'detail', stId],
     }),
 });
@@ -86,7 +86,7 @@ export const REACTOME = Object.freeze({
 export const CHEBI = Object.freeze({
   url: new HttpURL('http://identifiers.org/').freeze(),
   id: (eid: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(CHEBI.url, {
       pathSegments: ['chebi', `CHEBI:${eid}`],
     }),
 });
@@ -94,12 +94,12 @@ export const CHEBI = Object.freeze({
 export const CHEBI2 = Object.freeze({
   url: new HttpURL('https://www.ebi.ac.uk/').freeze(),
   search: (chebiId: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(CHEBI2.url, {
       pathSegments: ['chebi', 'searchId.do'],
       search: {chebiId},
     }),
   advancedSearch: (searchString: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(CHEBI2.url, {
       pathSegments: ['chebi', 'advancedSearchFT.do'],
       search: {searchString},
     }),
@@ -108,7 +108,7 @@ export const CHEBI2 = Object.freeze({
 export const MESHB = Object.freeze({
   url: new HttpURL('https://meshb.nlm.nih.gov/').freeze(),
   id: (ui: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(MESHB.url, {
       pathSegments: ['record', `ui`],
       search: {ui},
     }),
@@ -117,7 +117,7 @@ export const MESHB = Object.freeze({
 export const PUBCHEM = Object.freeze({
   url: new HttpURL('https://pubchem.ncbi.nlm.nih.gov/').freeze(),
   search: (query: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(PUBCHEM.url, {
       fragment: new URLSearchParams({query}),
     }),
 });
@@ -125,11 +125,11 @@ export const PUBCHEM = Object.freeze({
 export const GOOGLE = Object.freeze({
   url: new HttpURL('https://www.google.com/search').freeze(),
   search: (q: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(GOOGLE.url, {
       search: {q},
     }),
   searchWikipedia: (q: string) =>
-    new HttpURL(this.url, {
+    new HttpURL(GOOGLE.url, {
       search: {q: 'site:+wikipedia.org+' + q},
     }),
 });
