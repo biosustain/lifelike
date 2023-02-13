bb9c477020dd8cfc1e6a396949a45f195fa3c471
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-02-13 12:20:18 +0100

    fix pass style by ref in map (codependent node styling issue)

--- client/src/app/graph-viewer/utils/objects.ts
+++ client/src/app/graph-viewer/utils/objects.ts
@@ -1,6 +1,7 @@
-import { defaultsDeep, assign } from 'lodash-es';
+import { defaultsDeep, assign, chain } from 'lodash-es';
 
 import { makeid, uuidv4 } from 'app/shared/utils/identifiers';
+import { UniversalGraphGroup, UniversalGraphNode } from 'app/drawing-tool/services/interfaces';
 
 import { NODE_DEFAULTS, GROUP_DEFAULTS, IMAGE_DEFAULTS } from '../defaults';
 
@@ -8,25 +9,31 @@ import { NODE_DEFAULTS, GROUP_DEFAULTS, IMAGE_DEFAULTS } from '../defaults';
  * Create new node object (with new hash and default values)
  * @param partialNode - object to be transformed into node (it will mutate)
  */
-export const createNode = partialNode => assign(defaultsDeep(partialNode, NODE_DEFAULTS), { hash: uuidv4() });
+export const createNode: (partialNode: Partial<UniversalGraphNode>) => UniversalGraphNode =
+  partialNode => chain(partialNode)
+    .cloneDeep() // Prevent object pass by ref from partial value
+    .defaultsDeep(NODE_DEFAULTS)
+    .assign({hash: uuidv4()})
+    .value();
 
 /**
  * Create new group node object (with new hash and default values)
  * @param partialGroup - object to be transformed into group node (it will mutate)
  */
-export const createGroupNode = partialGroup =>
-  createNode(
-    defaultsDeep(partialGroup, GROUP_DEFAULTS)
-  );
+export const createGroupNode: (partialGroup: Partial<UniversalGraphGroup>) => UniversalGraphGroup =
+  partialGroup => chain(partialGroup)
+    .cloneDeep() // Prevent object pass by ref from partial value
+    .defaultsDeep(GROUP_DEFAULTS)
+    .value();
 
 /**
  * Create new image node object (with new hash and default values)
  * @param partialImage - object to be transformed into image node (it will mutate)
  */
-export const createImageNode = partialImage =>
-  assign(
-    createNode(
-      defaultsDeep(partialImage, IMAGE_DEFAULTS)
-    ),
-    { imageId: makeid() }
-  );
+export const createImageNode: (partialNode: Partial<UniversalGraphNode>) => UniversalGraphNode =
+  partialImage => chain(partialImage)
+    .cloneDeep() // Prevent object pass by ref from partial value
+    .defaultsDeep(IMAGE_DEFAULTS)
+    .thru(createNode)
+    .assign({imageId: makeid})
+    .value();
