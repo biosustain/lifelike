bf66c6ac9a0f8291b7b906c9abd11e97f213e045
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-02-14 11:21:23 -0800

    Update user creation to use master initial project

--- appserver/neo4japp/blueprints/account.py
+++ appserver/neo4japp/blueprints/account.py
@@ -1,25 +1,19 @@
-import json
-import logging
+from flask import Blueprint, g, jsonify, current_app
+from flask.views import MethodView
+from pathlib import Path
 import random
 import re
 import secrets
-import string
-from pathlib import Path
-from uuid import uuid4
-
-from flask import Blueprint, g, jsonify, current_app
-from flask.views import MethodView
 from sendgrid.helpers.mail import Mail
 from sqlalchemy import func, literal_column, or_
 from sqlalchemy.dialects.postgresql import aggregate_order_by
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.sql import select
+import string
 from webargs.flaskparser import use_args
 
-from neo4japp.blueprints.annotations import FileAnnotationsGenerationView
 from neo4japp.blueprints.auth import login_exempt
-from neo4japp.exceptions import RecordNotFound
 from neo4japp.constants import (
     MAX_ALLOWED_LOGIN_FAILURES,
     MESSAGE_SENDER_IDENTITY,
@@ -30,13 +24,11 @@ from neo4japp.constants import (
     RESET_PASSWORD_ALPHABET,
     SEND_GRID_API_CLIENT,
     RESET_PASSWORD_EMAIL_TITLE,
-    LogEventType, FILE_MIME_TYPE_MAP
+    LogEventType,
 )
 from neo4japp.database import db, get_authorization_service, get_projects_service
-from neo4japp.exceptions import ServerException, NotAuthorized
-from neo4japp.models import AppUser, AppRole, Projects, Files, FileContent
-from neo4japp.models.auth import user_role
-from neo4japp.models.files import FileAnnotationsVersion
+from neo4japp.exceptions import NotAuthorized, RecordNotFound, ServerException
+from neo4japp.models.auth import user_role, AppUser, AppRole
 from neo4japp.schemas.account import (
     UserListSchema,
     UserProfileSchema,
@@ -47,7 +39,6 @@ from neo4japp.schemas.account import (
     UserChangePasswordSchema
 )
 from neo4japp.schemas.common import PaginatedRequestSchema
-from neo4japp.services.file_types.providers import MapTypeProvider
 from neo4japp.utils.logger import EventLog, UserEventLog
 from neo4japp.utils.request import Pagination
 
@@ -124,123 +115,6 @@ class AccountView(MethodView):
             'results': results,
         }))
 
-    def create_initial_project(self, user: AppUser):
-        """
-        Create a initial project for the user.
-        This method is designed to fail siletly if the project name already exists
-        or if initial project template does not exist.
-        :param user: user to create initial project for
-        """
-        project = Projects()
-        project.name = f'{user.username}-example'
-        project.description = f'Initial project for {user.username}'
-
-        project_service = get_projects_service()
-
-        try:
-            db.session.begin_nested()
-            project_service.create_project_uncommitted(user, project)
-            db.session.commit()
-            db.session.flush()
-        except IntegrityError:
-            db.session.rollback()
-            logging.exception('Failed to create initial project with default naming for user %s',
-                              user.username)
-            project.name += '-' + uuid4().hex[:8]
-            try:
-                db.session.begin_nested()
-                project_service.create_project_uncommitted(user, project)
-                db.session.commit()
-                db.session.flush()
-            except IntegrityError:
-                db.session.rollback()
-                logging.exception('Failed to create initial project for user %s', user.username)
-                return
-
-        with open(INITIAL_PROJECT_PATH / "metadata.json", "r") as metadata_json:
-            metadata = json.load(metadata_json)
-
-            file_map = {}
-            for file_metadata in metadata['files']:
-                if file_metadata['mime_type'] != FILE_MIME_TYPE_MAP:
-                    file = Files()
-                    for key, value in file_metadata.items():
-                        if key != 'path':
-                            setattr(file, key, value)
-                    content_path = INITIAL_PROJECT_PATH / file_metadata['path']
-                    file.filename = file.filename or content_path.stem
-                    with open(content_path, "rb") as file_content:
-                        file.content_id = FileContent().get_or_create(file_content)
-
-                    file.user_id = user.id
-                    file.parent = project.***ARANGO_USERNAME***
-                    db.session.add(file)
-                    file_map[file_metadata['path']] = file
-
-            mapTypeProvider = MapTypeProvider()
-            new_link_re = r'^\/projects\/([^\/]+)\/[^\/]+\/([a-zA-Z0-9-]+)'
-            db.session.flush()
-
-            def update_map_links(map_json):
-                for node in map_json['nodes']:
-                    for source in node['data'].get('sources', []):
-                        link_search = re.search(new_link_re, source['url'])
-                        if link_search is not None:
-                            project_name = link_search.group(1)
-                            hash_id = link_search.group(2)
-                            if hash_id in metadata['hash_mapping']:
-                                source['url'] = source['url'].replace(
-                                    project_name,
-                                    project.name
-                                ).replace(
-                                    hash_id,
-                                    file_map[
-                                        metadata['hash_mapping'][hash_id]
-                                    ].hash_id
-                                )
-
-                for edge in map_json['edges']:
-                    if 'data' in edge:
-                        for source in edge['data'].get('sources', []):
-                            link_search = re.search(new_link_re, source['url'])
-                            if link_search is not None:
-                                project_name = link_search.group(1)
-                                hash_id = link_search.group(2)
-                                if hash_id in metadata['hash_mapping']:
-                                    source['url'] = source['url'].replace(
-                                        project_name,
-                                        project.name
-                                    ).replace(
-                                        hash_id,
-                                        file_map[
-                                            metadata['hash_mapping'][hash_id]
-                                        ].hash_id
-                                    )
-
-                return map_json
-
-            for file_metadata in metadata['files']:
-                if file_metadata['mime_type'] == FILE_MIME_TYPE_MAP:
-                    file = Files()
-                    for key, value in file_metadata.items():
-                        if key != 'path':
-                            setattr(file, key, value)
-                    content_path = INITIAL_PROJECT_PATH / file_metadata['path']
-                    file.filename = file.filename or content_path.stem
-                    updated_map_content = mapTypeProvider.update_map(
-                        {},
-                        content_path,
-                        update_map_links
-                    )
-                    file.content_id = FileContent().get_or_create(updated_map_content)
-
-                    file.user_id = user.id
-                    file.parent = project.***ARANGO_USERNAME***
-                    db.session.add(file)
-
-                    file_map[file_metadata['path']] = file
-
-        return file_map.values()
 
     @use_args(UserCreateSchema)
     def post(self, params: dict):
@@ -278,21 +152,15 @@ class AccountView(MethodView):
             for role in params['roles']:
                 app_user.roles.append(self.get_or_create_role(role))
         try:
-            new_files = self.create_initial_project(app_user)
+            projects_service = get_projects_service()
+            projects_service.create_initial_project(app_user)
             db.session.add(app_user)
             db.session.commit()
         except SQLAlchemyError:
             db.session.rollback()
             raise
-
-        updated_files, versions, results = FileAnnotationsGenerationView().annotate_files(
-            new_files, app_user.id, {})
-
-        db.session.bulk_insert_mappings(FileAnnotationsVersion, versions)
-        db.session.bulk_update_mappings(Files, updated_files)
-        db.session.commit()
-
         return jsonify(dict(result=app_user.to_dict()))
+
 
     @use_args(UserUpdateSchema)
     def put(self, params: dict, hash_id):
