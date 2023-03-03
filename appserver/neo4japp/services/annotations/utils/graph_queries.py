2d62c1b75c049e3c4345f4137b5c4d36c5fb67c5
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-03-02 16:05:16 -0800

    Update gene/protein fallback queries for ET

--- appserver/neo4japp/services/annotations/utils/graph_queries.py
+++ appserver/neo4japp/services/annotations/utils/graph_queries.py
@@ -77,19 +77,35 @@ def get_gene_to_organism_query():
     return """
     FOR s IN synonym
         FILTER s.name IN @genes
-        FOR g, synonym_rel IN 1..1 INBOUND s has_synonym
-            FILTER 'Gene' IN g.labels
-            FOR t, species_rel IN 1..2 OUTBOUND g GRAPH "all"
-                OPTIONS {vertexCollections: 'taxonomy'}
-                FILTER t.eid IN @organisms
-                FILTER species_rel.label IN ['has_taxonomy', 'has_parent']
-                RETURN DISTINCT {
-                    'gene_name': g.name,
-                    'gene_synonym': s.name,
-                    'gene_id': g.eid,
-                    'organism_id': t.eid,
-                    'data_source': g.data_source
-                }
+        FOR gene, synonym_rel IN INBOUND s has_synonym
+            FILTER 'Gene' IN gene.labels
+            LET t = FIRST(
+                FOR t IN OUTBOUND gene has_taxonomy
+                    FILTER t.eid IN @organisms
+                    RETURN t.eid
+            )
+            LET t_parent = FIRST(
+                FOR temp_t IN OUTBOUND gene has_taxonomy
+                    FOR parent IN OUTBOUND temp_t has_parent
+                        FILTER parent.eid IN @organisms
+                        RETURN parent.eid
+            )
+            LET t_grandparent = FIRST(
+                FOR temp_t IN OUTBOUND gene has_taxonomy
+                    FOR temp_parent IN OUTBOUND temp_t has_parent
+                        FOR grand_parent IN OUTBOUND temp_parent has_parent
+                            FILTER grand_parent.eid IN @organisms
+                            RETURN grand_parent.eid
+            )
+            LET organism_to_use = FIRST([t, t_parent, t_grandparent][* FILTER CURRENT != null])
+            FILTER organism_to_use != null
+            RETURN DISTINCT {
+                'gene_name': gene.name,
+                'gene_synonym': s.name,
+                'gene_id': gene.eid,
+                'organism_id': organism_to_use.eid,
+                'data_source': gene.data_source
+            }
     """
 
 
@@ -97,20 +113,34 @@ def get_protein_to_organism_query():
     return """
     FOR s IN synonym
         FILTER s.name IN @proteins
-        FOR p, synonym_rel IN 1..1 INBOUND s has_synonym
-            OPTIONS {vertexCollections: 'uniprot'}
-            FILTER 'Protein' IN p.labels
-            FILTER synonym_rel.label == 'has_synonym'
-            FOR t, species_rel IN 1..2 OUTBOUND p GRAPH "all"
-                OPTIONS {vertexCollections: 'taxonomy'}
-                FILTER t.eid IN @organisms
-                FILTER species_rel.label IN ['has_taxonomy', 'has_parent']
-                COLLECT organism = t.eid, protein = s.name INTO protein_ids
-                RETURN {
-                    'protein': protein,
-                    'protein_ids': protein_ids[*].p.eid,
-                    'organism_id': organism,
-                }
+        FOR protein IN INBOUND s has_synonym OPTIONS {vertexCollections: 'uniprot'}
+            FILTER 'Protein' IN protein.labels
+            LET t = FIRST(
+                FOR t IN OUTBOUND protein has_taxonomy
+                    FILTER t.eid IN @organisms
+                    RETURN t.eid
+            )
+            LET t_parent = FIRST(
+                FOR temp_t IN OUTBOUND protein has_taxonomy
+                    FOR parent IN OUTBOUND temp_t has_parent
+                        FILTER parent.eid IN @organisms
+                        RETURN parent.eid
+            )
+            LET t_grandparent = FIRST(
+                FOR temp_t IN OUTBOUND protein has_taxonomy
+                    FOR temp_parent IN OUTBOUND temp_t has_parent
+                        FOR grand_parent IN OUTBOUND temp_parent has_parent
+                            FILTER grand_parent.eid IN @organisms
+                            RETURN grand_parent.eid
+            )
+            LET organism_to_use = FIRST([t, t_parent, t_grandparent][* FILTER CURRENT != null])
+            FILTER organism_to_use != null
+            COLLECT organism = organism_to_use, protein_name = s.name INTO protein_ids
+            RETURN {
+                'protein': protein_name,
+                'protein_ids': protein_ids[*].protein.eid,
+                'organism_id': organism,
+            }
     """
 
 
