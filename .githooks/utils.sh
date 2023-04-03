a09891d99f24bdf9e028f424a6cdcda8391adf2e
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-04-03 14:29:21 +0200

    run envs independently

--- .githooks/utils.sh
+++ .githooks/utils.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Common utils used in git-hook scripts
 cache_dir=".cache"
+DOCKER_COMPOSE_RUN = 'docker compose -f docker-compose.override.yml -p ***ARANGO_DB_NAME***-pre-commit run -rm'
 
 # Formatting
 b=$(tput bold)
@@ -143,6 +144,7 @@ recurseHook () {
   export -f HEADER
   export -f getStaged
   export -f debug
+  export -f DOCKER_COMPOSE_RUN
   #endregion
 
   for d in */; do
