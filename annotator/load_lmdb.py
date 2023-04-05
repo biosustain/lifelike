605b0e9ef329aa7118bfe0c452fc79044cfd4c76
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-04-05 13:14:15 -0700

    Add `load_lmdb` script to annotator

---
+++ annotator/load_lmdb.py
@@ -0,0 +1,9 @@
+import os
+
+from .lmdb_manager import AzureStorageProvider, LMDBManager
+
+if __name__ == '__main__':
+    manager = LMDBManager(AzureStorageProvider(), 'lmdb')
+    lmdb_dir_path = os.path.join(os.getcwd(), 'app/services/lmdb')
+    manager.download_all(lmdb_dir_path)
+    manager.update_all_dates(lmdb_dir_path)
