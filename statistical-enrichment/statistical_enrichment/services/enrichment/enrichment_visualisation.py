a6f9b4e2326e0c741e4d8ec6eb46efc66d9a6607
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-04-25 17:39:28 +0200

    make stat enrichment use cluster id as base entity (lower significance of clustered genes)

--- statistical-enrichment/statistical_enrichment/services/enrichment/enrichment_visualisation.py
+++ statistical-enrichment/statistical_enrichment/services/enrichment/enrichment_visualisation.py
@@ -23,8 +23,33 @@ class EnrichmentVisualisationService:
             else:
                 go = self.get_go_terms(organism, gene_names)
                 go_count = self.get_go_term_count(organism)
-            return fisher(gene_names, go, go_count)
+            gene_ids = [g['geneId'] for g in self.get_gene_ids(organism, gene_names)]
+            return fisher(gene_ids, go, go_count)
         raise NotImplementedError
+
+    def query_gene_id(self, organism_id, gene_names):
+        return self.graph.read_transaction(
+            lambda tx: list(
+                tx.run(
+                    """
+                    UNWIND $gene_names AS geneName
+                    MATCH (g:Gene)-[:HAS_TAXONOMY]-(t:Taxonomy {eid:$taxId})
+                    WHERE g.name=geneName
+                    // Get cluster aware eid
+                    CALL {
+                        WITH g
+                        MATCH p=(g)-[:IS*0..2]-()
+                        UNWIND nodes(p) as gene
+                        WITH gene.eid as gene_eid ORDER BY gene_eid
+                        RETURN apoc.text.join(collect(DISTINCT gene_eid), ',') as geneId
+                    }
+                    RETURN geneId
+                    """,
+                    taxId=organism_id,
+                    gene_names=gene_names
+                ).data()
+            )
+        )
 
     def query_go_term(self, organism_id, gene_names):
         r = self.graph.read_transaction(
@@ -32,7 +57,7 @@ class EnrichmentVisualisationService:
                 tx.run(
                     """
                     UNWIND $gene_names AS geneName
-                    MATCH (g:Gene)-[:HAS_TAXONOMY]-(t:Taxonomy {eid:$taxId}) 
+                    MATCH (g:Gene)-[:HAS_TAXONOMY]-(t:Taxonomy {eid:$taxId})
                     WHERE g.name=geneName
                     CALL {
                         // Make simple run by relations we has from GO db
@@ -45,20 +70,31 @@ class EnrichmentVisualisationService:
                     UNION
                         // Fetch GO relations defined in BioCyc
                         WITH g
-                        MATCH (g)-[:IS]-(:db_BioCyc)-[:ENCODES]-(:Protein)-[:GO_LINK]-(go:db_GO)
-                        WITH DISTINCT go 
                         // BioCyc db 'GO_LINK's does not have tax_id property so we need to filter in this way
-                        MATCH (go)-[:GO_LINK]-(:Protein)-[:ENCODES]-(:db_BioCyc)-[:IS]-(go_gene:Gene {tax_id:$taxId})
+                        MATCH (g)-[:IS]-(:db_BioCyc)-[:ENCODES]-(:Protein)-[:GO_LINK]-(go:db_GO)
+                        WITH DISTINCT go
+                        MATCH (go)-[:GO_LINK]-(:Protein)-[:ENCODES]-(bgene:db_BioCyc)-[:IS]-(go_gene:Gene {tax_id:$taxId})
                         RETURN go, go_gene
                     }
-                    WITH 
-                        DISTINCT go, 
+                    WITH
+                        DISTINCT go,
                         collect(DISTINCT go_gene) AS go_genes
+                    // Get cluster aware eid
+                    CALL {
+                        WITH go_genes
+                        UNWIND go_genes as go_gene
+                        MATCH p=(go_gene)-[:IS*0..2]-()
+                        UNWIND nodes(p) as gene
+                        WITH go_gene, gene.eid as gene_eid ORDER BY gene_eid
+                        WITH go_gene, apoc.text.join(collect(DISTINCT gene_eid), ',') as go_genes_eid
+                        RETURN collect(DISTINCT go_genes_eid) as go_genes_eids
+                    }
                     RETURN
                         go.eid AS goId,
                         go.name AS goTerm,
-                        // Return all but 'db_GO' labels 
+                        // Return all but 'db_GO' labels
                         [lbl IN labels(go) WHERE lbl <> 'db_GO'] AS goLabel,
+                        go_genes_eids AS geneIds,
                         [g IN go_genes |g.name] AS geneNames
                     """,
                     taxId=organism_id,
@@ -71,6 +107,15 @@ class EnrichmentVisualisationService:
                 f'Could not find related GO terms for organism id: {organism_id}'
             )
         return r
+
+    def get_gene_ids(self, organism, gene_names):
+        cache_id = f"get_gene_ids_{organism}_{','.join(gene_names)}"
+        return redis_cached(
+                cache_id,
+                partial(self.query_gene_id, organism.id, gene_names),
+                load=json.loads,
+                dump=json.dumps
+        )
 
     def get_go_terms(self, organism, gene_names):
         cache_id = f"get_go_terms_{organism}_{','.join(gene_names)}"
