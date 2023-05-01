9ca40ab7cb4fc240684aed75db1c3b45d3982044
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-05-01 15:32:08 -0700

    Add `envsubst` to rabbitmq

--- rabbitmq/custom-entrypoint.sh
+++ rabbitmq/custom-entrypoint.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# Add expected environment variables here as necessary
-declare -a arr=(
-    "ANNOTATOR_QUEUE"
-    "POST_ANNOTATOR_QUEUE"
-    "ELASTIC_INDEXER_QUEUE"
-    "RMQ_ADMIN_USERNAME"
-    "RMQ_ADMIN_PASSWORD"
-    "RMQ_MESSENGER_USERNAME"
-    "RMQ_MESSENGER_PASSWORD"
-)
-
-for varname in "${arr[@]}"
-do
-    varval=$(eval "echo \$${varname}")
-    sed -i "s/__${varname}__/${varval}/" /etc/rabbitmq/definitions.json
-done
+envsubst < /etc/rabbitmq/definitions.json > /etc/rabbitmq/tmp.txt
+mv /etc/rabbitmq/tmp.txt /etc/rabbitmq/definitions.json
 
 # Run the original entrypoint once we're done with any init work.
 exec /usr/local/bin/docker-entrypoint.sh "$@"
