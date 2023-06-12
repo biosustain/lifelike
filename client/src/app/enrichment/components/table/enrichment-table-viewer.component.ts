4d81eceb59b7e66240faf2bb153c3ae245a5bdb9
Dominik Maszczyk <Skitionek@gmail.com>
2023-06-12 14:57:30 +0200

    [LL-5143] Fix enrichment scrolling issues (#2051)
    * fix enrichment scrolling issues
    
    * fix scroll top
    
    * fix hilight attach
    
    * correct controller spelling mistake

--- client/src/app/enrichment/components/table/enrichment-table-viewer.component.ts
+++ client/src/app/enrichment/components/table/enrichment-table-viewer.component.ts
@@ -10,15 +10,32 @@ import {
   QueryList,
   ViewChild,
   ViewChildren,
-  NgZone,
+  NgZone
 } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { escapeRegExp, isNil } from 'lodash-es';
-import { BehaviorSubject, combineLatest, Observable, Subscription, defer } from 'rxjs';
-import { finalize, map, mergeMap, shareReplay, take, tap } from 'rxjs/operators';
+import {
+  BehaviorSubject,
+  combineLatest,
+  Observable,
+  Subscription,
+  defer,
+  Subject,
+  ReplaySubject,
+} from 'rxjs';
+import {
+  finalize,
+  map,
+  mergeMap,
+  shareReplay,
+  switchMap,
+  take,
+  takeUntil,
+  tap,
+} from 'rxjs/operators';
 
 import { FilesystemObject } from 'app/file-browser/models/filesystem-object';
 import { FilesystemObjectActions } from 'app/file-browser/services/filesystem-object-actions';
@@ -32,6 +49,7 @@ import { AsyncElementFind } from 'app/shared/utils/find/async-element-find';
 import { Progress } from 'app/interfaces/common-dialog.interface';
 import { ModuleContext } from 'app/shared/services/module-context.service';
 import { closePopups } from 'app/shared/DOMutils';
+import { GenericTableComponent } from 'app/shared/components/table/generic-table.component';
 
 import { EnrichmentDocument } from '../../models/enrichment-document';
 import { EnrichmentTable } from '../../models/enrichment-table';
@@ -39,6 +57,8 @@ import { EnrichmentTableService } from '../../services/enrichment-table.service'
 import { EnrichmentTableOrderDialogComponent } from './dialog/enrichment-table-order-dialog.component';
 import { EnrichmentTableEditDialogComponent, EnrichmentTableEditDialogValue, } from './dialog/enrichment-table-edit-dialog.component';
 import { EnrichmentService } from '../../services/enrichment.service';
+import { FindControllerService } from '../../services/find-controller.service';
+import { EnrichmentTableComponent } from './enrichment-table.component';
 
 // TODO: Is there an existing interface we could use here?
 interface AnnotationData {
@@ -51,47 +71,44 @@ interface AnnotationData {
   selector: 'app-enrichment-table-viewer',
   templateUrl: './enrichment-table-viewer.component.html',
   styleUrls: ['./enrichment-table-viewer.component.scss'],
-  providers: [EnrichmentService, ModuleContext]
+  providers: [
+    EnrichmentService,
+    ModuleContext,
+    FindControllerService
+  ]
 })
-export class EnrichmentTableViewerComponent implements OnInit, OnDestroy, AfterViewInit {
+export class EnrichmentTableViewerComponent implements OnInit, OnDestroy {
   encodeURIComponent = encodeURIComponent;
 
-  constructor(protected readonly route: ActivatedRoute,
-              protected readonly worksheetViewerService: EnrichmentTableService,
-              protected readonly snackBar: MatSnackBar,
-              protected readonly modalService: NgbModal,
-              protected readonly errorHandler: ErrorHandler,
-              protected readonly enrichmentService: EnrichmentService,
-              protected readonly progressDialog: ProgressDialog,
-              protected readonly changeDetectorRef: ChangeDetectorRef,
-              protected readonly elementRef: ElementRef,
-              protected readonly filesystemObjectActions: FilesystemObjectActions,
-              private readonly ngZone: NgZone) {
+  constructor(
+    protected readonly route: ActivatedRoute,
+    protected readonly worksheetViewerService: EnrichmentTableService,
+    protected readonly snackBar: MatSnackBar,
+    protected readonly modalService: NgbModal,
+    protected readonly errorHandler: ErrorHandler,
+    protected readonly enrichmentService: EnrichmentService,
+    protected readonly progressDialog: ProgressDialog,
+    protected readonly filesystemObjectActions: FilesystemObjectActions,
+    private readonly findControllerService: FindControllerService,
+  ) {
     this.fileId = this.route.snapshot.params.file_id || '';
     this.annotation = this.parseAnnotationFromUrl(this.route.snapshot.fragment);
 
-    // If the url fragment contains entity id info, assume we're looking for a specific annotation. Otherwise just search text.
-    this.findController = new AsyncElementFind(
-      null, // We'll update this later, once the table is rendered
-      this.annotation.id.length ? this.generateAnnotationFindQueue : this.generateTextFindQueue
-    );
-    this.findController.query = this.annotation.id.length ? this.annotation.id : this.annotation.text;
+    this.findControllerService.type$.next(this.annotation.id.length ? 'annotation' : 'text');
+    this.findControllerService.query$.next(this.annotation.id.length ? this.annotation.id : this.annotation.text);
   }
 
   @Output() modulePropertiesChange = new EventEmitter<ModuleProperties>();
-  @ViewChild('tableScroll', {static: false}) tableScrollRef: ElementRef;
-  @ViewChildren('findTarget') findTarget: QueryList<ElementRef>;
+  @ViewChild(EnrichmentTableComponent) enrichmentTable: EnrichmentTableComponent;
 
   annotation: AnnotationData;
+  private destroy$ = new Subject<any>();
 
   fileId: string;
   object$: Observable<FilesystemObject>;
   document$: Observable<EnrichmentDocument>;
   table$: Observable<EnrichmentTable>;
-  scrollTopAmount: number;
-  findController: AsyncElementFind;
-  findTargetChangesSub: Subscription;
-  private tickAnimationFrameId: number;
+  findController$: Observable<AsyncElementFind> = this.findControllerService.elementFind$;
 
   /**
    * Keeps tracks of changes so they aren't saved to the server until you hit 'Save'. However,
@@ -103,17 +120,12 @@ export class EnrichmentTableViewerComponent implements OnInit, OnDestroy, AfterV
     map(object => object.getTransferData())
   ));
 
+  scrollTop() {
+    this.enrichmentTable.scrollTop();
+  }
+
   ngOnInit() {
     this.load();
-  }
-
-  scrollTop() {
-    this.scrollTopAmount = 0;
-  }
-
-  onTableScroll(e) {
-    closePopups();
-    this.scrollTopAmount = e.target.scrollTop;
   }
 
   load() {
@@ -134,43 +146,10 @@ export class EnrichmentTableViewerComponent implements OnInit, OnDestroy, AfterV
       this.errorHandler.create({label: 'Load enrichment table'}),
       shareReplay(),
     );
-    this.ngZone.runOutsideAngular(() =>
-      this.tickAnimationFrameId = requestAnimationFrame(this.tick.bind(this))
-    );
-  }
-
-  ngAfterViewInit() {
-    this.findTargetChangesSub = this.findTarget.changes.subscribe({
-      next: () => {
-        // If the enrichment table is rendered, and we haven't already set the findController target, set it
-        if (this.findTarget.first && this.findController.target === null) {
-          this.findController.target = this.findTarget.first.nativeElement.getElementsByTagName('tbody')[0];
-          // This may seem like an anti-pattern -- and it probably is -- but there is seemingingly no other way around Angular's
-          // `ExpressionChangedAfterItHasBeenCheckedError` here. Even Angular seems to think so, as they use this exact pattern in their
-          // own example: https://angular.io/api/core/ViewChildren#another-example
-          setTimeout(() => {
-            // TODO: Need to have a brief background color animation when the table is loaded and the first match is rendered. (?)
-            // Actually not sure if this the desired behavior.
-            this.findController.start();
-          }, 0);
-        // Only reset the findController target when the table is reset
-        } else if (isNil(this.findTarget.first)) {
-          this.findController.target = null;
-          setTimeout(() => {
-            this.findController.stop();
-          }, 0);
-        }
-      }
-    });
   }
 
   ngOnDestroy() {
-    cancelAnimationFrame(this.tickAnimationFrameId);
-    if (!isNil(this.findTargetChangesSub)) {
-      this.findTargetChangesSub.unsubscribe();
-    }
-    // Give the findController a chance to teardown any listeners/callbacks/subscriptions etc.
-    this.findController.stop();
+    this.destroy$.next();
   }
 
   parseAnnotationFromUrl(fragment: string): AnnotationData {
@@ -180,11 +159,6 @@ export class EnrichmentTableViewerComponent implements OnInit, OnDestroy, AfterV
       text: params.get('text') || '',
       color: params.get('color') || ''
     };
-  }
-
-  tick() {
-    this.findController.tick();
-    this.tickAnimationFrameId = requestAnimationFrame(this.tick.bind(this));
   }
 
   restore(version: ObjectVersion) {
@@ -329,112 +303,21 @@ export class EnrichmentTableViewerComponent implements OnInit, OnDestroy, AfterV
 
   switchToTextFind() {
     this.annotation = {id: '', text: '', color: ''};
-    this.findController.stop();
-    this.findController = new AsyncElementFind(
-      this.findController.target,
-      this.generateTextFindQueue
-    );
+    this.findControllerService.type$.next('text');
   }
 
   switchToAnnotationFind(id: string, text: string, color: string) {
     this.annotation = {id, text, color};
-    this.findController.stop();
-    this.findController = new AsyncElementFind(
-      this.findController.target,
-      this.generateAnnotationFindQueue
-    );
+    this.findControllerService.type$.next('annotation');
   }
 
   startAnnotationFind(annotationId: string, annotationText: string, annotationColor: string) {
     this.switchToAnnotationFind(annotationId, annotationText, annotationColor);
-    this.findController.query = annotationId;
-    this.findController.start();
+    this.findControllerService.query$.next(annotationId);
   }
 
   startTextFind(text: string) {
     this.switchToTextFind();
-    this.findController.query = text;
-    this.findController.start();
-  }
-
-  private* generateAnnotationFindQueue(root: Node, query: string) {
-    const annotations = Array.from((root as Element).querySelectorAll('[data-annotation-meta]')) as HTMLElement[];
-    for (const annoEl of annotations) {
-      const data = JSON.parse(annoEl.getAttribute('data-annotation-meta'));
-
-      if (data.id === query) {
-        yield {
-          // The elements with `data-annotation-meta` should have exactly one child: the TextNode representing the annotated text
-          startNode: annoEl.firstChild,
-          endNode: annoEl.firstChild,
-          start: 0,
-          end: annoEl.textContent.length, // IMPORTANT: `end` is EXCLUSIVE!
-        };
-      }
-    }
-  }
-
-  private* generateTextFindQueue(root: Node, query: string): IterableIterator<NodeTextRange | undefined> {
-    const queue: Node[] = [
-      root,
-    ];
-
-    while (queue.length !== 0) {
-      const node = queue.shift();
-      if (node === undefined) {
-        break;
-      }
-
-      switch (node.nodeType) {
-        case Node.ELEMENT_NODE:
-          const el = node as HTMLElement;
-          const style = window.getComputedStyle(el);
-          // Should be true when we find the top-level container for the table cell
-          if (style.display === 'block') {
-            const regex = new RegExp(escapeRegExp(query), 'ig');
-            let match = regex.exec(node.textContent);
-
-            // If there's no match in the root, then there's no reason to continue
-            if (match === null) {
-              break;
-            }
-
-            // Since there was a match, get all the descendant text nodes
-            const treeWalker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT);
-            const textNodes: Node[] = [];
-            let currentNode = treeWalker.nextNode(); // Using `nextNode` here skips the root node, which is intended
-            while (currentNode) {
-              textNodes.push(currentNode);
-              currentNode = treeWalker.nextNode();
-            }
-
-            // Create a map of the root text content indices to the descendant text node corresponding to that index
-            let index = 0;
-            const textNodeMap = new Map<number, [Node, number]>();
-            for (const textNode of textNodes) {
-              for (let i = 0; i < textNode.textContent.length; i++) {
-                textNodeMap.set(index++, [textNode, i]);
-              }
-            }
-
-            while (match !== null) {
-              // Need to catch the case where regex.lastIndex returns a value greater than the last index of the text
-              const lastIndexIsEOS = regex.lastIndex === node.textContent.length;
-              const endOfMatch = lastIndexIsEOS ? regex.lastIndex - 1 : regex.lastIndex;
-
-              yield {
-                startNode: textNodeMap.get(match.index)[0],
-                endNode: textNodeMap.get(endOfMatch)[0],
-                start: textNodeMap.get(match.index)[1],
-                end: textNodeMap.get(endOfMatch)[1] + (lastIndexIsEOS ? 1 : 0), // IMPORTANT: `end` is EXCLUSIVE!
-              };
-              match = regex.exec(node.textContent);
-            }
-            break;
-          }
-          queue.push(...Array.from(node.childNodes));
-          break;
-      }
-    }
+    this.findControllerService.query$.next(text);
   }
 }
--- client/src/app/shared/utils/dom.ts
+++ client/src/app/shared/utils/dom.ts
@@ -52,6 +52,7 @@ export function getAbsoluteBoundingClientRect(element: Element, rect: DOMRect | 
 }
 
 /**
+ * @deprecated - we have more powerfull version `relativePosition`
  * Get a DOMRect with its coordinates relative to the given container.
  *
  * @param rect the rect to make relative
@@ -92,18 +93,6 @@ export function* walkParentElements(start: Element,
   }
 }
 
-export function* walkOverflowElementPairs(start: Element): Iterable<{ target: Element, viewport: Element } | undefined> {
-  let current: Element = start.parentElement;
-  let target: Element = start;
-  while (current) {
-    if (nonStaticPositionPredicate(current)) {
-      yield {target, viewport: current};
-      target = current;
-    }
-    current = current.parentElement;
-  }
-}
-
 /**
  * A predicate that tests whether an element has a position that makes children elements
  * within have a position relative to the given element.
@@ -113,119 +102,6 @@ export function* walkOverflowElementPairs(start: Element): Iterable<{ target: El
 export const nonStaticPositionPredicate = (element: Element): boolean => {
   const position = window.getComputedStyle(element).getPropertyValue('position');
   return position === 'absolute' || position === 'fixed' || position === 'relative';
-};
-
-export function* walkElementVisibility(owner: Element, ownerRect: DOMRect | undefined): Iterable<ElementVisibility> {
-  ownerRect = (ownerRect || owner.getBoundingClientRect()) as DOMRect;
-
-  // TODO: NOT QUITE RIGHT - not sure it gets the right parent container that the scrollbar is from
-
-  let index = 0;
-  for (const {target, viewport} of walkOverflowElementPairs(owner)) {
-    const viewportRect = viewport.getBoundingClientRect() as DOMRect;
-    const viewportLeft = viewport.scrollLeft;
-    const viewportRight = viewport.scrollLeft + viewportRect.width;
-    const viewportTop = viewport.scrollTop;
-    const viewportBottom = viewport.scrollTop + viewportRect.height;
-
-    const relativeRect = getBoundingClientRectRelativeToContainer(
-      index === 0 ? ownerRect : target.getBoundingClientRect() as DOMRect, viewport,
-    );
-
-    // Check if the element is viewable so we can decide if we need to scroll
-    const fullyVisibleX = relativeRect.left >= viewportLeft && relativeRect.right <= viewportRight;
-    const fullyVisibleXIfNoHorizontalScroll = relativeRect.right <= viewportRect.width;
-    const fullyVisibleY = relativeRect.top >= viewportTop && relativeRect.bottom <= viewportBottom;
-    const partiallyVisibleX = !(relativeRect.right < viewportLeft || relativeRect.left > viewportRight);
-    const partiallyVisibleY = !(relativeRect.bottom < viewportTop || relativeRect.top > viewportBottom);
-
-    yield {
-      target,
-      targetRelativeRect: relativeRect,
-      viewport,
-      viewportRect,
-      fullyVisibleX,
-      fullyVisibleXIfNoHorizontalScroll,
-      fullyVisibleY,
-      partiallyVisibleX,
-      partiallyVisibleY,
-    };
-
-    index++;
-  }
-}
-
-export interface ElementVisibility {
-  target: Element;
-  targetRelativeRect: DOMRect;
-  viewport: Element;
-  viewportRect: DOMRect;
-  fullyVisibleX: boolean;
-  fullyVisibleXIfNoHorizontalScroll: boolean;
-  fullyVisibleY: boolean;
-  partiallyVisibleX: boolean;
-  partiallyVisibleY: boolean;
-}
-
-/**
- * Scroll the given DOMRect of an element into view.
- *
- * @param owner the element that the DOMRect is from
- * @param ownerRect the DOMRect (if not specified, the boundaries of the owner element are used)
- * @param options options for the scroll
- */
-export function scrollRectIntoView(owner: Element, ownerRect: DOMRect | undefined,
-                                   options: ScrollRectIntoViewOptions = {}) {
-  options = {...defaultScrollRectIntoViewOptions, ...options};
-
-  let index = 0;
-  for (const {targetRelativeRect, viewport, viewportRect, fullyVisibleX, fullyVisibleXIfNoHorizontalScroll, fullyVisibleY} of
-    walkElementVisibility(owner, ownerRect)) {
-
-    // Handle the viewport being too small for the provided inset
-    const insetX = options.inset <= viewportRect.width ? options.inset : viewportRect.width / 4;
-    const insetY = options.inset <= viewportRect.height ? options.inset : viewportRect.height / 4;
-
-    if (!fullyVisibleX) {
-      // Bias to having no horizontal scroll offset
-      if (index === 0 && options.preferNoHorizontalScroll && fullyVisibleXIfNoHorizontalScroll) {
-        viewport.scrollLeft = 0;
-      } else {
-        viewport.scrollLeft = Math.max(0, targetRelativeRect.left - insetX);
-      }
-    }
-
-    if (!fullyVisibleY) {
-      viewport.scrollTop = Math.max(0, targetRelativeRect.top - insetY);
-
-      // Bias to having no horizontal scroll offset
-      if (index === 0 && options.preferNoHorizontalScroll && fullyVisibleXIfNoHorizontalScroll) {
-        viewport.scrollLeft = 0;
-      }
-    }
-
-    index++;
-  }
-}
-
-export interface ScrollRectIntoViewOptions {
-  /**
-   * The viewport inset when we need to scroll. This value may be adjusted if the
-   * viewport is smaller than the inset.
-   */
-  inset?: number;
-  /**
-   * If set to true, whenever we need to force a scroll, set the horizontal scroll
-   * offset to 0 if it's possible. The rationale is that leaving a little bit of left
-   * scroll can be confusing (especially if we are navigating through find results) so we want
-   * to get rid of it whenever possible.
-   */
-  preferNoHorizontalScroll?: boolean;
-}
-
-const defaultScrollRectIntoViewOptions: ScrollRectIntoViewOptions = {
-  inset: 100,
-  preferNoHorizontalScroll: true,
 };
 
 export interface NodeTextRange {
