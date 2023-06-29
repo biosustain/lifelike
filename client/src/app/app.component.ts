58e7e31c3e65d341802b8deecb2922cf6a1022a7
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-06-29 12:32:32 -0700

    Remove console.log from app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -49,8 +49,6 @@ export class AppComponent {
     private storage: StorageService,
     private authService: AuthenticationService,
   ) {
-    console.log('constructor for app component');
-
     this.ngbModalConfig.backdrop = 'static';
     this.ngbPaginationConfig.maxSize = 5;
 
