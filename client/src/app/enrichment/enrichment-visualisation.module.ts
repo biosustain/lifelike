225cc9d792f5a3fad45a13b4d48bd6aaf1cdf81f
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-07-11 23:50:14 +0200

    fix lint

--- client/src/app/enrichment/enrichment-visualisation.module.ts
+++ client/src/app/enrichment/enrichment-visualisation.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from 'app/shared/shared.module';
 import { FileBrowserModule } from 'app/file-browser/file-browser.module';
+import { SharedModule } from 'app/shared/shared.module';
 
-import { EnrichmentVisualisationViewerComponent } from './components/visualisation/enrichment-visualisation-viewer.component';
-import { EnrichmentVisualisationService } from './services/enrichment-visualisation.service';
-import { GroupModule } from './components/visualisation/group/group.module';
+// tslint:disable-next-line:max-line-length
 import { EnrichmentVisualisationExplanationPanelComponent } from './components/visualisation/components/explanation-panel/explanation-panel.component';
+// tslint:disable-next-line:max-line-length
+import { EnrichmentVisualisationViewerComponent } from './components/visualisation/enrichment-visualisation-viewer.component';
+import { GroupModule } from './components/visualisation/group/group.module';
+import { EnrichmentVisualisationService } from './services/enrichment-visualisation.service';
 
 @NgModule({
   declarations: [
@@ -17,4 +19,5 @@ import { EnrichmentVisualisationExplanationPanelComponent } from './components/v
   exports: [EnrichmentVisualisationViewerComponent],
   providers: [EnrichmentVisualisationService],
 })
-export class EnrichmentVisualisationsModule {}
+export class EnrichmentVisualisationsModule {
+}
