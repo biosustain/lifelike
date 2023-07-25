381bdbb8f33e66638fca9b2572a7630b7531a903
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-07-25 15:45:37 +0200

    fix prettier

--- client/src/app/shared/rxjs/from-renderer-event.ts
+++ client/src/app/shared/rxjs/from-renderer-event.ts
@@ -2,16 +2,16 @@ import { Renderer2 } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
-export const fromRendererEvent = <E extends Event>(renderer: Renderer2, element: HTMLElement, eventName: string) =>
-  new Observable<E>(subscriber =>
+export const fromRendererEvent = <E extends Event>(
+  renderer: Renderer2,
+  element: HTMLElement,
+  eventName: string
+) =>
+  new Observable<E>((subscriber) =>
     renderer.listen(
       element,
       eventName,
 
-
-
-
-
-      (n) => subscriber.next(n),
-    ),
+      (n) => subscriber.next(n)
+    )
   );
