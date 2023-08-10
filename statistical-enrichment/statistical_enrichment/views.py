85f9c6cfd1f4663c03b5a4d7a0a2ca54adcada51
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-08-10 16:11:31 -0700

    Fix formatting in stats enrichment

--- statistical-enrichment/statistical_enrichment/views.py
+++ statistical-enrichment/statistical_enrichment/views.py
@@ -9,28 +9,28 @@ from .schemas import EnrichmentSchema
 from .services import get_enrichment_visualisation_service
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 @login_exempt
 def enrich():
-    raise Exception('No function provided!')
+    raise Exception("No function provided!")
 
 
-@app.route('/healthz', methods=['GET', 'POST'])
+@app.route("/healthz", methods=["GET", "POST"])
 @login_exempt
 def healthz():
     return "I am OK!"
 
 
-@app.route('/enrich-with-go-terms', methods=['POST'])
+@app.route("/enrich-with-go-terms", methods=["POST"])
 @use_args(EnrichmentSchema)
 def enrich_go(args):
-    gene_names = args['geneNames']
-    organism = args['organism']
-    analysis = args['analysis']
-    cache_id = '_'.join(['enrich_go', ','.join(gene_names), analysis, str(organism)])
+    gene_names = args["geneNames"]
+    organism = args["organism"]
+    analysis = args["analysis"]
+    cache_id = "_".join(["enrich_go", ",".join(gene_names), analysis, str(organism)])
     enrichment_visualisation = get_enrichment_visualisation_service()
     return redis_cached(
         cache_id,
         partial(enrichment_visualisation.enrich_go, gene_names, analysis, organism),
         dump=dumps,
-    ), dict(mimetype='application/json')
+    ), dict(mimetype="application/json")
