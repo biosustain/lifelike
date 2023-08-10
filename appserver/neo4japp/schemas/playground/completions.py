d0b612ea34787a9a2528561d2f6c720a2e293850
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-08-10 11:55:32 +0200

    apply comment: https://github.com/SBRG/kg-prototypes/pull/2098\#discussion_r1289338270

--- appserver/neo4japp/schemas/playground/completions.py
+++ appserver/neo4japp/schemas/playground/completions.py
@@ -2,12 +2,12 @@ import marshmallow
 from marshmallow import fields
 
 from neo4japp.schemas.base import CamelCaseSchema
+from neo4japp.schemas.playground.completions_base import CompletionsBaseRequestSchema
 from neo4japp.schemas.validators import DeferedOneOf
 from neo4japp.services.chat_gpt import ChatGPT
 
 
-class CompletionsRequestSchema(CamelCaseSchema):
-    timeout = fields.Float()
+class CompletionsRequestSchema(CompletionsBaseRequestSchema):
     model = fields.String(
         validate=DeferedOneOf(
             lambda: tuple(
@@ -18,23 +18,5 @@ class CompletionsRequestSchema(CamelCaseSchema):
     )
     prompt = fields.String(required=True)
     suffix = fields.String()
-    max_tokens = fields.Integer()
-    temperature = fields.Float(validate=marshmallow.validate.Range(min=0, max=2))
-    top_p = fields.Float()
-    n = fields.Integer()
-    stream = fields.Boolean()
-    logprobs = fields.Integer(
-        validate=marshmallow.validate.Range(min=0, max=5), allow_none=True
-    )
     echo = fields.Boolean()
-    stop = fields.List(
-        fields.String(required=True), validate=marshmallow.validate.Length(min=0, max=4)
-    )
-    presence_penalty = fields.Float(validate=marshmallow.validate.Range(min=-2, max=2))
-    frequency_penalty = fields.Float(validate=marshmallow.validate.Range(min=-2, max=2))
     best_of = fields.Integer()
-    logit_bias = fields.Dict(
-        keys=fields.String(),
-        values=fields.Float(validate=marshmallow.validate.Range(min=-100, max=100)),
-    )
-    user = fields.String()
