9041f6e044e6cde0324d32d9124aab44f7fd0920
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-08-22 19:18:54 -0700

    Fix possible transaction error
    Looks like alembic crashed, possibly due to an OOM error.

--- appserver/migrations/versions/2023_08_21-6c15e2920c50_.py
+++ appserver/migrations/versions/2023_08_21-6c15e2920c50_.py
@@ -90,7 +90,7 @@ def data_upgrades():
         sa.select([t_files.c.id, t_files.c.annotations])
     )
 
-    for chunk in window_chunk(files, 25):
+    for chunk in window_chunk(files, 15):
         for file_id, annotations_obj in chunk:
             # For some reason the annotations obj can be a string representing an empty array...
             if annotations_obj not in [[], '[]']:
@@ -114,7 +114,7 @@ def data_upgrades():
                         .values(annotations=annotations_obj)
                     )
                     session.flush()
-    session.commit()
+        session.commit()
 
 
 def data_downgrades():
