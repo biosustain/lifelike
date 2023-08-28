5009ca6d0a6f19cb10f8f8c44e6525f70bdf7477
Dominik Maszczyk <Skitionek@gmail.com>
2023-08-28 14:37:07 +0200

    [LL-5266] Make file browser list fill the available space on width (#2128)
    * Make file browser list fill the available space on width
    
    * fix prettify
    
    * manual column calculations outside of template
    
    * add explanation
    
    * fix iheritance
    
    * Fix code style issues with Black
    
    * Fix code style issues with Prettier
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- client/src/app/file-browser/components/object-list.component.ts
+++ client/src/app/file-browser/components/object-list.component.ts
@@ -1,11 +1,20 @@
-import { Component, ElementRef, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { isNil, uniqueId, merge } from 'lodash-es';
-import { BehaviorSubject } from 'rxjs';
-import { finalize, map, tap } from 'rxjs/operators';
+import { isNil, merge, uniqueId } from 'lodash-es';
+import { isEqual as _isEqual, defer as _defer } from 'lodash/fp';
+import { BehaviorSubject, defer } from 'rxjs';
+import { distinctUntilChanged, finalize, map, tap } from 'rxjs/operators';
 
 import { ErrorHandler } from 'app/shared/services/error-handler.service';
 import { WorkspaceManager, WorkspaceNavigationExtras } from 'app/shared/workspace-manager';
@@ -14,11 +23,24 @@ import { CollectionModel } from 'app/shared/utils/collection-model';
 import { ProgressDialog } from 'app/shared/services/progress-dialog.service';
 import { Progress } from 'app/interfaces/common-dialog.interface';
 import { openDownloadForBlob } from 'app/shared/utils/files';
+import { ElementObserverDirective } from 'app/shared/directives/element-observer.directive';
+import { debug } from 'app/shared/rxjs/debug';
 
 import { FilesystemObject } from '../models/filesystem-object';
 import { FilesystemObjectActions } from '../services/filesystem-object-actions';
 import { getObjectLabel } from '../utils/objects';
 import { FilesystemService } from '../services/filesystem.service';
+
+enum Columns {
+  checkbox,
+  star,
+  name,
+  annotation,
+  size,
+  modified,
+  author,
+  controls,
+}
 
 @Component({
   selector: 'app-object-list',
@@ -38,6 +60,9 @@ export class ObjectListComponent {
   @Output() refreshRequest = new EventEmitter<string>();
   @Output() objectOpen = new EventEmitter<FilesystemObject>();
   MAX_TOOLTIP_LENGTH = 800;
+  private readonly columns = Columns;
+  @ViewChild(ElementObserverDirective, { static: true })
+  tableContainerObserver: ElementObserverDirective;
 
   constructor(
     protected readonly router: Router,
@@ -49,8 +74,62 @@ export class ObjectListComponent {
     protected readonly actions: FilesystemObjectActions,
     protected readonly filesystemService: FilesystemService,
     protected readonly elementRef: ElementRef,
-    protected readonly progressDialog: ProgressDialog
+    protected readonly progressDialog: ProgressDialog,
+    private readonly cdr: ChangeDetectorRef
   ) {}
+
+  private readonly COLUMNS_WIDTHS_IN_IMPORTANCE_ORDER: Map<Columns, number> = new Map([
+    [Columns.name, 260],
+    [Columns.checkbox, 19.2 + 16 + 7.2], // left padding + checkbox + right padding
+    [Columns.controls, 80],
+    [Columns.star, 7.2 + 16 + 7.2], // left padding + star + right padding
+    [Columns.annotation, 150],
+    [Columns.author, 80],
+    [Columns.modified, 80],
+    [Columns.size, 90],
+  ]);
+
+  get activeColumnsWidths(): Map<Columns, number> {
+    // Zeroed disabled columns while preserving order
+    const activeColumnsWidth = new Map(this.COLUMNS_WIDTHS_IN_IMPORTANCE_ORDER);
+    if (!this.showStars) {
+      activeColumnsWidth.set(Columns.star, 0);
+    }
+    if (!this.objectControls) {
+      activeColumnsWidth.set(Columns.controls, 0);
+    }
+    return activeColumnsWidth;
+  }
+
+  columnsWidth$ = defer(() =>
+    // This is subscribed within tableContainer thereferore if we defer it,
+    // we can be sure that it will be subscribed after tableContainerObserver is initialized
+    this.tableContainerObserver.size$.pipe(
+      map((containerSize) => this.caluculateColumnsWidths(containerSize)),
+      distinctUntilChanged(_isEqual),
+      // Simply couldn't get it to work without this
+      tap(() => _defer(() => this.cdr.detectChanges())),
+      debug('columnsWidth$')
+    )
+  );
+
+  caluculateColumnsWidths(containerWidth: { width: number }): Record<Columns, number> {
+    let remainingWidth = containerWidth?.width ?? 0;
+    const columnsWidths = {
+      [Columns.name]: 0,
+    } as Record<Columns, number>;
+
+    for (const [column, defaultWidth] of this.activeColumnsWidths.entries()) {
+      if (remainingWidth > defaultWidth) {
+        columnsWidths[column] = defaultWidth;
+        remainingWidth -= defaultWidth;
+      } else {
+        break;
+      }
+    }
+    columnsWidths[Columns.name] += remainingWidth;
+    return columnsWidths;
+  }
 
   objectDragStart(event: DragEvent, object: FilesystemObject) {
     const dataTransfer: DataTransfer = event.dataTransfer;
