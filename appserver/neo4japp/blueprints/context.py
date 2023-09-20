23847a6777afbdbff277331cc03624a563316e95
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-09-20 16:58:48 +0200

    revert commit 06921f112fa4dc17e606e19bab57d679a972ad38

--- appserver/neo4japp/blueprints/context.py
+++ appserver/neo4japp/blueprints/context.py
@@ -4,7 +4,6 @@ from webargs.flaskparser import use_args
 from neo4japp.schemas.context import ContextRelationshipRequestSchema
 from neo4japp.services.chat_gpt import ChatGPT
 from neo4japp.utils.globals import current_username
-from neo4japp.utils.string import compose_lines, indent_lines
 
 bp = Blueprint('chat-gpt-api', __name__, url_prefix='/explain')
 
@@ -15,24 +14,17 @@ def relationship(params):
     entities = params.get('entities', [])
     context = params.get('context')
     options = params.get('options', {})
-    escaped_terms = (ChatGPT.escape(term) for term in entities + [context] if term)
     create_params = dict(
         model="gpt-3.5-turbo",
         messages=[
             dict(
                 role="user",
                 content=(
-                    f'Given list of terms delimited by {ChatGPT.DELIMITER},'
-                    f' explain what is the relationship between them?\n'
-                    f'List of terms: {ChatGPT.DELIMITER}'
-                    f'''{
-                        compose_lines(
-                            *indent_lines(
-                                *escaped_terms
-                            )
-                        )
-                    }'''
-                    f'{ChatGPT.DELIMITER}'
+                    'What is the relationship between '
+                    + ', '.join(entities)
+                    + (f', {context}' if context else '')
+                    + '?'
+                    # + '\nPlease provide URL sources for your answer.'
                 ),
             )
         ],
