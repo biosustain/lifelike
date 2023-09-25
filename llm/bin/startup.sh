f371583ea60fffd3e785011861055904f60e8569
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-09-25 14:11:12 +0200

    first working version

--- llm/bin/startup.sh
+++ llm/bin/startup.sh
@@ -4,9 +4,9 @@ set -o errexit                  # exit on command failure; use <cmd> || true to 
 set -o nounset                  # exit when script tries to use undeclared variables
 
 if [ "${FLASK_ENV}" = "development" ] && [ "${FLASK_APP_CONFIG}" = "Development" ]; then
-    flask run --host 0.0.0.0 -p 5010
+    flask run --host 0.0.0.0
 elif [ "${FLASK_APP_CONFIG}" = "Production" ] || [ "${FLASK_APP_CONFIG}" = "Staging" ] || [ "${FLASK_APP_CONFIG}" = "QA" ]; then
-    gunicorn -b 0.0.0.0:5010 --workers=2 --max-requests=200 statistical_enrichment:app --timeout 1200
+    gunicorn -b 0.0.0.0:5000 --workers=2 --max-requests=200 app:app --timeout 1200
 else
     echo "No environment setup for ${FLASK_ENV}"
 fi
