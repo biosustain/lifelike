100478706e00f464671541081a303d770fc5e9bc
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-10-17 14:44:37 +0200

    LL-5291: Prof of concept for drawing tool

---
+++ llm/llmlib/utils/lazy_neo4j_graph.py
@@ -0,0 +1,31 @@
+from functools import cache
+
+from langchain.graphs import Neo4jGraph
+
+
+class LazyNeo4jGraph(Neo4jGraph):
+    """
+    A lazy Neo4j graph wrapper that only refreshes the schema when it is accessed.
+    This allows for custom schema to be set without heavy call to the database.
+    """
+
+    @cache
+    def lazy_refresh_schema(self) -> None:
+        """
+        Refreshes the Neo4j graph schema information.
+        """
+        return super().refresh_schema()
+
+    def refresh_schema(self) -> None:
+        pass
+
+    _schema: str
+
+    @property
+    def schema(self):
+        self.lazy_refresh_schema()
+        return self._schema
+
+    @schema.setter
+    def schema(self, value):
+        self._schema = value
