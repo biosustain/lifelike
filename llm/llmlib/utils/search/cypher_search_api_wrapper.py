7ecf94f2c49bc028da1194805ca5b904af22fdc2
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-10-26 13:34:57 +0200

    refine on how we stuff data

--- llm/llmlib/utils/search/cypher_search_api_wrapper.py
+++ llm/llmlib/utils/search/cypher_search_api_wrapper.py
@@ -4,11 +4,20 @@ import yaml
 from langchain.schema import Document
 from llmlib.utils.lazy_neo4j_graph import LazyNeo4jGraph
 from llmlib.utils.search.graph_search_api_wrapper import GraphSearchAPIWrapper
+from math import floor
+
+CURRENCY_METABOLITES = [
+    'NAD-P-OR-NOP', 'NADH-P-OR-NOP', 'Donor-H2', 'Acceptor', 'HYDROGEN-PEROXIDE', 'OXYGEN-MOLECULE',
+    'NAD', 'NADP', 'NADH', 'NADPH', 'WATER', 'CARBON-DIOXIDE', 'FAD', 'CO-A', 'UDP', 'AMMONIA',
+    'NA+',
+    'AMMONIUM', 'PROTON', 'CARBON-MONOXIDE', 'GTP', 'ADP', 'GDP', 'AMP', 'ATP', '3-5-ADP', 'PPI',
+    'Pi'
+]
 
 RANKED_RELATED_NODES_MAPPING_QUERY = """
 UNWIND $terms AS term
 MATCH (s:Synonym {lowercase_name: toLower(term)})<-[:HAS_SYNONYM]-(n)
-WHERE n.eid IS NOT NULL
+WHERE n.eid IS NOT NULL AND NOT n.eid IN $exclude_eids
 WITH apoc.text.levenshteinSimilarity(term, n.name) as similarity, s, n, term
 ORDER BY similarity DESC
 WHERE similarity > 0.5
@@ -25,7 +34,10 @@ ORDER BY similarity DESC
 WHERE similarity > 0.5
 MATCH (a), (b)
 WHERE id(a) = matchA['id'] AND id(b) = matchB['id']
-MATCH path=shortestPath((a)-[:CONSUMED_BY|PRODUCES|CATALYZES|REGULATES|HAS_TXONOMY|IS|HAS_KO|ENCODES|GO_LINK*1..20]-(b))
+MATCH path=allShortestPaths((a)-[*1..]-(b))
+WHERE
+ all(r in relationships(path) where type(r) in $include_rels)
+ and all(n in nodes(path) where not n.eid in $exclude_eids)
 RETURN path LIMIT $top_k
 """
 
@@ -45,17 +57,26 @@ class TermMatches(TypedDict):
     matches: List[NodeMatch]
 
 
+def preformat_entity(entity):
+    return {
+        k: v
+        for k, v in entity.items()
+        if v and (
+            k not in (
+                'genes', 'inchi', 'inchi_key', 'original_entity_types', 'left_end_position',
+                'right_end_position',
+            )
+        )
+    }
+
+
 def term_match_template(obj):
     return yaml.dump(
         {
             obj['term']: [
                 {
                     'matches': [
-                        {
-                            'eid': match['node']['eid'],
-                            'name': match['node']['name'],
-                            'similarity': match['similarity'],
-                        }
+                        preformat_entity(match['node'])
                         for match in obj['matches']
                     ]
                 }
@@ -74,18 +95,10 @@ def relationship_template(obj):
                         obj['path'],
                     )
                 ),
-                'nodes': {
-                    node['eid']: {
-                        'name': node.get('displayName'),
-                        'type': node.get('entityType', node.get('type')),
-                        'description': node.get('description'),
-                    }
-                    for node in list(
-                        filter(
-                            lambda n: isinstance(n, dict) and n.get('eid'), obj['path']
-                        )
-                    )
-                },
+                'segments': [
+                    (preformat_entity(n) if isinstance(n, dict) else n)
+                    for n in obj['path']
+                ]
             }
         }
     )
@@ -98,9 +111,17 @@ class CypherSearchAPIWrapper(GraphSearchAPIWrapper):
         arbitrary_types_allowed = True
 
     related_nodes_query: str = RANKED_RELATED_NODES_MAPPING_QUERY
-    related_nodes_query_params: Dict[str, Any] = dict(top_k_matches=10)
+    related_nodes_query_params: Dict[str, Any] = dict(
+        top_k_matches=5,
+        exclude_eids=CURRENCY_METABOLITES,
+    )
     relationships_query: str = RELATED_SHORTEST_PATH_QUERY
-    relationships_query_params: Dict[str, Any] = dict(top_k=1)
+    relationships_query_params: Dict[str, Any] = dict(
+        top_k=3,
+        include_rels=['CONSUMED_BY', 'PRODUCES', 'CATALYZES', 'REGULATES', 'HAS_TXONOMY', 'IS',
+                      'HAS_KO', 'ELEMENT_OF', 'ENCODES'],
+        exclude_eids=CURRENCY_METABOLITES,
+    )
 
     def node_to_document(self, term_match: TermMatches) -> Document:
         return Document(
@@ -111,12 +132,13 @@ class CypherSearchAPIWrapper(GraphSearchAPIWrapper):
         )
 
     def relationship_to_document(self, relationship: dict) -> Document:
+        path_len = len(relationship['path'])
         return Document(
             page_content=relationship_template(relationship),
             metadata=dict(
                 start_eid=relationship['path'][0]['eid'],
-                end_eid=relationship['path'][len(relationship) - 1]['eid'],
-                distance=len(relationship['path']),
+                end_eid=relationship['path'][path_len - 1]['eid'],
+                distance=floor(path_len / 2)
             ),
         )
 
