c337aca241811a646bd8f54236f42ecef5468fc9
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-11-02 14:35:25 +0100

    LL-5326: properly show related graph

--- llm/llmlib/v0/intermediate_result_callback.py
+++
@@ -1,6 +0,0 @@
-from langchain.callbacks import StdOutCallbackHandler
-from llmlib.utils.callbacks.base import GraphRetrieverManagerMixin
-
-
-class IntermediateResultCallback(StdOutCallbackHandler, GraphRetrieverManagerMixin):
-    pass
