ffd7813f8469093393452c817ff8f2b8de6f4d50
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2023-11-08 17:37:22 +0100

    work in progress

---
+++ client/src/app/shared/components/exporters-ref/exporters-ref.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExportersRefComponent } from './exporters-ref.component';
+
+describe('ExportersRefComponent', () => {
+  let component: ExportersRefComponent;
+  let fixture: ComponentFixture<ExportersRefComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExportersRefComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExportersRefComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
