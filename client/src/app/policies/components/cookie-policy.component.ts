547143d54af0b18fa29a51e17d1d28e65ce5e4ad
Dominik Maszczyk <dommas@biosustain.dtu.dk>
2024-04-10 13:30:25 +0200

    use new cookiebot account (#5)

--- client/src/app/policies/components/cookie-policy.component.ts
+++ client/src/app/policies/components/cookie-policy.component.ts
@@ -13,7 +13,7 @@ export class CookiePolicyComponent implements AfterViewInit {
     const s = document.createElement('script');
     s.id = 'CookieDeclaration';
     s.type = 'text/javascript';
-    s.src = 'https://consent.cookiebot.com/57494afb-856d-4cf0-8b86-d56bb0002688/cd.js';
+    s.src = 'https://consent.cookiebot.com/2c4d1aa6-66d7-4d10-a8dc-4ddc145b829a/cd.js';
     this.cookiePolicyContainer.nativeElement.appendChild(s);
   }
 }
